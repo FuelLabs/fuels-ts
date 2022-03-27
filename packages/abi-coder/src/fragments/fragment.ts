1714d40c836992d826e377c013edcb906761ef76
Luiz Estácio <luizstacio@gmail.com>
2022-03-27 12:49:10 -0600

    fix: set json fragment name optional (#200)

--- packages/abi-coder/src/fragments/fragment.ts
+++ packages/abi-coder/src/fragments/fragment.ts
@@ -1,7 +1,7 @@
 import type { ParamType } from '@ethersproject/abi';
 
 export interface JsonFragmentType {
-  readonly name: string;
+  readonly name?: string;
   readonly type: string;
   // TODO: Remove `null` when forc doesn't output nulls (https://github.com/FuelLabs/sway/issues/926)
   readonly components?: ReadonlyArray<JsonFragmentType> | null;
