4e9e5c761c5dc2cce28e20f39acc65311dfcaca2
Pedro Nauck <pedronauck@gmail.com>
2022-05-02 23:35:27 -0300

    fix(wallet): add test utils for build
    (cherry picked from commit 8c8ee1883208c0f59a3eaf49be1ed7ac4c47449c)

--- packages/wallet/tsup.config.ts
+++ packages/wallet/tsup.config.ts
@@ -2,7 +2,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig((options) => ({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/test-utils.ts'],
   format: ['cjs', 'esm', 'iife'],
   splitting: false,
   sourcemap: true,
