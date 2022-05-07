c27e376e8ed2c3b18cda0c3d1cf09f6cd8715c19
Pedro Nauck <pedronauck@gmail.com>
2022-05-07 12:34:03 -0300

    fix: github action for publish on master and replaces Lerna publishing (#242)
    * chore: remove unnecessary github action
    * chore: add script for pre releases on push master
    * chore: add script for stable releases
    * chore: update pnpm-lock.yml
    * chore: use lerna just to generate changelogs
    * chore: add git push after publish
    * chore: improve release:stable script
    * chore: improve release utils scripts
    * refactor: use chglog to generate changelog instead of lerna
    * fix: main package.json version
    * fix: update pnpm-lock.yaml
    * fix: restore package.json on release:pre script

---
+++ scripts/release-stable.ts
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+import prompts from 'prompts';
+import sh from 'shelljs';
+
+import { changeAllPkgJSON } from './utils/changePackages';
+import { generateChangelogs } from './utils/generateChangelogs';
+import { getVersion } from './utils/getVersion';
+
+(async () => {
+  const nextVersion = await getVersion();
+  const res = await prompts({
+    type: 'text',
+    name: 'version',
+    message: 'For which version you need to bump?',
+    initial: nextVersion || '',
+  });
+
+  if (res.version) {
+    await changeAllPkgJSON(res.version);
+    await generateChangelogs(res.version);
+    sh.exec('git add ./CHANGELOG.md');
+    sh.exec('git add ./package.json');
+    sh.exec('git add ./packages/**/CHANGELOG.md');
+    sh.exec('git add ./packages/**/package.json');
+    sh.exec(`git commit -m "bump to ${res.version}"`);
+    sh.exec(`git tag -a ${res.version} -m "bump to ${res.version}"`);
+    sh.exec('git push origin master --tags');
+    sh.exec(`pnpm publish -r --tag=latest --no-git-checks --force`);
+  }
+})();
