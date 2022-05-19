6c8ce489f2384de7def5daec009b6af20bc38523
Cameron Manavian <cameron.manavian@fuel.sh>
2022-05-19 11:03:28 -0700

    Uses native crypto APIs (#276)
    * Use native crypto modules from Node and Web
    
    * Rename to web
    
    * Format

---
+++ packages/keystore/src/aes-ctr-node.ts
@@ -0,0 +1,52 @@
+import type { Keystore } from './aes-ctr';
+import { bufferFromString, stringFromBuffer, keyFromPassword } from './aes-ctr';
+import { crypto } from './universal-crypto';
+
+const ALGORITHM = 'aes-256-ctr';
+
+const randomBytes = (length: number) => crypto.randomBytes(length);
+
+/**
+ * Encrypts a data object that can be any serializable value using
+ * a provided password.
+ *
+ * @returns Promise<Keystore> object
+ */
+export async function encrypt<T>(password: string, data: T): Promise<Keystore> {
+  const iv = randomBytes(16);
+  const salt = randomBytes(32);
+  const secret = keyFromPassword(password, salt);
+  const dataBuffer = Uint8Array.from(Buffer.from(JSON.stringify(data), 'utf-8'));
+
+  const cipher = crypto.createCipheriv(ALGORITHM, secret, iv);
+  let cipherData = cipher.update(dataBuffer);
+  cipherData = Buffer.concat([cipherData, cipher.final()]);
+
+  return {
+    data: stringFromBuffer(cipherData),
+    iv: stringFromBuffer(iv),
+    salt: stringFromBuffer(salt),
+  };
+}
+
+/**
+ * Given a password and a keystore object, decrypts the text and returns
+ * the resulting value
+ */
+export async function decrypt<T>(password: string, keystore: Keystore): Promise<T> {
+  const iv = bufferFromString(keystore.iv);
+  const salt = bufferFromString(keystore.salt);
+  const secret = keyFromPassword(password, salt);
+  const encryptedText = bufferFromString(keystore.data);
+
+  const decipher = crypto.createDecipheriv(ALGORITHM, secret, iv);
+  const decrypted = decipher.update(encryptedText);
+  const deBuff = Buffer.concat([decrypted, decipher.final()]);
+  const decryptedData = Buffer.from(deBuff).toString('utf-8');
+
+  try {
+    return JSON.parse(decryptedData);
+  } catch {
+    throw new Error('Invalid credentials');
+  }
+}
--- packages/keystore/src/keystore.ts
+++ packages/keystore/src/keystore.ts
@@ -1,26 +1,10 @@
-import type { BytesLike } from '@ethersproject/bytes';
-import { arrayify } from '@ethersproject/bytes';
-import { pbkdf2 } from '@ethersproject/pbkdf2';
-import { randomBytes } from '@ethersproject/random';
-// TODO: Remove EAS and use native crypto libs on browser and node
-import aes from 'aes-js';
+import type { Keystore } from './aes-ctr';
+import { encrypt as encNode, decrypt as decNode } from './aes-ctr-node';
+import { encrypt as encWeb, decrypt as decWeb } from './aes-ctr-web';
+import { strategy } from './universal-crypto';
 
-export interface Keystore {
-  data: string;
-  iv: string;
-  salt: string;
-}
-
-/**
- * Generate a pbkdf2 key from a password and random salt
- */
-export function keyFromPassword(password: string, salt: BytesLike): Uint8Array {
-  const passBuffer = Buffer.from(String(password).normalize('NFKC'), 'utf-8');
-  const saltBuffer = arrayify(salt);
-  const key = pbkdf2(passBuffer, saltBuffer, 100000, 32, 'sha256');
-
-  return arrayify(key);
-}
+export type { Keystore } from './aes-ctr';
+export { keyFromPassword } from './aes-ctr';
 
 /**
  * Encrypts a data object that can be any serializable value using
@@ -28,40 +12,16 @@ export function keyFromPassword(password: string, salt: BytesLike): Uint8Array {
  *
  * @returns Promise<Keystore> Keystore object
  */
-export function encrypt<T>(password: string, data: T): Keystore {
-  const iv = randomBytes(16);
-  const salt = randomBytes(32);
-  const secret = keyFromPassword(password, salt);
-  const dataBuffer = Uint8Array.from(Buffer.from(JSON.stringify(data), 'utf-8'));
-  const counter = new aes.Counter(iv);
-  // eslint-disable-next-line new-cap
-  const aesCtr = new aes.ModeOfOperation.ctr(secret, counter);
-  const cipherdata = aesCtr.encrypt(dataBuffer);
-
-  return {
-    data: Buffer.from(cipherdata).toString('base64'),
-    iv: Buffer.from(iv).toString('base64'),
-    salt: Buffer.from(salt).toString('base64'),
-  };
+export async function encrypt<T>(password: string, data: T): Promise<Keystore> {
+  return strategy === 'Node' ? encNode<T>(password, data) : encWeb<T>(password, data);
 }
 
 /**
  * Given a password and a keystore object, decrypts the text and returns
  * the resulting value
+ *
+ *  @returns Promise<T> T object
  */
-export function decrypt<T>(password: string, keystore: Keystore): T {
-  const iv = arrayify(Buffer.from(keystore.iv, 'base64'));
-  const salt = arrayify(Buffer.from(keystore.salt, 'base64'));
-  const secret = keyFromPassword(password, salt);
-
-  const counter = new aes.Counter(iv);
-  // eslint-disable-next-line new-cap
-  const aesCtr = new aes.ModeOfOperation.ctr(secret, counter);
-  const decryptedData = aesCtr.decrypt(Buffer.from(keystore.data, 'base64'));
-
-  try {
-    return JSON.parse(Buffer.from(decryptedData).toString('utf-8'));
-  } catch {
-    throw new Error('Invalid credentials');
-  }
+export async function decrypt<T>(password: string, keystore: Keystore): Promise<T> {
+  return strategy === 'Node' ? decNode<T>(password, keystore) : decWeb<T>(password, keystore);
 }
