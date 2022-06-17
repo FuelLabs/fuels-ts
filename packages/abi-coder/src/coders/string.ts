a51ee83b09fe9cad941afceb8c78baa23c82b417
JC <alicancubukcuoglu@gmail.com>
2022-06-17 18:21:10 +0300

    fix: EnumCoder not padding its value (#336)

--- packages/abi-coder/src/coders/string.ts
+++ packages/abi-coder/src/coders/string.ts
@@ -5,25 +5,27 @@ import Coder from './abstract-coder';
 
 export default class StringCoder<TLength extends number = number> extends Coder<string, string> {
   length: TLength;
+  #paddingLength: number;
 
   constructor(length: TLength) {
-    super('string', `str[${length}]`);
+    let paddingLength = (8 - length) % 8;
+    paddingLength = paddingLength < 0 ? paddingLength + 8 : paddingLength;
+    super('string', `str[${length}]`, length + paddingLength);
     this.length = length;
+    this.#paddingLength = paddingLength;
   }
 
   encode(value: string): Uint8Array {
-    let pad = (8 - this.length) % 8;
-    pad = pad < 0 ? pad + 8 : pad;
-    const str = toUtf8Bytes(value.slice(0, this.length));
-    return concat([str, new Uint8Array(pad)]);
+    const encoded = toUtf8Bytes(value.slice(0, this.length));
+    const padding = new Uint8Array(this.#paddingLength);
+    return concat([encoded, padding]);
   }
 
   decode(data: Uint8Array, offset: number): [string, number] {
-    let pad = (8 - this.length) % 8;
-    pad = pad < 0 ? pad + 8 : pad;
-
     const bytes = data.slice(offset, offset + this.length);
     const value = toUtf8String(bytes);
-    return [value, offset + this.length + pad];
+
+    const padding = this.#paddingLength;
+    return [value, offset + this.length + padding];
   }
 }
