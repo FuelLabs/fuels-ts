3cab8247bd12ecace3cd97068b08dc2a08b4d7d6
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2022-06-18 02:37:18 -0300

    fix: coverage config

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@ const config: Config.InitialOptions = {
   setupFiles: ['./jest.env.ts'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   modulePathIgnorePatterns: ['/dist/'],
-  collectCoverageFrom: ['packages/**/*.[jt]s', '!**/node_modules/**', '!**/dist/**'],
+  coverageReporters: ['text', 'text-summary'],
 };
 
 export default config;
