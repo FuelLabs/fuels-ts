5ce9d2f4d9a73aed6a6b8c8b42341fb825ab9533
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2022-06-18 14:44:00 -0300

    revert broken test

--- packages/wallet/src/wallet.test.ts
+++ packages/wallet/src/wallet.test.ts
@@ -10,10 +10,6 @@ import Wallet from './wallet';
 import walletSpec from './wallet-spec';
 
 describe('Wallet', () => {
-  it.only('shoulf fail', () => {
-    expect(true).toEqual(false);
-  });
-
   it('Instantiate a new wallet', async () => {
     const wallet = new Wallet(signMessageTest.privateKey);
 
