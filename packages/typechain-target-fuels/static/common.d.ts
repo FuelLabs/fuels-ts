5583180ca6a65f81e7b8b425dfc88100a9c188aa
Quinn Lee <2522945+QuinnLee@users.noreply.github.com>
2022-07-07 10:07:56 -0400

    feat: use Sway Enum type in typechain target (#364)
    * feat: use Enum type in typechain target
    
    * fix: update Enum type

---
+++ packages/typechain-target-fuels/static/common.d.ts
@@ -0,0 +1,6 @@
+/* 
+  Enum from to mimic Sway Enum 
+  Requires at least one Key-Value but does not raise error on multiple pairs
+  This is done in the abi-coder
+*/
+export type Enum<T, U = {[K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U]
