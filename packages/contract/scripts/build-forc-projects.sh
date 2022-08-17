5b01f7720d2a4077bfaafd2b0c2051586e5de06f
Cameron Manavian <cameron.manavian@fuel.sh>
2022-08-16 22:14:20 -0700

    revert

--- packages/contract/scripts/build-forc-projects.sh
+++ packages/contract/scripts/build-forc-projects.sh
@@ -3,9 +3,9 @@
 set -euo pipefail
 
 scripts/build-multicall.sh
-forc build -p src/__test__/storage-test-contract
-forc build -p src/__test__/call-test-contract
-forc build -p src/__test__/token-test-contract/token_abi
-forc build -p src/__test__/token-test-contract/token_contract
+pnpm forc build -p src/__test__/storage-test-contract
+pnpm forc build -p src/__test__/call-test-contract
+pnpm forc build -p src/__test__/token-test-contract/token_abi
+pnpm forc build -p src/__test__/token-test-contract/token_contract
 pnpm forc build -p src/__test__/generic-types-contract
 pnpm forc build -p src/__test__/coverage-contract
