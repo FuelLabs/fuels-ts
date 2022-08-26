ffcd4f6ac140d4b02913f013f6671eb1e33f360b
Cameron Manavian <cameron.manavian@fuel.sh>
2022-08-26 00:28:54 -0700

    simplifiy

--- packages/abi-coder/src/coders/option.ts
+++ packages/abi-coder/src/coders/option.ts
@@ -1,4 +1,3 @@
-import type { DecodedValue } from './abstract-coder';
 import type Coder from './abstract-coder';
 import type { InputValueOf, DecodedValueOf } from './enum';
 import EnumCoder from './enum';
@@ -8,15 +7,11 @@ type SwayOption<T> = { None: [] } | { Some: T };
 
 export default class OptionCoder<TCoders extends Record<string, Coder>> extends EnumCoder<TCoders> {
   encode(value: InputValueOf<TCoders>): Uint8Array {
-    const result = super.encode(
-      this.toSwayOption(
-        value as unknown as Option<DecodedValue>
-      ) as unknown as InputValueOf<TCoders>
-    );
+    const result = super.encode(this.toSwayOption(value) as unknown as InputValueOf<TCoders>);
     return result;
   }
 
-  toSwayOption(input?: Option<DecodedValue>): SwayOption<DecodedValue> {
+  toSwayOption(input: InputValueOf<TCoders>): SwayOption<unknown> {
     if (input !== undefined) {
       return { Some: input };
     }
@@ -26,13 +21,10 @@ export default class OptionCoder<TCoders extends Record<string, Coder>> extends 
 
   decode(data: Uint8Array, offset: number): [DecodedValueOf<TCoders>, number] {
     const [decoded, newOffset] = super.decode(data, offset);
-    return [
-      this.toOption(decoded as unknown as SwayOption<DecodedValue>) as DecodedValueOf<TCoders>,
-      newOffset,
-    ];
+    return [this.toOption(decoded) as DecodedValueOf<TCoders>, newOffset];
   }
 
-  toOption(output?: SwayOption<DecodedValue>): Option<DecodedValue> {
+  toOption(output?: DecodedValueOf<TCoders>): Option<unknown> {
     if (output && 'Some' in output) {
       return output.Some;
     }
