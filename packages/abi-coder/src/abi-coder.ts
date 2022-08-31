a3c37ced0b8206d602a7a51ad90e4619bc598309
Cameron Manavian <cameron.manavian@fuel.sh>
2022-08-30 22:07:06 -0700

    Update packages/abi-coder/src/abi-coder.ts
    Co-authored-by: Luiz Estácio | stacio.eth <luizstacio@gmail.com>

--- packages/abi-coder/src/abi-coder.ts
+++ packages/abi-coder/src/abi-coder.ts
@@ -84,9 +84,11 @@ export default class AbiCoder {
         obj[component.name] = this.getCoder(component);
         return obj;
       }, {});
-      return param.type === OPTION_CODER_TYPE
-        ? new OptionCoder(enumMatch.name, coders)
-        : new EnumCoder(enumMatch.name, coders);
+     const isOptionEnum = param.type === OPTION_CODER_TYPE;
+     if (isOptionEnum) {
+         return new OptionCoder(enumMatch.name, coders);
+     }
+     return new EnumCoder(enumMatch.name, coders);
     }
 
     const tupleMatch = tupleRegEx.exec(param.type)?.groups;
