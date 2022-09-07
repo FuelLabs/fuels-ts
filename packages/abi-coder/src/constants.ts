4d219c729c2b0c6d5dfc0327207d78982638946b
Cameron Manavian <cameron.manavian@fuel.sh>
2022-09-06 23:04:06 -0700

    add const

--- packages/abi-coder/src/constants.ts
+++ packages/abi-coder/src/constants.ts
@@ -1,4 +1,5 @@
 export const OPTION_CODER_TYPE = 'enum Option';
+export const VEC_CODER_TYPE = 'struct Vec';
 export const stringRegEx = /str\[(?<length>[0-9]+)\]/;
 export const arrayRegEx = /\[(?<item>[\w\s\\[\]]+);\s*(?<length>[0-9]+)\]/;
 export const structRegEx = /^struct (?<name>\w+)$/;
