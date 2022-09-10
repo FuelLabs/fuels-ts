145d031283b7991b6f8d893153289e2fd455a45f
Cameron Manavian <cameron.manavian@fuel.sh>
2022-09-10 00:03:25 -0700

    relocate

--- packages/abi-coder/src/abi-coder.ts
+++ packages/abi-coder/src/abi-coder.ts
@@ -27,54 +27,9 @@ import {
   VEC_CODER_TYPE,
 } from './constants';
 import type { JsonAbiFragmentType } from './json-abi';
-import { filterEmptyParams, hasOptionTypes } from './utilities';
+import { filterEmptyParams, getVectorAdjustments, hasOptionTypes } from './utilities';
 
 const logger = new Logger(process.env.BUILD_VERSION || '~');
-type ByteInfo = { vecByteLength: number } | { byteLength: number };
-
-const getVectorAdjustments = (
-  coders: Coder<unknown, unknown>[],
-  values: InputValue[],
-  offset = 0
-) => {
-  const vectorData: Uint8Array[] = [];
-  const byteMap: ByteInfo[] = coders.map((encoder, i) => {
-    if (!(encoder instanceof VecCoder)) {
-      return { byteLength: encoder.encodedLength };
-    }
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const data = encoder.getEncodedVectorData(values[i] as any);
-    vectorData.push(data);
-    return { vecByteLength: data.byteLength };
-  });
-
-  const baseVectorOffset = vectorData.length * VecCoder.getBaseOffset() + offset;
-  const offsetMap = coders.map((encoder, paramIndex) => {
-    if (!(encoder instanceof VecCoder)) {
-      return 0;
-    }
-
-    return byteMap.reduce((sum, byteInfo, byteIndex) => {
-      if ('byteLength' in byteInfo) {
-        return sum + byteInfo.byteLength;
-      }
-
-      if (byteIndex === 0 && byteIndex === paramIndex) {
-        return baseVectorOffset;
-      }
-
-      if (byteIndex < paramIndex) {
-        return sum + byteInfo.vecByteLength + baseVectorOffset;
-      }
-
-      return sum;
-    }, 0);
-  });
-
-  coders.forEach((code, i) => code.setOffset(offsetMap[i]));
-  return vectorData;
-};
 
 export default class AbiCoder {
   constructor() {
