6403076bb9fce9055b436596e23713b0e7909d87
Quinn Lee <2522945+QuinnLee@users.noreply.github.com>
2022-09-23 21:31:49 -0400

    feat: Parse Logs and LogData (#517)

--- packages/providers/src/index.ts
+++ packages/providers/src/index.ts
@@ -7,4 +7,3 @@ export { default as Provider } from './provider';
 export * from './transaction-request';
 export * from './transaction-response';
 export * from './util';
-export { default as LogReader } from './LogReader';
