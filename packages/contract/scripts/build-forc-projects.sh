b7f4dfbedaab85d9bf6c1994c0dbc10152ffc73a
Cameron Manavian <cameron.manavian@fuel.sh>
2022-09-25 23:03:23 -0700

    rv

--- packages/contract/scripts/build-forc-projects.sh
+++ packages/contract/scripts/build-forc-projects.sh
@@ -9,4 +9,3 @@ pnpm forc build -p src/__test__/token-test-contract/token_abi --generate-logged-
 pnpm forc build -p src/__test__/token-test-contract/token_contract --generate-logged-types
 pnpm forc build -p src/__test__/generic-types-contract --generate-logged-types
 pnpm forc build -p src/__test__/coverage-contract --generate-logged-types
-pnpm exec fuelchain --target=../typechain-target-fuels --out-dir=src/__test__/coverage-contract-types src/__test__/coverage-contract/out/debug/coverage-contract-abi.json
