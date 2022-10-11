b6942b19574abb59378f01a13833116a6b05ceee
Cameron Manavian <cameron.manavian@fuel.sh>
2022-10-11 07:32:45 -0700

    chore: relocate contract related testing (#526)
    * rm
    
    * update ignore
    
    * relocate and update configs
    
    * relocate to new package
    
    * Adjust ignores
    
    * Check in contracts
    
    * Meta cleanup
    
    * rename folder, bring in some fuels-rs files for test
    
    * Add file to test small specs
    
    * Update package.json
    
    * Update process-predicate.ts
    
    * Update utils.ts
    
    * Update pnpm-lock.yaml
    
    * fmt
    
    * lint

---
+++ .changeset/eleven-hairs-carry.md
@@ -0,0 +1,6 @@
+---
+"@fuel-ts/contract": patch
+"@fuel-ts/predicate": patch
+---
+
+Relocated tests
---
+++ packages/fuel-gauge/src/generic-types-contract.test.ts
@@ -0,0 +1,112 @@
+import { readFileSync } from 'fs';
+import { toHex } from 'fuels';
+import { join } from 'path';
+
+import abiJSON from '../test-projects/generic-types-contract/out/debug/generic-types-contract-abi.json';
+
+import { setup } from './utils';
+
+const contractBytecode = readFileSync(
+  join(__dirname, '../test-projects/generic-types-contract/out/debug/generic-types-contract.bin')
+);
+
+describe('GenericTypesContract', () => {
+  it('should call complex contract function with generic type', async () => {
+    const contract = await setup({
+      abi: abiJSON,
+      contractBytecode,
+    });
+
+    const b256 = '0xd5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930b';
+    const bimArg1 = 'Yes';
+    const { value } = await contract.functions
+      .generic_type_function(
+        [
+          {
+            bim: bimArg1,
+            bam: {
+              Foo: 100,
+            },
+          },
+          {
+            bim: bimArg1,
+            bam: {
+              Bar: { value: true },
+            },
+          },
+          {
+            bim: bimArg1,
+            bam: {
+              Din: { value: true },
+            },
+          },
+        ],
+        [
+          {
+            bim: [b256, b256, b256],
+            bam: {
+              Foo: 100,
+            },
+          },
+          {
+            bim: [b256, b256, b256],
+            bam: {
+              Bar: { value: true },
+            },
+          },
+          {
+            bim: [b256, b256, b256],
+            bam: {
+              Din: { value: true },
+            },
+          },
+        ],
+        {
+          bim: 100,
+          bam: {
+            Din: 'Yes',
+          },
+        },
+        {
+          bim: { value: true },
+          bam: {
+            Din: 100,
+          },
+        }
+      )
+      .call();
+
+    const arg1 = {
+      bim: toHex(1),
+      bam: true,
+      never_used: toHex(2),
+      foo: {
+        x: b256,
+        b: toHex(32),
+      },
+      foo_list: new Array(10).fill(b256),
+    };
+    const arg2 = {
+      bim: toHex(1),
+      bam: 2,
+      never_used: toHex(3),
+      foo: {
+        x: {
+          x: toHex(21),
+          b: toHex(22),
+        },
+        b: toHex(32),
+      },
+      foo_list: new Array(10).fill({
+        x: toHex(31),
+        b: toHex(32),
+      }),
+    };
+    const { value: call2 } = await contract.functions
+      .generic_complex_type_function(arg1, arg2)
+      .call();
+
+    expect(value).toEqual(bimArg1);
+    expect(JSON.stringify([arg1, arg2])).toEqual(JSON.stringify(call2));
+  });
+});
