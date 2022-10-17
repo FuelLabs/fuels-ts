6bb67f40f8e7f286bd5d71ff85e204ab15265aa5
Cameron Manavian <cameron.manavian@fuel.sh>
2022-10-17 14:14:19 -0700

    up forc

--- packages/fuel-gauge/scripts/build.sh
+++ packages/fuel-gauge/scripts/build.sh
@@ -6,7 +6,7 @@ for PROJECT_DIR in ./test-projects/* ;
 do
   BIN_DIR="$PROJECT_DIR/out/debug"
 
-  forc build -p $PROJECT_DIR --print-finalized-asm --generate-logged-types
+  forc build -p $PROJECT_DIR --print-finalized-asm
   if [[ $PROJECT_DIR == *"predicate-"* ]]; then
     pnpm exec ts-node scripts/process-predicate.ts $PROJECT_DIR
   fi
