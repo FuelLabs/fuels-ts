f44a08c2d5212c5d01aea9995d02af17e8d01280
Luiz Estacio <luizstacio@gmail.com>
2022-10-27 18:34:33 -0300

    refact: add unused vars config

--- packages/wallet-manager/src/types.ts
+++ packages/wallet-manager/src/types.ts
@@ -32,10 +32,7 @@ export interface WalletManagerState {
 export abstract class Vault<TOptions = { secret?: string }> {
   static readonly type: string;
 
-  // The variables are not used on the abstract class
-  // Did not look enough to find a way to not need to use comments.
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  constructor(options: TOptions) {
+  constructor(_options: TOptions) {
     throw new Error('Not implemented');
   }
 
@@ -51,13 +48,11 @@ export abstract class Vault<TOptions = { secret?: string }> {
     throw new Error('Not implemented');
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  exportAccount(address: AbstractAddress): string {
+  exportAccount(_address: AbstractAddress): string {
     throw new Error('Not implemented');
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  getWallet(address: AbstractAddress): Wallet {
+  getWallet(_address: AbstractAddress): Wallet {
     throw new Error('Not implemented');
   }
 }
