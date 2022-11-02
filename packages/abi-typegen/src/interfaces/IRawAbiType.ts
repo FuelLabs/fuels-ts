cd7bd9b64b5d8576965c5a09f7c3878770996210
Anderson Arboleya <anderson@arboleya.me>
2022-11-02 16:17:58 -0300

    Adjusting typedefs for `IRawAbiType` interface

--- packages/abi-typegen/src/interfaces/IRawAbiType.ts
+++ packages/abi-typegen/src/interfaces/IRawAbiType.ts
@@ -2,12 +2,12 @@ export interface IRawAbiTypeRoot {
   type: string; // type name
   typeId: number; // type id (others will reference it)
   components: null | IRawAbiTypeComponent[];
-  typeParameters: null | Record<string, unknown>[];
+  typeParameters: null | number[];
 }
 
 export interface IRawAbiTypeComponent {
   name: string; // type name
   type: number; // foreing key for `typeId` (on `root` nodes)
   components?: null | IRawAbiTypeComponent[];
-  typeArguments: null | Record<string, unknown>[];
+  typeArguments: null | number[];
 }
