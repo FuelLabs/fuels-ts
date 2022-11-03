4c7f5eb3c9137c930ff906e3727212ba4dba4805
Anderson Arboleya <anderson@arboleya.me>
2022-11-03 17:27:13 -0300

    Refactoring whole test, integrates with new helpers

--- packages/abi-typegen/src/cli.test.ts
+++ packages/abi-typegen/src/cli.test.ts
@@ -1,33 +1,59 @@
 import { existsSync } from 'fs';
 import { join } from 'path';
+import rimraf from 'rimraf';
+
+import { contractPaths } from '../test/fixtures';
+import { executeAndCatch } from '../test/utils/executeAndCatch';
+import { createTempSwayProject } from '../test/utils/sway/createTempSwayProject';
 
 import { run } from './cli';
 
 describe('cli.js', () => {
-  test('should execute cli routine', async () => {
-    const contractDir = join(__dirname, '../test/fixtures/contract-enum-only');
-    const inputPath = join(contractDir, '/out/debug/*-abi.json');
-    const outDir = join(contractDir, 'generated');
+  test('should execute cli routine and generate files', async () => {
+    // setup temp sway project
+    const contractPath = contractPaths.full;
+    const autoBuild = true;
 
-    // First, we backup `process.argv` and re-define it, injecting
-    // the desired input/ouput options pointing to the dirs we want
-    const bkp = process.argv;
-    process.argv = ['typegen', '-i', inputPath, '-o', outDir, '-v', 'false'];
+    const { tempDir, normalizedContractName } = createTempSwayProject({
+      contractPath,
+      autoBuild,
+    });
+
+    // compute filepaths
+    const inputPath = join(tempDir, '/out/debug/*-abi.json');
+    const outDir = join(tempDir, 'generated');
+
+    // backup `process.argv` before re-defining it
+    const argvBkp = process.argv;
+
+    // inject desired input/ouput options pointing to the temp project
+    process.argv = ['typegen', '-i', inputPath, '-o', outDir, '-v', 'true'];
 
     // executes program
-    run({ programName: 'cli.js:test' });
+    const fn = () => run({ programName: 'cli.js:test' });
+    const { error, result } = await executeAndCatch(fn);
 
-    process.argv = bkp;
+    // validates execution was ok
+    expect(error).toBeFalsy;
+    expect(result).toBeTruthy;
 
+    // rollback argv
+    process.argv = argvBkp;
+
+    // check if all files were created
     const files = [
-      join(outDir, 'index.d.ts'),
+      join(outDir, 'index.ts'),
       join(outDir, 'common.d.ts'),
-      join(outDir, 'ContractEnumOnlyAbi.d.ts'),
-      join(outDir, 'factories', 'ContractEnumOnlyAbi__factory.ts'),
+      join(outDir, `${normalizedContractName}Abi.d.ts`),
+      join(outDir, 'factories', `${normalizedContractName}Abi__factory.ts`),
     ];
 
+    expect(files.length).toEqual(4);
+
     files.forEach((f) => {
-      expect(existsSync(f)).toBeTruthy;
+      expect(existsSync(f)).toEqual(true);
     });
+
+    rimraf.sync(tempDir);
   });
 });
