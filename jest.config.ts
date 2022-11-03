ecbdea8fc041419f08f4887d64c3ddd855ceda92
Anderson Arboleya <anderson@arboleya.me>
2022-11-03 16:26:28 -0300

    Ignoring test patterns in jest coverage settings

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@ const config: Config.InitialOptions = {
   setupFiles: ['./jest.env.ts'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   modulePathIgnorePatterns: ['/dist/'],
-  coveragePathIgnorePatterns: ['/dist/'],
+  coveragePathIgnorePatterns: ['/dist/', '/test/', '.test.ts'],
 };
 
 export default config;
