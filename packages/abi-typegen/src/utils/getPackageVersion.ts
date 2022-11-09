b3e8810e32afad7ec56cbf57deef35c72c4479c0
Anderson Arboleya <anderson@arboleya.me>
2022-11-09 16:29:16 -0300

    Dynamically finding repo root

--- packages/abi-typegen/src/utils/getPackageVersion.ts
+++ packages/abi-typegen/src/utils/getPackageVersion.ts
@@ -1,8 +1,24 @@
 import { readFileSync } from 'fs';
-import { join } from 'path';
+import { basename, join } from 'path';
+
+export function getPackageRootDir() {
+  let rootDir: string | undefined;
+
+  let dirPath = __dirname;
+
+  do {
+    dirPath = join(dirPath, '..');
+    const dirName = basename(dirPath);
+    if (dirName === 'abi-typegen') {
+      rootDir = dirPath;
+    }
+  } while (rootDir === undefined);
+
+  return rootDir;
+}
 
 export function getPackageVersion() {
-  const packagePath = join(__dirname, '..', '..', 'package.json');
+  const packagePath = join(getPackageRootDir(), 'package.json');
   const packageJson = readFileSync(packagePath, 'utf8');
   const { version } = JSON.parse(packageJson);
   return { version };
