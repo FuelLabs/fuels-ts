273892c0f22a3028b7248624ac131c57b158a82b
Anderson Arboleya <anderson@arboleya.me>
2022-11-10 17:19:22 -0300

    Removing obsolete method

--- packages/abi-typegen/src/types/OptionType.ts
+++ packages/abi-typegen/src/types/OptionType.ts
@@ -1,8 +1,4 @@
-import type { IRawAbiTypeComponent } from '../interfaces/IRawAbiType';
 import type { IType } from '../interfaces/IType';
-import type { TargetEnum } from '../interfaces/TargetEnum';
-import { findType } from '../utils/findType';
-import { parseTypeArguments } from '../utils/parseTypeArguments';
 
 import { AType } from './AType';
 
@@ -23,41 +19,5 @@ export class OptionType extends AType implements IType {
       outputLabel: `Option`,
     };
     return this.attributes;
-  }
-
-  public getStructContents(params: { types: IType[]; target: TargetEnum }) {
-    const { types, target } = params;
-    const { components } = this.rawAbiType;
-
-    // `components` array guaranteed to always exist for structs/enums
-    const structComponents = components as IRawAbiTypeComponent[];
-
-    // loop through all components
-    const members = structComponents.map((component) => {
-      const { name, type: typeId, typeArguments } = component;
-
-      const type = findType({ types, typeId });
-
-      let typeDecl: string;
-
-      if (typeArguments) {
-        // recursively process child `typeArguments`
-        typeDecl = parseTypeArguments({
-          types,
-          target,
-          parentTypeId: typeId,
-          typeArguments,
-        });
-      } else {
-        // or just collect type declaration
-        const attributeKey: 'inputLabel' | 'outputLabel' = `${target}Label`;
-        typeDecl = type.attributes[attributeKey];
-      }
-
-      // assemble it in `[key: string]: <Type>` fashion
-      return `${name}: ${typeDecl}`;
-    });
-
-    return members.join(', ');
   }
 }
