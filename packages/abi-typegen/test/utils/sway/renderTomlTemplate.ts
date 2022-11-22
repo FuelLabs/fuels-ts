b2c7effbde41a3432fbdfde5dcca8a6425430918
Anderson Arboleya <anderson@arboleya.me>
2022-11-22 17:09:00 -0300

    Simplifying template formatting

--- packages/abi-typegen/test/utils/sway/renderTomlTemplate.ts
+++ packages/abi-typegen/test/utils/sway/renderTomlTemplate.ts
@@ -3,14 +3,13 @@
 */
 export function renderTomlTemplate(params: { contractFilename: string; contractName: string }) {
   const template = `
-    [project]
-    authors = ["fuellabs"]
-    entry = "${params.contractFilename}"
-    license = "Apache-2.0"
-    name = "${params.contractName}"
+[project]
+authors = ["fuellabs"]
+entry = "${params.contractFilename}"
+license = "Apache-2.0"
+name = "${params.contractName}"
 
-    [dependencies]`;
+[dependencies]`;
 
-  // trim left white spaces
-  return template.replace(/^[\s]+/gm, '');
+  return template;
 }
