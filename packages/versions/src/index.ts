82469d9ec42a524eb27cf048fba7e2df27c190a0
Anderson Arboleya <anderson@arboleya.me>
2022-11-23 10:15:11 -0300

    Docs

--- packages/versions/src/index.ts
+++ packages/versions/src/index.ts
@@ -1,3 +1,13 @@
+/*
+  Variables:
+    `FUELS` — comes from `/packages/fuels/package.json`
+    `FUEL_CORE` — comes from `/services/fuel-core/Dockerfile`
+    `FORC` — comes from `/packages/forc-bin/package.json`
+
+  Check the injection script at:
+      ../scripts/injectVersions.ts
+*/
+
 export const versions = {
   FUELS: '0.21.2',
   FUEL_CORE: '0.14.0',
