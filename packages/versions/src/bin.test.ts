330440c4e61d346d0b2e697b853e802d7f9c7661
Anderson Arboleya <anderson@arboleya.me>
2022-11-23 18:49:32 -0300

    Fixing broken test

--- packages/versions/src/bin.test.ts
+++ packages/versions/src/bin.test.ts
@@ -1,14 +1,15 @@
-import { ImportMock } from 'ts-mock-imports';
-
-import * as cliMod from './cli';
-
 describe('bin.js', () => {
   test('should run cli program with proper name', async () => {
-    const run = ImportMock.mockFunction(cliMod, 'run');
+    // mocking
+    const run = jest.fn();
+    jest.mock('./cli', () => ({ run }));
 
+    // executing
     const expected = { programName: 'fuels-versions' };
     await import('./bin');
-    expect(run.callCount).toEqual(1);
-    expect(run.firstCall.firstArg).toStrictEqual(expected);
+
+    // validating
+    expect(run).toHaveBeenCalledTimes(1);
+    expect(run.mock.calls[0][0]).toStrictEqual(expected);
   });
 });
