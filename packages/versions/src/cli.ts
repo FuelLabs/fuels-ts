2dc741f6e5054be3ed907531b5ef34b6c0194bb8
Anderson Arboleya <anderson@arboleya.me>
2022-11-23 09:07:28 -0300

    Implementing CLI, validating toolchain semver, warning users as needed

--- packages/versions/src/cli.ts
+++ packages/versions/src/cli.ts
@@ -1,17 +1,47 @@
+import { bold, red, green } from 'chalk';
 import { execSync } from 'child_process';
+import semver from 'semver';
 
 import { versions } from './index';
 
 export async function run(_params: { programName: string }) {
   const { log } = console;
 
-  const userForc = execSync('forc --version');
-  const userFuelCore = execSync('fuel-core --version');
+  const errorFooter: string = [
+    '\nYou can install/update them with:',
+    ` ${green('https://github.com/fuellabs/fuelup')}`,
+  ].join('\n');
 
-  console.log({ versions });
-  console.log({ userForc, userFuelCore });
+  let userForc: string | undefined;
+  let userFuelCore: string | undefined;
 
-  // TODO: Implement version's check
+  try {
+    const reg = /[^0-9.]+/g;
+    userForc = execSync('forc --version').toString().replace(reg, '');
+    userFuelCore = execSync('fuel-core --version').toString().replace(reg, '');
+  } catch (err) {
+    log('Make sure you have Forc and fuel-core installed.');
+    log(errorFooter);
+    throw err;
+  }
 
-  log('\nDone.⚡');
+  const forcOk = semver.satisfies(userForc, versions.FORC);
+  const coreOk = semver.satisfies(userFuelCore, versions.FUEL_CORE);
+
+  if (!forcOk) {
+    log(`Supported Forc version: ${green(versions.FORC)}`);
+    log(`You're using Forc: ${red(userForc)}`);
+    log(errorFooter);
+    process.exit(1);
+  } else if (!coreOk) {
+    log(`Supported ${bold('fuel-core')} version: ${green(versions.FUEL_CORE)}`);
+    log(`You're using ${bold('fuel-core')}: ${red(userFuelCore)}`);
+    log(errorFooter);
+    process.exit(1);
+  }
+  log(`You have all the right versions! ⚡`);
+  log(` ${bold('Forc')}: ${green(userForc)}`);
+  log(` ${bold('fuel-core')}: ${green(userFuelCore)}`);
+
+  process.exit(0);
 }
