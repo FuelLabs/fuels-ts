dc40b8395e9d91102c0389d120d2110393202663
Anderson Arboleya <anderson@arboleya.me>
2022-11-24 14:05:42 -0300

    Renaming contract fixture to match its contents

--- packages/abi-typegen/test/fixtures/index.ts
+++ packages/abi-typegen/test/fixtures/index.ts
@@ -3,7 +3,6 @@ import { join } from 'path';
 const fixturesDir = join(__dirname);
 
 const arrayNested = join(fixturesDir, 'array-nested.sw');
-const arraySimple = join(fixturesDir, 'array-simple.sw');
 const enumOfEnums = join(fixturesDir, 'enum-of-enums.sw');
 const enumOfStructs = join(fixturesDir, 'enum-of-structs.sw');
 const enumSimple = join(fixturesDir, 'enum-simple.sw');
@@ -12,12 +11,12 @@ const minimal = join(fixturesDir, 'minimal.sw');
 const optionSimple = join(fixturesDir, 'option-simple.sw');
 const structNested = join(fixturesDir, 'struct-nested.sw');
 const structSimple = join(fixturesDir, 'struct-simple.sw');
+const structWithArray = join(fixturesDir, 'struct-with-array.sw');
 const tupleSimple = join(fixturesDir, 'tuple-simple.sw');
 const vectorSimple = join(fixturesDir, 'vector-simple.sw');
 
 export const contractPaths: { [key: string]: string } = {
   arrayNested,
-  arraySimple,
   enumOfEnums,
   enumOfStructs,
   enumSimple,
@@ -26,6 +25,7 @@ export const contractPaths: { [key: string]: string } = {
   optionSimple,
   structNested,
   structSimple,
+  structWithArray,
   tupleSimple,
   vectorSimple,
 };
