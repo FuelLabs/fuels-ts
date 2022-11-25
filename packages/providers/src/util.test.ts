fe29cdf345c9b51d3f329999b8bd358e98762201
Dhaiwat / dhai.eth <dhaiwatpandya@gmail.com>
2022-11-26 02:38:47 +0530

    refactor: `buildBlockExplorerUrl` test code

--- packages/providers/src/util.test.ts
+++ packages/providers/src/util.test.ts
@@ -1,39 +1,30 @@
 import { buildBlockExplorerUrl } from './util';
 
-const DEFAULT_BLOCK_EXPLORER_URL = 'https://fuellabs.github.io/block-explorer-v2';
-
-interface TestCase {
-  path: string;
-  blockExplorerUrl?: string;
-  expected: string;
-}
-
-const basePath = 'transaction/0x123';
-const customBlockExplorerUrl = 'https://explorer.fuel.sh';
-
-const generateTestCases = (): TestCase[] => {
-  // iterate over all possible combinations of blockExplorerUrl and path by adding leading and trailing slashes, and generate test cases
-  const testCases: TestCase[] = [];
-  [undefined, customBlockExplorerUrl, `${customBlockExplorerUrl}/`].forEach((blockExplorerUrl) => {
-    [basePath, `/${basePath}`, `${basePath}/`, `/${basePath}/`].forEach((path) => {
-      const trimSlashes = /^\/|\/$/gm;
-      const cleanPath = path.replace(trimSlashes, '');
-      const cleanBlockExplorerUrl = blockExplorerUrl?.replace(trimSlashes, '');
-      testCases.push({
-        path,
-        blockExplorerUrl,
-        expected: `${cleanBlockExplorerUrl || DEFAULT_BLOCK_EXPLORER_URL}/${cleanPath}`,
-      });
-    });
-  });
-  return testCases;
-};
-
 describe('Providers utils', () => {
-  test('Builds block explorer links using buildBlockExplorerUrl', () => {
-    const testCases = generateTestCases();
-    testCases.forEach(({ path, blockExplorerUrl, expected }) => {
-      expect(buildBlockExplorerUrl({ path, blockExplorerUrl })).toBe(expected);
+  const DEFAULT_BLOCK_EXPLORER_URL = 'https://fuellabs.github.io/block-explorer-v2';
+
+  test('Builds block explorer URL in every possible way', () => {
+    const trimSlashes = /^\/|\/$/gm;
+    const basePath = 'transaction/0x123';
+    const baseBlockExplorerUrl = 'https://explorer.fuel.sh';
+
+    // Here, we assemble and iterate over all possible combinations of
+    // `blockExplorerUrl` and `path` by adding leading/trailing slashes to them
+    const pathUrls = [basePath, `/${basePath}`, `${basePath}/`, `/${basePath}/`];
+    const blockExplorerUrls = [undefined, baseBlockExplorerUrl, `${baseBlockExplorerUrl}/`];
+
+    pathUrls.forEach((path) => {
+      blockExplorerUrls.forEach((blockExplorerUrl) => {
+        // First, we clean paths manually
+        const cleanPath = path.replace(trimSlashes, '');
+        const cleanBlockExplorerUrl = blockExplorerUrl
+          ? blockExplorerUrl.replace(trimSlashes, '')
+          : DEFAULT_BLOCK_EXPLORER_URL;
+
+        // Then we compare them with the ones returned by our method
+        const expected = `${cleanBlockExplorerUrl}/${cleanPath}`;
+        expect(buildBlockExplorerUrl({ path, blockExplorerUrl })).toBe(expected);
+      });
     });
   });
 });
