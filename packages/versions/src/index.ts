dbe950c360669baeaa18db324cbf201a69d1353e
Anderson Arboleya <anderson@arboleya.me>
2022-11-29 10:16:00 -0300

    KISS, deleting all scripts, favoring env variables for simplicity

--- packages/versions/src/index.ts
+++ packages/versions/src/index.ts
@@ -4,16 +4,17 @@
     `FUEL_CORE` — comes from `/services/fuel-core/Dockerfile`
     `FORC` — comes from `/packages/forc-bin/package.json`
 
-  Check the injection script at:
-      ../scripts/writeIndex.ts
+  The CI build routine reads the aforementioned files,
+  and set all the variables prior to building the packages.
 
-  NOTE:
-    The entry `process.env.BUILD_VERSION` will be replaced
-    at build time by the CI routine prior to release
+  The build then replaces all `process.env.<VAR_NAME>` entries
+  bellow with their respective values from the environment.
+
+  Take a look at the /.github/workflows/release` file.
 */
 
 export const versions = {
-  FUELS: process.env.BUILD_VERSION || '0.21.2',
-  FUEL_CORE: '0.14.0',
-  FORC: '0.30.0',
+  FUELS: process.env.BUILD_VERSION || '~',
+  FUEL_CORE: process.env.FUEL_CORE_VERSION || '~',
+  FORC: process.env.FORC_VERSION || '~',
 };
