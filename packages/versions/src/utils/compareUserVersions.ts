70c795ee702dd323c6cac2b12d10c9353c972576
Anderson Arboleya <anderson@arboleya.me>
2022-11-29 18:19:15 -0300

    Adding new utility to compare user x fuels supported versions

---
+++ packages/versions/src/utils/compareUserVersions.ts
@@ -0,0 +1,27 @@
+import semver from 'semver';
+
+import { versions } from '..';
+
+export interface ICompareVersionsParams {
+  userForcVersion: string;
+  userFuelCoreVersion: string;
+}
+
+export function compareUserVersions(params: ICompareVersionsParams) {
+  const { userForcVersion, userFuelCoreVersion } = params;
+
+  // are user's versions GREATER than the ones supported by the SDK?
+  const userForcIsGt = semver.gt(userForcVersion, versions.FORC);
+  const userFuelCoreIsGt = semver.gt(userFuelCoreVersion, versions.FUEL_CORE);
+
+  // are user's versions EXACTLY the ones supported by the SDK?
+  const userForcIsEq = semver.eq(userForcVersion, versions.FORC);
+  const userFuelCoreIsEq = semver.eq(userFuelCoreVersion, versions.FUEL_CORE);
+
+  return {
+    userForcIsGt,
+    userFuelCoreIsGt,
+    userForcIsEq,
+    userFuelCoreIsEq,
+  };
+}
