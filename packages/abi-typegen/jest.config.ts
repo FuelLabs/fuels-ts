15271b000c60d96759f02248be19522150d68457
Anderson Arboleya <anderson@arboleya.me>
2022-11-30 13:39:39 -0300

    Configuring jest projects, setting up `.hbs` text transformer

--- packages/abi-typegen/jest.config.ts
+++ packages/abi-typegen/jest.config.ts
@@ -3,12 +3,12 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFiles: ['./jest.env.ts'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   modulePathIgnorePatterns: ['/dist/'],
   coveragePathIgnorePatterns: ['/dist/', '/test/', '.test.ts'],
-  testTimeout: 15000,
-  projects: ['<rootDir>/packages/**/jest.config.ts'],
+  transform: {
+    '\\.hbs': 'jest-text-transformer',
+  },
 };
 
 export default config;
