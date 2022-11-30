b1f5e0e2853f04afd960a51b9394866d6cdbcacf
Anderson Arboleya <anderson@arboleya.me>
2022-11-30 15:55:04 -0300

    Refactoring all tests, perfecting logic and template isolation

--- packages/abi-typegen/src/templates/common.test.ts
+++ packages/abi-typegen/src/templates/common.test.ts
@@ -1,11 +1,8 @@
-import { getPackageVersion } from '../utils/getPackageVersion';
-
 import { renderCommonTemplate } from './common';
 
 describe('templates/common', () => {
   test('should render common template', () => {
-    const { version: fuelsVersion } = getPackageVersion();
-    const expectedVersion = `Fuels version: ${fuelsVersion}`;
-    expect(renderCommonTemplate({ fuelsVersion })).toMatch(expectedVersion);
+    const expectedVersion = /(Fuels|Forc|Fuel-Core) version: 0.0.0/;
+    expect(renderCommonTemplate()).toMatch(expectedVersion);
   });
 });
