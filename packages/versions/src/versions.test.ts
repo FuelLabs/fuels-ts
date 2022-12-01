6365b3e34354f75fba2f4a522232c4fd59849912
Anderson Arboleya <anderson@arboleya.me>
2022-12-01 13:05:55 -0300

    Splitting logic across different files

---
+++ packages/versions/src/versions.test.ts
@@ -0,0 +1,41 @@
+describe('versions.js', () => {
+  /*
+    Hooks
+  */
+  beforeEach(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  /*
+    Tests
+  */
+  test('should export default toolchain versions', async () => {
+    const { versions } = await import('./versions');
+
+    expect(versions.FUELS).toEqual('0.0.0');
+    expect(versions.FUEL_CORE).toEqual('0.0.0');
+    expect(versions.FORC).toEqual('0.0.0');
+  });
+
+  test('should default to zeroed versions', async () => {
+    const { thisVersionOrDefault } = await import('./index');
+
+    expect(thisVersionOrDefault()).toEqual('0.0.0');
+    expect(thisVersionOrDefault(!0)).toEqual('0.0.0'); // the exp tsup uses
+    expect(thisVersionOrDefault(true)).toEqual('0.0.0');
+    expect(thisVersionOrDefault(undefined)).toEqual('0.0.0');
+  });
+
+  test('should pass-through received versions', async () => {
+    const { thisVersionOrDefault } = await import('./index');
+
+    expect(thisVersionOrDefault('1.1.1')).toEqual('1.1.1');
+    expect(thisVersionOrDefault('2.2.2')).toEqual('2.2.2');
+    expect(thisVersionOrDefault('3.3.3')).toEqual('3.3.3');
+  });
+});
