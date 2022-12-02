5b682487a241ef98c3829c97e776029e84a92d80
Cameron Manavian <cameron.manavian@fuel.sh>
2022-12-02 00:20:15 -0800

    add logging advanced tests

---
+++ packages/fuel-gauge/src/advanced-logging.test.ts
@@ -0,0 +1,56 @@
+import type { Contract } from 'fuels';
+
+import { getSetupContract } from './utils';
+
+const setupContract = getSetupContract('advanced-logging');
+
+let contractInstance: Contract;
+
+beforeAll(async () => {
+  contractInstance = await setupContract();
+});
+
+describe('Advanced Logging', () => {
+  it('can get log data', async () => {
+    const { value, logs } = await contractInstance.functions.test_function().call();
+
+    expect(value).toBeTruthy();
+    logs[5].game_id = logs[5].game_id.toHex();
+    logs[9].game_id = logs[9].game_id.toHex();
+
+    expect(logs).toEqual([
+      'Game State',
+      { Playing: 1 },
+      'Contract Id',
+      {
+        value: '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+      },
+      'Game Ref',
+      {
+        score: 0,
+        time_left: 100,
+        ammo: 10,
+        game_id: '0x18af8',
+        state: { Playing: 1 },
+        contract_Id: {
+          value: '0xfffffffffffffffffffffffffffffffff00fffffffffffffffffffffffffffff',
+        },
+        difficulty: { Medium: true },
+      },
+      'Game Ref Score',
+      0,
+      'Direct Game',
+      {
+        score: 101,
+        time_left: 12,
+        ammo: 3,
+        game_id: '0x20157',
+        state: { Playing: 1 },
+        contract_Id: {
+          value: '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+        },
+        difficulty: { Hard: true },
+      },
+    ]);
+  });
+});
