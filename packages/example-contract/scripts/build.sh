18e6d772c91e0c1d45e3549606bffb9edc6117d2
Anderson Arboleya <anderson@arboleya.me>
2022-12-08 09:09:55 -0300

    Fixing broken reference to typegen bin entry point

--- packages/example-contract/scripts/build.sh
+++ packages/example-contract/scripts/build.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-# We run install again to make sure `fuels typegen` CLI is correct setup
+# We run install again to make sure `fuels-typegen` CLI is correct setup
 # this issue ocorrus becuase the CLI tool is under the same monorepo
 pnpm install
 pnpm forc build
-pnpm exec fuels typegen -i out/debug/example-contract-abi.json -o src/example-contract-types
+pnpm exec fuels-typegen -i out/debug/example-contract-abi.json -o src/example-contract-types
