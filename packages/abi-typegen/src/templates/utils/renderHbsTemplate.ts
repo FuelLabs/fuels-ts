f44da673cb9b1c00189b0ec3cfc23c666aaa4963
Anderson Arboleya <anderson@arboleya.me>
2022-12-09 15:28:23 -0300

    Resolving TODO

--- packages/abi-typegen/src/templates/utils/renderHbsTemplate.ts
+++ packages/abi-typegen/src/templates/utils/renderHbsTemplate.ts
@@ -1,3 +1,4 @@
+import { versions } from '@fuel-ts/versions';
 import { compile } from 'handlebars';
 
 import headerTemplate from '../hbs/_header.hbs';
@@ -8,13 +9,6 @@ import headerTemplate from '../hbs/_header.hbs';
 */
 export function renderHbsTemplate(params: { template: string; data?: Record<string, unknown> }) {
   const { data, template } = params;
-
-  // TODO: Integrate this on top of `packages/versions` PR
-  const versions = {
-    FUELS_VERSION: '0.0.0',
-    FORC_VERSION: '0.0.0',
-    FUEL_CORE_VERSION: '0.0.0',
-  };
 
   const options = {
     strict: true,
