ab7d2dd25768f4fe4c0cf612686bd3b23495d6cc
Anderson Arboleya <anderson@arboleya.me>
2022-12-09 11:02:49 -0300

    Stop exporting non-browsers modules in the package indexer/exporter

--- packages/abi-typegen/src/index.test.ts
+++ packages/abi-typegen/src/index.test.ts
@@ -3,6 +3,5 @@ import * as indexMod from './index';
 describe('index.ts', () => {
   test('should export AbiTypeGen class', () => {
     expect(indexMod.AbiTypeGen).toBeTruthy();
-    expect(indexMod.runTypegen).toBeTruthy();
   });
 });
