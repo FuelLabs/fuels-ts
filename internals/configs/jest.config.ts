afe7c111ac390e49779bf7567d1369d560e5b190
Anderson Arboleya <anderson@arboleya.me>
2022-12-13 17:00:46 -0300

    Adding new workspace dir, adding new `configs` package

---
+++ internals/configs/jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  // setupFiles: ['./jest.env.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  modulePathIgnorePatterns: ['/dist/'],
+  coveragePathIgnorePatterns: ['/dist/', '/test/', '.test.ts'],
+  testTimeout: 15000,
+};
+
+export default config;
