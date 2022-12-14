7ed8f2d972d06c756e4c9cf03bf24ff5b1de0f33
Cameron Manavian <cameron.manavian@fuel.sh>
2022-12-14 00:29:23 -0800

    Add new test suite

---
+++ packages/fuel-gauge/src/auth-testing.test.ts
@@ -0,0 +1,43 @@
+import type { Contract } from 'fuels';
+
+import { getSetupContract, getWallet } from './utils';
+
+const setupContract = getSetupContract('auth_testing_contract');
+
+let contractInstance: Contract;
+
+beforeAll(async () => {
+  contractInstance = await setupContract();
+});
+
+describe('Auth Testing', () => {
+  it('can get is_caller_external', async () => {
+    const { value } = await contractInstance.functions.is_caller_external().call();
+
+    expect(value).toBeTruthy();
+  });
+
+  it('can check_msg_sender [with correct id]', async () => {
+    const wallet = getWallet();
+
+    const { value } = await contractInstance.functions
+      .check_msg_sender({ value: wallet.address.toB256() })
+      .call();
+
+    expect(value).toBeTruthy();
+  });
+
+  it('can check_msg_sender [with incorrect id]', async () => {
+    const wallet = getWallet();
+
+    try {
+      await contractInstance.functions
+        .check_msg_sender({ value: wallet.address.toB256().replace('x', 'y') })
+        .call();
+
+      throw new Error('it should have thrown');
+    } catch (error) {
+      expect(error).toBeTruthy();
+    }
+  });
+});
