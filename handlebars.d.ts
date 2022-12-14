1f67e30d255a1bf9d3471d91c75a9b959829f816
Anderson Arboleya <anderson@arboleya.me>
2022-12-14 10:09:27 -0300

    Adding TODO

--- handlebars.d.ts
+++ handlebars.d.ts
@@ -16,3 +16,5 @@ declare module '*.hbs' {
   const value: string;
   export default value;
 }
+
+// TODO: Segreate packages' tests-and-configs and move all handlebars-related stuff inside `packages/abi-typegen`
