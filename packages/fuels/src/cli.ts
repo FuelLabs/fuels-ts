46c746318c66ab70d91056c9412ce8a504e1952e
Anderson Arboleya <anderson@arboleya.me>
2022-12-15 12:53:16 -0300

    Testing empty placeholder

--- packages/fuels/src/cli.ts
+++ packages/fuels/src/cli.ts
@@ -3,25 +3,20 @@
 // import { run as runVersions } from '@fuel-ts/versions/cli';
 // import { Command } from 'commander';
 
-// export function run(argv: string[]) {
-//   const program = new Command();
-
-//   program.name('fuels');
-//   program.version(versions.FUELS);
-
-//   // routing `versions` sub-command
-//   program
-//     .command('versions')
-//     .description('check for version incompatibilities')
-//     .action(runVersions);
-
-//   // routing `typegen` sub-command
-//   const typegen = program
-//     .command('typegen')
-//     .description(`generate typescript from contract abi json files`);
-
-//   routeTypeGen(typegen);
-
-//   // vroom vroom
-//   program.parse(argv);
-// }
+export function run(_argv: string[]) {
+  // const program = new Command();
+  // program.name('fuels');
+  // program.version(versions.FUELS);
+  // // routing `versions` sub-command
+  // program
+  //   .command('versions')
+  //   .description('check for version incompatibilities')
+  //   .action(runVersions);
+  // // routing `typegen` sub-command
+  // const typegen = program
+  //   .command('typegen')
+  //   .description(`generate typescript from contract abi json files`);
+  // routeTypeGen(typegen);
+  // // vroom vroom
+  // program.parse(argv);
+}
