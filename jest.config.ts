9c88a0bf44040050eb45c603758a2f19ace07ab9
Anderson Arboleya <anderson@arboleya.me>
2022-12-15 08:33:08 -0300

    Debugging CI

--- jest.config.ts
+++ jest.config.ts
@@ -2,15 +2,20 @@ import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
   // reporters: [['jest-simple-dot-reporter', { color: true }]],
-  projects: ['<rootDir>/packages/*/jest.config.ts'],
-  coverageThreshold: {
-    global: {
-      branches: 59.19,
-      functions: 73.75,
-      lines: 83.14,
-      statements: 83.12,
-    },
-  },
+  // projects: ['<rootDir>/packages/*/jest.config.ts'],
+  projects: [
+    '<rootDir>/packages/abi-typegen/jest.config.ts',
+    '<rootDir>/packages/fuels/jest.config.ts',
+    '<rootDir>/packages/versions/jest.config.ts',
+  ],
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 59.19,
+  //     functions: 73.75,
+  //     lines: 83.14,
+  //     statements: 83.12,
+  //   },
+  // },
 };
 
 export default config;
