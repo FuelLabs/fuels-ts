fcc8ed8eeb17409b07cf946256902cf5042663c6
Anderson Arboleya <anderson@arboleya.me>
2022-12-15 12:49:45 -0300

    Temporarily commenting file

--- packages/fuels/src/cli.ts
+++ packages/fuels/src/cli.ts
@@ -1,27 +1,27 @@
-import { configureCliOptions as routeTypeGen } from '@fuel-ts/abi-typegen/cli';
-import { versions } from '@fuel-ts/versions';
-import { run as runVersions } from '@fuel-ts/versions/cli';
-import { Command } from 'commander';
+// import { configureCliOptions as routeTypeGen } from '@fuel-ts/abi-typegen/cli';
+// import { versions } from '@fuel-ts/versions';
+// import { run as runVersions } from '@fuel-ts/versions/cli';
+// import { Command } from 'commander';
 
-export function run(argv: string[]) {
-  const program = new Command();
+// export function run(argv: string[]) {
+//   const program = new Command();
 
-  program.name('fuels');
-  program.version(versions.FUELS);
+//   program.name('fuels');
+//   program.version(versions.FUELS);
 
-  // routing `versions` sub-command
-  program
-    .command('versions')
-    .description('check for version incompatibilities')
-    .action(runVersions);
+//   // routing `versions` sub-command
+//   program
+//     .command('versions')
+//     .description('check for version incompatibilities')
+//     .action(runVersions);
 
-  // routing `typegen` sub-command
-  const typegen = program
-    .command('typegen')
-    .description(`generate typescript from contract abi json files`);
+//   // routing `typegen` sub-command
+//   const typegen = program
+//     .command('typegen')
+//     .description(`generate typescript from contract abi json files`);
 
-  routeTypeGen(typegen);
+//   routeTypeGen(typegen);
 
-  // vroom vroom
-  program.parse(argv);
-}
+//   // vroom vroom
+//   program.parse(argv);
+// }
