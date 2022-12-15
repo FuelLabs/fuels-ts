8be85af0eac5ea83b52a59e8f15faa3e5468dbc0
Cameron Manavian <cameron.manavian@fuel.sh>
2022-12-14 21:43:08 -0800

    format

--- packages/fuel-gauge/src/auth-testing.test.ts
+++ packages/fuel-gauge/src/auth-testing.test.ts
@@ -1,8 +1,8 @@
-import type { Contract } from "fuels";
+import type { Contract } from 'fuels';
 
-import { getSetupContract, getWallet } from "./utils";
+import { getSetupContract, getWallet } from './utils';
 
-const setupContract = getSetupContract("auth_testing_contract");
+const setupContract = getSetupContract('auth_testing_contract');
 
 let contractInstance: Contract;
 
@@ -10,16 +10,14 @@ beforeAll(async () => {
   contractInstance = await setupContract();
 });
 
-describe("Auth Testing", () => {
-  it("can get is_caller_external", async () => {
-    const { value } = await contractInstance.functions
-      .is_caller_external()
-      .call();
+describe('Auth Testing', () => {
+  it('can get is_caller_external', async () => {
+    const { value } = await contractInstance.functions.is_caller_external().call();
 
     expect(value).toBeTruthy();
   });
 
-  it("can check_msg_sender [with correct id]", async () => {
+  it('can check_msg_sender [with correct id]', async () => {
     const wallet = getWallet();
 
     const { value } = await contractInstance.functions
@@ -29,22 +27,20 @@ describe("Auth Testing", () => {
     expect(value).toBeTruthy();
   });
 
-  it("can check_msg_sender [with correct id, using get]", async () => {
+  it('can check_msg_sender [with correct id, using get]', async () => {
     const wallet = getWallet();
 
     expect(async () => {
-      await contractInstance.functions
-        .check_msg_sender({ value: wallet.address.toB256() })
-        .get();
+      await contractInstance.functions.check_msg_sender({ value: wallet.address.toB256() }).get();
     }).toThrow();
   });
 
-  it("can check_msg_sender [with incorrect id]", async () => {
+  it('can check_msg_sender [with incorrect id]', async () => {
     const wallet = getWallet();
 
     expect(async () => {
       await contractInstance.functions
-        .check_msg_sender({ value: wallet.address.toB256().replace("x", "y") })
+        .check_msg_sender({ value: wallet.address.toB256().replace('x', 'y') })
         .call();
     }).toThrow();
   });
