149c07d13abfda6504781fc741cae356ddfaa61e
Anderson Arboleya <anderson@arboleya.me>
2022-12-15 18:36:29 -0300

    Stop ignoring `abi-typegen` and friends in jest configs

--- jest.config.ts
+++ jest.config.ts
@@ -4,14 +4,7 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFiles: ['./jest.env.ts'],
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '/dist/',
-    // TODO: Fix configs and remove ignore patterns below
-    'packages/abi-typegen',
-    'cli.test.ts',
-    'bin.test.ts',
-  ],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   modulePathIgnorePatterns: ['/dist/'],
   coveragePathIgnorePatterns: ['/dist/', '/test/', '.test.ts'],
   testTimeout: 15000,
