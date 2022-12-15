b266ba266cbcc1a2d0eefa23f1ea8f1f41e4e02d
Anderson Arboleya <anderson@arboleya.me>
2022-12-15 09:29:55 -0300

    Debugging CI

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,20 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  modulePathIgnorePatterns: ['/dist/'],
+  coveragePathIgnorePatterns: ['/dist/', '/test/', '.test.ts'],
+  testEnvironment: 'node',
+  setupFiles: ['./internals/tests/jest.env.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', 'packages/abi-typegen'],
+
   // reporters: [['jest-simple-dot-reporter', { color: true }]],
   // projects: ['<rootDir>/packages/*/jest.config.ts'],
-  projects: [
-    '<rootDir>/packages/abi-typegen/jest.config.ts',
-    // '<rootDir>/packages/fuels/jest.config.ts',
-    // '<rootDir>/packages/versions/jest.config.ts',
-  ],
+  // projects: [
+  //   '<rootDir>/packages/abi-typegen/jest.config.ts',
+  //   '<rootDir>/packages/fuels/jest.config.ts',
+  //   '<rootDir>/packages/versions/jest.config.ts',
+  // ],
   // coverageThreshold: {
   //   global: {
   //     branches: 59.19,
