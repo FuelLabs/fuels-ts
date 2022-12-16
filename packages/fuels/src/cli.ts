77bc9678b04ba8c30dd21374414579580850788d
Anderson Arboleya <anderson@arboleya.me>
2022-12-15 23:24:39 -0300

    Re-enabling `fuels` CLI

--- packages/fuels/src/cli.ts
+++ packages/fuels/src/cli.ts
@@ -1,23 +1,27 @@
-// import { configureCliOptions as routeTypeGen } from '@fuel-ts/abi-typegen/cli';
-// import { versions } from '@fuel-ts/versions';
-// import { run as runVersions } from '@fuel-ts/versions/cli';
-// import { Command } from 'commander';
-// import { argv } from 'process';
+import { configureCliOptions as routeTypeGen } from '@fuel-ts/abi-typegen/cli';
+import { versions } from '@fuel-ts/versions';
+import { run as runVersions } from '@fuel-ts/versions/cli';
+import { Command } from 'commander';
+import { argv } from 'process';
 
 export function run(_argv: string[]) {
-  // const program = new Command();
-  // program.name('fuels');
-  // program.version(versions.FUELS);
-  // // routing `versions` sub-command
-  // program
-  //   .command('versions')
-  //   .description('check for version incompatibilities')
-  //   .action(runVersions);
-  // // routing `typegen` sub-command
-  // const typegen = program
-  //   .command('typegen')
-  //   .description(`generate typescript from contract abi json files`);
-  // routeTypeGen(typegen);
-  // // vroom vroom
-  // program.parse(argv);
+  const program = new Command();
+  program.name('fuels');
+  program.version(versions.FUELS);
+
+  // routing `versions` sub-command
+  program
+    .command('versions')
+    .description('check for version incompatibilities')
+    .action(runVersions);
+
+  // routing `typegen` sub-command
+  const typegen = program
+    .command('typegen')
+    .description(`generate typescript from contract abi json files`);
+
+  routeTypeGen(typegen);
+
+  // vroom vroom
+  program.parse(argv);
 }
