b109023c5df124884625d7128aa6b27872dfffe1
Anderson Arboleya <anderson@arboleya.me>
2022-12-28 08:39:55 -0300

    Stop exporting test utilities in main umbrella package

--- packages/fuels/tsup.config.ts
+++ packages/fuels/tsup.config.ts
@@ -6,7 +6,6 @@ export default defineConfig((options) => ({
     index: 'src/index.ts',
     cli: 'src/cli.ts',
     bin: 'src/bin.ts',
-    'test-utils': 'src/test-utils.ts',
   },
   format: ['cjs', 'esm', 'iife'],
   splitting: false,
