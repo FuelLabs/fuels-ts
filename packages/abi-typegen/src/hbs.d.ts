f5b61f13cdfd6fc23345dfebf4cf90d20d0eea96
Anderson Arboleya <anderson@arboleya.me>
2023-01-13 13:55:07 -0300

    Improving docs

--- packages/abi-typegen/src/hbs.d.ts
+++ packages/abi-typegen/src/hbs.d.ts
@@ -1,6 +1,10 @@
 /**
- * This informs TS about our Handlebar `.hbs` templates,
- * setting it as string.
+ * This informs TS about our Handlebar `.hbs` templates. It placed inside
+ * the `/packages/abi-typegen/src` dir, so the `.hbs` imports are
+ * properly handled by the Typegen package.
+ *
+ * However, it is also symlinked in the repo root, so Jest can
+ * process these imports accordingly as well.
  */
 declare module '*.hbs' {
   const value: string;
