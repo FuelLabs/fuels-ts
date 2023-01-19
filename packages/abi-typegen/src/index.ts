6bb183ac69519dc3a00f2331a11df39dda82c007
Anderson Arboleya <anderson@arboleya.me>
2023-01-19 18:32:08 -0300

    Exporting all interfaces on package index

--- packages/abi-typegen/src/index.ts
+++ packages/abi-typegen/src/index.ts
@@ -1,1 +1,8 @@
 export * from './AbiTypeGen';
+
+export * from './interfaces/IFile';
+export * from './interfaces/IFunction';
+export * from './interfaces/IRawAbi';
+export * from './interfaces/IRawAbiFunction';
+export * from './interfaces/IRawAbiType';
+export * from './interfaces/IRawAbiLoggedTypes';
