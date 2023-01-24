49b246e18e18ce2fab9af27198b85b18ed827619
Anderson Arboleya <anderson@arboleya.me>
2023-01-24 10:30:11 -0300

    Fixing script factory renderer method, gathering prop definitions

--- packages/abi-typegen/src/templates/script/factory.ts
+++ packages/abi-typegen/src/templates/script/factory.ts
@@ -4,16 +4,26 @@ import { renderHbsTemplate } from '../utils/renderHbsTemplate';
 import factoryTemplate from './factory.hbs';
 
 export function renderFactoryTemplate(params: { abi: Abi }) {
+  const { abi } = params;
   const { name: capitalizedName, rawContents } = params.abi;
+
   const abiJsonString = JSON.stringify(rawContents, null, 2);
+
+  const func = abi.functions.find((f) => f.name === 'main');
+
+  if (!func) {
+    throw new Error(`ABI doesn't have a 'main()' method.`);
+  }
+
+  const { prefixedInputs: inputs, output } = func.attributes;
 
   const text = renderHbsTemplate({
     template: factoryTemplate,
     data: {
-      capitalizedName,
+      inputs,
+      output,
       abiJsonString,
-      inputs: '{}', // TODO: fix place holder
-      output: '{}', // TODO: fix place holder
+      capitalizedName,
     },
   });
 
