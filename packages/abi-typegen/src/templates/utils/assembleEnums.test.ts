7ec043d88cc650b742adc33eb3b9612c41b60599
Anderson Arboleya <anderson@arboleya.me>
2023-02-02 14:38:15 -0300

    Adding test stubs/placeholders

---
+++ packages/abi-typegen/src/templates/utils/assembleEnums.test.ts
@@ -0,0 +1,7 @@
+import { assembleEnums } from './assembleEnums';
+
+describe('assembleEnums.ts', () => {
+  test('should assemble enums just fine', () => {
+    expect(assembleEnums).toBeTruthy();
+  });
+});
---
+++ packages/abi-typegen/src/templates/utils/assembleStructs.test.ts
@@ -0,0 +1,7 @@
+import { assembleStructs } from './assembleStructs';
+
+describe('assembleStructs.ts', () => {
+  test('should assemble structs just fine', () => {
+    expect(assembleStructs).toBeTruthy();
+  });
+});
