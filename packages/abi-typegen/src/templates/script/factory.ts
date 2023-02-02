6f5627cdee401d7894c25d9c78990fd07f790eaa
Anderson Arboleya <anderson@arboleya.me>
2023-02-02 14:29:50 -0300

    Updating script/factory template, adjusting data format and rendering

--- packages/abi-typegen/src/templates/script/factory.ts
+++ packages/abi-typegen/src/templates/script/factory.ts
@@ -1,11 +1,20 @@
 import type { Abi } from '../../abi/Abi';
 import { renderHbsTemplate } from '../renderHbsTemplate';
+import { assembleEnums } from '../utils/assembleEnums';
+import { assembleStructs } from '../utils/assembleStructs';
 
 import factoryTemplate from './factory.hbs';
 
 export function renderFactoryTemplate(params: { abi: Abi }) {
   const { abi } = params;
-  const { name: capitalizedName, rawContents } = params.abi;
+
+  const { types } = abi;
+
+  const {
+    rawContents,
+    name: capitalizedName,
+    hexlifiedBinContents: hexlifiedBinString,
+  } = params.abi;
 
   const abiJsonString = JSON.stringify(rawContents, null, 2);
 
@@ -15,6 +24,9 @@ export function renderFactoryTemplate(params: { abi: Abi }) {
     throw new Error(`ABI doesn't have a 'main()' method.`);
   }
 
+  const enums = assembleEnums({ types });
+  const structs = assembleStructs({ types });
+
   const { prefixedInputs: inputs, output } = func.attributes;
 
   const text = renderHbsTemplate({
@@ -22,7 +34,10 @@ export function renderFactoryTemplate(params: { abi: Abi }) {
     data: {
       inputs,
       output,
+      structs,
+      enums,
       abiJsonString,
+      hexlifiedBinString,
       capitalizedName,
     },
   });
