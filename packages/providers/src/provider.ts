dc235c3a15dd20683700969d46de02661334e6aa
Cameron Manavian <cameron.manavian@fuel.sh>
2023-02-01 22:58:21 -0800

    restore

--- packages/providers/src/provider.ts
+++ packages/providers/src/provider.ts
@@ -190,6 +190,10 @@ export type BuildPredicateOptions = {
   fundTransaction?: boolean;
 } & Pick<TransactionRequestLike, 'gasLimit' | 'gasPrice' | 'maturity'>;
 
+export type BuildScriptOptions = {
+  fundTransaction?: boolean;
+} & Pick<TransactionRequestLike, 'gasLimit' | 'gasPrice' | 'maturity'>;
+
 /**
  * Provider Call transaction params
  */
