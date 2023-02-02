d5cb1b7ec16246f902e89de9ea626dca1e006006
Anderson Arboleya <anderson@arboleya.me>
2023-02-02 14:24:05 -0300

    Extracting code chunks into separate utilities

---
+++ packages/abi-typegen/src/templates/utils/assembleStructs.ts
@@ -0,0 +1,26 @@
+import type { StructType } from '../../abi/types/StructType';
+import { TargetEnum } from '../../types/enums/TargetEnum';
+import type { IType } from '../../types/interfaces/IType';
+
+export function assembleStructs(params: { types: IType[] }) {
+  const { types } = params;
+
+  const structs = types
+    .filter((t) => t.name === 'struct')
+    .map((t) => {
+      const st = t as StructType; // only structs here
+      const structName = st.getStructName();
+      const inputValues = st.getStructContents({ types, target: TargetEnum.INPUT });
+      const outputValues = st.getStructContents({ types, target: TargetEnum.OUTPUT });
+      const typeAnnotations = st.getStructDeclaration({ types });
+      return {
+        structName,
+        typeAnnotations,
+        inputValues,
+        outputValues,
+        recycleRef: inputValues === outputValues, // reduces duplication
+      };
+    });
+
+  return structs;
+}
