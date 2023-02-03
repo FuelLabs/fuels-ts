4894ee97e9ce6ce43285d18d8a8aa7d47434333c
Anderson Arboleya <anderson@arboleya.me>
2023-02-03 06:50:12 -0300

    Implementing tests for new template utilities

--- packages/abi-typegen/src/templates/utils/assembleEnums.test.ts
+++ packages/abi-typegen/src/templates/utils/assembleEnums.test.ts
@@ -1,7 +1,33 @@
+import enumOfEnumsAbiJson from '../../../test/fixtures/out/abis/enum-of-enums-abi.json';
+import { Abi } from '../../abi/Abi';
+
 import { assembleEnums } from './assembleEnums';
 
 describe('assembleEnums.ts', () => {
-  test('should assemble enums just fine', () => {
-    expect(assembleEnums).toBeTruthy();
+  test('should assemble enums just fine', async () => {
+    const abi = new Abi({
+      filepath: './enum-simple-abi.json',
+      outputDir: './contracts',
+      rawContents: enumOfEnumsAbiJson,
+    });
+
+    // executing
+    const enums = assembleEnums({ types: abi.types });
+
+    // validating
+    expect(enums).toStrictEqual([
+      {
+        structName: 'LetterEnum',
+        inputValues: 'a: [], b: [], c: []',
+        outputValues: 'a: [], b: [], c: []',
+        recycleRef: true,
+      },
+      {
+        structName: 'MyEnum',
+        inputValues: 'letter: LetterEnumInput',
+        outputValues: 'letter: LetterEnumOutput',
+        recycleRef: false,
+      },
+    ]);
   });
 });
--- packages/abi-typegen/src/templates/utils/assembleStructs.test.ts
+++ packages/abi-typegen/src/templates/utils/assembleStructs.test.ts
@@ -1,7 +1,42 @@
+import structSimpleAbiJson from '../../../test/fixtures/out/abis/struct-simple-abi.json';
+import { Abi } from '../../abi/Abi';
+
 import { assembleStructs } from './assembleStructs';
 
 describe('assembleStructs.ts', () => {
-  test('should assemble structs just fine', () => {
-    expect(assembleStructs).toBeTruthy();
+  test('should assemble structs just fine', async () => {
+    const abi = new Abi({
+      filepath: './struct-simple-abi.json',
+      outputDir: './contracts',
+      rawContents: structSimpleAbiJson,
+    });
+
+    // executing
+    const structs = assembleStructs({ types: abi.types });
+
+    // validating
+    expect(structs).toStrictEqual([
+      {
+        structName: 'StructA',
+        typeAnnotations: '<T, U>',
+        inputValues: 'propA1: T, propA2: U',
+        outputValues: 'propA1: T, propA2: U',
+        recycleRef: true,
+      },
+      {
+        structName: 'StructB',
+        typeAnnotations: '<T>',
+        inputValues: 'propB1: T',
+        outputValues: 'propB1: T',
+        recycleRef: true,
+      },
+      {
+        structName: 'StructC',
+        typeAnnotations: '',
+        inputValues: 'propC1: StructAInput<StructBInput<BigNumberish>, BigNumberish>',
+        outputValues: 'propC1: StructAOutput<StructBOutput<number>, number>',
+        recycleRef: false,
+      },
+    ]);
   });
 });
