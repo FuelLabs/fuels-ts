c56478702ee2400448bb7d9d6777547468a14641
Dhaiwat / dhai.eth <dhaiwatpandya@gmail.com>
2023-02-10 12:06:25 +0530

    remove more code

--- packages/abi-typegen/src/interfaces/IRawAbiFunction.ts
+++ packages/abi-typegen/src/interfaces/IRawAbiFunction.ts
@@ -2,16 +2,10 @@ export interface IRawAbiFunction {
   name: string;
   inputs: IRawAbiFunctionIO[];
   output: IRawAbiFunctionIO;
-  attributes?: IRawAbiFunctionAttribute[];
 }
 
 export interface IRawAbiFunctionIO {
   name: string;
   type: number;
   typeArguments: null | IRawAbiFunctionIO[];
-}
-
-export interface IRawAbiFunctionAttribute {
-  name: string;
-  arguments: string[];
 }
