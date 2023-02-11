39d2d0e7e1658ed8093a43dda0679793d3e35880
Cameron Manavian <cameron.manavian@fuel.sh>
2023-02-10 21:45:32 -0800

    simplify

--- packages/script/src/script-factory.ts
+++ packages/script/src/script-factory.ts
@@ -1,25 +1,12 @@
 import type { BytesLike } from '@ethersproject/bytes';
-import { Logger } from '@ethersproject/logger';
 import { Interface } from '@fuel-ts/abi-coder';
 import type { JsonAbi, InputValue } from '@fuel-ts/abi-coder';
 import type { BN } from '@fuel-ts/math';
-import type {
-  BuildScriptOptions,
-  CoinQuantityLike,
-  Provider,
-  TransactionResponse,
-  TransactionResult,
-} from '@fuel-ts/providers';
-import { getDecodedLogs, ScriptTransactionRequest } from '@fuel-ts/providers';
-import { MAX_GAS_PER_TX, ReceiptType } from '@fuel-ts/transactions';
-import { versions } from '@fuel-ts/versions';
+import type { Provider } from '@fuel-ts/providers';
 import type { BaseWalletLocked } from '@fuel-ts/wallet';
 
 import { Script } from './script';
-
-const logger = new Logger(versions.FUELS);
-
-const FUNCTION_FRAGMENT_NAME = 'main';
+import type { ScriptRequest } from './script-request';
 
 type Result<T> = {
   value: T | BN | undefined;
@@ -28,7 +15,7 @@ type Result<T> = {
 
 export class ScriptFactory<TInput, TOutput> {
   bytecode: BytesLike;
-  script!: Script<InputValue<void>[], Result<TOutput>>;
+  script!: ScriptRequest<InputValue<void>[], Result<TOutput>>;
   provider: Provider;
   interface: Interface;
   wallet: BaseWalletLocked | null;
@@ -44,102 +31,9 @@ export class ScriptFactory<TInput, TOutput> {
       this.provider = walletOrProvider;
       this.wallet = null;
     }
-
-    this.buildScript();
   }
 
-  private buildScript() {
-    this.script = new Script(
-      this.bytecode,
-      (args: InputValue<void>[]) =>
-        this.interface.encodeFunctionData(FUNCTION_FRAGMENT_NAME, args, 0, true),
-      (scriptResult): Result<TOutput> => {
-        const logs = getDecodedLogs(scriptResult.receipts, this.interface);
-
-        if (scriptResult.returnReceipt.type === ReceiptType.Revert) {
-          logger.throwError('Script Reverted', Logger.errors.CALL_EXCEPTION, logs);
-        }
-
-        if (
-          scriptResult.returnReceipt.type !== ReceiptType.Return &&
-          scriptResult.returnReceipt.type !== ReceiptType.ReturnData
-        ) {
-          logger.throwError(
-            `Script Return Type [${scriptResult.returnReceipt.type}] Invalid`,
-            Logger.errors.CALL_EXCEPTION,
-            {
-              logs,
-              receipt: scriptResult.returnReceipt,
-            }
-          );
-        }
-
-        let value;
-        if (scriptResult.returnReceipt.type === ReceiptType.Return) {
-          value = scriptResult.returnReceipt.val;
-        }
-        if (scriptResult.returnReceipt.type === ReceiptType.ReturnData) {
-          const decoded = this.interface.decodeFunctionResult(
-            FUNCTION_FRAGMENT_NAME,
-            scriptResult.returnReceipt.data
-          );
-          value = (decoded as [TOutput])[0];
-        }
-
-        return {
-          value,
-          logs,
-        };
-      }
-    );
-  }
-
-  async buildScriptTransaction(
-    data: InputValue<TInput>[],
-    scriptOptions?: BuildScriptOptions
-  ): Promise<ScriptTransactionRequest> {
-    const options = {
-      fundTransaction: true,
-      ...scriptOptions,
-    };
-    const request = new ScriptTransactionRequest({
-      gasLimit: MAX_GAS_PER_TX,
-      ...options,
-    });
-    request.setScript(this.script, data as InputValue[]);
-
-    const requiredCoinQuantities: CoinQuantityLike[] = [];
-    if (options.fundTransaction) {
-      requiredCoinQuantities.push(request.calculateFee());
-    }
-
-    if (requiredCoinQuantities.length && this.wallet) {
-      const resources = await this.wallet.getResourcesToSpend(requiredCoinQuantities);
-      request.addResources(resources);
-    }
-
-    return request;
-  }
-
-  async call(
-    data: InputValue<TInput>[],
-    options?: BuildScriptOptions
-  ): Promise<
-    {
-      transactionResult: TransactionResult<'success' | 'failure'>;
-      response: TransactionResponse;
-    } & Result<TOutput>
-  > {
-    const request = await this.buildScriptTransaction(data, options);
-    const response = await this.provider.sendTransaction(request);
-    const transactionResult = await response.waitForResult();
-    const result = this.script.decodeCallResult(transactionResult);
-
-    return {
-      transactionResult,
-      response,
-      value: result.value,
-      logs: result.logs,
-    };
+  prepareScript(): Script<TInput, TOutput> {
+    return new Script<TInput, TOutput>(this.bytecode, this.interface, this.provider, this.wallet);
   }
 }
