1fb6c3544f316b4f9e662056b76c36b3bce514b6
Cameron Manavian <cameron.manavian@fuel.sh>
2023-02-20 23:36:36 -0800

    fix wires

--- packages/program/src/index.ts
+++ packages/program/src/index.ts
@@ -1,4 +1,5 @@
 export * from './types';
+export * from './utils';
 export { FunctionInvocationScope } from './functions/invocation-scope';
 export { MultiCallInvocationScope } from './functions/multicall-scope';
 export { InvocationResult, FunctionInvocationResult } from './functions/invocation-results';
