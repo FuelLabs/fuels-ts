Pifagors12/fuel

feat: remove Address object and use bech32

packages/providers/src/provider.ts
@@ -3,7 +3,7 @@ import type { BytesLike } from '@ethersproject/bytes';
 import { arrayify, hexlify } from '@ethersproject/bytes';
 import type { Network } from '@ethersproject/networks';
 import { Bech32 } from '@fuel-ts/address';
-import type { Bech32Address, ContractIdLike } from '@fuel-ts/interfaces';
+import type { AccountAddress, ContractAddress } from '@fuel-ts/interfaces';
 import type { BN } from '@fuel-ts/math';
 import { max, bn } from '@fuel-ts/math';
 import type { Transaction } from '@fuel-ts/transactions';
@@ -441,7 +441,7 @@ export default class Provider {
    */
   async getCoins(
     /** The address to get coins for */
-    owner: Bech32Address,
+    owner: AccountAddress,
     /** The asset ID of coins to get */
     assetId?: BytesLike,
     /** Pagination arguments */
@@ -450,7 +450,7 @@ export default class Provider {
     const result = await this.operations.getCoins({
       first: 10,
       ...paginationArgs,
-      filter: { owner: Bech32.toB256FromString(owner), assetId: assetId && hexlify(assetId) },
+      filter: { owner: Bech32.toB256(owner), assetId: assetId && hexlify(assetId) },
     });
 
     const coins = result.coins.edges!.map((edge) => edge!.node!);
@@ -459,7 +459,7 @@ export default class Provider {
       id: coin.utxoId,
       assetId: coin.assetId,
       amount: bn(coin.amount),
-      owner: Bech32.fromString(coin.owner),
+      owner: Bech32.fromB256(coin.owner),
       status: coin.coinStatus,
       maturity: bn(coin.maturity).toNumber(),
       blockCreated: bn(coin.blockCreated),
@@ -471,7 +471,7 @@ export default class Provider {
    */
   async getResourcesToSpend(
     /** The address to get coins for */
-    owner: Bech32Address,
+    owner: AccountAddress,
     /** The quantities to get */
     quantities: CoinQuantityLike[],
     /** IDs of excluded resources from the selection. */
@@ -482,7 +482,7 @@ export default class Provider {
       utxos: excludedIds?.utxos?.map((id) => hexlify(id)) || [],
     };
     const result = await this.operations.getResourcesToSpend({
-      owner: Bech32.toB256FromString(owner),
+      owner: Bech32.toB256(owner),
       queryPerAsset: quantities
         .map(coinQuantityfy)
         .map(({ assetId, amount, max: maxPerAsset }) => ({
@@ -500,15 +500,15 @@ export default class Provider {
           amount: bn(resource.amount),
           status: resource.coinStatus,
           assetId: resource.assetId,
-          owner: Bech32.fromString(resource.owner),
+          owner: Bech32.fromB256(resource.owner),
           maturity: bn(resource.maturity).toNumber(),
           blockCreated: bn(resource.blockCreated),
         };
       }
 
       return {
-        sender: Bech32.fromString(resource.sender),
-        recipient: Bech32.fromString(resource.recipient),
+        sender: Bech32.fromB256(resource.sender),
+        recipient: Bech32.fromB256(resource.recipient),
         nonce: bn(resource.nonce),
         amount: bn(resource.amount),
         data: InputMessageCoder.decodeData(resource.data),
@@ -615,12 +615,12 @@ export default class Provider {
    */
   async getContractBalance(
     /** The contract ID to get the balance for */
-    contractId: ContractIdLike,
+    contractId: ContractAddress,
     /** The asset ID of coins to get */
     assetId: BytesLike
   ): Promise<BN> {
     const { contractBalance } = await this.operations.getContractBalance({
-      contract: Bech32.toB256FromString(contractId),
+      contract: contractId,
       asset: hexlify(assetId),
     });
     return bn(contractBalance.amount, 10);
@@ -631,12 +631,12 @@ export default class Provider {
    */
   async getBalance(
     /** The address to get coins for */
-    owner: Bech32Address,
+    owner: AccountAddress,
     /** The asset ID of coins to get */
     assetId: BytesLike
   ): Promise<BN> {
     const { balance } = await this.operations.getBalance({
-      owner: Bech32.toB256FromString(owner),
+      owner: Bech32.toB256(owner),
       assetId: hexlify(assetId),
     });
     return bn(balance.amount, 10);
@@ -647,14 +647,14 @@ export default class Provider {
    */
   async getBalances(
     /** The address to get coins for */
-    owner: Bech32Address,
+    owner: AccountAddress,
     /** Pagination arguments */
     paginationArgs?: CursorPaginationArgs
   ): Promise<CoinQuantity[]> {
     const result = await this.operations.getBalances({
       first: 10,
       ...paginationArgs,
-      filter: { owner: Bech32.toB256FromString(owner) },
+      filter: { owner: Bech32.toB256(owner) },
     });
 
     const balances = result.balances.edges!.map((edge) => edge!.node!);
@@ -670,21 +670,21 @@ export default class Provider {
    */
   async getMessages(
     /** The address to get message from */
-    address: Bech32Address,
+    address: AccountAddress,
     /** Pagination arguments */
     paginationArgs?: CursorPaginationArgs
   ): Promise<Message[]> {
     const result = await this.operations.getMessages({
       first: 10,
       ...paginationArgs,
-      owner: Bech32.toB256FromString(address),
+      owner: Bech32.toB256(address),
     });
 
     const messages = result.messages.edges!.map((edge) => edge!.node!);
 
     return messages.map((message) => ({
-      sender: Bech32.fromString(message.sender),
-      recipient: Bech32.fromString(message.recipient),
+      sender: Bech32.fromB256(message.sender),
+      recipient: Bech32.fromB256(message.recipient),
       nonce: bn(message.nonce),
       amount: bn(message.amount),
       data: InputMessageCoder.decodeData(message.data),
@@ -714,8 +714,8 @@ export default class Provider {
     return {
       proofSet: result.messageProof.proofSet,
       proofIndex: bn(result.messageProof.proofIndex),
-      sender: Bech32.fromString(result.messageProof.sender),
-      recipient: Bech32.fromString(result.messageProof.recipient),
+      sender: Bech32.fromB256(result.messageProof.sender),
+      recipient: Bech32.fromB256(result.messageProof.recipient),
       nonce: result.messageProof.nonce,
       amount: bn(result.messageProof.amount),
       data: result.messageProof.data,

packages/providers/src/transaction-request/transaction-request.ts
@@ -3,13 +3,7 @@ import type { BytesLike } from '@ethersproject/bytes';
 import { arrayify, hexlify } from '@ethersproject/bytes';
 import { Bech32 } from '@fuel-ts/address';
 import { NativeAssetId, ZeroBytes32 } from '@fuel-ts/constants';
-import type {
-  AbstractAddress,
-  ContractIdLike,
-  AbstractScript,
-  Bech32Address,
-  AddressLike,
-} from '@fuel-ts/interfaces';
+import type { ContractAddress, AbstractScript, AccountAddress } from '@fuel-ts/interfaces';
 import type { BigNumberish, BN } from '@fuel-ts/math';
 import { bn } from '@fuel-ts/math';
 import type { TransactionCreate, TransactionScript } from '@fuel-ts/transactions';
@@ -107,7 +101,7 @@ export class NoWitnessAtIndexError extends Error {
 
 export class NoWitnessByOwnerError extends Error {
   name = 'NoWitnessByOwnerError';
-  constructor(public readonly owner: AbstractAddress) {
+  constructor(public readonly owner: AccountAddress) {
     super();
     this.message = `A witness for the given owner "${owner}" was not found`;
   }
@@ -195,7 +189,7 @@ abstract class BaseTransactionRequest implements BaseTransactionRequestLike {
     return this.witnesses.length - 1;
   }
 
-  updateWitnessByOwner(address: AbstractAddress, signature: BytesLike) {
+  updateWitnessByOwner(address: AccountAddress, signature: BytesLike) {
     const witnessIndex = this.getCoinInputWitnessIndexByOwner(address);
     if (typeof witnessIndex === 'number') {
       this.updateWitness(witnessIndex, signature);
@@ -233,7 +227,7 @@ abstract class BaseTransactionRequest implements BaseTransactionRequestLike {
   /**
    * Returns the witnessIndex of the found CoinInput
    */
-  getCoinInputWitnessIndexByOwner(owner: AbstractAddress): number | null {
+  getCoinInputWitnessIndexByOwner(owner: AccountAddress): number | null {
     return (
       this.inputs.find(
         (input): input is CoinTransactionRequestInput =>
@@ -250,7 +244,7 @@ abstract class BaseTransactionRequest implements BaseTransactionRequestLike {
   /**
    * Updates the witness for the given CoinInput owner
    */
-  updateWitnessByCoinInputOwner(owner: Bech32Address, witness: BytesLike) {
+  updateWitnessByCoinInputOwner(owner: AccountAddress, witness: BytesLike) {
     const witnessIndex = this.getCoinInputWitnessIndexByOwner(owner);
 
     if (!witnessIndex) {
@@ -280,15 +274,15 @@ abstract class BaseTransactionRequest implements BaseTransactionRequestLike {
         ? ({
             type,
             ...resource,
-            owner: Bech32.toB256FromString(resource.owner),
+            owner: Bech32.toB256(resource.owner),
             witnessIndex,
             txPointer: '0x00000000000000000000000000000000',
           } as CoinTransactionRequestInput)
         : ({
             type,
             ...resource,
-            sender: Bech32.toB256FromString(resource.sender),
-            recipient: Bech32.toB256FromString(resource.recipient),
+            sender: Bech32.toB256(resource.sender),
+            recipient: Bech32.toB256(resource.recipient),
             witnessIndex,
             txPointer: '0x00000000000000000000000000000000',
           } as MessageTransactionRequestInput)
@@ -320,7 +314,7 @@ abstract class BaseTransactionRequest implements BaseTransactionRequestLike {
 
   addCoinOutput(
     /** Address of the destination */
-    to: AbstractAddress,
+    to: AccountAddress,
     /** Amount of coins */
     amount: BigNumberish,
     /** Asset ID of coins */
@@ -328,7 +322,7 @@ abstract class BaseTransactionRequest implements BaseTransactionRequestLike {
   ) {
     this.pushOutput({
       type: OutputType.Coin,
-      to: Bech32.toB256FromString(to),
+      to: Bech32.toB256(to),
       amount,
       assetId,
     });
@@ -336,14 +330,14 @@ abstract class BaseTransactionRequest implements BaseTransactionRequestLike {
 
   addCoinOutputs(
     /** Address of the destination */
-    to: AbstractAddress,
+    to: AccountAddress,
     /** Quantities of coins */
     quantities: CoinQuantityLike[]
   ) {
     quantities.map(coinQuantityfy).forEach((quantity) => {
       this.pushOutput({
         type: OutputType.Coin,
-        to: Bech32.toB256FromString(to),
+        to: Bech32.toB256(to),
         amount: quantity.amount,
         assetId: quantity.assetId,
       });
@@ -478,7 +472,7 @@ export class ScriptTransactionRequest extends BaseTransactionRequest {
     return this.outputs.length - 1;
   }
 
-  addContract(contract: ContractIdLike) {
+  addContract(contract: ContractAddress) {
     const contractAddress = Bech32.toB256FromString(contract);
 
     // Add only one input contract per contractId

FuelLabs/fuels-ts

packages/providers/src/provider.ts
@@ -2,8 +2,8 @@
 import type { BytesLike } from '@ethersproject/bytes';
 import { arrayify, hexlify } from '@ethersproject/bytes';
 import type { Network } from '@ethersproject/networks';
-import { Address } from '@fuel-ts/address';
-import type { AbstractAddress } from '@fuel-ts/interfaces';
+import { Bech32 } from '@fuel-ts/address';
+import type { ContractAddress, AccountAddress } from '@fuel-ts/interfaces';
 import type { BN } from '@fuel-ts/math';
 import { max, bn } from '@fuel-ts/math';
 import type { Transaction } from '@fuel-ts/transactions';
@@ -369,7 +369,7 @@ export default class Provider {
       transactionRequest.addVariableOutputs(missingOutputVariableCount);
 
       missingOutputContractIds.forEach(({ contractId }) =>
-        transactionRequest.addContract(Address.fromString(contractId))
+        transactionRequest.addContract(contractId)
       );
       tries += 1;
     } while (tries < MAX_RETRIES);
@@ -441,7 +441,7 @@ export default class Provider {
    */
   async getCoins(
     /** The address to get coins for */
-    owner: AbstractAddress,
+    owner: AccountAddress,
     /** The asset ID of coins to get */
     assetId?: BytesLike,
     /** Pagination arguments */
@@ -450,7 +450,7 @@ export default class Provider {
     const result = await this.operations.getCoins({
       first: 10,
       ...paginationArgs,
-      filter: { owner: owner.toB256(), assetId: assetId && hexlify(assetId) },
+      filter: { owner: Bech32.toB256(owner), assetId: assetId && hexlify(assetId) },
     });
 
     const coins = result.coins.edges!.map((edge) => edge!.node!);
@@ -459,7 +459,7 @@ export default class Provider {
       id: coin.utxoId,
       assetId: coin.assetId,
       amount: bn(coin.amount),
-      owner: Address.fromAddressOrString(coin.owner),
+      owner: Bech32.fromB256(coin.owner),
       status: coin.coinStatus,
       maturity: bn(coin.maturity).toNumber(),
       blockCreated: bn(coin.blockCreated),
@@ -471,7 +471,7 @@ export default class Provider {
    */
   async getResourcesToSpend(
     /** The address to get coins for */
-    owner: AbstractAddress,
+    owner: AccountAddress,
     /** The quantities to get */
     quantities: CoinQuantityLike[],
     /** IDs of excluded resources from the selection. */
@@ -482,7 +482,7 @@ export default class Provider {
       utxos: excludedIds?.utxos?.map((id) => hexlify(id)) || [],
     };
     const result = await this.operations.getResourcesToSpend({
-      owner: owner.toB256(),
+      owner: Bech32.toB256(owner),
       queryPerAsset: quantities
         .map(coinQuantityfy)
         .map(({ assetId, amount, max: maxPerAsset }) => ({
@@ -500,15 +500,15 @@ export default class Provider {
           amount: bn(resource.amount),
           status: resource.coinStatus,
           assetId: resource.assetId,
-          owner: Address.fromAddressOrString(resource.owner),
+          owner: Bech32.fromB256(resource.owner),
           maturity: bn(resource.maturity).toNumber(),
           blockCreated: bn(resource.blockCreated),
         };
       }
 
       return {
-        sender: Address.fromAddressOrString(resource.sender),
-        recipient: Address.fromAddressOrString(resource.recipient),
+        sender: Bech32.fromB256(resource.sender),
+        recipient: Bech32.fromB256(resource.recipient),
         nonce: bn(resource.nonce),
         amount: bn(resource.amount),
         data: InputMessageCoder.decodeData(resource.data),
@@ -615,12 +615,12 @@ export default class Provider {
    */
   async getContractBalance(
     /** The contract ID to get the balance for */
-    contractId: AbstractAddress,
+    contractId: ContractAddress,
     /** The asset ID of coins to get */
     assetId: BytesLike
   ): Promise<BN> {
     const { contractBalance } = await this.operations.getContractBalance({
-      contract: contractId.toB256(),
+      contract: contractId,
       asset: hexlify(assetId),
     });
     return bn(contractBalance.amount, 10);
@@ -631,12 +631,12 @@ export default class Provider {
    */
   async getBalance(
     /** The address to get coins for */
-    owner: AbstractAddress,
+    owner: AccountAddress,
     /** The asset ID of coins to get */
     assetId: BytesLike
   ): Promise<BN> {
     const { balance } = await this.operations.getBalance({
-      owner: owner.toB256(),
+      owner: Bech32.toB256(owner),
       assetId: hexlify(assetId),
     });
     return bn(balance.amount, 10);
@@ -647,14 +647,14 @@ export default class Provider {
    */
   async getBalances(
     /** The address to get coins for */
-    owner: AbstractAddress,
+    owner: AccountAddress,
     /** Pagination arguments */
     paginationArgs?: CursorPaginationArgs
   ): Promise<CoinQuantity[]> {
     const result = await this.operations.getBalances({
       first: 10,
       ...paginationArgs,
-      filter: { owner: owner.toB256() },
+      filter: { owner: Bech32.toB256(owner) },
     });
 
     const balances = result.balances.edges!.map((edge) => edge!.node!);
@@ -670,21 +670,21 @@ export default class Provider {
    */
   async getMessages(
     /** The address to get message from */
-    address: AbstractAddress,
+    address: AccountAddress,
     /** Pagination arguments */
     paginationArgs?: CursorPaginationArgs
   ): Promise<Message[]> {
     const result = await this.operations.getMessages({
       first: 10,
       ...paginationArgs,
-      owner: address.toB256(),
+      owner: Bech32.toB256(address),
     });
 
     const messages = result.messages.edges!.map((edge) => edge!.node!);
 
     return messages.map((message) => ({
-      sender: Address.fromAddressOrString(message.sender),
-      recipient: Address.fromAddressOrString(message.recipient),
+      sender: Bech32.fromB256(message.sender),
+      recipient: Bech32.fromB256(message.recipient),
       nonce: bn(message.nonce),
       amount: bn(message.amount),
       data: InputMessageCoder.decodeData(message.data),
@@ -714,8 +714,8 @@ export default class Provider {
     return {
       proofSet: result.messageProof.proofSet,
       proofIndex: bn(result.messageProof.proofIndex),
-      sender: Address.fromAddressOrString(result.messageProof.sender),
-      recipient: Address.fromAddressOrString(result.messageProof.recipient),
+      sender: Bech32.fromB256(result.messageProof.sender),
+      recipient: Bech32.fromB256(result.messageProof.recipient),
       nonce: result.messageProof.nonce,
       amount: bn(result.messageProof.amount),
       data: result.messageProof.data,

packages/providers/src/transaction-request/transaction-request.ts
@@ -1,14 +1,9 @@
 /* eslint-disable max-classes-per-file */
 import type { BytesLike } from '@ethersproject/bytes';
 import { arrayify, hexlify } from '@ethersproject/bytes';
-import { addressify } from '@fuel-ts/address';
+import { Bech32 } from '@fuel-ts/address';
 import { NativeAssetId, ZeroBytes32 } from '@fuel-ts/constants';
-import type {
-  AddressLike,
-  AbstractAddress,
-  ContractIdLike,
-  AbstractScript,
-} from '@fuel-ts/interfaces';
+import type { AbstractScript, ContractAddress, AccountAddress } from '@fuel-ts/interfaces';
 import type { BigNumberish, BN } from '@fuel-ts/math';
 import { bn } from '@fuel-ts/math';
 import type { TransactionCreate, TransactionScript } from '@fuel-ts/transactions';
@@ -22,7 +17,6 @@ import {
 
 import type { CoinQuantity, CoinQuantityLike } from '../coin-quantity';
 import { coinQuantityfy } from '../coin-quantity';
-import type { Message } from '../message';
 import type { Resource } from '../resource';
 import { isCoin } from '../resource';
 import { calculatePriceWithFactor, normalizeJSON } from '../utils';
@@ -107,7 +101,7 @@ export class NoWitnessAtIndexError extends Error {
 
 export class NoWitnessByOwnerError extends Error {
   name = 'NoWitnessByOwnerError';
-  constructor(public readonly owner: AbstractAddress) {
+  constructor(public readonly owner: AccountAddress) {
     super();
     this.message = `A witness for the given owner "${owner}" was not found`;
   }
@@ -195,7 +189,7 @@ abstract class BaseTransactionRequest implements BaseTransactionRequestLike {
     return this.witnesses.length - 1;
   }
 
-  updateWitnessByOwner(address: AbstractAddress, signature: BytesLike) {
+  updateWitnessByOwner(address: AccountAddress, signature: BytesLike) {
     const witnessIndex = this.getCoinInputWitnessIndexByOwner(address);
     if (typeof witnessIndex === 'number') {
       this.updateWitness(witnessIndex, signature);
@@ -233,16 +227,15 @@ abstract class BaseTransactionRequest implements BaseTransactionRequestLike {
   /**
    * Returns the witnessIndex of the found CoinInput
    */
-  getCoinInputWitnessIndexByOwner(owner: AddressLike): number | null {
-    const ownerAddress = addressify(owner);
+  getCoinInputWitnessIndexByOwner(owner: AccountAddress): number | null {
     return (
       this.inputs.find(
         (input): input is CoinTransactionRequestInput =>
-          input.type === InputType.Coin && hexlify(input.owner) === ownerAddress.toB256()
+          input.type === InputType.Coin && hexlify(input.owner) === Bech32.toB256(owner)
       )?.witnessIndex ??
       this.inputs.find(
         (input): input is MessageTransactionRequestInput =>
-          input.type === InputType.Message && hexlify(input.recipient) === ownerAddress.toB256()
+          input.type === InputType.Message && hexlify(input.recipient) === Bech32.toB256(owner)
       )?.witnessIndex ??
       null
     );
@@ -251,11 +244,11 @@ abstract class BaseTransactionRequest implements BaseTransactionRequestLike {
   /**
    * Updates the witness for the given CoinInput owner
    */
-  updateWitnessByCoinInputOwner(owner: AddressLike, witness: BytesLike) {
+  updateWitnessByCoinInputOwner(owner: AccountAddress, witness: BytesLike) {
     const witnessIndex = this.getCoinInputWitnessIndexByOwner(owner);
 
     if (!witnessIndex) {
-      throw new NoWitnessByOwnerError(addressify(owner));
+      throw new NoWitnessByOwnerError(owner);
     }
 
     this.updateWitness(witnessIndex, witness);
@@ -281,15 +274,15 @@ abstract class BaseTransactionRequest implements BaseTransactionRequestLike {
         ? ({
             type,
             ...resource,
-            owner: resource.owner.toB256(),
+            owner: Bech32.toB256(resource.owner),
             witnessIndex,
             txPointer: '0x00000000000000000000000000000000',
           } as CoinTransactionRequestInput)
         : ({
             type,
             ...resource,
-            sender: resource.sender.toB256(),
-            recipient: resource.recipient.toB256(),
+            sender: Bech32.toB256(resource.sender),
+            recipient: Bech32.toB256(resource.recipient),
             witnessIndex,
             txPointer: '0x00000000000000000000000000000000',
           } as MessageTransactionRequestInput)
@@ -301,7 +294,7 @@ abstract class BaseTransactionRequest implements BaseTransactionRequestLike {
     );
 
     // Throw if the existing ChangeOutput is not for the same owner
-    if (changeOutput && hexlify(changeOutput.to) !== ownerAddress.toB256()) {
+    if (changeOutput && hexlify(changeOutput.to) !== Bech32.toB256(ownerAddress)) {
       throw new ChangeOutputCollisionError();
     }
 
@@ -309,7 +302,7 @@ abstract class BaseTransactionRequest implements BaseTransactionRequestLike {
     if (!changeOutput) {
       this.pushOutput({
         type: OutputType.Change,
-        to: ownerAddress.toB256(),
+        to: Bech32.toB256(ownerAddress),
         assetId,
       });
     }
@@ -321,7 +314,7 @@ abstract class BaseTransactionRequest implements BaseTransactionRequestLike {
 
   addCoinOutput(
     /** Address of the destination */
-    to: AddressLike,
+    to: AccountAddress,
     /** Amount of coins */
     amount: BigNumberish,
     /** Asset ID of coins */
@@ -329,7 +322,7 @@ abstract class BaseTransactionRequest implements BaseTransactionRequestLike {
   ) {
     this.pushOutput({
       type: OutputType.Coin,
-      to: addressify(to).toB256(),
+      to: Bech32.toB256(to),
       amount,
       assetId,
     });
@@ -337,14 +330,14 @@ abstract class BaseTransactionRequest implements BaseTransactionRequestLike {
 
   addCoinOutputs(
     /** Address of the destination */
-    to: AddressLike,
+    to: AccountAddress,
     /** Quantities of coins */
     quantities: CoinQuantityLike[]
   ) {
     quantities.map(coinQuantityfy).forEach((quantity) => {
       this.pushOutput({
         type: OutputType.Coin,
-        to: addressify(to).toB256(),
+        to: Bech32.toB256(to),
         amount: quantity.amount,
         assetId: quantity.assetId,
       });
@@ -479,17 +472,15 @@ export class ScriptTransactionRequest extends BaseTransactionRequest {
     return this.outputs.length - 1;
   }
 
-  addContract(contract: ContractIdLike) {
-    const contractAddress = addressify(contract);
-
+  addContract(contractId: ContractAddress) {
     // Add only one input contract per contractId
-    if (this.getContractInputs().find((i) => i.contractId === contractAddress.toB256())) {
+    if (this.getContractInputs().find((i) => i.contractId === contractId)) {
       return;
     }
 
     const inputIndex = super.pushInput({
       type: InputType.Contract,
-      contractId: contractAddress.toB256(),
+      contractId,
       txPointer: '0x00000000000000000000000000000000',
     });
 