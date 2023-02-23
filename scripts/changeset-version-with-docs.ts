1abfd3944d07a16811adbe0c3054eb61d1aeaba3
Torres-ssf <sergio.uft@gmail.com>
2023-02-23 15:04:42 -0300

    stop script exec on error

--- scripts/changeset-version-with-docs.ts
+++ scripts/changeset-version-with-docs.ts
@@ -3,6 +3,9 @@
 import sh from 'shelljs';
 
 (async () => {
+  // Force exit on error
+  sh.exec(`set -e`);
+
   // Update doc version references
   sh.exec(
     `echo "# generated-file\nfuels: $BUILD_VERSION\nfuel-core: $FUEL_CORE_VERSION\nsway: $FORC_VERSION\nforc: $FORC_VERSION" > docs/_data/versions.yml`
