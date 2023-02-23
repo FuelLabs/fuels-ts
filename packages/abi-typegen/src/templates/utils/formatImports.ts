b4e34478f8c9cca002cafec272120697a5a9ad3c
Anderson Arboleya <anderson@arboleya.me>
2023-02-23 17:24:21 -0300

    Improving array sorting by running it in a case-insensitive manner

--- packages/abi-typegen/src/templates/utils/formatImports.ts
+++ packages/abi-typegen/src/templates/utils/formatImports.ts
@@ -2,12 +2,15 @@ import uniq from 'lodash.uniq';
 
 import type { IType } from '../../types/interfaces/IType';
 
+const caseInsensitiveSort = (a: string, b: string) =>
+  a.toLowerCase().localeCompare(b.toLowerCase());
+
 export function formatImports(params: { types: IType[]; baseMembers?: string[] }) {
   const { types, baseMembers = [] } = params;
 
   const fuelTypes = types.filter((t) => t.requireImportFromFuels);
   const members = fuelTypes.map((t) => t.attributes.inputLabel);
-  const imports = uniq(baseMembers.concat(members).sort());
+  const imports = uniq(baseMembers.concat(members).sort(caseInsensitiveSort));
 
   return {
     imports: imports.length ? imports : undefined,
