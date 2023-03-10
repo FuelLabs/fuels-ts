52bd3cdc552873bea0429bcf55bee5dbee70c805
Cameron Manavian <cameron.manavian@fuel.sh>
2023-03-10 11:12:19 -0800

    use current code

--- packages/abi-coder/src/abi-coder.ts
+++ packages/abi-coder/src/abi-coder.ts
@@ -195,7 +195,7 @@ export default class AbiCoder {
 
     const coders = nonEmptyTypes.map((type) => this.getCoder(type));
     if (nonEmptyTypes[0] && nonEmptyTypes[0].type === 'raw untyped slice') {
-      (coders[0] as ArrayCoder<any>).length = bytes.length / 8;
+      (coders[0] as ArrayCoder<U64Coder>).length = bytes.length / 8;
     }
     const coder = new TupleCoder(coders);
     const [decoded, newOffset] = coder.decode(bytes, 0);
