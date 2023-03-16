f79b3fdab3cd35426e4f432c8110fd0a99ae422c
Torres-ssf <sergio.uft@gmail.com>
2023-03-16 12:22:22 -0300

    remove use of versions package on docs

--- apps/docs/src/versions.data.ts
+++ apps/docs/src/versions.data.ts
@@ -1,53 +1,38 @@
-import { versions } from '@fuel-ts/versions';
 import fs from 'fs';
 import path from 'path';
 
-type TVersions = {
-  FUELS: string;
-  FUEL_CORE: string;
-  FORC: string;
-};
-
-const normalizeVersions = (params: TVersions) => {
-  let { FORC, FUELS, FUEL_CORE } = params;
-
-  const versionRegex = /0\.0\.0/;
+const readVersionsFromFiles = () => {
+  const versions = {
+    FORC: '0.0.0',
+    FUELS: '0.0.0',
+    FUEL_CORE: '0.0.0',
+  };
 
   try {
-    if (versionRegex.test(FORC)) {
-      const forcBinPath = path.join(__dirname, '../../../packages/forc-bin/package.json');
-      const forcFile = fs.readFileSync(forcBinPath, 'utf8');
-      const { config } = JSON.parse(forcFile);
-      ({ forcVersion: FORC } = config);
-    }
+    const forcBinPath = path.join(__dirname, '../../../packages/forc-bin/package.json');
+    const forcFile = fs.readFileSync(forcBinPath, 'utf8');
+    const { config } = JSON.parse(forcFile);
+    ({ forcVersion: versions.FORC } = config);
 
-    if (versionRegex.test(FUELS)) {
-      const fuelsPath = path.join(__dirname, '../../../packages/fuels/package.json');
-      const fuelsFile = fs.readFileSync(fuelsPath, 'utf8');
-      ({ version: FUELS } = JSON.parse(fuelsFile));
-    }
+    const fuelsPath = path.join(__dirname, '../../../packages/fuels/package.json');
+    const fuelsFile = fs.readFileSync(fuelsPath, 'utf8');
+    ({ version: versions.FUELS } = JSON.parse(fuelsFile));
 
-    if (versionRegex.test(FUEL_CORE)) {
-      const dockerfilePath = path.join(__dirname, '../../../services/fuel-core/Dockerfile');
-      const dockerfile = fs.readFileSync(dockerfilePath, 'utf8');
-      const regexFuelcore = /FROM ghcr\.io\/fuellabs\/fuel-core:v(\d+\.\d+\.\d+)/;
-      const match = dockerfile.match(regexFuelcore);
-      FUEL_CORE = match?.[1] || FUEL_CORE;
-    }
+    const dockerfilePath = path.join(__dirname, '../../../services/fuel-core/Dockerfile');
+    const dockerfile = fs.readFileSync(dockerfilePath, 'utf8');
+    const regexFuelcore = /FROM ghcr\.io\/fuellabs\/fuel-core:v(\d+\.\d+\.\d+)/;
+    const match = dockerfile.match(regexFuelcore);
+    versions.FUEL_CORE = match?.[1] || versions.FUEL_CORE;
   } catch {
-    return params;
+    return versions;
   }
 
-  return {
-    FORC,
-    FUELS,
-    FUEL_CORE,
-  };
+  return versions;
 };
 
 export default {
   load() {
-    const { FORC, FUELS, FUEL_CORE } = normalizeVersions(versions);
+    const { FORC, FUELS, FUEL_CORE } = readVersionsFromFiles();
 
     return {
       forc: FORC,
