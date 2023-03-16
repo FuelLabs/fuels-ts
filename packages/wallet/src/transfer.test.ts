645f6617fbbeacd49d081330714c1a65bd833dff
Torres-ssf <sergio.uft@gmail.com>
2023-03-16 11:28:18 -0300

    using imported snippets instead of hardcoded (transferring assets wallets docs)

--- packages/wallet/src/transfer.test.ts
+++ packages/wallet/src/transfer.test.ts
@@ -10,7 +10,7 @@ import { Wallet } from '.';
 
 describe('Wallet', () => {
   it('can transfer a single type of coin to a single destination', async () => {
-    // #region typedoc:wallet-transfer
+    // #region wallet-transfer
     // setup a provider and two test wallets
     const provider = new Provider('http://127.0.0.1:4000/graphql');
     const sender = await generateTestWallet(provider, [[100, NativeAssetId]]);
@@ -26,7 +26,7 @@ describe('Wallet', () => {
     // validate new balances
     expect(senderBalances).toEqual([{ assetId: NativeAssetId, amount: bn(99) }]);
     expect(receiverBalances).toEqual([{ assetId: NativeAssetId, amount: bn(1) }]);
-    // #endregion
+    // #endregion wallet-transfer
   });
 
   it('can transfer with custom TX Params', async () => {
