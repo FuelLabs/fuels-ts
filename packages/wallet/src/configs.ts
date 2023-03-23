d9f8c8c0e993cc1abca19877eafd617ca0d2ee38
Anderson Arboleya <anderson@arboleya.me>
2023-03-23 10:33:27 +0100

    fix: adjusting constants evaluation by removing usage of `process.env` (#847)

--- packages/wallet/src/configs.ts
+++ packages/wallet/src/configs.ts
@@ -1,9 +1,1 @@
-export const getFuelNetworkURL = (params: {
-  source: Record<string, string | undefined> | undefined;
-}) => {
-  const { source } = params;
-
-  return {
-    FUEL_NETWORK_URL: source?.FUEL_NETWORK_URL || 'http://127.0.0.1:4000/graphql',
-  };
-};
+export const FUEL_NETWORK_URL = 'http://127.0.0.1:4000/graphql';
