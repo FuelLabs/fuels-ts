e2f482fa0fde0067f2bd9bc8086a4a30e5b46530
Anderson Arboleya <anderson@arboleya.me>
2023-03-23 17:21:43 -0300

    Rewriting and expanding doc/comments about where versions come from

--- packages/versions/src/index.ts
+++ packages/versions/src/index.ts
@@ -1,20 +1,38 @@
 /*
-  Variables:
+  1) Variables
+  ------------
     `FUELS` — comes from `/packages/fuels/package.json`
     `FUEL_CORE` — comes from `/services/fuel-core/Dockerfile`
     `FORC` — comes from `/packages/forc-bin/package.json`
 
-  The CI release routine reads the aforementioned files,
-  and set all the variables prior to building the packages.
 
-  Take a look at the /.github/workflows/release` file.
+  2)  CI
+  ------------
+    The CI release routine reads the aforementioned files,
+    and set all the variables prior to building the packages.
 
-  The TSUP build then replaces all `process.env.<VAR_NAME>`
-  entries bellow with their respective values from the env.
+    Take a look at the /.github/workflows/release` file.
 
-  If no env variables are set, the expression `!0` seems to be
-  used by TSUP, which then results to `true`. For this reason,
-  we check for it and use default values instead.
+
+  3) Pre Build
+  ------------
+    There's a `prebuild` script in:
+     - packages/versions/package.json
+
+    Pior to build, it will call this file:
+      - packages/versions/scripts/injectVersions.ts
+
+    Which will inject those versions at:
+      - packages/versions/src/lib/getSupportedVersions.ts
+
+    If no env variables are set, it uses the current versions
+    from the original locations mentioned in the 1st step.
+
+
+  4) Build
+  ------------
+    By the time we get to the `build` step, everything is in
+    place already and ready to be built and released.
 */
 
 import { getSupportedVersions } from './lib/getSupportedVersions';
