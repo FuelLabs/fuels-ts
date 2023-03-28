6858aa5bfc24824b5fd9d9eb700d5654f7f967ae
Anderson Arboleya <anderson@arboleya.me>
2023-03-28 08:44:43 -0300

    Removing needless async/await

--- packages/fuel-gauge/scripts/process-predicates.ts
+++ packages/fuel-gauge/scripts/process-predicates.ts
@@ -8,13 +8,13 @@ const files = readdirSync(projectsDir).filter((file) => file.includes('predicate
 
 const { log } = console;
 
-files.forEach(async (filePath) => {
+files.forEach((filePath) => {
   log('Process predicate: ', filePath);
 
   const basePath = join(projectsDir, filePath);
   const binaryPath = join(basePath, '/out/debug/', `${filePath}.bin`);
   const binaryTSPath = join(basePath, 'index.ts');
-  const bytes = await readFileSync(binaryPath);
+  const bytes = readFileSync(binaryPath);
 
   // Put hexlified binary in a TS file so it can be imported
   const predicateTs = `export default '${hexlify(bytes)}';\n`;
