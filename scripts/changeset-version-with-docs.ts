eb453df1d3f8b62f4eb2b72f9accdfca48e2bf2b
Anderson Arboleya <anderson@arboleya.me>
2023-03-28 17:18:52 +0200

    fix: executing `prebuild` routine before committing changes (#860)

--- scripts/changeset-version-with-docs.ts
+++ scripts/changeset-version-with-docs.ts
@@ -7,6 +7,7 @@ import sh from 'shelljs';
   sh.set(`-e`);
 
   // Commit versions generated at pre-build step
+  sh.exec(`pnpm -C packages/versions prebuild`);
   sh.exec(`git add packages/versions/src/lib/getSupportedVersions.ts`);
   sh.exec(`git commit -m"ci(scripts): update versions"`);
 
