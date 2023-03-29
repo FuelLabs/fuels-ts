191f5711426cb5e586fabbad3df2437cd39eb4cd
Anderson Arboleya <anderson@arboleya.me>
2023-03-29 21:02:48 +0200

    fix: ignoring possible git errors when auto-committing generated versions file (#863)

--- scripts/changeset-version-with-docs.ts
+++ scripts/changeset-version-with-docs.ts
@@ -3,13 +3,13 @@
 import sh from 'shelljs';
 
 (() => {
-  // Force exit on error
-  sh.set(`-e`);
-
   // Commit versions generated at pre-build step
   sh.exec(`pnpm -C packages/versions prebuild`);
   sh.exec(`git add packages/versions/src/lib/getSupportedVersions.ts`);
   sh.exec(`git commit -m"ci(scripts): update versions"`);
+
+  // Force exit on error
+  sh.set(`-e`);
 
   // Update doc version references
   sh.exec(
