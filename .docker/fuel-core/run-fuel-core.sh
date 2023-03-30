2ee449aa3b008e1d316af9b46d1825ee775ef616
Anderson Arboleya <anderson@arboleya.me>
2023-03-30 13:22:10 -0300

    Docs

--- .docker/fuel-core/run-fuel-core.sh
+++ .docker/fuel-core/run-fuel-core.sh
@@ -1,6 +1,12 @@
+# This script mimics what the Dockerfile does,
+# but using your local `fuel-core` instad of Docker
+
 IP=0.0.0.0
 PORT=4000
 MIN_GAS_PRICE=0
+
+# This is the private key of the consensus.PoA.signing_key in the chainConfig.json
+# this key is responsible for validating the transactions
 CONSENSUS_KEY=0xa449b1ffee0e2205fa924c6740cc48b3b473aa28587df6dab12abc245d1f5298
 
 fuel-core run \
