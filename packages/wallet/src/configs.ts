320b6e7060cd4db256ebc6ee0b9ecf67994ba798
Theprotocolwhisperer <chrismata10@gmail.com>
2023-04-03 01:00:14 -0500

    category(fix): Add getFuelNetworkUrl function

--- packages/wallet/src/configs.ts
+++ packages/wallet/src/configs.ts
@@ -1,1 +1,2 @@
 export const FUEL_NETWORK_URL = 'http://127.0.0.1:4000/graphql';
+export const FUEL_NETWORK_BACKUP_URL = "https://beta-3.fuel.network/graphql";
--- packages/wallet/src/wallet.ts
+++ packages/wallet/src/wallet.ts
@@ -1,14 +1,40 @@
 import type { BytesLike } from '@ethersproject/bytes';
 import type { AbstractAddress } from '@fuel-ts/interfaces';
 import type { Provider } from '@fuel-ts/providers';
+import http from "http";
 
-import { FUEL_NETWORK_URL } from './configs';
+import { FUEL_NETWORK_URL , FUEL_NETWORK_BACKUP_URL} from './configs';
 import { WalletLocked, WalletUnlocked } from './wallets';
 
+async function getFuelNetworkUrl(): Promise<string> {
+  return new Promise<string>((resolve) => {
+    const request = http.request(FUEL_NETWORK_URL, { method: "HEAD" }, (response) => {
+      if (response.statusCode === 200) {
+        resolve(FUEL_NETWORK_URL);
+      } else {
+        console.error("Fuel Network URL is not available, switching to backup URL");
+        resolve(FUEL_NETWORK_BACKUP_URL);
+      }
+    });
+
+    request.on("error", () => {
+      console.error("Error checking Fuel Network URL, switching to backup URL");
+      resolve(FUEL_NETWORK_BACKUP_URL);
+    });
+
+    request.end();
+  });
+}
+
+const promiseString: Promise<string> = getFuelNetworkUrl();
+  let resultString: string;
+  promiseString.then(value => {
+    resultString = value;
+  });
 export class Wallet {
   static fromAddress(
     address: string | AbstractAddress,
-    provider: string | Provider = FUEL_NETWORK_URL
+    provider: string | Provider = resultString
   ): WalletLocked {
     return new WalletLocked(address, provider);
   }
