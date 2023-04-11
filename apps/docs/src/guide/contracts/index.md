b2c8bbe83fc1be896711f76b4e609cab7ade0441
Torres-ssf <sergio.uft@gmail.com>
2023-04-11 10:25:59 -0300

    implement snippet test file for contracts index page

--- apps/docs/src/guide/contracts/index.md
+++ apps/docs/src/guide/contracts/index.md
@@ -9,10 +9,6 @@ Once you've deployed your contract, as seen in the previous sections, you'll lik
 
 Here's an example. Suppose your Sway contract has two ABI methods called `echo_str_8(str[8])` and `echo_u8(u8)`. Once you've deployed the contract, you can call the methods like this:
 
-<<< @/../../../packages/fuel-gauge/src/coverage-contract.test.ts#String-size8{ts:line-numbers}
+<<< @/../../snippets/src/guide/contracts/index.test.ts#echo-values{ts:line-numbers}
 
-<<< @/../../../packages/fuel-gauge/src/coverage-contract.test.ts#U8{ts:line-numbers}
-
-The examples above uses all the default configurations and performs a simple contract call.
-
-Next, we'll see how we can further configure the many different parameters in a contract call
+The example above uses all the default configurations and performs a simple contract call. On the next sections, we'll see how we can further configure the many different parameters in a contract call.
---
+++ apps/snippets/src/guide/contracts/index.test.ts
@@ -0,0 +1,31 @@
+import type { Contract } from 'fuels';
+import { ContractFactory } from 'fuels';
+
+import { SnippetContractEnum, getSnippetContractArtifacts } from '../../../contracts';
+import { getTestWallet } from '../../utils';
+
+describe(__filename, () => {
+  let contract: Contract;
+
+  beforeAll(async () => {
+    const wallet = await getTestWallet();
+
+    const { abi, bin } = getSnippetContractArtifacts(SnippetContractEnum.ECHO_VALUES);
+
+    const factory = new ContractFactory(bin, abi, wallet);
+    contract = await factory.deployContract();
+  });
+
+  it('should call contract and echo values just fine', async () => {
+    // #region echo-values
+    const u8Value = 10;
+    const str8Value = 'fuel-sdk';
+
+    const res1 = await contract.functions.echo_u8(u8Value).get();
+    const res2 = await contract.functions.echo_str_8(str8Value).get();
+
+    expect(res1.value).toBe(u8Value);
+    expect(res2.value).toBe(str8Value);
+    // #endregion echo-values
+  });
+});
