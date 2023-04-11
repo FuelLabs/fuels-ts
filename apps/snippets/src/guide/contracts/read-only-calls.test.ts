a19db5c08d3ad9207bef65fdc5ef46910faf86c3
Torres-ssf <sergio.uft@gmail.com>
2023-04-11 10:42:48 -0300

    implement snippet test file for contracts read-only-calls page

---
+++ apps/snippets/src/guide/contracts/read-only-calls.test.ts
@@ -0,0 +1,27 @@
+import type { Contract, WalletUnlocked } from 'fuels';
+import { ContractFactory } from 'fuels';
+
+import { SnippetContractEnum, getSnippetContractArtifacts } from '../../../contracts';
+import { getTestWallet } from '../../utils';
+
+describe(__filename, () => {
+  let contract: Contract;
+  let wallet: WalletUnlocked;
+
+  beforeAll(async () => {
+    wallet = await getTestWallet();
+
+    const { abi, bin } = getSnippetContractArtifacts(SnippetContractEnum.ECHO_VALUES);
+
+    const factory = new ContractFactory(bin, abi, wallet);
+    contract = await factory.deployContract();
+  });
+
+  it('should execute read only call just fine', async () => {
+    // #region Contract-read-only-call
+    const { value } = await contract.functions.echo_u8(15).get();
+
+    expect(value).toEqual(15);
+    // #endregion Contract-read-only-call
+  });
+});
