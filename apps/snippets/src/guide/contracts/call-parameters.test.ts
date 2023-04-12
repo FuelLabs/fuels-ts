30ce753b0c23bd0bf5678eda164c298f4ef6aa5a
Torres-ssf <sergio.uft@gmail.com>
2023-04-12 11:23:10 -0300

    implement snippet test file for contracts call-parameters page

---
+++ apps/snippets/src/guide/contracts/call-parameters.test.ts
@@ -0,0 +1,73 @@
+import type { Contract } from 'fuels';
+import { BN, NativeAssetId, ContractFactory } from 'fuels';
+
+import { getSnippetContractArtifacts, SnippetContractEnum } from '../../../contracts';
+import { getTestWallet } from '../../utils';
+
+describe(__filename, () => {
+  let contract: Contract;
+  const { abi, bin } = getSnippetContractArtifacts(SnippetContractEnum.RETURN_CONTEXT);
+
+  beforeAll(async () => {
+    const wallet = await getTestWallet();
+
+    const factory = new ContractFactory(bin, abi, wallet);
+    contract = await factory.deployContract();
+  });
+
+  it('should execute contract call with forwarded amount just fine', async () => {
+    // #region call-params-1
+    const amountToForward = 10;
+
+    const { value } = await contract.functions
+      .return_context_amount()
+      .callParams({
+        forward: [amountToForward, NativeAssetId],
+      })
+      .call();
+
+    expect(new BN(value).toNumber()).toBe(amountToForward);
+    // #endregion call-params-1
+  });
+
+  it('should throw error due not enough gas', async () => {
+    // #region call-params-2
+    await expect(
+      contract.functions
+        .return_context_amount()
+        .callParams({
+          forward: [10, NativeAssetId],
+          gasLimit: 1,
+        })
+        .call()
+    ).rejects.toThrow(/OutOfGas/);
+    // #endregion call-params-2
+  });
+
+  it('should successfully execute transaction with `txParams` and `callParams`', async () => {
+    // #region call-params-3
+    const amountToForward = 10;
+    const contractCallGasLimit = 100;
+    const transactionGasLimit = 10000;
+
+    const result = await contract.functions
+      .return_context_amount()
+      .callParams({
+        forward: [amountToForward, NativeAssetId],
+        gasLimit: contractCallGasLimit,
+      })
+      .txParams({
+        gasLimit: transactionGasLimit,
+      })
+      .call();
+
+    const {
+      transactionResult: { transaction },
+      value,
+    } = result;
+
+    expect(new BN(value).toNumber()).toBe(10);
+    expect(new BN(transaction.gasLimit).toNumber()).toBe(10000);
+    // #endregion call-params-3
+  });
+});
