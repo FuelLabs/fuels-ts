c43649605bcbde02db0e0eac96447b5c5d3d4687
Torres-ssf <sergio.uft@gmail.com>
2023-04-12 15:22:25 -0300

    implement snippet test file for contracts cost-estimation page

---
+++ apps/docs-snippets/src/guide/contracts/cost-estimation.test.ts
@@ -0,0 +1,54 @@
+import type { Contract } from 'fuels';
+import { ContractFactory, NativeAssetId } from 'fuels';
+
+import { SnippetContractEnum, getSnippetContractArtifacts } from '../../../contracts';
+import { getTestWallet } from '../../utils';
+
+describe(__filename, () => {
+  let contract: Contract;
+  beforeAll(async () => {
+    const wallet = await getTestWallet();
+
+    const { abi, bin } = getSnippetContractArtifacts(SnippetContractEnum.RETURN_CONTEXT);
+
+    const contractFactory = new ContractFactory(bin, abi, wallet);
+
+    contract = await contractFactory.deployContract();
+  });
+
+  it('should get transaction cost estimate for a single contract call just fine', async () => {
+    // #region cost-estimation-1
+    const cost = await contract.functions
+      .return_context_amount()
+      .callParams({
+        forward: [100, NativeAssetId],
+      })
+      .getTransactionCost();
+
+    expect(cost.fee).toBeDefined();
+    expect(cost.gasPrice).toBeDefined();
+    expect(cost.gasUsed).toBeDefined();
+    expect(cost.minGasPrice).toBeDefined();
+    // #endregion cost-estimation-1
+  });
+
+  it('should get transaction cost estimate for a single contract call just fine', async () => {
+    // #region cost-estimation-2
+    const scope = contract.multiCall([
+      contract.functions.return_context_amount().callParams({
+        forward: [100, NativeAssetId],
+      }),
+      contract.functions.return_context_amount().callParams({
+        forward: [300, NativeAssetId],
+      }),
+    ]);
+
+    const cost = await scope.getTransactionCost();
+
+    expect(cost.fee).toBeDefined();
+    expect(cost.gasPrice).toBeDefined();
+    expect(cost.gasUsed).toBeDefined();
+    expect(cost.minGasPrice).toBeDefined();
+    // #endregion cost-estimation-2
+  });
+});
