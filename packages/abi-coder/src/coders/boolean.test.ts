ac01662cc11b0786da575ac5110d8fa35f49ca08
danielbate <--global>
2023-04-24 23:00:48 +0100

    test: refactor and increase verbosity of boolean coder tests

---
+++ packages/abi-coder/src/coders/boolean.test.ts
@@ -0,0 +1,50 @@
+import BooleanCoder from './boolean';
+
+describe('BooleanCoder', () => {
+  const TRUE_DECODED = true;
+  const TRUE_ENCODED = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 1]);
+  const FALSE_DECODED = false;
+  const FALSE_ENCODED = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0]);
+
+  const coder = new BooleanCoder();
+
+  it('should encode a true boolean', () => {
+    const expected = TRUE_ENCODED;
+    const actual = coder.encode(TRUE_DECODED);
+
+    expect(actual).toStrictEqual(expected);
+  });
+
+  it('should encode a false boolean', () => {
+    const expected = FALSE_ENCODED;
+    const actual = coder.encode(FALSE_DECODED);
+
+    expect(actual).toStrictEqual(expected);
+  });
+
+  it('should decode a true boolean', () => {
+    const expectedValue = TRUE_DECODED;
+    const expectedLength = TRUE_ENCODED.length;
+    const [actualValue, actualLength] = coder.decode(TRUE_ENCODED, 0);
+
+    expect(actualValue).toStrictEqual(expectedValue);
+    expect(actualLength).toBe(expectedLength);
+  });
+
+  it('should decode a false boolean', () => {
+    const expectedValue = FALSE_DECODED;
+    const expectedLength = FALSE_ENCODED.length;
+    const [actualValue, actualLength] = coder.decode(FALSE_ENCODED, 0);
+
+    expect(actualValue).toStrictEqual(expectedValue);
+    expect(actualLength).toBe(expectedLength);
+  });
+
+  it('should throw an error when decoding an invalid boolean value', () => {
+    const invalidInput = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 2]);
+
+    expect(() => {
+      coder.decode(invalidInput, 0);
+    }).toThrow('Invalid boolean value');
+  });
+});
