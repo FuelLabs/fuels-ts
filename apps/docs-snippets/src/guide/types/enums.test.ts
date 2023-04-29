a16957a51b672a9a1f8ff019ba7c70e33a3e63b3
Torres-ssf <sergio.uft@gmail.com>
2023-04-29 17:46:10 -0300

    testing enums code snippets

---
+++ apps/docs-snippets/src/guide/types/enums.test.ts
@@ -0,0 +1,53 @@
+import type { Contract } from 'fuels';
+import { ContractFactory } from 'fuels';
+
+import { getSnippetContractArtifacts, SnippetContractEnum } from '../../../contracts';
+import { getTestWallet } from '../../utils';
+
+describe(__filename, () => {
+  let contract: Contract;
+
+  beforeAll(async () => {
+    const wallet = await getTestWallet();
+
+    const { abi, bin } = getSnippetContractArtifacts(SnippetContractEnum.ECHO_ENUM);
+
+    const factory = new ContractFactory(bin, abi, wallet);
+
+    contract = await factory.deployContract();
+  });
+
+  it('should successfully echo a simple enum in a contract call', async () => {
+    // #region enum-3
+    const enumVariant = 'Completed';
+
+    const { value } = await contract.functions.echo_state_error_enum(enumVariant).get();
+
+    expect(value).toEqual(enumVariant);
+    // #endregion enum-3
+  });
+
+  it('should successfully echo a enum in a contract call (UserError Enum)', async () => {
+    // #region enum-6
+    const userErroVar = 'InsufficientPermissions';
+
+    const enumParam = { UserError: userErroVar };
+
+    const { value } = await contract.functions.echo_error_enum(enumParam).get();
+
+    expect(value).toEqual(enumParam);
+    // #endregion enum-6
+  });
+
+  it('should successfully echo a enum in a contract call (StateError Enum)', async () => {
+    // #region enum-7
+    const stateErrorVar = 'Completed';
+
+    const enumParam = { StateError: stateErrorVar };
+
+    const { value } = await contract.functions.echo_error_enum(enumParam).get();
+
+    expect(value).toEqual(enumParam);
+    // #endregion enum-7
+  });
+});
