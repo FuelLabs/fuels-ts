fccae6a9bbc42b01833b6290b2c46cae303243e0
danielbate <--global>
2023-05-03 13:29:39 +0100

    test: add missing test cases for encoding non array inputs in vec coder

--- packages/abi-coder/src/coders/vec.test.ts
+++ packages/abi-coder/src/coders/vec.test.ts
@@ -2,7 +2,7 @@ import BooleanCoder from './boolean';
 import VecCoder from './vec';
 
 describe('VecCoder', () => {
-  it('should encode a Vec of Numbers', () => {
+  it('should encode a Vec of Booleans', () => {
     const coder = new VecCoder(new BooleanCoder());
     const expected = new Uint8Array([
       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2,
@@ -12,12 +12,28 @@ describe('VecCoder', () => {
     expect(actual).toStrictEqual(expected);
   });
 
-  it('should get encoded vector data offset', () => {
+  it('should get encoded vector data', () => {
     const coder = new VecCoder(new BooleanCoder());
     const expected = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
     const actual = coder.getEncodedVectorData([true, false]);
 
     expect(actual).toStrictEqual(expected);
+  });
+
+  it('should throw when encoding non array input', () => {
+    const coder = new VecCoder(new BooleanCoder());
+
+    expect(() => {
+      coder.encode('Nope' as never);
+    }).toThrow('expected array value');
+  });
+
+  it('should throw when getting encoded vector data with non array input', () => {
+    const coder = new VecCoder(new BooleanCoder());
+
+    expect(() => {
+      coder.getEncodedVectorData('Nope' as never);
+    }).toThrow('expected array value');
   });
 
   it('should get base offset', () => {
