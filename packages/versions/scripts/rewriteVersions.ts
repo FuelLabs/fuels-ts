123232407acc475009884df31cfb47ca00a2d0d8
Anderson Arboleya <anderson@arboleya.me>
2023-05-04 06:10:02 -0300

    Renaming variable (fixing semantics)

--- packages/versions/scripts/rewriteVersions.ts
+++ packages/versions/scripts/rewriteVersions.ts
@@ -4,14 +4,14 @@ import { join } from 'path';
 export const readVersionsFromFiles = () => {
   const rootDir = join(__dirname, '../../..');
   const packagesDir = join(rootDir, 'packages');
-  const servicesDir = join(rootDir, '.docker');
+  const dockerDir = join(rootDir, '.docker');
 
   // forc-bin
   const forcPath = join(packagesDir, 'forc-bin', 'package.json');
   const forcPkgJson = JSON.parse(readFileSync(forcPath, 'utf8'));
 
   // fuel-core
-  const dockerFilePath = join(servicesDir, 'fuel-core', 'Dockerfile');
+  const dockerFilePath = join(dockerDir, 'fuel-core', 'Dockerfile');
   const dockerFileContents = readFileSync(dockerFilePath, 'utf8');
   const regexFuelcore = /FROM ghcr\.io\/fuellabs\/fuel-core:v(\d+\.\d+\.\d+)/;
   const match = dockerFileContents.match(regexFuelcore);
