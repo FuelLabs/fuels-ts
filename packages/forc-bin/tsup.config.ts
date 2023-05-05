4277a96922efaf7080c34dacb528077a3e9dfe51
Anderson Arboleya <anderson@arboleya.me>
2023-05-05 14:50:23 -0300

    Adjusting tsup configs for `forc-bin`

--- packages/forc-bin/tsup.config.ts
+++ packages/forc-bin/tsup.config.ts
@@ -1,6 +1,17 @@
-import { index } from '@internal/tsup';
+/**
+ * We don't inherit from '@internal/tsup' for this
+ * case because at this point, the package might not
+ * have been built yet, since this config will probably
+ * be called before the `build` even starts.
+ */
+// import { index } from '@internal/tsup';
 
 export default {
-  ...index,
+  clean: true,
+  dts: true,
   entry: ['src/index.ts', 'src/cli.ts', 'src/install.ts', 'src/update.ts'],
+  format: ['cjs', 'esm', 'iife'],
+  minify: false,
+  sourcemap: true,
+  splitting: false,
 };
