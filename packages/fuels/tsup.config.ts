932cf2a605ab63b1ee1d5f113a129cf9d458ec04
Anderson Arboleya <anderson@arboleya.me>
2023-05-05 17:08:04 -0300

    DRY tsup config for `fuels`

--- packages/fuels/tsup.config.ts
+++ packages/fuels/tsup.config.ts
@@ -1,29 +1,3 @@
-import { index } from '@internal/tsup';
-import type { Options } from 'tsup';
+import { indexBinAndCli } from '@internal/tsup';
 
-const primary = {
-  index: 'src/index.ts',
-};
-
-const secondaries = {
-  bin: 'src/bin.ts',
-  cli: 'src/cli.ts',
-};
-
-const configs: Options[] = [
-  {
-    ...index,
-    entry: { ...primary },
-    dts: {
-      banner: Object.keys(secondaries)
-        .map((key) => `import './${key}';`)
-        .join('\n'),
-    },
-  },
-  {
-    ...index,
-    entry: { ...secondaries },
-  },
-];
-
-export default configs;
+export default indexBinAndCli;
