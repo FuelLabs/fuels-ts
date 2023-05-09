eb6755ed34c6c38b403762de52b9ad2a9dd7affd
Torres-ssf <sergio.uft@gmail.com>
2023-05-09 14:42:22 -0300

    add more mocks

--- packages/abi-coder/test/fixtures/mocks.ts
+++ packages/abi-coder/test/fixtures/mocks.ts
@@ -1,27 +1,97 @@
-import type { JsonFlatAbi } from '../../src/json-abi';
+import type { ConfigurableFragment, JsonAbiFragmentType, JsonFlatAbi } from '../../src/json-abi';
 
 export const jsonFlatAbiMock: JsonFlatAbi = {
-  types: [],
+  types: [
+    {
+      typeId: 0,
+      type: '(_, _)',
+      components: [
+        {
+          name: '__tuple_element',
+          type: 2,
+          typeArguments: null,
+        },
+        {
+          name: '__tuple_element',
+          type: 1,
+          typeArguments: null,
+        },
+      ],
+      typeParameters: null,
+    },
+    {
+      typeId: 1,
+      type: 'bool',
+      components: null,
+      typeParameters: null,
+    },
+    {
+      typeId: 2,
+      type: 'u8',
+      components: null,
+      typeParameters: null,
+    },
+  ],
   functions: [],
   loggedTypes: [],
   configurables: [
     {
-      name: 'U8',
-      configurableType: {
-        name: '',
-        type: 12,
-        typeArguments: null,
-      },
-      offset: 204,
-    },
-    {
       name: 'BOOL',
       configurableType: {
         name: '',
-        type: 5,
+        type: 1,
         typeArguments: null,
       },
       offset: 212,
     },
+    {
+      name: 'U8',
+      configurableType: {
+        name: '',
+        type: 2,
+        typeArguments: null,
+      },
+      offset: 204,
+    },
   ],
 };
+
+export const fragmentType: JsonAbiFragmentType = {
+  type: 'dummy',
+  name: 'dummy',
+  components: null,
+  typeArguments: null,
+};
+
+export const configurableFragmentMock: ReadonlyArray<ConfigurableFragment> = [
+  {
+    name: 'one',
+    configurableType: {
+      name: '',
+      type: 1,
+      typeArguments: null,
+    },
+    offset: 1,
+    fragmentType,
+  },
+  {
+    name: 'two',
+    configurableType: {
+      name: '',
+      type: 2,
+      typeArguments: null,
+    },
+    offset: 2,
+    fragmentType,
+  },
+  {
+    name: 'three',
+    configurableType: {
+      name: '',
+      type: 3,
+      typeArguments: null,
+    },
+    offset: 3,
+    fragmentType,
+  },
+];
