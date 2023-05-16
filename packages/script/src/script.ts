8549d9f001f3e071ad0393a596a0b33846c7e611
Torres-ssf <sergio.uft@gmail.com>
2023-05-16 10:23:56 -0300

    add setConfigurables method to Script class

--- packages/script/src/script.ts
+++ packages/script/src/script.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { BytesLike } from '@ethersproject/bytes';
 import { arrayify } from '@ethersproject/bytes';
-import { Interface } from '@fuel-ts/abi-coder';
+import { AbiCoder, Interface } from '@fuel-ts/abi-coder';
 import type { InputValue, JsonAbi } from '@fuel-ts/abi-coder';
 import { AbstractScript } from '@fuel-ts/interfaces';
 import type { BN } from '@fuel-ts/math';
@@ -40,5 +40,29 @@ export class Script<TInput extends Array<any>, TOutput> extends AbstractScript {
       main: (...args: TInput) =>
         new ScriptInvocationScope(this, this.interface.getFunction('main'), args),
     };
+  }
+
+  setConfigurables(configurables: { [name: string]: unknown }) {
+    try {
+      if (!Object.keys(this.interface.configurables).length) {
+        throw new Error('Script has no configurable constants to be set');
+      }
+
+      Object.entries(configurables).forEach(([key, value]) => {
+        if (!this.interface.configurables[key]) {
+          throw new Error(`Script has no configurable constant named: ${key}`);
+        }
+
+        const { fragmentType, offset } = this.interface.configurables[key];
+
+        const encoded = new AbiCoder().getCoder(fragmentType).encode(value);
+
+        this.bytes.set(encoded, offset);
+      });
+    } catch (err) {
+      throw new Error(`Error setting configurable: ${err}`);
+    }
+
+    return this;
   }
 }
