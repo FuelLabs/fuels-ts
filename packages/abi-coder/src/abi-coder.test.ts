25278657f4425d9b57b8a521d36c6d2a23d371f0
Daniel Bate <djbate23@gmail.com>
2023-05-16 16:27:09 +0100

    test: refactor `EnumCoder` tests (#938)
    * test: refactor and increase verbosity of enum coder tests
    
    * chore: changeset
    
    * test: introduce test cases for native enums
    
    * test: add feature test in abi coder for native enum
    
    * test: remove abi coder test case for native enum as redundant
    
    ---------
    
    Co-authored-by: danielbate <--global>

--- packages/abi-coder/src/abi-coder.test.ts
+++ packages/abi-coder/src/abi-coder.test.ts
@@ -3,6 +3,7 @@ import { bn, toHex } from '@fuel-ts/math';
 
 import AbiCoder from './abi-coder';
 import type { DecodedValue } from './coders/abstract-coder';
+import type { JsonAbiFragmentType } from './json-abi';
 
 const B256 = '0xd5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930b';
 
