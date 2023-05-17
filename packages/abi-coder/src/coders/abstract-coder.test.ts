34e9a242bf5195d2b17569d52a9d0f3123f2848e
danielbate <--global>
2023-05-17 10:39:03 +0100

    test: abstract coder tests

---
+++ packages/abi-coder/src/coders/abstract-coder.test.ts
@@ -0,0 +1,43 @@
+import { Logger } from '@ethersproject/logger';
+import * as loggerMod from '@ethersproject/logger';
+
+import Coder from './abstract-coder';
+
+jest.mock('@ethersproject/logger', () => ({
+  __esModule: true,
+  ...jest.requireActual('@ethersproject/logger'),
+  Logger: jest.fn().mockImplementation(() => ({
+    throwArgumentError: jest.fn().mockImplementation(() => null),
+  })),
+}));
+
+class TestCoder extends Coder {
+  constructor() {
+    super('test', 'test', 8);
+  }
+
+  encode(_value: unknown): Uint8Array {
+    return new Uint8Array();
+  }
+
+  decode(_data: Uint8Array, _offset: number): [boolean, number] {
+    return [false, 8];
+  }
+}
+
+describe('Coder', () => {
+  afterEach(jest.restoreAllMocks);
+
+  const coder = new TestCoder();
+
+  it('should throw unreachable on throwError', () => {
+    expect(() => coder.throwError('test', 'test')).toThrowError('unreachable');
+  });
+
+  it('should set offset', () => {
+    expect(coder.offset).toBeUndefined();
+
+    coder.setOffset(8);
+    expect(coder.offset).toBe(8);
+  });
+});
