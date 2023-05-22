cb11146aa1f998673c02d440eb0b000de03a95cb
danielbate <--global>
2023-05-22 15:35:46 +0100

    feat: add evm address type to typegen

---
+++ packages/abi-typegen/src/abi/types/EvmAddressType.ts
@@ -0,0 +1,28 @@
+import type { IType } from '../../types/interfaces/IType';
+
+import { AType } from './AType';
+
+export class EvmAddressType extends AType implements IType {
+  public static swayType = 'struct EvmAddress';
+
+  public name = 'evmAddress';
+
+  static MATCH_REGEX: RegExp = /^struct EvmAddress$/m;
+
+  static isSuitableFor(params: { type: string }) {
+    return EvmAddressType.MATCH_REGEX.test(params.type);
+  }
+
+  public parseComponentsAttributes(_params: { types: IType[] }) {
+    const capitalizedName = 'EvmAddress';
+
+    this.attributes = {
+      inputLabel: capitalizedName,
+      outputLabel: capitalizedName,
+    };
+
+    this.requiredFuelsMembersImports = ['Address', capitalizedName];
+
+    return this.attributes;
+  }
+}
