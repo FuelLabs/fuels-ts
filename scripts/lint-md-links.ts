30b0945b58bdffae9d85e86362ccc29b59402413
Dhaiwat / dhai.eth <dhaiwatpandya@gmail.com>
2023-05-24 16:11:58 +0800

    use `glob` and bump its version

--- scripts/lint-md-links.ts
+++ scripts/lint-md-links.ts
@@ -1,42 +1,18 @@
 #!/usr/bin/env node
-import fs from 'fs';
-import path from 'path';
+import glob from 'glob';
 import sh from 'shelljs';
 
-function getAllMarkdownFiles(directory: string): string[] {
-  const markdownFiles: string[] = [];
-
-  function traverseDirectory(currentPath: string): void {
-    const files = fs.readdirSync(currentPath);
-
-    files.forEach((file) => {
-      const filePath = path.join(currentPath, file);
-      const fileStat = fs.statSync(filePath);
-
-      if (fileStat.isDirectory() && file !== 'node_modules') {
-        traverseDirectory(filePath);
-      } else if (fileStat.isFile() && path.extname(file) === '.md' && file !== 'CHANGELOG.md') {
-        markdownFiles.push(filePath);
-      }
-    });
-  }
-
-  traverseDirectory(directory);
-  return markdownFiles;
-}
-
 (() => {
-  const mdFiles = getAllMarkdownFiles(process.cwd());
-
+  const mdFiles = glob.sync('**/*.md', {
+    ignore: ['**/node_modules/**', '**/CHANGELOG.md'],
+  });
   const filesWithLintErrors: string[] = [];
-
   mdFiles.forEach((file) => {
     const { code } = sh.exec(`npx textlint ${file}`);
     if (code !== 0) {
       filesWithLintErrors.push(file);
     }
   });
-
   if (filesWithLintErrors.length > 0) {
     process.exit(1);
   }
