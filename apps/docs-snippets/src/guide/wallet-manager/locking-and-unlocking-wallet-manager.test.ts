69f3024bd4f2e27b6c20d451297c390de58c2b3a
Torres-ssf <sergio.uft@gmail.com>
2023-06-05 15:59:26 -0300

    add code snippet for locking and unlocking wallet manager

---
+++ apps/docs-snippets/src/guide/wallet-manager/locking-and-unlocking-wallet-manager.test.ts
@@ -0,0 +1,50 @@
+import { WalletManager } from '@fuel-ts/wallet-manager';
+
+describe(__filename, () => {
+  it('should', async () => {
+    // #region locking-and-unlocking-wallet-manager-1
+    const walletManager = new WalletManager();
+
+    const password = 'my-password';
+
+    await walletManager.unlock(password);
+    // #endregion locking-and-unlocking-wallet-manager-1
+
+    const privateKey = '0xc7079e195cca2495e47b056115f850a8be1f2c4ffe2a806922accb36d2ff0dde';
+
+    await walletManager.addVault({
+      type: 'privateKey',
+      secret: privateKey,
+    });
+
+    // #region locking-and-unlocking-wallet-manager-2
+    await walletManager.lock();
+    // #endregion locking-and-unlocking-wallet-manager-2
+
+    // #region locking-and-unlocking-wallet-manager-3
+    await walletManager.unlock(password);
+    // #endregion locking-and-unlocking-wallet-manager-3
+
+    // #region locking-and-unlocking-wallet-manager-4
+    const isLocked = walletManager.isLocked;
+
+    console.log(isLocked); // Outputs true if locked, false if unlocked
+    // #endregion locking-and-unlocking-wallet-manager-4
+
+    // #region locking-and-unlocking-wallet-manager-5
+    const newPassword = 'my-new-password';
+
+    walletManager.updatePassphrase(password, newPassword);
+    // #endregion locking-and-unlocking-wallet-manager-5
+
+    // #region locking-and-unlocking-wallet-manager-6
+    await walletManager.unlock('your password');
+
+    // perform your tasks...
+
+    walletManager.lock(); // Always lock your WalletManager when you're done
+    // #endregion locking-and-unlocking-wallet-manager-6
+
+    expect(walletManager).toBeTruthy();
+  });
+});
