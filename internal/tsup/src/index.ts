d52a6e5996c31b82252918d885ca95967d637ef3
Anderson Arboleya <anderson@arboleya.me>
2023-06-08 18:26:18 -0300

    Refactoring TSUP base config

--- internal/tsup/src/index.ts
+++ internal/tsup/src/index.ts
@@ -1,13 +1,5 @@
 /**
- * [NOTE.1]
- * All `primary` DTS files must import `secondary` ones for all
- * packages containing multiple entry-points. We do this by injecting
- * the required `import` lines in the primary DTS file via the
- * TSUP `dts.banner` config property.
- */
-
-/**
- * [NOTE.2]
+ * [NOTE]
  * Due to the limitations of TSUP regarding config inheritance, we
  * exports ready-to-go configurations for most used scenarios in the
  * monorepo. More complex configs can be done in isolation.
@@ -15,50 +7,32 @@
 
 import type { Options } from 'tsup';
 
-/**
- * Entrypoints:
- *  - src/index.ts
- */
-export const index: Options = {
+export const tsupDefaults: Options = {
   clean: true,
-  dts: true,
-  entry: ['src/index.ts'],
+  dts: false,
   format: ['cjs', 'esm', 'iife'],
   minify: false,
   sourcemap: true,
   splitting: false,
 };
 
-/**
- * Entrypoints:
- *  - src/index.ts
- *  - src/configs.ts
- */
 export const indexAndConfigs: Options[] = [
   {
-    ...index,
-    entry: { index: 'src/index.ts' },
-    dts: { banner: `import './configs';` },
-  },
-  {
-    ...index,
-    entry: { configs: 'src/configs.ts' },
+    ...tsupDefaults,
+    entry: {
+      index: 'src/index.ts',
+      configs: 'src/configs.ts',
+    },
   },
 ];
 
-/**
- * Entrypoints:
- *  - src/index.ts
- *  - src/bin.ts
- *  - src/cli.ts
- */
 export const indexBinAndCli: Options[] = [
   {
-    ...index,
-    dts: { banner: `import './bin'; import './cli';` },
-  },
-  {
-    ...index,
-    entry: { bin: 'src/bin.ts', cli: 'src/cli.ts' },
+    ...tsupDefaults,
+    entry: {
+      index: 'src/index.ts',
+      bin: 'src/bin.ts',
+      cli: 'src/cli.ts',
+    },
   },
 ];
