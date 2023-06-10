511839a28369fdc885b86ae1608ced00989f1b5d
Anderson Arboleya <anderson@arboleya.me>
2023-06-10 07:16:43 -0300

    Updating forc option

--- packages/program/scripts/build-multicall.sh
+++ packages/program/scripts/build-multicall.sh
@@ -6,7 +6,7 @@ FUELS_DEBUG=${FUELS_DEBUG:-}
 SCRIPT_DIR="src/multicall"
 BIN_DIR="$SCRIPT_DIR/out/debug"
 
-pnpm forc build -p $SCRIPT_DIR --print-finalized-asm
+pnpm forc build -p $SCRIPT_DIR --finalized-asm
 pnpm tsx scripts/process-multicall.ts
 
 if [[ -n "$FUELS_DEBUG" ]]; then
