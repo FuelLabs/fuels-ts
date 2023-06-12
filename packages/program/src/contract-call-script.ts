305ef8faae74e6aeaa4751b87d0080767fc2193c
Cameron Manavian <cameron.manavian@fuel.sh>
2023-06-12 08:16:55 -0700

    replace instructions

--- packages/program/src/contract-call-script.ts
+++ packages/program/src/contract-call-script.ts
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import type { BytesLike } from '@ethersproject/bytes';
 import { arrayify, concat } from '@ethersproject/bytes';
 import type { ArrayCoder, StructCoder } from '@fuel-ts/abi-coder';
-import { AbiCoder, U64Coder } from '@fuel-ts/abi-coder';
+import { ASSET_ID_LEN, WORD_SIZE, AbiCoder, U64Coder } from '@fuel-ts/abi-coder';
+import { Opcode, toBytesFromProgram } from '@fuel-ts/asm';
 import { bn, toNumber } from '@fuel-ts/math';
 import { ReceiptType } from '@fuel-ts/transactions';
 
@@ -9,6 +11,32 @@ import contractCallScriptAbi from './multicall/static-out/multicall-abi.json';
 import contractCallScriptBin from './multicall/static-out/multicall-bin';
 import { ScriptRequest } from './script-request';
 import type { ContractCall } from './types';
+
+type CallOpcodeParamsOffset = {
+  callDataOffset: number;
+  gasForwardedOffset: number;
+  amountOffset: number;
+  assetIdOffset: number;
+};
+
+const getSingleCallInstructions = ({
+  callDataOffset,
+  gasForwardedOffset,
+  amountOffset,
+  assetIdOffset,
+}: CallOpcodeParamsOffset): BytesLike => {
+  const program = [
+    Opcode.movi(0x10, callDataOffset),
+    Opcode.movi(0x11, gasForwardedOffset),
+    Opcode.lw(0x11, 0x11, 0),
+    Opcode.movi(0x12, amountOffset),
+    Opcode.lw(0x12, 0x12, 0),
+    Opcode.movi(0x13, assetIdOffset),
+    Opcode.call(0x10, 0x12, 0x13, 0x11),
+    Opcode.ret(0x01),
+  ];
+  return toBytesFromProgram(program);
+};
 
 /**
  * A script that calls contracts
@@ -18,8 +46,26 @@ import type { ContractCall } from './types';
  */
 export const contractCallScript = new ScriptRequest<ContractCall[], Uint8Array[]>(
   // Script to call the contract
-  contractCallScriptBin,
+  getSingleCallInstructions({
+    assetIdOffset: 0,
+    amountOffset: 0 + ASSET_ID_LEN,
+    gasForwardedOffset: 0 + ASSET_ID_LEN + WORD_SIZE,
+    callDataOffset: ASSET_ID_LEN + 2 * WORD_SIZE,
+  }),
   (contractCalls) => {
+    const OFFSET = 0;
+    // @ts-expect-error nnn
+    this.bytes = arrayify(
+      getSingleCallInstructions({
+        assetIdOffset: OFFSET,
+        amountOffset: OFFSET + ASSET_ID_LEN,
+        gasForwardedOffset: OFFSET + ASSET_ID_LEN + WORD_SIZE,
+        callDataOffset: OFFSET + ASSET_ID_LEN + 2 * WORD_SIZE,
+      })
+    );
+
+    // todo loop through calls
+
     const inputs = contractCallScriptAbi[0].inputs;
     const scriptDataCoder = new AbiCoder().getCoder(inputs[0]) as StructCoder<any>;
     const callSlotsLength = (scriptDataCoder.coders.calls as ArrayCoder<any>).length;
