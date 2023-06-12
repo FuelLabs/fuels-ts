312b6790aa1bc9b6cc21e98a98ce2a742703c93d
Cameron Manavian <cameron.manavian@fuel.sh>
2023-06-12 08:26:08 -0700

    rm

--- packages/abi-coder/src/coders/vec.test.ts
+++ packages/abi-coder/src/coders/vec.test.ts
@@ -12,35 +12,12 @@ describe('VecCoder', () => {
     expect(actual).toStrictEqual(expected);
   });
 
-  it('should get encoded vector data', () => {
-    const coder = new VecCoder(new BooleanCoder());
-    const expected = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
-    const actual = coder.getEncodedVectorData([true, false]);
-
-    expect(actual).toStrictEqual(expected);
-  });
-
   it('should throw when encoding non array input', () => {
     const coder = new VecCoder(new BooleanCoder());
 
     expect(() => {
       coder.encode('Nope' as never);
     }).toThrow('expected array value');
-  });
-
-  it('should throw when getting encoded vector data with non array input', () => {
-    const coder = new VecCoder(new BooleanCoder());
-
-    expect(() => {
-      coder.getEncodedVectorData('Nope' as never);
-    }).toThrow('expected array value');
-  });
-
-  it('should get base offset', () => {
-    const expected = 24;
-    const actual = VecCoder.getBaseOffset();
-
-    expect(actual).toBe(expected);
   });
 
   it('should throw an error when decoding', () => {
