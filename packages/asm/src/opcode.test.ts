40e5031604070bcc77ee53689c5a87b381b2ba8b
Torres-ssf <sergio.uft@gmail.com>
2023-06-19 10:01:58 -0300

    add helper for opcode unit test

--- packages/asm/src/opcode.test.ts
+++ packages/asm/src/opcode.test.ts
@@ -4,6 +4,25 @@ import { OpcodeRepr } from './constants';
 import { Opcode } from './opcode';
 
 describe('opcode', () => {
+  const getExpectedOpcodeInstruction = (
+    opCode: number,
+    registers: number[] = [],
+    immediates: number[] = []
+  ) => {
+    let instruction = registers.reduce((acc, register, index) => {
+      const expectedRegister = register << (18 - index * 6);
+      const expected = acc | expectedRegister;
+      return expected;
+    }, opCode << 24);
+
+    instruction = immediates.reduce((acc, immediate) => {
+      const expected = acc | immediate;
+      return expected;
+    }, instruction);
+
+    return instruction;
+  };
+
   it('should correctly convert instruction to byte array', () => {
     const instruction = 1234;
     const opcode = new Opcode(instruction);
@@ -19,7 +38,7 @@ describe('opcode', () => {
   });
 
   it('should correctly generate the opcode for a "noop" command', () => {
-    const expectedValue = OpcodeRepr.NOOP << 24;
+    const expectedValue = getExpectedOpcodeInstruction(OpcodeRepr.NOOP);
 
     const opcode = Opcode.noop();
 
@@ -27,200 +46,135 @@ describe('opcode', () => {
   });
 
   it('should correctly generate the opcode for a "ecr" command', () => {
-    const registerIdA = 10;
-    const registerIdB = 20;
-    const RegisterIdC = 30;
+    const registers: [number, number, number] = [10, 20, 30];
 
-    const expectedCode = OpcodeRepr.ECR << 24;
-    const expectedRegisterA = registerIdA << 18;
-    const expectedRegisterB = registerIdB << 12;
-    const expectedRegisterC = RegisterIdC << 6;
-    const expectedValue = expectedCode | expectedRegisterA | expectedRegisterB | expectedRegisterC;
+    const expectedValue = getExpectedOpcodeInstruction(OpcodeRepr.ECR, registers);
 
-    const opcode = Opcode.ecr(registerIdA, registerIdB, RegisterIdC);
+    const opcode = Opcode.ecr(...registers);
 
     expect(opcode.instruction).toBe(expectedValue);
   });
 
   it('should correctly generate the opcode for a "smo" command', () => {
-    const registerIdA = 1;
-    const registerIdB = 5;
-    const RegisterIdC = 10;
-    const RegisterIdD = 20;
+    const registers: [number, number, number, number] = [1, 5, 10, 20];
 
-    const expectedCode = OpcodeRepr.SMO << 24;
-    const expectedRegisterA = registerIdA << 18;
-    const expectedRegisterB = registerIdB << 12;
-    const expectedRegisterC = RegisterIdC << 6;
-    const expectedRegisterD = RegisterIdD;
-    const expectedValue =
-      expectedCode | expectedRegisterA | expectedRegisterB | expectedRegisterC | expectedRegisterD;
+    const expectedValue = getExpectedOpcodeInstruction(OpcodeRepr.SMO, registers);
 
-    const opcode = Opcode.smo(registerIdA, registerIdB, RegisterIdC, RegisterIdD);
+    const opcode = Opcode.smo(...registers);
 
     expect(opcode.instruction).toBe(expectedValue);
   });
 
   it('should correctly generate the opcode for a "log" command', () => {
-    const registerIdA = 10;
-    const registerIdB = 20;
-    const RegisterIdC = 30;
-    const RegisterIdD = 40;
+    const registers: [number, number, number, number] = [10, 20, 30, 40];
 
-    const expectedCode = OpcodeRepr.LOG << 24;
-    const expectedRegisterA = registerIdA << 18;
-    const expectedRegisterB = registerIdB << 12;
-    const expectedRegisterC = RegisterIdC << 6;
-    const expectedRegisterD = RegisterIdD;
-    const expectedValue =
-      expectedCode | expectedRegisterA | expectedRegisterB | expectedRegisterC | expectedRegisterD;
+    const expectedValue = getExpectedOpcodeInstruction(OpcodeRepr.LOG, registers);
 
-    const opcode = Opcode.log(registerIdA, registerIdB, RegisterIdC, RegisterIdD);
+    const opcode = Opcode.log(...registers);
 
     expect(opcode.instruction).toBe(expectedValue);
   });
 
   it('should correctly generate the opcode for a "aloc" command', () => {
-    const registerIdA = 10;
+    const register = 30;
 
-    const expectedCode = OpcodeRepr.ALOC << 24;
-    const expectedRegisterA = registerIdA << 18;
+    const expectedValue = getExpectedOpcodeInstruction(OpcodeRepr.ALOC, [register]);
 
-    const expectedValue = expectedCode | expectedRegisterA;
-
-    const opcode = Opcode.aloc(registerIdA);
+    const opcode = Opcode.aloc(register);
 
     expect(opcode.instruction).toBe(expectedValue);
   });
 
   it('should correctly generate the opcode for a "lw" command', () => {
-    const registerIdA = 10;
-    const registerIdB = 20;
-    const immediate12 = 12;
+    const registers: [number, number] = [10, 20];
+    const immediates: [number] = [12];
 
-    const expectedCode = OpcodeRepr.LW << 24;
-    const expectedRegisterA = registerIdA << 18;
-    const expectedRegisterB = registerIdB << 12;
-    const expectedValue = expectedCode | expectedRegisterA | expectedRegisterB | immediate12;
+    const expectedValue = getExpectedOpcodeInstruction(OpcodeRepr.LW, registers, immediates);
 
-    const opcode = Opcode.lw(registerIdA, registerIdB, immediate12);
+    const opcode = Opcode.lw(...registers, ...immediates);
 
     expect(opcode.instruction).toBe(expectedValue);
   });
 
   it('should correctly generate the opcode for a "retd" command', () => {
-    const registerIdA = 10;
-    const registerIdB = 20;
+    const registers: [number, number] = [15, 30];
 
-    const expectedCode = OpcodeRepr.RETD << 24;
-    const expectedRegisterA = registerIdA << 18;
-    const expectedRegisterB = registerIdB << 12;
-    const expectedValue = expectedCode | expectedRegisterA | expectedRegisterB;
+    const expectedValue = getExpectedOpcodeInstruction(OpcodeRepr.RETD, registers);
 
-    const opcode = Opcode.retd(registerIdA, registerIdB);
+    const opcode = Opcode.retd(...registers);
 
     expect(opcode.instruction).toBe(expectedValue);
   });
 
   it('should correctly generate the opcode for a "ret" command', () => {
-    const registerIdA = 10;
+    const register: [number] = [10];
 
-    const expectedCode = OpcodeRepr.RET << 24;
-    const expectedRegisterA = registerIdA << 18;
-    const expectedValue = expectedCode | expectedRegisterA;
+    const expectedValue = getExpectedOpcodeInstruction(OpcodeRepr.RET, register);
 
-    const opcode = Opcode.ret(registerIdA);
+    const opcode = Opcode.ret(...register);
 
     expect(opcode.instruction).toBe(expectedValue);
   });
 
   it('should correctly generate the opcode for a "slli" command', () => {
-    const registerIdA = 10;
-    const registerIdB = 20;
-    const immediate12 = 12;
+    const registers: [number, number] = [15, 30];
+    const immediate: [number] = [12];
 
-    const expectedCode = OpcodeRepr.SLLI << 24;
-    const expectedRegisterA = registerIdA << 18;
-    const expectedRegisterB = registerIdB << 12;
-    const expectedValue = expectedCode | expectedRegisterA | expectedRegisterB | immediate12;
+    const expectedValue = getExpectedOpcodeInstruction(OpcodeRepr.SLLI, registers, immediate);
 
-    const opcode = Opcode.slli(registerIdA, registerIdB, immediate12);
+    const opcode = Opcode.slli(...registers, ...immediate);
 
     expect(opcode.instruction).toBe(expectedValue);
   });
 
   it('should correctly generate the opcode for a "move" command', () => {
-    const registerIdA = 10;
-    const registerIdB = 20;
+    const registers: [number, number] = [12, 24];
 
-    const expectedCode = OpcodeRepr.MOVE << 24;
-    const expectedRegisterA = registerIdA << 18;
-    const expectedRegisterB = registerIdB << 12;
-    const expectedValue = expectedCode | expectedRegisterA | expectedRegisterB;
+    const expectedValue = getExpectedOpcodeInstruction(OpcodeRepr.MOVE, registers);
 
-    const opcode = Opcode.move(registerIdA, registerIdB);
+    const opcode = Opcode.move(...registers);
 
     expect(opcode.instruction).toBe(expectedValue);
   });
 
   it('should correctly generate the opcode for a "addi" command', () => {
-    const registerIdA = 10;
-    const registerIdB = 20;
-    const immediate12 = 12;
+    const registers: [number, number] = [12, 24];
+    const immediate: [number] = [12];
 
-    const expectedCode = OpcodeRepr.ADDI << 24;
-    const expectedRegisterA = registerIdA << 18;
-    const expectedRegisterB = registerIdB << 12;
-    const expectedValue = expectedCode | expectedRegisterA | expectedRegisterB | immediate12;
+    const expectedValue = getExpectedOpcodeInstruction(OpcodeRepr.ADDI, registers, immediate);
 
-    const opcode = Opcode.addi(registerIdA, registerIdB, immediate12);
+    const opcode = Opcode.addi(...registers, ...immediate);
 
     expect(opcode.instruction).toBe(expectedValue);
   });
 
   it('should correctly generate the opcode for a "add" command', () => {
-    const registerIdA = 10;
-    const registerIdB = 20;
-    const RegisterIdC = 30;
+    const registers: [number, number, number] = [4, 9, 12];
 
-    const expectedCode = OpcodeRepr.ADD << 24;
-    const expectedRegisterA = registerIdA << 18;
-    const expectedRegisterB = registerIdB << 12;
-    const expectedRegisterC = RegisterIdC << 6;
-    const expectedValue = expectedCode | expectedRegisterA | expectedRegisterB | expectedRegisterC;
+    const expectedValue = getExpectedOpcodeInstruction(OpcodeRepr.ADD, registers);
 
-    const opcode = Opcode.add(registerIdA, registerIdB, RegisterIdC);
+    const opcode = Opcode.add(...registers);
 
     expect(opcode.instruction).toBe(expectedValue);
   });
 
   it('should correctly generate the opcode for a "gtf" command', () => {
-    const registerIdA = 10;
-    const registerIdB = 20;
-    const immediate12 = 12;
+    const registers: [number, number] = [2, 5];
+    const immediate: [number] = [12];
 
-    const expectedCode = OpcodeRepr.GTF << 24;
-    const expectedRegisterA = registerIdA << 18;
-    const expectedRegisterB = registerIdB << 12;
-    const expectedValue = expectedCode | expectedRegisterA | expectedRegisterB | immediate12;
+    const expectedValue = getExpectedOpcodeInstruction(OpcodeRepr.GTF, registers, immediate);
 
-    const opcode = Opcode.gtf(registerIdA, registerIdB, immediate12);
+    const opcode = Opcode.gtf(...registers, ...immediate);
 
     expect(opcode.instruction).toBe(expectedValue);
   });
 
   it('should correctly generate the opcode for a "tr" command', () => {
-    const registerIdA = 10;
-    const registerIdB = 20;
-    const RegisterIdC = 30;
+    const registers: [number, number, number] = [2, 5, 14];
 
-    const expectedCode = OpcodeRepr.TR << 24;
-    const expectedRegisterA = registerIdA << 18;
-    const expectedRegisterB = registerIdB << 12;
-    const expectedRegisterC = RegisterIdC << 6;
-    const expectedValue = expectedCode | expectedRegisterA | expectedRegisterB | expectedRegisterC;
+    const expectedValue = getExpectedOpcodeInstruction(OpcodeRepr.TR, registers);
 
-    const opcode = Opcode.tr(registerIdA, registerIdB, RegisterIdC);
+    const opcode = Opcode.tr(...registers);
 
     expect(opcode.instruction).toBe(expectedValue);
   });
