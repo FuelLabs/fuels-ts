dd0a395687431671bb7e151a5028a486e67462de
danielbate <--global>
2023-06-21 10:19:44 +0100

    refactor: refactor transaction request into seperate files

---
+++ packages/providers/src/transaction-request/create-transaction-request.ts
@@ -0,0 +1,81 @@
+import type { BytesLike } from '@ethersproject/bytes';
+import { hexlify } from '@ethersproject/bytes';
+import { ZeroBytes32 } from '@fuel-ts/address/configs';
+import { TransactionType, OutputType, TransactionCreate } from '@fuel-ts/transactions';
+import { BaseTransactionRequestLike, BaseTransactionRequest } from './transaction-request';
+import { ContractCreatedTransactionRequestOutput } from './output';
+import { TransactionRequestStorageSlot, storageSlotify } from './storage-slot';
+
+export interface CreateTransactionRequestLike extends BaseTransactionRequestLike {
+  /** Witness index of contract bytecode to create */
+  bytecodeWitnessIndex?: number;
+  /** Salt */
+  salt?: BytesLike;
+  /** List of storage slots to initialize */
+  storageSlots?: TransactionRequestStorageSlot[];
+}
+
+export class CreateTransactionRequest extends BaseTransactionRequest {
+  static from(obj: CreateTransactionRequestLike) {
+    if (obj instanceof this) {
+      return obj;
+    }
+    return new this(obj);
+  }
+
+  /** Type of the transaction */
+  type = TransactionType.Create as const;
+  /** Witness index of contract bytecode to create */
+  bytecodeWitnessIndex: number;
+  /** Salt */
+  salt: string;
+  /** List of storage slots to initialize */
+  storageSlots: TransactionRequestStorageSlot[];
+
+  constructor({
+    bytecodeWitnessIndex,
+    salt,
+    storageSlots,
+    ...rest
+  }: CreateTransactionRequestLike = {}) {
+    super(rest);
+    this.bytecodeWitnessIndex = bytecodeWitnessIndex ?? 0;
+    this.salt = hexlify(salt ?? ZeroBytes32);
+    this.storageSlots = [...(storageSlots ?? [])];
+  }
+
+  toTransaction(): TransactionCreate {
+    const baseTransaction = this.getBaseTransaction();
+    const bytecodeWitnessIndex = this.bytecodeWitnessIndex;
+    const storageSlots = this.storageSlots?.map(storageSlotify) ?? [];
+    return {
+      type: TransactionType.Create,
+      ...baseTransaction,
+      bytecodeLength: baseTransaction.witnesses[bytecodeWitnessIndex].dataLength / 4,
+      bytecodeWitnessIndex,
+      storageSlotsCount: storageSlots.length,
+      salt: this.salt ? hexlify(this.salt) : ZeroBytes32,
+      storageSlots,
+    };
+  }
+
+  getContractCreatedOutputs(): ContractCreatedTransactionRequestOutput[] {
+    return this.outputs.filter(
+      (output): output is ContractCreatedTransactionRequestOutput =>
+        output.type === OutputType.ContractCreated
+    );
+  }
+
+  addContractCreatedOutput(
+    /** Contract ID */
+    contractId: BytesLike,
+    /** State Root */
+    stateRoot: BytesLike
+  ) {
+    this.pushOutput({
+      type: OutputType.ContractCreated,
+      contractId,
+      stateRoot,
+    });
+  }
+}
--- packages/providers/src/transaction-request/index.ts
+++ packages/providers/src/transaction-request/index.ts
@@ -1,3 +1,7 @@
 export * from './input';
 export * from './output';
 export * from './transaction-request';
+export * from './create-transaction-request';
+export * from './script-transaction-request';
+export * from './errors';
+export * from './scripts';
