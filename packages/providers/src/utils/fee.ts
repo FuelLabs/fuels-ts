7d5907de6275a701a857c5744d772292d9e82e3d
Torres-ssf <sergio.uft@gmail.com>
2023-07-16 19:52:29 -0300

    update getGasUsedFromReceipts function

--- packages/providers/src/utils/fee.ts
+++ packages/providers/src/utils/fee.ts
@@ -3,19 +3,22 @@ import { bn, multiply } from '@fuel-ts/math';
 import { ReceiptType } from '@fuel-ts/transactions';
 import { GAS_PRICE_FACTOR } from '@fuel-ts/transactions/configs';
 
-import type { TransactionResultReceipt } from '../transaction-response';
+import type {
+  TransactionResultReceipt,
+  TransactionResultScriptResultReceipt,
+} from '../transaction-response';
 
 export const calculatePriceWithFactor = (gasUsed: BN, gasPrice: BN, priceFactor: BN): BN =>
   bn(Math.ceil(gasUsed.toNumber() / priceFactor.toNumber()) * gasPrice.toNumber());
 
 export const getGasUsedFromReceipts = (receipts: Array<TransactionResultReceipt>): BN => {
-  const scriptResult = receipts.find((receipt) => receipt.type === ReceiptType.ScriptResult);
+  const scriptResult = receipts.filter(
+    (receipt) => receipt.type === ReceiptType.ScriptResult
+  ) as TransactionResultScriptResultReceipt[];
 
-  if (scriptResult && scriptResult.type === ReceiptType.ScriptResult) {
-    return scriptResult.gasUsed;
-  }
+  const gasUsed = scriptResult.reduce((prev, receipt) => prev.add(receipt.gasUsed), bn(0));
 
-  return bn(0);
+  return gasUsed;
 };
 
 export const calculateTransactionFee = ({
