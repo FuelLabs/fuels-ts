c3dc181621b92c140c465c1d36696f0c13c3b38d
Dhaiwat / dhai.eth <dhaiwatpandya@gmail.com>
2023-07-25 18:48:43 +0530

    fix import

--- packages/example-contract/src/example-contract.test.ts
+++ packages/example-contract/src/example-contract.test.ts
@@ -1,7 +1,7 @@
 // #region typedoc:Testing-with-jest
 import { generateTestWallet } from '@fuel-ts/wallet/test-utils';
 import fs from 'fs';
-import { ContractFactory, Provider, toHex, NativeAssetId } from 'fuels';
+import { ContractFactory, Provider, toHex, BaseAssetId } from 'fuels';
 import path from 'path';
 
 import { ExampleContractAbi__factory } from './example-contract-types';
@@ -9,7 +9,7 @@ import { ExampleContractAbi__factory } from './example-contract-types';
 describe('ExampleContract', () => {
   it('should return the input', async () => {
     const provider = new Provider('http://127.0.0.1:4000/graphql');
-    const wallet = await generateTestWallet(provider, [[1_000, NativeAssetId]]);
+    const wallet = await generateTestWallet(provider, [[1_000, BaseAssetId]]);
 
     // Deploy
     const bytecode = fs.readFileSync(path.join(__dirname, '../out/debug/example-contract.bin'));
