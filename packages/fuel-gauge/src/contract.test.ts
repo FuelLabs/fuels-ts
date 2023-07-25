34e0814b6ebeeec66b80f0839ef59c8217233048
Torres-ssf <sergio.uft@gmail.com>
2023-07-25 08:25:44 -0300

    remove unused dummy

--- packages/fuel-gauge/src/contract.test.ts
+++ packages/fuel-gauge/src/contract.test.ts
@@ -38,59 +38,6 @@ const setupContract = createSetupConfig({
   abi: abiJSON,
 });
 
-export const jsonAbiFragmentMock: JsonAbi = {
-  configurables: [],
-  loggedTypes: [],
-  types: [
-    {
-      typeId: 0,
-      type: 'bool',
-      components: null,
-      typeParameters: null,
-    },
-    {
-      typeId: 1,
-      type: 'u64',
-      components: null,
-      typeParameters: null,
-    },
-    {
-      typeId: 2,
-      type: 'struct MyStruct',
-      components: [
-        {
-          type: 0,
-          name: 'arg_one',
-          typeArguments: null,
-        },
-        {
-          type: 1,
-          name: 'arg_two',
-          typeArguments: null,
-        },
-      ],
-      typeParameters: null,
-    },
-  ],
-  functions: [
-    {
-      name: 'main',
-      inputs: [
-        {
-          name: 'my_struct',
-          type: 2,
-          typeArguments: null,
-        },
-      ],
-      output: {
-        name: 'my_struct',
-        type: 2,
-        typeArguments: null,
-      },
-      attributes: [],
-    },
-  ],
-};
 const jsonFragment: JsonAbi = {
   configurables: [],
   loggedTypes: [],
