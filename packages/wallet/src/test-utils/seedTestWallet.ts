23f036db38d84a6fa66a342bdaef15e696b1267c
Luiz Estácio | stacio.eth <luizstacio@gmail.com>
2023-07-26 15:19:55 +0100

    fix: predicate estimation for inputs with predicate field (#1130)

--- packages/wallet/src/test-utils/seedTestWallet.ts
+++ packages/wallet/src/test-utils/seedTestWallet.ts
@@ -2,9 +2,10 @@ import { randomBytes } from '@fuel-ts/keystore';
 import { coinQuantityfy, ScriptTransactionRequest } from '@fuel-ts/providers';
 import type { CoinQuantityLike } from '@fuel-ts/providers';
 
+import type { Account } from '../account';
 import { WalletUnlocked } from '../wallets';
 
-export const seedTestWallet = async (wallet: WalletUnlocked, quantities: CoinQuantityLike[]) => {
+export const seedTestWallet = async (wallet: Account, quantities: CoinQuantityLike[]) => {
   const genesisWallet = new WalletUnlocked(
     process.env.GENESIS_SECRET || randomBytes(32),
     wallet.provider
