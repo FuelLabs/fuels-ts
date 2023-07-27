435077b003909094479fb52ba9807ab1b394cbd6
Torres-ssf <sergio.uft@gmail.com>
2023-07-27 08:24:44 -0300

    ensure crypto functions conform with CryptoApi interface

---
+++ packages/crypto/src/node/cryptoWrapper.ts
@@ -0,0 +1,15 @@
+import type { CryptoApi } from '../types';
+
+import { bufferFromString, stringFromBuffer, decrypt, encrypt, keyFromPassword } from './aes-ctr';
+import { randomBytes } from './randomBytes';
+
+export class CryptoWrapper implements CryptoApi {
+  bufferFromString = bufferFromString;
+  stringFromBuffer = stringFromBuffer;
+  decrypt = decrypt;
+  encrypt = encrypt;
+  keyFromPassword = keyFromPassword;
+  randomBytes = randomBytes;
+}
+
+export default new CryptoWrapper();
