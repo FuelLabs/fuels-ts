4d8ee182a9cedfc43cf53c3c70017560a32b7b5e
Torres-ssf <sergio.uft@gmail.com>
2023-07-28 11:37:35 -0300

    move status related code to its own function

--- packages/providers/src/transaction-summary/assemble-transaction-summary.ts
+++ packages/providers/src/transaction-summary/assemble-transaction-summary.ts
@@ -12,29 +12,8 @@ import {
   isTypeCreate,
   isTypeScript,
 } from './operations';
-import type {
-  BlockId,
-  GqlTransactionStatusesNames,
-  GraphqlTransactionStatus,
-  Time,
-  TransactionSummary,
-} from './types';
-import { SimplifiedTransactionStatusNameEnum } from './types';
-
-export const getTransactionStatusName = (gqlStatus: GqlTransactionStatusesNames) => {
-  switch (gqlStatus) {
-    case 'FailureStatus':
-      return SimplifiedTransactionStatusNameEnum.failure;
-    case 'SuccessStatus':
-      return SimplifiedTransactionStatusNameEnum.success;
-    case 'SubmittedStatus':
-      return SimplifiedTransactionStatusNameEnum.submitted;
-    case 'SqueezedOutStatus':
-      return SimplifiedTransactionStatusNameEnum.squeezedout;
-    default:
-      throw new Error('Unknown transaction status');
-  }
-};
+import { processGraphqlStatus } from './status';
+import type { GraphqlTransactionStatus, TransactionSummary } from './types';
 
 export function assembleTransactionSummary<TTransactionType = void>(params: {
   id?: string;
@@ -78,37 +57,8 @@ export function assembleTransactionSummary<TTransactionType = void>(params: {
 
   const typeName = getTransactionTypeName(transaction.type);
 
-  let time: Time;
-  let blockId: BlockId | undefined;
-  let status: SimplifiedTransactionStatusNameEnum | undefined;
-
-  let isStatusFailure = false;
-  let isStatusSuccess = false;
-  let isStatusPending = false;
-
-  if (gqlTransactionStatus?.type) {
-    status = getTransactionStatusName(gqlTransactionStatus.type);
-
-    switch (gqlTransactionStatus.type) {
-      case 'SuccessStatus':
-        time = gqlTransactionStatus.time;
-        blockId = gqlTransactionStatus.block.id;
-        isStatusSuccess = true;
-        break;
-
-      case 'FailureStatus':
-        time = gqlTransactionStatus.time;
-        blockId = gqlTransactionStatus.block.id;
-        isStatusFailure = true;
-        break;
-
-      case 'SubmittedStatus':
-        time = gqlTransactionStatus.time;
-        isStatusPending = true;
-        break;
-      default:
-    }
-  }
+  const { isStatusFailure, isStatusPending, isStatusSuccess, blockId, status, time } =
+    processGraphqlStatus(gqlTransactionStatus);
 
   const transactionSummary: TransactionSummary<TTransactionType> = {
     id,
