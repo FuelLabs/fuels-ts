8735828008f9b58c895b3e33267bee837b01c2af
Anderson Arboleya <anderson@arboleya.me>
2023-07-31 13:23:48 -0300

    Adding extra Generic type for greater flexibility

--- packages/utils/src/test-utils/safeExec.ts
+++ packages/utils/src/test-utils/safeExec.ts
@@ -1,11 +1,15 @@
-export const safeExec = async <T>(lambda: () => unknown) => {
-  let error: Error | undefined;
-  let result: T | undefined;
+import { FuelError } from 'fuels-ts/.history/packages/errors/src/index_20230731034941';
+
+export const safeExec = async <TResult = unknown, TError extends Error = Error>(
+  lambda: () => unknown
+) => {
+  let error: TError | undefined;
+  let result: TResult | undefined;
 
   try {
-    result = (await lambda()) as T;
+    result = (await lambda()) as TResult;
   } catch (_error) {
-    error = _error as unknown as Error;
+    error = _error as unknown as TError;
   }
 
   return { error, result };
