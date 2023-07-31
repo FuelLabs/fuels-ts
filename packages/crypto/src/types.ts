2b7221f645f46967754dd97a37a93fe0d5eca2a4
Torres-ssf <sergio.uft@gmail.com>
2023-07-31 11:01:11 -0300

    add scrypt method to CryptoApi interface

--- packages/crypto/src/types.ts
+++ packages/crypto/src/types.ts
@@ -4,6 +4,15 @@ export interface Keystore {
   salt: string;
 }
 
+export interface IScryptParams {
+  password: Uint8Array;
+  salt: Uint8Array;
+  n: number;
+  p: number;
+  r: number;
+  dklen: number;
+}
+
 export interface CryptoApi {
   bufferFromString(string: string, encoding?: 'utf-8' | 'base64'): Uint8Array;
   decrypt<T>(password: string, keystore: Keystore): Promise<T>;
@@ -11,4 +20,5 @@ export interface CryptoApi {
   keyFromPassword(password: string, saltBuffer: Uint8Array): Uint8Array;
   stringFromBuffer(buffer: Uint8Array, encoding?: 'utf-8' | 'base64'): string;
   randomBytes(length: number): Uint8Array;
+  scrypt(params: IScryptParams): Uint8Array;
 }
