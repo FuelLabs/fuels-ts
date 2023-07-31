19cbf0c7b66280929a9a097ac34e2df51169cfab
Anderson Arboleya <anderson@arboleya.me>
2023-07-31 13:57:41 -0300

    Tyop

--- packages/errors/src/test-utils/expect-to-throw-fuel-error.test.ts
+++ packages/errors/src/test-utils/expect-to-throw-fuel-error.test.ts
@@ -4,8 +4,8 @@ import { expectToThrowFuelError } from './expect-to-throw-fuel-error';
 
 
 describe('FuelError', () => {
-  it('shold compare whole or partial error objects', () => {
-    const error = new FuelError(FuelError.CODES.PARSE_FAILED, 'asdf')
+  it('should compare whole or partial error objects', () => {
+    const error = new FuelError(FuelError.CODES.PARSE_FAILED, 'It happens')
     expectToThrowFuelError(() => {throw error}, error)
     expectToThrowFuelError(() => {throw error}, { code: FuelError.CODES.PARSE_FAILED })
   });
