f1c494d039ff7a8f9fb825ee1f36daf17d814c29
nedsalk <nedim.salkic@fuel.sh>
2023-08-01 12:58:49 +0200

    test: partial or full equality with expected error

--- packages/errors/src/test-utils/expect-to-throw-fuel-error.ts
+++ packages/errors/src/test-utils/expect-to-throw-fuel-error.ts
@@ -1,17 +1,20 @@
 import { safeExec } from '@fuel-ts/utils/test-utils';
 
-import type { ErrorCode, FuelError } from '../index';
+import { FuelError } from '../index';
 
 export const expectToThrowFuelError = async (
   lambda: () => unknown,
-  expectedError: FuelError | { code: ErrorCode, message?: string}
+  expectedError: FuelError | (Partial<FuelError> & Required<Pick<FuelError, 'code'>>)
 ) => {
   const { error } = await safeExec<unknown, FuelError>(lambda);
-
-  expect(error?.code).toEqual(expectedError.code)
-  expect(error?.name).toEqual('FuelError')
-
-  if (expectedError?.message) {
-    expect(error?.message).toEqual(expectedError.message)
+  if (error === undefined) {
+    // this is a safeguard to not
+    expect(error).toBeDefined();
+    return;
   }
+  expect(error).toBeInstanceOf(FuelError);
+  expect(error.code).toEqual(expectedError.code);
+  (Object.keys(expectedError) as Array<keyof typeof expectedError>).forEach((key) => {
+    expect(expectedError[key]).toStrictEqual(error[key]);
+  });
 };
