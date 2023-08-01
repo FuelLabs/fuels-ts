009105c46c76d7cbfc2a84c709396f3511230ebd
nedsalk <nedim.salkic@fuel.sh>
2023-08-01 14:06:10 +0200

    refactor: type inference

--- packages/utils/src/test-utils/safeExec.ts
+++ packages/utils/src/test-utils/safeExec.ts
@@ -1,13 +1,13 @@
 export const safeExec = async <TResult = unknown, TError extends Error = Error>(
-  lambda: () => unknown
+  lambda: () => TResult
 ) => {
   let error: TError | undefined;
   let result: TResult | undefined;
 
   try {
-    result = (await lambda()) as TResult;
-  } catch (_error) {
-    error = _error as unknown as TError;
+    result = await lambda();
+  } catch (_error: unknown) {
+    error = _error as TError;
   }
 
   return { error, result };
