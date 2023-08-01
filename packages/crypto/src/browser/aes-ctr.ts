5413c9ad74a1953cb8689c3544c4ebe5422eaf66
Torres-ssf <sergio.uft@gmail.com>
2023-08-01 12:31:33 -0300

    made browser stringFromBuffer support hex encoding

--- packages/crypto/src/browser/aes-ctr.ts
+++ packages/crypto/src/browser/aes-ctr.ts
@@ -42,8 +42,32 @@ export const bufferFromString: CryptoApi['bufferFromString'] = (
 
 export const stringFromBuffer: CryptoApi['stringFromBuffer'] = (
   buffer: Uint8Array,
-  _encoding: 'utf-8' | 'base64' = 'base64'
-): string => btoa(String.fromCharCode.apply(null, new Uint8Array(buffer) as unknown as number[]));
+  encoding: Encoding = 'base64'
+): string => {
+  switch (encoding) {
+    case 'utf-8': {
+      return new TextDecoder().decode(buffer);
+    }
+    case 'base64': {
+      let binary = '';
+      const len = buffer.byteLength;
+      for (let i = 0; i < len; i += 1) {
+        binary += String.fromCharCode(buffer[i]);
+      }
+      return btoa(binary);
+    }
+
+    case 'hex':
+    default: {
+      let hexString = '';
+      for (let i = 0; i < buffer.length; i += 1) {
+        const hex = buffer[i].toString(16);
+        hexString += hex.length === 1 ? `0${hex}` : hex;
+      }
+      return hexString;
+    }
+  }
+};
 
 /**
  * Generate a pbkdf2 key from a password and random salt
