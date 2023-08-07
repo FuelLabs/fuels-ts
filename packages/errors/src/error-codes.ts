faab9a84503dec563012da847a26b87ae9024ee5
nedsalk <nedim.salkic@fuel.sh>
2023-08-07 15:54:49 +0200

    docs: added typedoc example to ErrorCode

--- packages/errors/src/error-codes.ts
+++ packages/errors/src/error-codes.ts
@@ -1,3 +1,7 @@
+/**
+ * Represents the different error codes thrown by the SDK
+ * @enum
+ */
 export enum ErrorCode {
   // address
   INVALID_BECH32_ADDRESS = 'invalid-bech32-address',
@@ -6,9 +10,13 @@ export enum ErrorCode {
   INVALID_URL = 'invalid-url',
 
   // wallet
+  /**
+   * The wallet doesn't have enough assets to cover the requirements of the transaction
+   */
   INSUFFICIENT_BALANCE = 'insufficient-balance',
 
   // errors
+
   PARSE_FAILED = 'parse-failed',
 
   // coder
