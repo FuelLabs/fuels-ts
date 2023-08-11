928239116d2b4f3d4b4357b59b1064b6f38cd159
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2023-08-11 09:31:42 -0300

    feat: support encrypt and decrypt json wallets (#1041)

---
+++ packages/crypto/src/shared/keccak256.test.ts
@@ -0,0 +1,20 @@
+import * as ethereumCryptography from 'ethereum-cryptography/keccak';
+
+import { bufferFromString } from '..';
+
+import { keccak256 } from './keccak256';
+
+describe('keccak256', () => {
+  afterEach(jest.restoreAllMocks);
+
+  it('hashes using keccak256', () => {
+    const data = bufferFromString('hashedKey');
+
+    const mock = jest.spyOn(ethereumCryptography, 'keccak256').mockImplementationOnce(() => data);
+
+    const hashedKey = keccak256(data);
+
+    expect(mock).toBeCalledTimes(1);
+    expect(hashedKey).toEqual(data);
+  });
+});
