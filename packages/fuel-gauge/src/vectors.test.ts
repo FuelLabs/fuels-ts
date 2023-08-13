ff75e54480ecfcd0268f420c3cf1f1f42bdeb68c
Cameron Manavian <cameron.manavian@fuel.sh>
2023-08-12 22:47:49 -0700

    all numerics pass

--- packages/fuel-gauge/src/vectors.test.ts
+++ packages/fuel-gauge/src/vectors.test.ts
@@ -17,4 +17,28 @@ describe('Vector Tests', () => {
 
     expect(value.map((num) => bn(num).toNumber())).toStrictEqual(INPUT);
   });
+
+  it('should test u16 vector input/output', async () => {
+    const INPUT = [8, 6, 7, 5, 3, 0, 9];
+
+    const { value } = await contractInstance.functions.echo_u16(INPUT).call<BN[]>();
+
+    expect(value.map((num) => bn(num).toNumber())).toStrictEqual(INPUT);
+  });
+
+  it('should test u32 vector input/output', async () => {
+    const INPUT = [8, 6, 7, 5, 3, 0, 9];
+
+    const { value } = await contractInstance.functions.echo_u32(INPUT).call<BN[]>();
+
+    expect(value.map((num) => bn(num).toNumber())).toStrictEqual(INPUT);
+  });
+
+  it('should test u64 vector input/output', async () => {
+    const INPUT = [8, 6, 7, 5, 3, 0, 9];
+
+    const { value } = await contractInstance.functions.echo_u64(INPUT).call<BN[]>();
+
+    expect(value.map((num) => bn(num).toNumber())).toStrictEqual(INPUT);
+  });
 });
