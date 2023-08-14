7fdb3c74c7940e1f756fac0cc62d7772f2b57ac2
nedsalk <nedim.salkic@fuel.sh>
2023-08-14 13:55:02 +0200

    test: added failing tests to CI

---
+++ packages/address/src/ungraceful-fail.test.ts
@@ -0,0 +1,16 @@
+import { FuelError } from '@fuel-ts/errors';
+import { expectToThrowFuelError } from '@fuel-ts/errors/test-utils';
+import type { Bech32Address } from '@fuel-ts/interfaces';
+
+import * as utils from './utils';
+
+const ADDRESS_B256 = '0xef86afa9696cf0dc6385e2c407a6e159a1103cefb7e2ae0636fb33d3cb2a9e4a';
+
+test('toB256 (b256 to b256) (ungraceful fail)', () => {
+  const address = ADDRESS_B256 as Bech32Address;
+  const expectedError = new FuelError(
+    FuelError.CODES.INVALID_BECH32_ADDRESS,
+    `Message that fails: ${address}`
+  );
+  expectToThrowFuelError(() => utils.toB256(address), expectedError);
+});
