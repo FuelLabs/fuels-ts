271d9de3c0e75699222529587d1c72116f4b254d
Cameron Manavian <cameron.manavian@fuel.sh>
2023-08-14 08:41:23 -0700

    refactor test

--- packages/fuel-gauge/src/coverage-contract.test.ts
+++ packages/fuel-gauge/src/coverage-contract.test.ts
@@ -528,19 +528,19 @@ describe('Coverage Contract', () => {
 
   it('should handle multiple calls, some with vectors', async () => {
     const INPUT_A = [hexlify(randomBytes(32)), hexlify(randomBytes(32)), hexlify(randomBytes(32))];
-    const INPUT_B = [hexlify(randomBytes(32)), hexlify(randomBytes(32)), hexlify(randomBytes(32))];
+    const INPUT_B = [hexlify(randomBytes(32))];
     const INPUT_C = hexlify(randomBytes(32));
     const INPUT_D = hexlify(randomBytes(32));
 
     const { value: results } = await contractInstance
       .multiCall([
         contractInstance.functions.echo_b256_middle(INPUT_A, INPUT_B, INPUT_C, INPUT_D),
-        contractInstance.functions.echo_enum_small(SmallEnum.Empty),
+        contractInstance.functions.echo_u8(13),
+        contractInstance.functions.echo_u8(23),
         contractInstance.functions.echo_enum_small(SmallEnum.Empty),
         contractInstance.functions.echo_b256_middle(INPUT_B, INPUT_A, INPUT_C, INPUT_D),
-        contractInstance.functions.echo_u8(13),
       ])
       .call();
-    expect(results).toStrictEqual([INPUT_B, SmallEnum.Empty, SmallEnum.Empty, INPUT_A, 13]);
+    expect(results).toStrictEqual([INPUT_B, 13, 23, SmallEnum.Empty, INPUT_A]);
   });
 });
