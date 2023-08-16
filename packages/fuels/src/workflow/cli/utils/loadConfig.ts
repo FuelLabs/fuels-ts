2978112ec1a560679a8d9c45789b0ed1fbe7b512
Anderson Arboleya <anderson@arboleya.me>
2023-08-16 15:19:02 -0300

    Reducing indentation levels; aborting method prematurely if necessary

--- packages/fuels/src/workflow/cli/utils/loadConfig.ts
+++ packages/fuels/src/workflow/cli/utils/loadConfig.ts
@@ -20,67 +20,65 @@ export async function loadConfig(cwd: string): Promise<LoadedConfig> {
     packageKey: 'tsup',
   });
 
-  if (configPath) {
-    const esbuildOptions: BuildOptions = {
-      target: 'ES2021',
-      platform: 'node',
-      format: 'esm',
-    };
-    const result = await bundleRequire({
-      filepath: configPath,
-      esbuildOptions,
-      cwd,
-    });
-    const config = result.mod.default;
-
-    await validateConfig(config);
-
-    // Set the base path on loaded config
-    config.basePath = cwd;
-    // Resolve the output path on loaded config
-    config.output = path.resolve(cwd, config.output);
-
-    // If workspace is set, resolve the members of the workspace and
-    // set the contract paths on loaded config
-    if (config.workspace) {
-      config.workspace = path.resolve(cwd, config.workspace);
-      const forcToml = await readForcToml(config.workspace);
-      const members = forcToml.workspace.members.map((member) =>
-        path.resolve(config.workspace, member)
-      );
-      const projectTypes = await Promise.all(
-        members.map(async (m) => ({
-          type: await readSwayType(m),
-          path: m,
-        }))
-      );
-
-      // Set the contracts paths on loaded config
-      config.contracts = projectTypes
-        .filter((pt) => pt.type === SwayType.contract)
-        .map((pt) => pt.path);
-      // Set the predicates paths on loaded config
-      config.predicates = projectTypes
-        .filter((pt) => pt.type === SwayType.predicate)
-        .map((pt) => pt.path);
-      // Set the scripts paths on loaded config
-      config.scripts = projectTypes
-        .filter((pt) => pt.type === SwayType.script)
-        .map((pt) => pt.path);
-      // If workspace is not set, resolve the contract paths
-      // on loaded config
-    } else if (config.contracts || config.scripts || config.predicates) {
-      config.contracts = (config.contracts || []).map((contract: string) =>
-        path.resolve(cwd, contract)
-      );
-      config.scripts = (config.scripts || []).map((script: string) => path.resolve(cwd, script));
-      config.predicates = (config.predicates || []).map((predicate: string) =>
-        path.resolve(cwd, predicate)
-      );
-    }
-
-    return config;
+  if (!configPath) {
+    throw new Error('Config file not found!');
   }
 
-  throw new Error('Config file not found!');
+  const esbuildOptions: BuildOptions = {
+    target: 'ES2021',
+    platform: 'node',
+    format: 'esm',
+  };
+  const result = await bundleRequire({
+    filepath: configPath,
+    esbuildOptions,
+    cwd,
+  });
+  const config = result.mod.default;
+
+  await validateConfig(config);
+
+  // Set the base path on loaded config
+  config.basePath = cwd;
+  // Resolve the output path on loaded config
+  config.output = path.resolve(cwd, config.output);
+
+  // If workspace is set, resolve the members of the workspace and
+  // set the contract paths on loaded config
+  if (config.workspace) {
+    config.workspace = path.resolve(cwd, config.workspace);
+    const forcToml = await readForcToml(config.workspace);
+    const members = forcToml.workspace.members.map((member) =>
+      path.resolve(config.workspace, member)
+    );
+    const projectTypes = await Promise.all(
+      members.map(async (m) => ({
+        type: await readSwayType(m),
+        path: m,
+      }))
+    );
+
+    // Set the contracts paths on loaded config
+    config.contracts = projectTypes
+      .filter((pt) => pt.type === SwayType.contract)
+      .map((pt) => pt.path);
+    // Set the predicates paths on loaded config
+    config.predicates = projectTypes
+      .filter((pt) => pt.type === SwayType.predicate)
+      .map((pt) => pt.path);
+    // Set the scripts paths on loaded config
+    config.scripts = projectTypes.filter((pt) => pt.type === SwayType.script).map((pt) => pt.path);
+    // If workspace is not set, resolve the contract paths
+    // on loaded config
+  } else if (config.contracts || config.scripts || config.predicates) {
+    config.contracts = (config.contracts || []).map((contract: string) =>
+      path.resolve(cwd, contract)
+    );
+    config.scripts = (config.scripts || []).map((script: string) => path.resolve(cwd, script));
+    config.predicates = (config.predicates || []).map((predicate: string) =>
+      path.resolve(cwd, predicate)
+    );
+  }
+
+  return config;
 }
