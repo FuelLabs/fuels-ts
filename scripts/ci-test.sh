87c93af0124eb78c33eb5b61cb98adf20642f3ee
danielbate <--global>
2023-08-17 16:29:54 +0100

    feat: modify ci test script to run env tests independently

--- scripts/ci-test.sh
+++ scripts/ci-test.sh
@@ -2,32 +2,24 @@
 
 pkill fuel-core
 
-pnpm run node:clean
+pnpm node:clean
 
-pnpm run node:run > /dev/null 2>&1 &
+pnpm node:run > /dev/null 2>&1 &
 
 echo "Started Fuel-Core node in background."
 
-pnpm run pretest
-
-pnpm run test:node-coverage
-NODE_TEST_RESULT=$?
-
-echo "Restarting Fuel-Core node."
-
-pnpm run node:clean
-
-pnpm run node:run > /dev/null 2>&1 &
-
-pnpm run test:browser-coverage
-BROWSER_TEST_RESULT=$?
-
-pnpm run test:report-coverage
+if [[ "$*" == *"--browser"* ]]; then
+    pnpm test:browser
+    TEST_RESULT=$?
+else
+    pnpm test:node
+    TEST_RESULT=$?
+fi
 
 echo "Killing Fuel-Core node."
 
 pkill fuel-core
 
-pnpm run node:clean
+pnpm node:clean
 
-exit $NODE_TEST_RESULT && $BROWSER_TEST_RESULT
+exit $TEST_RESULT
