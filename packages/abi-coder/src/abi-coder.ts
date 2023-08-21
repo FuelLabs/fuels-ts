18a73c4039e3ed6b55b6a38ce79ecf5e2438ba1f
Cameron Manavian <cameron.manavian@fuel.sh>
2023-08-20 23:00:32 -0700

    rm byte coder

--- packages/abi-coder/src/abi-coder.ts
+++ packages/abi-coder/src/abi-coder.ts
@@ -7,7 +7,6 @@ import { ArrayCoder } from './coders/array';
 import { B256Coder } from './coders/b256';
 import { B512Coder } from './coders/b512';
 import { BooleanCoder } from './coders/boolean';
-import { ByteCoder } from './coders/byte';
 import { EnumCoder } from './coders/enum';
 import { NumberCoder } from './coders/number';
 import { OptionCoder } from './coders/option';
@@ -61,8 +60,6 @@ export abstract class AbiCoder {
         return new U64Coder();
       case 'bool':
         return new BooleanCoder();
-      case 'byte':
-        return new ByteCoder();
       case 'b256':
         return new B256Coder();
       case 'struct B512':
