6ad92d92d4ead1fc7dc6e7befd7b688567c92578
danielbate <--global>
2023-08-21 15:58:38 +0100

    feat: fixes for the merge coverage script

--- scripts/merge-coverage.ts
+++ scripts/merge-coverage.ts
@@ -1,19 +1,22 @@
-import { readdirSync, renameSync, rmSync } from 'fs';
+import { readdirSync, renameSync, rmSync, lstatSync } from 'fs';
 import { join } from 'path';
 
 const coverageDir = join(__dirname, '../coverage/');
 const environmentsDir = join(coverageDir, '/environments/');
+const validEnvironments = ['browser', 'node'];
 
 const main = () => {
   const environments = readdirSync(environmentsDir);
   environments.forEach(async (environment) => {
-    // Move environment coverage directories to a single file
-    await renameSync(
-      join(environmentsDir, `${environment}/coverage-final.json`),
-      join(environmentsDir, `${environment}.json`)
-    );
-    // Remove environment coverage directory
-    rmSync(join(environmentsDir, environment), { recursive: true, force: true });
+    if (validEnvironments.includes(environment)) {
+      // Move environment coverage directories to a single file
+      await renameSync(
+        join(environmentsDir, `${environment}/coverage-final.json`),
+        join(environmentsDir, `${environment}.json`)
+      );
+      // Remove environment coverage directory
+      rmSync(join(environmentsDir, environment), { recursive: true, force: true });
+    }
   });
 };
 
