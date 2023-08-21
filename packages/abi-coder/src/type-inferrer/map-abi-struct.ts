6b2580aa66407dca1319a9aa8c61256b429bd298
nedsalk <nedim.salkic@fuel.sh>
2023-08-21 17:12:27 +0200

    docs: wording

--- packages/abi-coder/src/type-inferrer/map-abi-struct.ts
+++ packages/abi-coder/src/type-inferrer/map-abi-struct.ts
@@ -7,7 +7,7 @@ import type { TupleToUnion } from './type-utilities';
  * This maps structs by going over each component of the struct and inferring its type.
  *
  * Given that struct components have names and we're mapping a struct into an object,
- * we're mapping the whole struct into \{ component_name:inferred_type_of_component \}.
+ * we're mapping the whole struct into a record of type \{ component_name:inferred_type_of_component \}.
  */
 export type MapAbiStruct<
   Types extends JsonAbi['types'],
@@ -15,7 +15,7 @@ export type MapAbiStruct<
   C extends JsonAbiArgument = TupleToUnion<Components>
 > = {
   /**
-   * C extends \{ readonly name: Name \} is necessary to remove all types from the C UNION
+   * C extends \{ readonly name: Name \} is necessary to remove all types from the C union
    * except the one that corresponds to the specific Name who's type we're inferring.
    * If it wasn't implemented like this, every field's value would be a union of ALL the inferred component types.
    * This way we're type constraining to ONLY the component that has the specific Name that's being iterated over in [Name in C['name']]
