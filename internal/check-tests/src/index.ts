50a12319d9841f93f791c94e71bfde27706c12f8
danielbate <--global>
2023-08-25 17:39:32 +0100

    feat: modify env tests

--- internal/check-tests/src/index.ts
+++ internal/check-tests/src/index.ts
@@ -1,10 +1,14 @@
+const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined';
+
+const isNode =
+  typeof process !== 'undefined' && process.versions != null && process.versions.node != null;
+
 export const testEach = (): string => {
   let env: string;
 
-  // @ts-expect-error
-  if (globalThis?.context?.browser()?.isConnected()) {
+  if (isBrowser) {
     env = 'browser';
-  } else if (globalThis?.process?.versions?.node) {
+  } else if (isNode) {
     env = 'node';
   } else {
     throw new Error('Oops, no browser/node detected');
