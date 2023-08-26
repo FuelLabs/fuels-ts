0bc70cbf6bcc4cd07d614050e7e573acd245bb4e
danielbate <--global>
2023-08-26 19:44:17 +0100

    feat: add describe to provider time test

--- packages/providers/src/utils/time.node.browser.test.ts
+++ packages/providers/src/utils/time.node.browser.test.ts
@@ -1,9 +1,11 @@
 import { fromTai64ToUnix, fromUnixToTai64 } from './time';
 
-test('fromTai64ToUnix', () => {
-  expect(fromTai64ToUnix('4611686020108779312')).toEqual(1681391398);
-});
+describe(__filename, () => {
+  test('fromTai64ToUnix', () => {
+    expect(fromTai64ToUnix('4611686020108779312')).toEqual(1681391398);
+  });
 
-test('fromUnixToTai64', () => {
-  expect(fromUnixToTai64(1681391398)).toEqual('4611686020108779312');
+  test('fromUnixToTai64', () => {
+    expect(fromUnixToTai64(1681391398)).toEqual('4611686020108779312');
+  });
 });
