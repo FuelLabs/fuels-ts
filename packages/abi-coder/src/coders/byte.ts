f0a44f39d75e114d45f4519939cb38968797b5cc
Cameron Manavian <cameron.manavian@fuel.sh>
2023-08-29 23:01:45 -0700

    fix test

---
+++ packages/abi-coder/src/coders/byte.ts
@@ -0,0 +1,26 @@
+import { toBytes, toHex, bn } from '@fuel-ts/math';
+
+import { BASE_VECTOR_OFFSET } from '../utilities';
+
+import { Coder } from './abstract-coder';
+import { U64Coder } from './u64';
+
+const MEMORY_SIZE_OF_U8 = 1;
+
+export class ByteCoder extends Coder<number, Uint8Array> {
+  constructor() {
+    super('struct', 'struct Bytes', MEMORY_SIZE_OF_U8);
+  }
+
+  encode(value: number): Uint8Array {
+    return new Uint8Array();
+  }
+
+  decode(data: Uint8Array, offset: number): [Uint8Array, number] {
+    const len = data.slice(16, 24);
+    const length = bn(new U64Coder().decode(len, 0)[0]).toNumber();
+    const byteData = data.slice(BASE_VECTOR_OFFSET, BASE_VECTOR_OFFSET + length * 8);
+
+    return [byteData, offset + 8];
+  }
+}
--- packages/fuel-gauge/src/bytes.test.ts
+++ packages/fuel-gauge/src/bytes.test.ts
@@ -9,11 +9,19 @@ beforeAll(async () => {
 });
 
 describe('Bytes Tests', () => {
-  it('should test bytes input/output', async () => {
+  it('should test bytes output', async () => {
     const INPUT = 10;
 
     const { value } = await contractInstance.functions.return_bytes(INPUT).call<number[]>();
 
-    expect(value).toStrictEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    expect(value).toStrictEqual(new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
+  });
+
+  it('should test bytes output [100 items]', async () => {
+    const INPUT = 100;
+
+    const { value } = await contractInstance.functions.return_bytes(INPUT).call<number[]>();
+
+    expect(value).toStrictEqual(new Uint8Array(Array.from({ length: 100 }, (e, i) => i)));
   });
 });
