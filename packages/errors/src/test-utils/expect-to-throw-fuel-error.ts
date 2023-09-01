063072a90bee72c001b7be05cff4f17eaa7fe5b2
Anderson Arboleya <anderson@arboleya.me>
2023-09-01 19:45:22 -0300

    Re-ordering lines

--- packages/errors/src/test-utils/expect-to-throw-fuel-error.ts
+++ packages/errors/src/test-utils/expect-to-throw-fuel-error.ts
@@ -5,12 +5,21 @@ import { ErrorCode } from '../index';
 
 type ExpectedFuelError = Partial<FuelError> & Required<Pick<FuelError, 'code'>>;
 
+const codes = Object.values(ErrorCode);
+
 export const expectToThrowFuelError = async (
   lambda: () => unknown,
   expectedError: ExpectedFuelError
 ) => {
+  if (!expectedError.code) {
+    throw new Error('Expected error must contain a code.');
+  }
+
+  if (!codes.includes(expectedError.code)) {
+    throw new Error(`Expected error code '${expectedError.code}' is not a valid FuelError code.`);
+  }
+
   const { error: thrownError } = await safeExec<unknown, FuelError>(lambda);
-  const codes = Object.values(ErrorCode);
 
   if (!thrownError) {
     throw new Error("Passed-in lambda didn't throw.");
@@ -20,16 +29,8 @@ export const expectToThrowFuelError = async (
     throw new Error('Thrown error must contain a code.');
   }
 
-  if (!expectedError.code) {
-    throw new Error('Expected error must contain a code.');
-  }
-
   if (!codes.includes(thrownError.code)) {
     throw new Error(`Thrown error code '${thrownError.code}' is not a valid FuelError code.`);
-  }
-
-  if (!codes.includes(expectedError.code)) {
-    throw new Error(`Expected error code '${expectedError.code}' is not a valid FuelError code.`);
   }
 
   expect(thrownError.name).toEqual('FuelError');
