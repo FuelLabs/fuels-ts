2ffbc4c4c6a037089cb6ca0786a0ff3c2f76f0f5
Torres-ssf <sergio.uft@gmail.com>
2023-09-13 14:18:33 -0300

    add unit tests for date helpers

---
+++ packages/providers/src/transaction-summary/date.test.ts
@@ -0,0 +1,49 @@
+import * as tai64Mod from 'tai64';
+
+import * as dateMod from './date';
+
+const { tai64toDate, formatDate, dateToTai64 } = dateMod;
+
+describe('transaction-summary/date', () => {
+  afterEach(jest.restoreAllMocks);
+
+  it('should convert TAI64 to Date correctly', () => {
+    const fromStringSpy = jest.spyOn(tai64Mod.TAI64, 'fromString');
+    const toUnixSpy = jest.spyOn(tai64Mod.TAI64.prototype, 'toUnix');
+
+    const tai64Timestamp = '4611686020121838636';
+
+    tai64toDate(tai64Timestamp);
+
+    expect(fromStringSpy).toHaveBeenCalledTimes(1);
+    expect(fromStringSpy).toHaveBeenCalledWith(tai64Timestamp, 10);
+
+    expect(toUnixSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should format TAI64 to relative time string', () => {
+    const tai64toDateSpy = jest.spyOn(dateMod, 'tai64toDate');
+
+    const tai64Timestamp = '4611686020121838636';
+
+    formatDate(tai64Timestamp);
+
+    expect(tai64toDateSpy).toHaveBeenCalledTimes(1);
+    expect(tai64toDateSpy).toHaveBeenCalledWith(tai64Timestamp);
+  });
+
+  it('should convert Date to TAI64 correctly', () => {
+    const fromUnixSpy = jest.spyOn(tai64Mod.TAI64, 'fromUnix');
+    const toStringSpy = jest.spyOn(tai64Mod.TAI64.prototype, 'toString');
+
+    const date = new Date();
+
+    dateToTai64(date);
+
+    expect(fromUnixSpy).toHaveBeenCalledTimes(1);
+    expect(fromUnixSpy).toHaveBeenCalledWith(Math.floor(date.getTime() / 1000));
+
+    expect(toStringSpy).toHaveBeenCalledTimes(1);
+    expect(toStringSpy).toHaveBeenCalledWith(10);
+  });
+});
