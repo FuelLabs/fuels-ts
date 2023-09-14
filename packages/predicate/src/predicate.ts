3a39fea1bb942825c6faa0d3a9a5ad8cada77d27
Daniel Bate <djbate23@gmail.com>
2023-09-14 12:24:45 +0100

    feat: improve predicate vector input support (#1247)
    * feat: add predicate gas used to input coin offset
    
    * feat: enable predicate vector input test
    
    * feat: add script fixed size comment to constants
    
    * feat: update predicate vector types contract
    
    * feat: fix offset in predicate data
    
    * chore: linting
    
    * feat: changeset
    
    * feat: enable predicate vector types test
    
    * feat: reorder offset vars

--- packages/predicate/src/predicate.ts
+++ packages/predicate/src/predicate.ts
@@ -1,13 +1,14 @@
 import type { BytesLike } from '@ethersproject/bytes';
 import { hexlify, arrayify } from '@ethersproject/bytes';
 import { Logger } from '@ethersproject/logger';
+import type { JsonAbi, InputValue } from '@fuel-ts/abi-coder';
 import {
   Interface,
-  TRANSACTION_PREDICATE_COIN_FIXED_SIZE,
-  TRANSACTION_SCRIPT_FIXED_SIZE,
+  INPUT_COIN_FIXED_SIZE,
+  SCRIPT_FIXED_SIZE,
   VM_TX_MEMORY,
+  WORD_SIZE,
 } from '@fuel-ts/abi-coder';
-import type { JsonAbi, InputValue } from '@fuel-ts/abi-coder';
 import { Address } from '@fuel-ts/address';
 import type { AbstractPredicate } from '@fuel-ts/interfaces';
 import type {
@@ -116,11 +117,7 @@ export class Predicate<ARGS extends InputValue[]> extends Account implements Abs
     const paddedCode = new ByteArrayCoder(this.bytes.length).encode(this.bytes);
 
     const OFFSET =
-      VM_TX_MEMORY +
-      TRANSACTION_SCRIPT_FIXED_SIZE +
-      TRANSACTION_PREDICATE_COIN_FIXED_SIZE +
-      paddedCode.byteLength -
-      17;
+      VM_TX_MEMORY + SCRIPT_FIXED_SIZE + INPUT_COIN_FIXED_SIZE + WORD_SIZE + paddedCode.byteLength;
 
     this.predicateData = mainFn?.encodeArguments(args, OFFSET) || new Uint8Array();
     return this;
