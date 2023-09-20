dddef028c4b388c330d22a3f2f4e6f171be4d164
Torres-ssf <sergio.uft@gmail.com>
2023-09-20 15:15:09 -0300

    getting maxGasPerTx from the chain on Account class

--- packages/wallet/src/account.ts
+++ packages/wallet/src/account.ts
@@ -25,7 +25,6 @@ import {
   ScriptTransactionRequest,
   transactionRequestify,
 } from '@fuel-ts/providers';
-import { MAX_GAS_PER_TX } from '@fuel-ts/transactions/configs';
 
 import {
   composeScriptForTransferringToContract,
@@ -231,8 +230,8 @@ export class Account extends AbstractAccount {
     /** Tx Params */
     txParams: TxParamsType = {}
   ): Promise<TransactionResponse> {
-    const params: TxParamsType = { gasLimit: MAX_GAS_PER_TX, ...txParams };
-
+    const { maxGasPerTx } = this.provider.getGasConfig();
+    const params: TxParamsType = { gasLimit: maxGasPerTx, ...txParams };
     const request = new ScriptTransactionRequest(params);
     request.addCoinOutput(destination, amount, assetId);
     const fee = request.calculateFee();
@@ -278,8 +277,9 @@ export class Account extends AbstractAccount {
       assetId
     );
 
+    const { maxGasPerTx } = this.provider.getGasConfig();
     const request = new ScriptTransactionRequest({
-      gasLimit: MAX_GAS_PER_TX,
+      gasLimit: maxGasPerTx,
       ...txParams,
       script,
       scriptData,
@@ -334,7 +334,8 @@ export class Account extends AbstractAccount {
     ]);
 
     // build the transaction
-    const params = { script, gasLimit: MAX_GAS_PER_TX, ...txParams };
+    const { maxGasPerTx } = this.provider.getGasConfig();
+    const params = { script, gasLimit: maxGasPerTx, ...txParams };
     const request = new ScriptTransactionRequest(params);
     const fee = request.calculateFee();
     let quantities: CoinQuantityLike[] = [];
