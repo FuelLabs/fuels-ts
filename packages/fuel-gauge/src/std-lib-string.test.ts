8c4020d230bec2f1335373a80c5dbc43484d947a
Cameron Manavian <cameron.manavian@fuel.sh>
2023-09-20 23:14:04 -0700

    add basic tests

---
+++ packages/fuel-gauge/src/std-lib-string.test.ts
@@ -0,0 +1,25 @@
+import { type Contract } from 'fuels';
+
+import { getSetupContract } from './utils';
+
+const setupContract = getSetupContract('std-lib-string');
+let contractInstance: Contract;
+beforeAll(async () => {
+  contractInstance = await setupContract();
+});
+
+describe('std-lib-string Tests', () => {
+  it('should test std-lib-string return', async () => {
+    const { value } = await contractInstance.functions.return_dynamic_string().call<string>();
+
+    expect(value).toStrictEqual('Hello World');
+  });
+
+  it('should test std-lib-string input', async () => {
+    const INPUT = 'Hello World';
+
+    await contractInstance.functions.accepts_dynamic_string(INPUT).call<number[]>();
+
+    expect(true).toStrictEqual(true);
+  });
+});
