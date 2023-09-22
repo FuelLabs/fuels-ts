c1404783643070f847f837bce2cb01ba8c550e5c
nedsalk <nedim.salkic@fuel.sh>
2023-09-22 07:04:40 +0200

    fix: abi-main-method-missing was already defined

--- packages/errors/src/error-codes.ts
+++ packages/errors/src/error-codes.ts
@@ -11,7 +11,7 @@ export enum ErrorCode {
   TYPE_NOT_FOUND = 'type-not-found',
   TYPE_NOT_SUPPORTED = 'type-not-supported',
   INVALID_DECODE_VALUE = 'invalid-decode-value',
-  JSON_ABI_ERROR = 'abi-main-method-missing',
+  JSON_ABI_ERROR = 'json-abi-error',
   TYPE_ID_NOT_FOUND = 'type-id-not-found',
   BIN_FILE_NOT_FOUND = 'bin-file-not-found',
   CODER_NOT_FOUND = 'coder-not-found',
