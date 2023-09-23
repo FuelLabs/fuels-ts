52cfe7b9143fce0545ec220ac0a18bb814876962
Anderson Arboleya <anderson@arboleya.me>
2023-09-23 05:24:47 -0300

    Moving option upwards

--- packages/fuels/src/cli.ts
+++ packages/fuels/src/cli.ts
@@ -22,6 +22,8 @@ export async function run(argv: string[]) {
   program.addHelpCommand('help [command]', 'Display help for command');
   program.enablePositionalOptions(true);
 
+  program.option('-S, --silent [boolean]', 'Omit output messages', false);
+
   /**
    * Defining local commands
    */
@@ -31,17 +33,11 @@ export async function run(argv: string[]) {
     'Path to the directory where `fuel.config.ts` file is located'
   ).default(process.cwd());
 
-  const silentOption = new Option(
-    '-V, --verbose [boolean]',
-    'Omit output and status messages'
-  ).default(true); // TODO: change to false before release
-
   let command: Command;
 
   (command = program.command(Commands.init))
     .description('Create a sample `fuel.config.ts` file')
     .addOption(pathOption)
-    .addOption(silentOption)
     .option('-w, --workspace <path>', 'Relative dir path to Sway workspace', './sway-workspace')
     .option('-o, --output <path>', 'Relative dir path to generating Typescript', './types')
     .action(withProgram(command, Commands.init, init));
@@ -49,19 +45,16 @@ export async function run(argv: string[]) {
   (command = program.command(Commands.dev))
     .description('Start a Fuel node and run build + deploy on every file change')
     .addOption(pathOption)
-    .addOption(silentOption)
     .action(withConfig(command, Commands.dev, dev));
 
   (command = program.command(Commands.build))
     .description('Build Sway programs and generate Typescript for them')
     .addOption(pathOption)
-    .addOption(silentOption)
     .action(withConfig(command, Commands.build, build));
 
   (command = program.command(Commands.deploy))
     .description('Deploy contracts to Fuel network')
     .addOption(pathOption)
-    .addOption(silentOption)
     .action(withConfig(command, Commands.deploy, deploy));
 
   /**
