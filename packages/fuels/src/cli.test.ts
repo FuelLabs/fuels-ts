a60debe9ec2077e83f2b4766a979f1bd3437d3a6
Anderson Arboleya <anderson@arboleya.me>
2023-09-26 15:06:07 -0300

    Implementing tests for CLI entrypoint

--- packages/fuels/src/cli.test.ts
+++ packages/fuels/src/cli.test.ts
@@ -1,34 +1,79 @@
-import { versions } from '@fuel-ts/versions';
 import { Command } from 'commander';
 
-import { run } from './cli';
+import * as cliMod from './cli';
+import { Commands } from './cli/types';
+import * as loggingMod from './cli/utils/logger';
 
-describe.skip('cli.js', () => {
-  test('should call `versions` sub-program', async () => {
-    // mocking
-    const name = jest.spyOn(Command.prototype, 'name');
-    const version = jest.spyOn(Command.prototype, 'version');
-    const command = jest.spyOn(Command.prototype, 'command');
-    const description = jest.spyOn(Command.prototype, 'description');
-    const parse = jest.spyOn(Command.prototype, 'parseAsync').mockImplementation();
+describe('cli.js', () => {
+  const { configureCli, run, onPreAction } = cliMod;
 
-    // executing
-    jest.spyOn(process, 'exit').mockImplementation();
-    await run([]);
+  beforeEach(() => jest.resetAllMocks());
+  afterAll(() => jest.resetAllMocks());
 
-    // validating
-    expect(name).toHaveBeenCalledWith('fuels');
-    expect(version).toHaveBeenCalledWith(versions.FUELS);
+  it('shoud configure cli', () => {
+    const program = configureCli();
 
-    const cmds = ['init', 'dev', 'flow', 'build', 'types', 'deploy', 'versions', 'typegen'];
-    cmds.forEach((cmd, index) => {
-      expect(command).toHaveBeenNthCalledWith(index + 1, cmd);
+    // top level props and opts
+    expect(program.name()).toEqual('fuels');
+    expect(program.opts()).toEqual({
+      debug: false,
+      silent: false,
     });
 
-    // All commands + 1 main description of the program
-    expect(description).toHaveBeenCalledTimes(cmds.length + 1);
+    // checks if all commands are defined
+    const cmds = program.commands;
+    const cmd = (name: string) => cmds.find((c) => c.name() === name);
 
-    expect(parse).toHaveBeenCalledTimes(1);
-    expect(parse).toHaveBeenCalledWith([]);
+    const init = cmd(Commands.init);
+    const dev = cmd(Commands.dev);
+    const build = cmd(Commands.build);
+    const deploy = cmd(Commands.deploy);
+    const forc = cmd('forc');
+    const core = cmd('core');
+
+    expect(init).toBeTruthy();
+    expect(dev).toBeTruthy();
+    expect(build).toBeTruthy();
+    expect(deploy).toBeTruthy();
+    expect(forc).toBeTruthy();
+    expect(core).toBeTruthy();
+
+    // checks commands options
+    const path = process.cwd();
+    const output = './types';
+    const workspace = './sway-workspace';
+
+    expect(init?.opts()).toEqual({ path, output, workspace });
+    expect(dev?.opts()).toEqual({ path });
+    expect(build?.opts()).toEqual({ path });
+    expect(deploy?.opts()).toEqual({ path });
+    expect(forc?.opts()).toEqual({});
+    expect(core?.opts()).toEqual({});
+  });
+
+  it('preAction should configure logging', () => {
+    const spy = jest.spyOn(loggingMod, 'configureLogging');
+
+    const command = new Command();
+    command.option('-D, --debug', 'Enables verbose logging', false);
+    command.option('-S, --silent', 'Omit output messages', false);
+    command.parse([]);
+
+    onPreAction(command);
+    expect(spy).toBeCalledWith({
+      isDebugEnabled: false,
+      isLoggingEnabled: true,
+    });
+  });
+
+  it('should run cli program', async () => {
+    const command = new Command();
+    const parseAsync = jest.spyOn(command, 'parseAsync');
+    const $configureCli = jest.spyOn(cliMod, 'configureCli').mockReturnValue(command);
+
+    await run([]);
+
+    expect($configureCli).toHaveBeenCalledTimes(1);
+    expect(parseAsync).toHaveBeenCalledTimes(1);
   });
 });
