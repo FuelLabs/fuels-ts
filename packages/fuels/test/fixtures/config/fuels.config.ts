bb55d8bafb32d410a50530e66f049a7b3dffee05
Anderson Arboleya <anderson@arboleya.me>
2023-09-28 12:09:54 -0300

    Re-adding lost file

---
+++ packages/fuels/test/fixtures/config/fuels.config.ts
@@ -0,0 +1,20 @@
+import { FUEL_NETWORK_URL } from '@fuel-ts/wallet/configs';
+
+import type { FuelsConfig } from '../../../src';
+
+export const fuelsConfig: FuelsConfig = {
+  basePath: '/root',
+  // workspace: '/root/project',
+  contracts: ['/root/project/foo', '/root/project/bar'],
+  predicates: ['/root/project/predicate'],
+  scripts: ['/root/project/script'],
+  output: '/types',
+  deployConfig: {
+    gasPrice: 5,
+  },
+  useSystemForc: false,
+  useSystemFuelCore: false,
+  autoStartFuelCore: false,
+  fuelCorePort: 4000,
+  providerUrl: FUEL_NETWORK_URL,
+};
