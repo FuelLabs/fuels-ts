352c993cdb0bbb0394fca8de4d82eddd5c386637
Anderson Arboleya <anderson@arboleya.me>
2023-09-29 11:28:03 -0300

    Trimming test, stop re-testing things covered in other places

--- packages/fuels/test/dev.test.ts
+++ packages/fuels/test/dev.test.ts
@@ -1,45 +1,20 @@
-import { existsSync, readFileSync, statSync, writeFileSync } from 'fs';
+import * as chokidar from 'chokidar';
 
 import type { FuelsConfig } from '../src';
-import { fileHandlers } from '../src/cli/commands/dev';
+import * as buildMod from '../src/cli/commands/build/index';
+import * as deployMod from '../src/cli/commands/deploy/index';
 import * as startCoreMod from '../src/cli/commands/dev/startFuelCore';
 
-import {
-  clean,
-  contractsJson,
-  fooContractSway,
-  fooContractTs,
-  runDev,
-  runInit,
-} from './utils/runCommands';
+import { clean, runInit, runDev } from './utils/runCommands';
+
+jest.mock('chokidar', () => ({
+  __esModule: true,
+  ...jest.requireActual('chokidar'),
+}));
 
 describe('dev', () => {
-  let fooContractSwayContentsBefore: string;
-
-  async function ensureFileChanged(path: string, mtime: Date) {
-    const stat = statSync(path);
-    if (stat.mtime.getTime() > mtime.getTime()) {
-      return true;
-    }
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(ensureFileChanged(path, mtime));
-      }, 1000);
-    });
-  }
-
   beforeEach(clean);
-
-  afterAll(async () => {
-    // Close file-watching handlers
-    await fileHandlers.watcher?.close();
-
-    // Restore original Sway contract contents
-    writeFileSync(fooContractSway, fooContractSwayContentsBefore);
-
-    // Clean generated files
-    clean();
-  });
+  afterAll(clean);
 
   it('should run `dev` command', async () => {
     const startFuelCore = jest
@@ -55,61 +30,23 @@ describe('dev', () => {
         })
       );
 
+    const build = jest.spyOn(buildMod, 'build').mockImplementation();
+    const deploy = jest.spyOn(deployMod, 'deploy').mockImplementation();
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const on: any = jest.fn(() => ({ on }));
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const watch = jest.spyOn(chokidar, 'watch').mockReturnValue({ on } as any);
+
     await runInit();
     await runDev();
 
     // Validates if fuel-core has been started
     expect(startFuelCore).toHaveBeenCalledTimes(1);
+    expect(build).toHaveBeenCalledTimes(1);
+    expect(deploy).toHaveBeenCalledTimes(1);
 
-    /**
-     * Before editing
-     */
-    // Caches and double-checks initial contents of Sway contract
-    fooContractSwayContentsBefore = readFileSync(fooContractSway, 'utf-8');
-    expect(fooContractSwayContentsBefore).toMatch('fn foo() -> u64');
-    expect(fooContractSwayContentsBefore).toMatch('fn foo() -> u64 {');
-    expect(fooContractSwayContentsBefore).toMatch('12345');
-
-    // Validates contract IDs inside `contracts.json`
-    expect(existsSync(contractsJson)).toBeTruthy();
-    const contractJsonContentsBefore = JSON.parse(readFileSync(contractsJson, 'utf-8'));
-    expect(contractJsonContentsBefore.barFoo).toMatch(/0x/);
-    expect(contractJsonContentsBefore.fooBar).toMatch(/0x/);
-
-    // Validates contents of generated TS contract file
-    expect(existsSync(fooContractTs)).toBeTruthy();
-    const fooContractTsContentsBefore = readFileSync(fooContractTs, 'utf-8');
-    expect(fooContractTsContentsBefore).toMatch('"type": "u64",');
-
-    /**
-     * Editing Sway Contract (must auto-rebuild everything)
-     */
-    const mtime = statSync(contractsJson).mtime;
-
-    writeFileSync(
-      fooContractSway,
-      fooContractSwayContentsBefore.replace(/u64/g, 'u8').replace('12345', '123')
-    );
-
-    await ensureFileChanged(contractsJson, mtime);
-
-    /**
-     * After editing
-     */
-    // Validates contract IDs inside `contracts.json`
-    const contractJsonContentsAfter = JSON.parse(readFileSync(contractsJson, 'utf-8'));
-    expect(contractJsonContentsAfter.barFoo).not.toEqual(contractJsonContentsBefore.barFoo);
-    expect(contractJsonContentsAfter.fooBar).not.toEqual(contractJsonContentsBefore.barFoo);
-
-    // Validates modified contents of Sway contract
-    const fooContractSwayContentsAfter = readFileSync(fooContractSway, 'utf-8');
-    expect(fooContractSwayContentsAfter).toMatch('fn foo() -> u8');
-    expect(fooContractSwayContentsAfter).toMatch('fn foo() -> u8 {');
-    expect(fooContractSwayContentsAfter).toMatch('123');
-
-    // Validates contents of generated TS contract file
-    expect(existsSync(fooContractTs)).toBeTruthy();
-    const fooContractTsContentsAfter = readFileSync(fooContractTs, 'utf-8');
-    expect(fooContractTsContentsAfter).toMatch('"type": "u8",');
+    expect(watch).toHaveBeenCalledTimes(1);
+    expect(on).toHaveBeenCalledTimes(3);
   });
 });
