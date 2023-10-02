5c1abb86cc86ae1018ade7396092e5d8d6f5cdb3
Torres-ssf <sergio.uft@gmail.com>
2023-10-02 12:09:03 -0300

    refact tests for calculateFee and its helpers

--- packages/providers/src/utils/fee.test.ts
+++ packages/providers/src/utils/fee.test.ts
@@ -1,13 +1,15 @@
+import { arrayify } from '@ethersproject/bytes';
 import { BN, bn } from '@fuel-ts/math';
 import { ReceiptType, type Witness } from '@fuel-ts/transactions';
 
+import { MOCK_TX_BYTES_HEX } from '../../test/fixtures/transaction-summary';
 import type { TransactionResultReceipt } from '../transaction-response';
 
 import {
   calculatePriceWithFactor,
-  calculateTransactionFeeForContractCreated,
+  calculateTransactionFee,
+  calculateTxChargeableBytesFee,
   getGasUsedFromReceipts,
-  calculateTransactionFeeForScript,
 } from './fee';
 
 describe(__filename, () => {
@@ -33,12 +35,21 @@ describe(__filename, () => {
     });
   });
 
-  describe('calculateTransactionFeeForScript', () => {
+  describe('calculateTransactionFee', () => {
     it('should calculate transaction fee for script correctly', () => {
+      const margin = 1;
       const gasUsed = new BN(1700);
       const gasPrice = new BN(50);
-      const gasPriceFactor = new BN(1000000);
-      const margin = 1;
+      const gasPriceFactor = new BN(92);
+      const gasPerByte = new BN(1);
+      const transactionWitnesses: Witness[] = [
+        {
+          dataLength: 64,
+          data: '0x3d8943b87436d54d7f1282ed9b01d38633f8003a37676276bbaf0560390d21f826b5f6e9ea85b1bca435af2bec4982436c20c1294876025f78aaa0143867eb7f',
+        },
+      ];
+
+      const transactionBytes = arrayify(MOCK_TX_BYTES_HEX);
 
       const receipts: TransactionResultReceipt[] = [
         {
@@ -48,21 +59,21 @@ describe(__filename, () => {
         },
       ];
 
-      const result = calculateTransactionFeeForScript({
+      const result = calculateTransactionFee({
         receipts,
         gasPrice,
         gasPriceFactor,
         margin,
+        transactionBytes,
+        transactionWitnesses,
+        gasPerByte,
       });
 
-      const expectedFee = bn(
-        Math.ceil(gasUsed.toNumber() / gasPriceFactor.toNumber()) * gasPrice.toNumber()
-      );
-
+      const expectedFee = 1200;
       const expectedGasUsed = gasUsed;
 
-      expect(result.fee).toStrictEqual(expectedFee);
-      expect(result.gasUsed).toStrictEqual(expectedGasUsed);
+      expect(result.fee.toNumber()).toStrictEqual(expectedFee);
+      expect(result.gasUsed.toNumber()).toStrictEqual(expectedGasUsed.toNumber());
     });
 
     it('should calculate transaction fee for multiple receipts', () => {
@@ -71,6 +82,15 @@ describe(__filename, () => {
       const gasPrice = new BN(50);
       const gasPriceFactor = new BN(1000000);
       const margin = 1;
+      const gasPerByte = new BN(1);
+      const transactionWitnesses: Witness[] = [
+        {
+          dataLength: 64,
+          data: '0x741af9e1379d78d1882d6058888cb704f3ba5b4854ca919e6a26b759dbd6931877323cfbf6ced0ba63b18fbb15926fadd877218c93b8d9972564168b6198208d',
+        },
+      ];
+
+      const transactionBytes = arrayify(MOCK_TX_BYTES_HEX);
 
       const receipts: TransactionResultReceipt[] = [
         {
@@ -85,34 +105,33 @@ describe(__filename, () => {
         },
       ];
 
-      const result = calculateTransactionFeeForScript({
+      const result = calculateTransactionFee({
         receipts,
         gasPrice,
         gasPriceFactor,
         margin,
+        transactionBytes,
+        transactionWitnesses,
+        gasPerByte,
       });
 
       const expectedGasUsed = bn(gasUsed1.toNumber() + gasUsed2.toNumber());
+      const expectedFee = 100;
 
-      const expectedFee = bn(
-        Math.ceil(expectedGasUsed.toNumber() / gasPriceFactor.toNumber()) * gasPrice.toNumber()
-      );
-
-      expect(result.fee).toStrictEqual(expectedFee);
-      expect(result.gasUsed).toStrictEqual(expectedGasUsed);
+      expect(result.fee.toNumber()).toStrictEqual(expectedFee);
+      expect(result.gasUsed.toNumber()).toStrictEqual(expectedGasUsed.toNumber());
     });
   });
 
-  describe('calculateTransactionFeeForContractCreated', () => {
+  describe('calculateTxChargeableBytesFee', () => {
     it('should calculate gas used for contract created correctly', () => {
-      const transactionBytes = new Uint8Array([0, 1, 2, 3, 4, 5]);
+      const transactionBytes = arrayify(MOCK_TX_BYTES_HEX);
       const gasPerByte = new BN(1);
       const gasPriceFactor = new BN(2);
-      const transactionWitnesses: Witness[] = [{ dataLength: 2, data: 'data' }];
-
+      const transactionWitnesses: Witness[] = [{ dataLength: 0, data: '0x' }];
       const gasPrice = new BN(4);
 
-      const { fee, gasUsed } = calculateTransactionFeeForContractCreated({
+      const chargeableBytesFee = calculateTxChargeableBytesFee({
         transactionBytes,
         gasPerByte,
         gasPriceFactor,
@@ -120,19 +139,18 @@ describe(__filename, () => {
         gasPrice,
       });
 
-      expect(gasUsed.toNumber()).toEqual(2); // (6-2)*1/2 = 2
-      expect(fee.toNumber()).toEqual(8); // 4*2 = 8
+      expect(chargeableBytesFee.toNumber()).toEqual(1040);
     });
 
     it('should handle an empty witnesses array', () => {
-      const transactionBytes = new Uint8Array([0, 1, 2, 3, 4, 5]);
+      const transactionBytes = arrayify(MOCK_TX_BYTES_HEX);
       const gasPerByte = new BN(1);
       const gasPriceFactor = new BN(2);
       const transactionWitnesses: Witness[] = [];
 
       const gasPrice = new BN(2);
 
-      const { fee, gasUsed } = calculateTransactionFeeForContractCreated({
+      const chargeableBytesFee = calculateTxChargeableBytesFee({
         transactionBytes,
         gasPerByte,
         gasPriceFactor,
@@ -140,19 +158,19 @@ describe(__filename, () => {
         gasPrice,
       });
 
-      expect(gasUsed.toNumber()).toEqual(3); // 6*1/2 = 3
-      expect(fee.toNumber()).toEqual(6); // 3*2 = 6
+      expect(chargeableBytesFee.toNumber()).toEqual(520);
     });
 
     it('should round up the result', () => {
-      const transactionBytes = new Uint8Array([0, 1, 2]);
+      const transactionBytes = arrayify(MOCK_TX_BYTES_HEX);
+
       const gasPerByte = new BN(1);
       const gasPriceFactor = new BN(2);
       const transactionWitnesses: Witness[] = [];
 
       const gasPrice = new BN(1);
 
-      const { fee, gasUsed } = calculateTransactionFeeForContractCreated({
+      const chargeableBytesFee = calculateTxChargeableBytesFee({
         transactionBytes,
         gasPerByte,
         gasPriceFactor,
@@ -160,8 +178,7 @@ describe(__filename, () => {
         gasPrice,
       });
 
-      expect(gasUsed.toNumber()).toEqual(2); // 3*1/2 = 1.5 which rounds up to 2
-      expect(fee.toNumber()).toEqual(2); // 2*1 = 2
+      expect(chargeableBytesFee.toNumber()).toEqual(260);
     });
   });
 
@@ -187,9 +204,9 @@ describe(__filename, () => {
         },
       ];
 
-      const result = getGasUsedFromReceipts(receipts);
+      const gasUsed = getGasUsedFromReceipts(receipts);
 
-      expect(result.toNumber()).toEqual(12); // 5 + 7 = 12
+      expect(gasUsed.toNumber()).toEqual(12);
     });
 
     it('should return zero if there are no ScriptResult receipts', () => {
