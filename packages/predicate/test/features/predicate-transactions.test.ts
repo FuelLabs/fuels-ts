66aa5964199c50f559e23fed0e618a25ec4e089d
nedsalk <nedim.salkic@fuel.sh>
2023-10-04 17:25:22 +0200

    fix: add missing brackets for function

--- packages/predicate/test/features/predicate-transactions.test.ts
+++ packages/predicate/test/features/predicate-transactions.test.ts
@@ -22,41 +22,44 @@ describe('Predicate', () => {
       predicate = new Predicate(defaultPredicateBytecode, provider, defaultPredicateAbi);
       const predicateAddress = '0x4f780df441f7a02b5c1e718fcd779776499a0d1069697db33f755c82d7bae02b';
 
-    const request = new ScriptTransactionRequest();
-    request.addResources([{
-      id: '0x01',
-      assetId: '0x0000000000000000000000000000000000000000000000000000000000000000',
-      amount: bn(1),
-      owner: Address.fromB256(predicateAddress),
-      maturity: 0,
-      blockCreated: bn(0),
-      txCreatedIdx: bn(0),
-    }]);
+      request = new ScriptTransactionRequest();
+      request.addResources([
+        {
+          id: '0x01',
+          assetId: '0x0000000000000000000000000000000000000000000000000000000000000000',
+          amount: bn(1),
+          owner: Address.fromB256(predicateAddress),
+          maturity: 0,
+          blockCreated: bn(0),
+          txCreatedIdx: bn(0),
+        },
+      ]);
 
-    it('includes predicate as input when sending a transaction', async () => {
-      const sendTransactionMock = jest
-        .spyOn(Account.prototype, 'sendTransaction')
-        .mockImplementation();
+      it('includes predicate as input when sending a transaction', async () => {
+        const sendTransactionMock = jest
+          .spyOn(Account.prototype, 'sendTransaction')
+          .mockImplementation();
 
-      await predicate.sendTransaction(request);
+        await predicate.sendTransaction(request);
 
-      const inputCoinMock = sendTransactionMock.mock.calls[0][0]
-        .inputs?.[0] as unknown as InputCoin;
-      expect(hexlify(inputCoinMock.predicate)).toBe(defaultPredicateBytecode);
-      expect(hexlify(inputCoinMock.predicateData)).toBe(b256);
-    });
+        const inputCoinMock = sendTransactionMock.mock.calls[0][0]
+          .inputs?.[0] as unknown as InputCoin;
+        expect(hexlify(inputCoinMock.predicate)).toBe(defaultPredicateBytecode);
+        expect(hexlify(inputCoinMock.predicateData)).toBe(b256);
+      });
 
-    it('includes predicate as input when simulating a transaction', async () => {
-      const sendTransactionMock = jest
-        .spyOn(Account.prototype, 'simulateTransaction')
-        .mockImplementation();
+      it('includes predicate as input when simulating a transaction', async () => {
+        const sendTransactionMock = jest
+          .spyOn(Account.prototype, 'simulateTransaction')
+          .mockImplementation();
 
-      await predicate.simulateTransaction(request);
+        await predicate.simulateTransaction(request);
 
-      const inputCoinMock = sendTransactionMock.mock.calls[0][0]
-        .inputs?.[0] as unknown as InputCoin;
-      expect(hexlify(inputCoinMock.predicate)).toBe(defaultPredicateBytecode);
-      expect(hexlify(inputCoinMock.predicateData)).toBe(b256);
+        const inputCoinMock = sendTransactionMock.mock.calls[0][0]
+          .inputs?.[0] as unknown as InputCoin;
+        expect(hexlify(inputCoinMock.predicate)).toBe(defaultPredicateBytecode);
+        expect(hexlify(inputCoinMock.predicateData)).toBe(b256);
+      });
     });
   });
 });
