28987bf97439de4834966120ac3d66f15824d894
Anderson Arboleya <anderson@arboleya.me>
2023-10-09 09:28:03 -0300

    Testing individual entry points in config file

--- packages/fuels/src/cli/config/loadConfig.test.ts
+++ packages/fuels/src/cli/config/loadConfig.test.ts
@@ -1,12 +1,15 @@
 import { safeExec } from '@fuel-ts/errors/test-utils';
 import { readFileSync } from 'fs';
+import { join, relative } from 'path';
 
 import {
-  runInit,
-  fixtures,
-  flagsUseBuiltinBinaries,
   clean,
-  fuelsConfig,
+  runInit,
+  fixturesDir,
+  fuelsConfigPath,
+  initFlagsUseBuiltinBinaries,
+  generatedDir,
+  workspaceDir,
 } from '../../../test/utils/runCommands';
 
 import { loadConfig } from './loadConfig';
@@ -23,22 +26,39 @@ describe('loadConfig', () => {
   });
 
   test(`should auto start fuel core by default`, async () => {
-    await runInit(flagsUseBuiltinBinaries);
+    await runInit(initFlagsUseBuiltinBinaries);
 
-    const config = await loadConfig(fixtures);
-    const fuelsContents = readFileSync(fuelsConfig, 'utf-8');
+    const config = await loadConfig(fixturesDir);
+    const fuelsContents = readFileSync(fuelsConfigPath, 'utf-8');
 
     expect(fuelsContents).toMatch(`  // autoStartFuelCore: true,`); // comment
     expect(config.autoStartFuelCore).toEqual(true);
   });
 
   test(`should auto start fuel core explicitly`, async () => {
-    await runInit([flagsUseBuiltinBinaries, '--auto-start-fuel-core'].flat());
+    await runInit([initFlagsUseBuiltinBinaries, '--auto-start-fuel-core'].flat());
 
-    const config = await loadConfig(fixtures);
-    const fuelsContents = readFileSync(fuelsConfig, 'utf-8');
+    const config = await loadConfig(fixturesDir);
+    const fuelsContents = readFileSync(fuelsConfigPath, 'utf-8');
 
     expect(fuelsContents).toMatch(`  autoStartFuelCore: true,`); // not comment
     expect(config.autoStartFuelCore).toEqual(true);
+  });
+
+  test(`should resolve individually paths when not using workspaces`, async () => {
+    await runInit(
+      [
+        ['--contracts', 'project/contracts/*'],
+        ['--scripts', 'project/scripts/*'],
+        ['--predicates', 'project/predicates/*'],
+        ['-o', generatedDir],
+      ].flat()
+    );
+
+    const config = await loadConfig(fixturesDir);
+
+    expect(config.contracts.length).toEqual(2);
+    expect(config.scripts.length).toEqual(1);
+    expect(config.predicates.length).toEqual(1);
   });
 });
