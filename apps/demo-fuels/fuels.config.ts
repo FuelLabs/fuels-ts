ed25b81846daac999d806052ade14972b60f4588
Anderson Arboleya <anderson@arboleya.me>
2023-10-10 14:56:41 -0300

    Adjusting demo project

--- apps/demo-fuels/fuels.config.ts
+++ apps/demo-fuels/fuels.config.ts
@@ -3,4 +3,6 @@ import { createConfig } from 'fuels';
 export default createConfig({
   workspace: './backend',
   output: './src/backend-api',
+  useBuiltinForc: true,
+  useBuiltinFuelCore: true,
 });
