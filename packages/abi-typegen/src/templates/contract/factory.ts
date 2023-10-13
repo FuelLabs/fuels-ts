baefc8e71fd24bf3cdc0128c0c69cae34b82daf7
Anderson Arboleya <anderson@arboleya.me>
2023-10-13 10:18:49 -0300

    Removing unnecessary stringify step

--- packages/abi-typegen/src/templates/contract/factory.ts
+++ packages/abi-typegen/src/templates/contract/factory.ts
@@ -6,7 +6,7 @@ import factoryTemplate from './factory.hbs';
 export function renderFactoryTemplate(params: { abi: Abi }) {
   const { name: capitalizedName, rawContents, storageSlotsContents } = params.abi;
   const abiJsonString = JSON.stringify(rawContents, null, 2);
-  const storageSlotsJsonString = JSON.stringify(storageSlotsContents ?? [], null, 2);
+  const storageSlotsJsonString = storageSlotsContents ?? [];
 
   const text = renderHbsTemplate({
     template: factoryTemplate,
