9bb57b089553e9e02c4e10c29e109650231d0b5b
nedsalk <nedim.salkic@fuel.sh>
2023-10-15 11:59:42 +0200

    test: added tests for deploying multiple contracts

--- packages/contract/src/test-utils/fuel-node-launcher.test.ts
+++ packages/contract/src/test-utils/fuel-node-launcher.test.ts
@@ -43,17 +43,39 @@ describe('TestNodeLauncher', () => {
     expect(response.value).toBe(true);
   });
 
-  test('a contract can be deployed with a non-default wallet', async () => {
+  test('multiple contracts can be deployed', async () => {
     await using launched = await TestNodeLauncher.launch({
-      walletConfig: new WalletConfig({ numWallets: 2 }),
-      deployContracts: [{ contractDir: simpleContractPath, walletIndex: 1 }],
+      deployContracts: [
+        { contractDir: simpleContractPath },
+        { contractDir: simpleContractPath },
+        { contractDir: simpleContractPath },
+      ],
     });
 
     const {
-      contracts: [contract],
-      wallets: [, wallet2],
+      contracts: [contract1, contract2, contract3],
     } = launched;
 
-    expect(contract.account).toEqual(wallet2);
+    expect((await contract1.functions.test_function().call()).value).toBe(true);
+    expect((await contract2.functions.test_function().call()).value).toBe(true);
+    expect((await contract3.functions.test_function().call()).value).toBe(true);
+  });
+
+  test('multiple contracts can be deployed with different wallets', async () => {
+    await using launched = await TestNodeLauncher.launch({
+      walletConfig: new WalletConfig({ numWallets: 2 }),
+      deployContracts: [
+        { contractDir: simpleContractPath },
+        { contractDir: simpleContractPath, walletIndex: 1 },
+      ],
+    });
+
+    const {
+      contracts: [contract1, contract2],
+      wallets: [wallet1, wallet2],
+    } = launched;
+
+    expect(contract1.account).toEqual(wallet1);
+    expect(contract2.account).toEqual(wallet2);
   });
 });
