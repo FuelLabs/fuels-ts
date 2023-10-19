a693d1e29efa5f4325503050a3cc4d2c5373ecf6
Daniel Bate <djbate23@gmail.com>
2023-10-19 09:48:33 +0100

    feat: add e2e to all find tests flag

--- scripts/tests-find.sh
+++ scripts/tests-find.sh
@@ -5,7 +5,7 @@ ROOT=$(cd "$(dirname "$0")/.."; pwd)
 FILES=$(find $ROOT/{apps,packages,internal} -name '*.test.ts')
 
 if [[ $* == *--all* ]]; then
-  grep -lE "@group\s+(node|browser)" $FILES
+  grep -lE "@group\s+(node|browser|e2e)" $FILES
 elif [[ $* == *--browser* ]]; then
   grep -lE "@group\s+browser" $FILES
 elif [[ $* == *--node* ]]; then
