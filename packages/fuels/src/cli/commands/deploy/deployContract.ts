550439ca968b6fa58e8b857b0f3bc11a9b6a0b35
Anderson Arboleya <anderson@arboleya.me>
2023-10-19 19:53:13 -0300

    Auto-setting `gasPrice` when deploying a contract if necessary

--- packages/fuels/src/cli/commands/deploy/deployContract.ts
+++ packages/fuels/src/cli/commands/deploy/deployContract.ts
@@ -12,11 +12,16 @@ export async function deployContract(
   deployConfig?: DeployContractOptions
 ) {
   debug(`Deploying contract for ABI: ${abiPath}`);
+
   const bytecode = await readFileSync(binaryPath);
+  const { minGasPrice: gasPrice } = wallet.provider.getGasConfig();
 
   const abi = JSON.parse(readFileSync(abiPath, 'utf-8'));
   const contractFactory = new ContractFactory(bytecode, abi, wallet);
 
-  const contract = await contractFactory.deployContract(deployConfig);
+  const config = deployConfig ?? { gasPrice };
+  config.gasPrice = config.gasPrice ?? gasPrice;
+
+  const contract = await contractFactory.deployContract(config);
   return contract.id.toB256();
 }
