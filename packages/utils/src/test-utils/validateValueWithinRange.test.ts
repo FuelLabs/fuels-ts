31980bdc0123e0c48aa3251fb635beb24c756909
Torres-ssf <sergio.uft@gmail.com>
2023-10-20 10:57:08 -0300

    add test util

---
+++ packages/utils/src/test-utils/validateValueWithinRange.test.ts
@@ -0,0 +1,50 @@
+import { validateValueWithinRange } from './validateValueWithinRange';
+
+describe('expectValueToBeInRange', () => {
+  it('should throw an error when value is less than the minimum', () => {
+    expect(() => {
+      validateValueWithinRange({
+        value: 4,
+        min: 5,
+        max: 10,
+      });
+    }).toThrow(`Expected value: '4' to be within range: '5-10'`);
+  });
+
+  it('should throw an error when value is greater than the maximum', () => {
+    expect(() => {
+      validateValueWithinRange({
+        value: 11,
+        min: 5,
+        max: 10,
+      });
+    }).toThrow(`Expected value: '11' to be within range: '5-10'`);
+  });
+
+  it('should return true when value is equal to the minimum', () => {
+    const result = validateValueWithinRange({
+      value: 5,
+      min: 5,
+      max: 10,
+    });
+    expect(result).toBe(true);
+  });
+
+  it('should return true when value is equal to the maximum', () => {
+    const result = validateValueWithinRange({
+      value: 10,
+      min: 5,
+      max: 10,
+    });
+    expect(result).toBe(true);
+  });
+
+  it('should return true when value is within the range', () => {
+    const result = validateValueWithinRange({
+      value: 7,
+      min: 5,
+      max: 10,
+    });
+    expect(result).toBe(true);
+  });
+});
