f2ec3f8cd795c4f7c11e62dc8a9e127d3ce254ef
Anderson Arboleya <anderson@arboleya.me>
2023-10-20 10:49:03 -0300

    Fixing broken tests

--- packages/fuels/src/cli/commands/init/shouldUseBuiltinForc.test.ts
+++ packages/fuels/src/cli/commands/init/shouldUseBuiltinForc.test.ts
@@ -1,6 +1,5 @@
 import { safeExec } from '@fuel-ts/errors/test-utils';
 import * as getSystemFuelCoreMod from '@fuel-ts/versions/cli';
-import * as promptMod from 'prompts';
 
 import * as loggerMod from '../../utils/logger';
 
@@ -20,73 +19,50 @@ jest.mock('prompts', () => ({
 describe('shouldUseBuiltinFuelCore', () => {
   beforeEach(jest.restoreAllMocks);
 
-  function mockAll(returns: { getSystemFuelCore: string | null; prompt: boolean }) {
+  function mockAll(returns: { getSystemFuelCore: string | null }) {
     const getSystemFuelCore = jest
       .spyOn(getSystemFuelCoreMod, 'getSystemFuelCore')
       .mockReturnValue({ error: null, systemFuelCoreVersion: returns.getSystemFuelCore });
-
-    const prompt = jest
-      .spyOn(promptMod, 'prompt')
-      .mockReturnValue(Promise.resolve({ useBuiltinFuelCore: returns.prompt }));
 
     const warn = jest.spyOn(loggerMod, 'warn').mockImplementation();
     const error = jest.spyOn(loggerMod, 'error').mockImplementation();
 
     return {
       getSystemFuelCore,
-      prompt,
       warn,
       error,
     };
   }
 
-  it('should use system fuel-core', async () => {
-    expect(await shouldUseBuiltinFuelCore(false)).toEqual(false);
-  });
+  it('should select [built-in] fuel-core', () => {
+    const { getSystemFuelCore, warn, error } = mockAll({ getSystemFuelCore: null });
 
-  it('should use built-in fuel-core', async () => {
-    expect(await shouldUseBuiltinFuelCore(true)).toEqual(true);
-  });
-
-  it('should select [built-in] fuel-core', async () => {
-    const { getSystemFuelCore, prompt, warn, error } = mockAll({
-      getSystemFuelCore: null,
-      prompt: true,
-    });
-
-    const useBuiltinFuelCore = await shouldUseBuiltinFuelCore();
+    const useBuiltinFuelCore = shouldUseBuiltinFuelCore();
 
     expect(useBuiltinFuelCore).toEqual(true);
     expect(getSystemFuelCore).toHaveBeenCalledTimes(1);
-    expect(prompt).toHaveBeenCalledTimes(1);
 
     expect(warn).toHaveBeenCalledTimes(1);
     expect(warn.mock.calls[0][0]).toEqual(makeWarnMessage('fuel-core'));
     expect(error).toHaveBeenCalledTimes(0);
   });
 
-  it('should select [system] fuel-core', async () => {
-    const { getSystemFuelCore, prompt, warn, error } = mockAll({
+  it('should select [system] fuel-core', () => {
+    const { getSystemFuelCore, warn, error } = mockAll({
       getSystemFuelCore: '1.0.0',
-      prompt: true,
     });
 
-    const useBuiltinFuelCore = await shouldUseBuiltinFuelCore();
+    const useBuiltinFuelCore = shouldUseBuiltinFuelCore();
 
     expect(useBuiltinFuelCore).toEqual(false);
     expect(getSystemFuelCore).toHaveBeenCalledTimes(1);
-
-    expect(prompt).toHaveBeenCalledTimes(0);
 
     expect(warn).toHaveBeenCalledTimes(0);
     expect(error).toHaveBeenCalledTimes(0);
   });
 
   it('should error if nothing satisfies', async () => {
-    const { getSystemFuelCore, prompt, warn, error } = mockAll({
-      getSystemFuelCore: null,
-      prompt: false,
-    });
+    const { getSystemFuelCore, warn, error } = mockAll({ getSystemFuelCore: null });
 
     const safe = await safeExec(() => shouldUseBuiltinFuelCore());
 
@@ -95,7 +71,6 @@ describe('shouldUseBuiltinFuelCore', () => {
     expect(safe.result).not.toBeTruthy();
 
     expect(getSystemFuelCore).toHaveBeenCalledTimes(1);
-    expect(prompt).toHaveBeenCalledTimes(1);
 
     expect(warn).toHaveBeenCalledTimes(0);
     expect(error).toHaveBeenCalledTimes(1);
--- packages/fuels/src/cli/commands/init/shouldUseBuiltinFuelCore.test.ts
+++ packages/fuels/src/cli/commands/init/shouldUseBuiltinFuelCore.test.ts
@@ -1,6 +1,5 @@
 import { safeExec } from '@fuel-ts/errors/test-utils';
 import * as getSystemForcMod from '@fuel-ts/versions/cli';
-import * as promptMod from 'prompts';
 
 import * as loggerMod from '../../utils/logger';
 
@@ -12,81 +11,51 @@ jest.mock('@fuel-ts/versions/cli', () => ({
   ...jest.requireActual('@fuel-ts/versions/cli'),
 }));
 
-jest.mock('prompts', () => ({
-  __esModule: true,
-  ...jest.requireActual('prompts'),
-}));
-
 describe('shouldUseBuiltinForc', () => {
   beforeEach(jest.restoreAllMocks);
 
-  function mockAll(returns: { getSystemForc: string | null; prompt: boolean }) {
+  function mockAll(returns: { getSystemForc: string | null }) {
     const getSystemForc = jest
       .spyOn(getSystemForcMod, 'getSystemForc')
       .mockReturnValue({ error: null, systemForcVersion: returns.getSystemForc });
-
-    const prompt = jest
-      .spyOn(promptMod, 'prompt')
-      .mockReturnValue(Promise.resolve({ useBuiltinForc: returns.prompt }));
 
     const warn = jest.spyOn(loggerMod, 'warn').mockImplementation();
     const error = jest.spyOn(loggerMod, 'error').mockImplementation();
 
     return {
       getSystemForc,
-      prompt,
       warn,
       error,
     };
   }
 
-  it('should use system forc', async () => {
-    expect(await shouldUseBuiltinForc(false)).toEqual(false);
-  });
+  it('should select [built-in] forc', () => {
+    const { getSystemForc, warn, error } = mockAll({ getSystemForc: null });
 
-  it('should use built-in forc', async () => {
-    expect(await shouldUseBuiltinForc(true)).toEqual(true);
-  });
-
-  it('should select [built-in] forc', async () => {
-    const { getSystemForc, prompt, warn, error } = mockAll({
-      getSystemForc: null,
-      prompt: true,
-    });
-
-    const useBuiltinForc = await shouldUseBuiltinForc();
+    const useBuiltinForc = shouldUseBuiltinForc();
 
     expect(useBuiltinForc).toEqual(true);
     expect(getSystemForc).toHaveBeenCalledTimes(1);
-    expect(prompt).toHaveBeenCalledTimes(1);
 
     expect(warn).toHaveBeenCalledTimes(1);
     expect(warn.mock.calls[0][0]).toEqual(makeWarnMessage('forc'));
     expect(error).toHaveBeenCalledTimes(0);
   });
 
-  it('should select [system] forc', async () => {
-    const { getSystemForc, prompt, warn, error } = mockAll({
-      getSystemForc: '1.0.0',
-      prompt: true,
-    });
+  it('should select [system] forc', () => {
+    const { getSystemForc, warn, error } = mockAll({ getSystemForc: '1.0.0' });
 
-    const useBuiltinForc = await shouldUseBuiltinForc();
+    const useBuiltinForc = shouldUseBuiltinForc();
 
     expect(useBuiltinForc).toEqual(false);
     expect(getSystemForc).toHaveBeenCalledTimes(1);
-
-    expect(prompt).toHaveBeenCalledTimes(0);
 
     expect(warn).toHaveBeenCalledTimes(0);
     expect(error).toHaveBeenCalledTimes(0);
   });
 
   it('should error if nothing satisfies', async () => {
-    const { getSystemForc, prompt, warn, error } = mockAll({
-      getSystemForc: null,
-      prompt: false,
-    });
+    const { getSystemForc, warn, error } = mockAll({ getSystemForc: null });
 
     const safe = await safeExec(() => shouldUseBuiltinForc());
 
@@ -95,7 +64,6 @@ describe('shouldUseBuiltinForc', () => {
     expect(safe.result).not.toBeTruthy();
 
     expect(getSystemForc).toHaveBeenCalledTimes(1);
-    expect(prompt).toHaveBeenCalledTimes(1);
 
     expect(warn).toHaveBeenCalledTimes(0);
     expect(error).toHaveBeenCalledTimes(1);
