1ae684e9c92942cc4437b83cfdda928d94e337bb
nedsalk <nedim.salkic@fuel.sh>
2023-10-23 20:00:40 +0200

    almost no breaking changes for the time being

--- packages/wallet/src/test-utils.ts
+++ packages/wallet/src/test-utils.ts
@@ -4,3 +4,4 @@ export * from './test-utils/launchNode';
 export * from './test-utils/launch-custom-provider-and-get-wallets';
 export { WalletConfig } from './test-utils/wallet-config';
 export { AssetId } from './test-utils/asset-id';
+export { launchTestNode as launchNode } from '@fuel-ts/providers/test-utils';
