fde36c53ab1f96a43963b0798418c2f73d59ca6c
Anderson Arboleya <anderson@arboleya.me>
2023-10-25 14:00:27 -0300

    Adding missing test case

--- packages/fuels/src/cli/commands/withConfig.test.ts
+++ packages/fuels/src/cli/commands/withConfig.test.ts
@@ -14,7 +14,7 @@ describe('withConfig', () => {
   beforeEach(mockLogger);
   afterEach(resetDiskAndMocks);
 
-  function mockAll(params: { shouldError: boolean }) {
+  function mockAll(params?: { shouldErrorOnDeploy?: boolean; shouldErrorOnLoadConfig?: boolean }) {
     const onSuccess = jest.fn();
     const onFailure = jest.fn();
 
@@ -30,12 +30,15 @@ describe('withConfig', () => {
       .command(Commands.deploy)
       .option('-p, --path <path>', 'Path to project root', configPath);
 
-    const loadConfig = jest
-      .spyOn(loadConfigMod, 'loadConfig')
-      .mockImplementation((..._) => Promise.resolve(copyConfig));
+    const loadConfig = jest.spyOn(loadConfigMod, 'loadConfig').mockImplementation((..._) => {
+      if (params?.shouldErrorOnLoadConfig) {
+        throw new Error('Something happened');
+      }
+      return Promise.resolve(copyConfig);
+    });
 
     const deploy = jest.fn(() => {
-      if (params.shouldError) {
+      if (params?.shouldErrorOnDeploy) {
         throw new Error('Something happened');
       }
       return Promise.resolve([]);
@@ -56,7 +59,7 @@ describe('withConfig', () => {
 
   test('onSuccess hook in config file', async () => {
     const { command, deploy, configPath, loadConfig, onSuccess, onFailure } = mockAll({
-      shouldError: false,
+      shouldErrorOnDeploy: false,
     });
 
     await withConfig(command, Commands.deploy, deploy)();
@@ -72,7 +75,7 @@ describe('withConfig', () => {
 
   test('onFailure hook in config file', async () => {
     const { command, deploy, error, loadConfig, configPath, onSuccess, onFailure } = mockAll({
-      shouldError: true,
+      shouldErrorOnDeploy: true,
     });
 
     await withConfig(command, Commands.deploy, deploy)();
@@ -85,5 +88,20 @@ describe('withConfig', () => {
     expect(error).toHaveBeenCalledTimes(1);
     expect(onFailure).toHaveBeenCalledTimes(1);
     expect(onFailure.mock.calls[0][0].toString()).toMatch(/something.+happened/i);
+  });
+
+  test('should handle error when loading config file', async () => {
+    const { command, deploy, error, loadConfig, configPath, onSuccess, onFailure } = mockAll({
+      shouldErrorOnLoadConfig: true,
+    });
+
+    await withConfig(command, Commands.deploy, deploy)();
+
+    expect(loadConfig).toHaveBeenCalledTimes(1);
+    expect(loadConfig.mock.calls[0][0]).toEqual(configPath);
+
+    expect(onSuccess).toHaveBeenCalledTimes(0);
+
+    expect(error).toHaveBeenCalledTimes(1);
   });
 });
