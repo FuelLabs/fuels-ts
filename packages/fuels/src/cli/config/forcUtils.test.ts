3b66aaffe411f1479ead39ee2bd8d35e293b222c
Anderson Arboleya <anderson@arboleya.me>
2023-10-25 13:54:49 -0300

    Fixing broken assertion

--- packages/fuels/src/cli/config/forcUtils.test.ts
+++ packages/fuels/src/cli/config/forcUtils.test.ts
@@ -5,7 +5,7 @@ import { readForcToml } from './forcUtils';
 describe('forcUtils', () => {
   test('should throw if Toml file is not found', async () => {
     const tomlPath = '/non/existent/path';
-    const errorMsg = `Toml file not found: \n - ${tomlPath}/Forc.toml`;
+    const errorMsg = `Toml file not found:\n  ${tomlPath}/Forc.toml`;
     const { error, result } = await safeExec(() => readForcToml(tomlPath));
     expect(error?.message).toEqual(errorMsg);
     expect(result).not.toBeTruthy();
