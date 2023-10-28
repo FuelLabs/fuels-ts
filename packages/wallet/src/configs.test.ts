4d225e532a23f9b1b2eb2314a6ec76cd5541fee5
Anderson Arboleya <anderson@arboleya.me>
2023-10-28 08:35:47 -0300

    Adjusting new tests (merged from master) to vite

--- packages/wallet/src/configs.test.ts
+++ packages/wallet/src/configs.test.ts
@@ -9,7 +9,7 @@ describe('Configs - undefined process', () => {
   const originalProcess = process;
 
   beforeEach(() => {
-    jest.resetModules();
+    vi.resetModules();
 
     // @ts-expect-error - test to assert undefined process
     // eslint-disable-next-line no-global-assign
@@ -35,7 +35,7 @@ describe('Configs - overridden env', () => {
   const originalEnv = process.env;
 
   beforeEach(() => {
-    jest.resetModules();
+    vi.resetModules();
 
     process.env = { ...originalEnv, FUEL_NETWORK_URL: 'some-other-network-url' };
   });
