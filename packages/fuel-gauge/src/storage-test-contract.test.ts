5a48c400d5b311ad2379b63601bb08be73491197
nedsalk <nedim.salkic@fuel.sh>
2023-10-31 19:13:41 +0100

    test: storage-test to `TestNodeLauncher`

--- packages/fuel-gauge/src/storage-test-contract.test.ts
+++ packages/fuel-gauge/src/storage-test-contract.test.ts
@@ -1,16 +1,20 @@
+import { TestNodeLauncher } from '@fuel-ts/test-utils';
 import { generateTestWallet } from '@fuel-ts/wallet/test-utils';
 import { readFileSync } from 'fs';
-import type { BN } from 'fuels';
 import { toHex, Provider, ContractFactory, BaseAssetId, FUEL_NETWORK_URL } from 'fuels';
 import { join } from 'path';
 
-import abi from '../fixtures/forc-projects/storage-test-contract/out/debug/storage-test-abi.json';
-import storageSlots from '../fixtures/forc-projects/storage-test-contract/out/debug/storage-test-storage_slots.json';
+import abi from '../fixtures/forc-projects/storage-test/out/debug/storage-test-abi.json';
+import storageSlots from '../fixtures/forc-projects/storage-test/out/debug/storage-test-storage_slots.json';
+
+import { getProgramDir } from './utils';
 
 const binPath = join(
   __dirname,
   '../fixtures/forc-projects/storage-test-contract/out/debug/storage-test.bin'
 );
+
+const contractDir = getProgramDir('storage-test');
 
 const setup = async () => {
   const provider = await Provider.create(FUEL_NETWORK_URL);
@@ -36,13 +40,21 @@ const setup = async () => {
  * @group node
  */
 describe('StorageTestContract', () => {
-  let gasPrice: BN;
-  beforeAll(async () => {
-    const provider = await Provider.create(FUEL_NETWORK_URL);
-    gasPrice = provider.getGasConfig().minGasPrice;
+  beforeAll(async (ctx) => {
+    await TestNodeLauncher.prepareCache(ctx.tasks.length);
+
+    return () => TestNodeLauncher.killCachedNodes();
   });
+
   it('can increment counter', async () => {
-    const contract = await setup();
+    await using launched = await TestNodeLauncher.launch({
+      deployContracts: [contractDir],
+    });
+    const {
+      contracts: [contract],
+      provider,
+    } = launched;
+    const { minGasPrice: gasPrice } = provider.getGasConfig();
 
     // Call contract
     const { value: initializeResult } = await contract.functions
@@ -61,36 +73,38 @@ describe('StorageTestContract', () => {
   });
 
   it('can increment counter - using custom inline storage slots', async () => {
-    const provider = await Provider.create(FUEL_NETWORK_URL);
-    const wallet = await generateTestWallet(provider, [[500_000, BaseAssetId]]);
-    const bytecode = readFileSync(binPath);
-    const factory = new ContractFactory(bytecode, abi, wallet);
-    // #region contract-deployment-storage-slots-inline
-    const contract = await factory.deployContract({
-      gasPrice,
-      storageSlots: [
-        {
-          key: '02dac99c283f16bc91b74f6942db7f012699a2ad51272b15207b9cc14a70dbae',
-          value: '0000000000000001000000000000000000000000000000000000000000000000',
-        },
-        {
-          key: '6294951dcb0a9111a517be5cf4785670ff4e166fb5ab9c33b17e6881b48e964f',
-          value: '0000000000000001000000000000003200000000000000000000000000000000',
-        },
-        {
-          key: 'b48b753af346966d0d169c0b2e3234611f65d5cfdb57c7b6e7cd6ca93707bee0',
-          value: '000000000000001e000000000000000000000000000000000000000000000000',
-        },
-        {
-          key: 'de9090cb50e71c2588c773487d1da7066d0c719849a7e58dc8b6397a25c567c0',
-          value: '0000000000000014000000000000000000000000000000000000000000000000',
-        },
-        {
-          key: 'f383b0ce51358be57daa3b725fe44acdb2d880604e367199080b4379c41bb6ed',
-          value: '000000000000000a000000000000000000000000000000000000000000000000',
-        },
-      ],
+    const customStorageSlots = [
+      {
+        key: '02dac99c283f16bc91b74f6942db7f012699a2ad51272b15207b9cc14a70dbae',
+        value: '0000000000000001000000000000000000000000000000000000000000000000',
+      },
+      {
+        key: '6294951dcb0a9111a517be5cf4785670ff4e166fb5ab9c33b17e6881b48e964f',
+        value: '0000000000000001000000000000003200000000000000000000000000000000',
+      },
+      {
+        key: 'b48b753af346966d0d169c0b2e3234611f65d5cfdb57c7b6e7cd6ca93707bee0',
+        value: '000000000000001e000000000000000000000000000000000000000000000000',
+      },
+      {
+        key: 'de9090cb50e71c2588c773487d1da7066d0c719849a7e58dc8b6397a25c567c0',
+        value: '0000000000000014000000000000000000000000000000000000000000000000',
+      },
+      {
+        key: 'f383b0ce51358be57daa3b725fe44acdb2d880604e367199080b4379c41bb6ed',
+        value: '000000000000000a000000000000000000000000000000000000000000000000',
+      },
+    ];
+
+    await using launched = await TestNodeLauncher.launch({
+      deployContracts: [{ contractDir, options: { storageSlots: customStorageSlots } }],
     });
+    const {
+      contracts: [contract],
+      provider,
+    } = launched;
+    const { minGasPrice: gasPrice } = provider.getGasConfig();
+
     // #endregion contract-deployment-storage-slots-inline
     const { value: initializeResult } = await contract.functions
       .initialize_counter(1300)
