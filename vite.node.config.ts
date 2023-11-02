50392f97fb29562541473c0bf2ff19ccfdd7a4f0
nedsalk <nedim.salkic@fuel.sh>
2023-11-02 15:11:13 +0100

    just coverage-contract to see whats up

--- vite.node.config.ts
+++ vite.node.config.ts
@@ -5,7 +5,7 @@ import baseConfig from './vite.base.config';
 
 const config: UserConfig = {
   test: {
-    include: ['packages/fuel-gauge/src/**/*.test.ts'],
+    include: ['packages/fuel-gauge/src/coverage-contract.test.ts'],
     coverage: {
       reportsDirectory: 'coverage/environments/node',
     },
