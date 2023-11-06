930ab6a1f34f0f694132fed5f5d0efc80ff694dc
Torres-ssf <sergio.uft@gmail.com>
2023-11-06 14:55:14 -0300

    implement draft for PoliciesCoder

--- packages/transactions/src/coders/policy.ts
+++ packages/transactions/src/coders/policy.ts
@@ -1,3 +1,4 @@
+import { Coder, NumberCoder, U64Coder } from '@fuel-ts/abi-coder';
 import type { BN } from '@fuel-ts/math';
 
 // Bitfield of used policy types.
@@ -29,3 +30,44 @@ export type PolicyMaxFee = {
   type: PolicyType.MaxFee;
   data: BN;
 };
+
+export class PoliciesCoder extends Coder<Policy[], Policy[]> {
+  constructor() {
+    super('Policies', 'array Policy', 0);
+  }
+
+  encode(policies: Policy[]): Uint8Array {
+    throw new Error('Implement me');
+  }
+
+  decode(data: Uint8Array, offset: number, policyTypes: number): [Policy[], number] {
+    let o = offset;
+    const policies: Policy[] = [];
+
+    if (policyTypes & PolicyType.GasPrice) {
+      const [gasPrice, nextOffset] = new U64Coder().decode(data, o);
+      o = nextOffset;
+      policies.push({ type: PolicyType.GasPrice, data: gasPrice });
+    }
+
+    if (policyTypes & PolicyType.WitnessLimit) {
+      const [witnessLimit, nextOffset] = new U64Coder().decode(data, o);
+      o = nextOffset;
+      policies.push({ type: PolicyType.WitnessLimit, data: witnessLimit });
+    }
+
+    if (policyTypes & PolicyType.Maturity) {
+      const [maturity, nextOffset] = new NumberCoder('u32').decode(data, o);
+      o = nextOffset;
+      policies.push({ type: PolicyType.Maturity, data: maturity });
+    }
+
+    if (policyTypes & PolicyType.MaxFee) {
+      const [maxFee, nextOffset] = new U64Coder().decode(data, o);
+      o = nextOffset;
+      policies.push({ type: PolicyType.MaxFee, data: maxFee });
+    }
+
+    return [policies, o];
+  }
+}
