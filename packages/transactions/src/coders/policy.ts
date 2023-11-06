af5231df6311efd2109a39fed644a19e81e264e3
Torres-ssf <sergio.uft@gmail.com>
2023-11-06 14:45:40 -0300

    defining policies types

--- packages/transactions/src/coders/policy.ts
+++ packages/transactions/src/coders/policy.ts
@@ -1,6 +1,31 @@
+import type { BN } from '@fuel-ts/math';
+
+// Bitfield of used policy types.
 export enum PolicyType {
   GasPrice = 1,
   WitnessLimit = 2,
   Maturity = 4,
   MaxFee = 8,
 }
+
+export type Policy = PolicyGasPrice | PolicyWitnessLimit | PolicyMaturity | PolicyMaxFee;
+
+export type PolicyGasPrice = {
+  type: PolicyType.GasPrice;
+  data: BN;
+};
+
+export type PolicyWitnessLimit = {
+  type: PolicyType.WitnessLimit;
+  data: BN;
+};
+
+export type PolicyMaturity = {
+  type: PolicyType.Maturity;
+  data: number;
+};
+
+export type PolicyMaxFee = {
+  type: PolicyType.MaxFee;
+  data: BN;
+};
