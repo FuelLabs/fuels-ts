13ca4f5e58367a91f1956e4cd14bd193e5a6db01
nedsalk <nedim.salkic@fuel.sh>
2023-11-07 10:40:50 +0100

    parallelize: managing-deployed-contracts

--- apps/docs-snippets/src/guide/contracts/managing-deployed-contracts.test.ts
+++ apps/docs-snippets/src/guide/contracts/managing-deployed-contracts.test.ts
@@ -1,30 +1,24 @@
-import type { AbstractAddress, WalletUnlocked } from 'fuels';
-import { ContractFactory, Contract } from 'fuels';
+import { TestNodeLauncher } from '@fuel-ts/test-utils';
+import { Contract } from 'fuels';
 
 import { SnippetProjectEnum, getSnippetProjectArtifacts } from '../../../projects';
-import { getTestWallet } from '../../utils';
+import { getProgramDir } from '../../utils';
 
 /**
  * @group node
  */
 describe(__filename, () => {
-  let contract: Contract;
-  let contractId: AbstractAddress;
-  let wallet: WalletUnlocked;
-  const { abiContents: abi, binHexlified: bin } = getSnippetProjectArtifacts(
-    SnippetProjectEnum.ECHO_VALUES
-  );
-
-  beforeAll(async () => {
-    wallet = await getTestWallet();
-    const factory = new ContractFactory(bin, abi, wallet);
-    const { minGasPrice: gasPrice } = wallet.provider.getGasConfig();
-    contract = await factory.deployContract({ gasPrice });
-
-    contractId = contract.id;
-  });
+  const { abiContents: abi } = getSnippetProjectArtifacts(SnippetProjectEnum.ECHO_VALUES);
 
   it('should successfully interact with a deployed contract', async () => {
+    await using launched = await TestNodeLauncher.launch({
+      deployContracts: [getProgramDir('echo-values')],
+    });
+    const {
+      contracts: [{ id: contractId }],
+      wallets: [wallet],
+    } = launched;
+
     // #region managing-deployed-contracts-1
     const deployedContract = new Contract(contractId, abi, wallet);
 
@@ -35,7 +29,15 @@ describe(__filename, () => {
   });
 
   it('should successfully interact with a deployed contract [hexed contract id]', async () => {
-    const b256 = contract.id.toB256();
+    await using launched = await TestNodeLauncher.launch({
+      deployContracts: [getProgramDir('echo-values')],
+    });
+    const {
+      contracts: [{ id: contractId }],
+      wallets: [wallet],
+    } = launched;
+
+    const b256 = contractId.toB256();
 
     // #region managing-deployed-contracts-2
     // #context const b256 = '0x50007a55ccc29075bc0e9c0ea0524add4a7ed4f91afbe1fdcc661caabfe4a82f';
