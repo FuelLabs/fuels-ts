dfb798a22892d3e1864df419e2dbad8f7535f6e2
nedsalk <nedim.salkic@fuel.sh>
2023-11-08 18:22:58 +0100

    remove console.log

--- packages/providers/src/utils/test-utils/launchTestNode.test.ts
+++ packages/providers/src/utils/test-utils/launchTestNode.test.ts
@@ -60,7 +60,7 @@ describe('launchNode', () => {
 
   it('can launch a node on a specific port', async () => {
     const port = '5678';
-    const { cleanup, ip } = await launchTestNode({ port, logger: console.log });
+    const { cleanup, ip } = await launchTestNode({ port });
     expect(await nodeIsRunning(ip, port)).toBe(true);
 
     await cleanup();
