dd931826d2f7dade24a459d87656fcd5dca4884a
Anderson Arboleya <anderson@arboleya.me>
2023-11-09 06:24:51 -0300

    feat: add `deploy` flag to fuels cli `build` command (#1419)

--- packages/fuels/test/features/build.test.ts
+++ packages/fuels/test/features/build.test.ts
@@ -1,8 +1,11 @@
 import { existsSync } from 'fs';
 import { join } from 'path';
 
+import * as deployMod from '../../src/cli/commands/deploy/index';
+import { mockStartFuelCore } from '../utils/mockStartFuelCore';
 import { resetDiskAndMocks } from '../utils/resetDiskAndMocks';
 import {
+  buildFlagsDeploy,
   contractsFooDir,
   generatedDir,
   initFlagsUseBuiltinBinaries,
@@ -14,7 +17,16 @@ describe('build', () => {
   beforeEach(resetDiskAndMocks);
   afterEach(resetDiskAndMocks);
 
+  function mockAll() {
+    const { startFuelCore, killChildProcess } = mockStartFuelCore();
+    const deploy = jest.spyOn(deployMod, 'deploy').mockImplementation();
+
+    return { startFuelCore, killChildProcess, deploy };
+  }
+
   it('should run `build` command', async () => {
+    const { startFuelCore, killChildProcess, deploy } = mockAll();
+
     await runInit();
     await runBuild();
 
@@ -34,9 +46,15 @@ describe('build', () => {
     ].map((f) => join(__dirname, '..', 'fixtures', 'generated', f));
 
     files.forEach((file) => expect(existsSync(file)).toBeTruthy());
+
+    expect(startFuelCore).toHaveBeenCalledTimes(0);
+    expect(deploy).toHaveBeenCalledTimes(0);
+    expect(killChildProcess).toHaveBeenCalledTimes(0);
   });
 
   it('should run `build` command with contracts-only', async () => {
+    const { startFuelCore, killChildProcess, deploy } = mockAll();
+
     await runInit([initFlagsUseBuiltinBinaries, '-c', contractsFooDir, '-o', generatedDir].flat());
     await runBuild();
 
@@ -49,5 +67,20 @@ describe('build', () => {
     ].map((f) => join(__dirname, '..', 'fixtures', 'generated', f));
 
     files.forEach((file) => expect(existsSync(file)).toBeTruthy());
+
+    expect(startFuelCore).toHaveBeenCalledTimes(0);
+    expect(deploy).toHaveBeenCalledTimes(0);
+    expect(killChildProcess).toHaveBeenCalledTimes(0);
+  });
+
+  it('should run `build` command with `--deploy` flag', async () => {
+    const { startFuelCore, killChildProcess, deploy } = mockAll();
+
+    await runInit();
+    await runBuild([buildFlagsDeploy]);
+
+    expect(startFuelCore).toHaveBeenCalledTimes(1);
+    expect(deploy).toHaveBeenCalledTimes(1);
+    expect(killChildProcess).toHaveBeenCalledTimes(1);
   });
 });
