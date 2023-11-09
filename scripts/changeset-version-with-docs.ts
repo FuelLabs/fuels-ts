4cc7c73690854463d671144c5096acba6a2a1b97
Anderson Arboleya <anderson@arboleya.me>
2023-11-09 08:34:30 -0300

    Checking if git is clean before trying to add/commit/release things

--- scripts/changeset-version-with-docs.ts
+++ scripts/changeset-version-with-docs.ts
@@ -1,13 +1,25 @@
 #!/usr/bin/env node
 
 import { execSync } from 'child_process';
+import { error } from 'console';
 
 (() => {
-  // Commit versions generated at pre-build step
-  execSync(`pnpm -C packages/versions prebuild`);
-  execSync(`git add packages/versions/src/lib/getBuiltinVersions.ts`);
-  execSync(`git commit -m"ci(scripts): update versions"`);
+  try {
+    // Invoke versions' prebuild script (will rewrite version files if needed)
+    execSync(`pnpm -C packages/versions prebuild`);
 
-  // run changeset version
-  execSync(`changeset version`);
+    // Checks git status
+    const isGitClean = !execSync(`git status --porcelain`).toString().trim();
+
+    // Skip adding/committing/releasing stuff if there's notthing there
+    if (!isGitClean) {
+      execSync(`git add packages/versions/src/lib/getBuiltinVersions.ts`);
+      execSync(`git commit -m"ci(scripts): update versions"`);
+
+      // Run changeset version
+      execSync(`changeset version`);
+    }
+  } catch (err) {
+    error(err.toString());
+  }
 })();
