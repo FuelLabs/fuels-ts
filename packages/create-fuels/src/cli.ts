253f2cd114ff14ae4a8adc9963ff0b1a699219a7
Dhaiwat <dhaiwatpandya@gmail.com>
2023-11-10 23:50:43 +0300

    add a test

--- packages/create-fuels/src/cli.ts
+++ packages/create-fuels/src/cli.ts
@@ -7,18 +7,24 @@ import prompts from 'prompts';
 
 import packageJson from '../package.json';
 
-export const runScaffoldCli = async () => {
-  let projectPath = '';
-  const program = new Command(packageJson.name).version(packageJson.version).parse(process.argv);
+const log = (...data: unknown[]) => {
+  process.stdout.write(`${data.join(' ')}\n`);
+};
 
-  const res = await prompts({
-    type: 'text',
-    name: 'projectName',
-    message: 'What is the name of your project?',
-    initial: 'my-fuel-project',
-  });
+export const runScaffoldCli = async (explicitProjectPath?: string) => {
+  let projectPath = explicitProjectPath || '';
+  const program = new Command(packageJson.name).version(packageJson.version);
 
-  projectPath = res.projectName;
+  if (!explicitProjectPath) {
+    const res = await prompts({
+      type: 'text',
+      name: 'projectName',
+      message: 'What is the name of your project?',
+      initial: 'my-fuel-project',
+    });
+
+    projectPath = res.projectName;
+  }
 
   if (!projectPath) {
     console.log(
@@ -32,22 +38,22 @@ export const runScaffoldCli = async () => {
 
   await cp(join(__dirname, '../templates/nextjs'), projectPath, { recursive: true });
 
-  console.log();
-  console.log();
-  console.log(chalk.green(`⚡️ Success! Created a a fullstack Fuel dapp at ${projectPath}`));
-  console.log();
-  console.log();
-  console.log('To get started:');
-  console.log();
-  console.log(`- cd into the project directory: cd ${projectPath}`);
-  console.log(`- Install the dependencies: pnpm install`);
-  console.log('- Start a local Fuel dev server: pnpx fuels dev`');
-  console.log('- Run the frontend by running pnpm run dev');
-  console.log();
-  console.log();
-  console.log('-> TS SDK docs: https://fuellabs.github.io/fuels-ts/');
-  console.log('-> Sway docs: https://fuellabs.github.io/sway/');
-  console.log('-> If you have any questions, check the Fuel forum: https://forum.fuel.network/');
-  console.log();
-  console.log();
+  log();
+  log();
+  log(chalk.green(`⚡️ Success! Created a a fullstack Fuel dapp at ${projectPath}`));
+  log();
+  log();
+  log('To get started:');
+  log();
+  log(`- cd into the project directory: cd ${projectPath}`);
+  log(`- Install the dependencies: pnpm install`);
+  log(`- Start a local Fuel dev server: pnpx fuels dev`);
+  log(`- Run the frontend by running pnpm run dev`);
+  log();
+  log();
+  log('-> TS SDK docs: https://fuellabs.github.io/fuels-ts/');
+  log('-> Sway docs: https://fuellabs.github.io/sway/');
+  log('-> If you have any questions, check the Fuel forum: https://forum.fuel.network/');
+  log();
+  log();
 };
