4ad681d21f3439e55edb91637d755374353babc0
Dhaiwat <dhaiwatpandya@gmail.com>
2023-11-10 05:40:09 +0530

    rename stuff

--- templates/nextjs/fuels.config.ts
+++ templates/nextjs/fuels.config.ts
@@ -1,13 +1,6 @@
 import { createConfig } from 'fuels';
 
 export default createConfig({
-  contracts: [
-        'sway-contracts',
-  ],
+  contracts: ['sway-contracts'],
   output: './src/sway-api',
 });
-
-/**
- * Check the docs:
- * https://fuellabs.github.io/fuels-ts/guide/cli/config-file
- */
