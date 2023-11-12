c5cf5874daf81d862b6f7e0cb77f72beab9919b2
Anderson Arboleya <anderson@arboleya.me>
2023-11-12 19:32:52 -0300

    Revamping test utilities

--- packages/fuels/test/utils/mockLogger.ts
+++ packages/fuels/test/utils/mockLogger.ts
@@ -1,10 +1,10 @@
 import * as logger from '../../src/cli/utils/logger';
 
 export function mockLogger() {
-  const error = vi.spyOn(logger, 'error').mockImplementation();
-  const warn = vi.spyOn(logger, 'warn').mockImplementation();
-  const log = vi.spyOn(logger, 'log').mockImplementation();
-  const debug = vi.spyOn(logger, 'debug').mockImplementation();
+  const error = vi.spyOn(logger, 'error').mockReturnValue();
+  const warn = vi.spyOn(logger, 'warn').mockReturnValue();
+  const log = vi.spyOn(logger, 'log').mockReturnValue();
+  const debug = vi.spyOn(logger, 'debug').mockReturnValue();
   return {
     error,
     warn,
