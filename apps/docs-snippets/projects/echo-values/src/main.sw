49160b9d67a4ff71b452f60eb1aa7bdc8f1cf6c7
Daniel Bate <djbate23@gmail.com>
2023-11-15 19:44:15 +0000

    REVERT: test suite for encoding

--- apps/docs-snippets/projects/echo-values/src/main.sw
+++ apps/docs-snippets/projects/echo-values/src/main.sw
@@ -3,10 +3,36 @@ contract;
 
 use std::b512::B512;
 
+struct MyStruct {
+    a: u8,
+    b: bool,
+}
+
+enum MyEnum {
+    a: u8,
+    b: bool,
+}
+
 abi EchoValues {
     fn echo_u8(value: u8) -> u8;
 
     fn echo_u8_array() -> [u8; 2];
+
+    fn echo_u8_array_with_value(value: [u8; 2]) -> [u8; 2];
+
+    fn echo_boolean_array() -> [bool; 2];
+
+    fn echo_boolean_array_with_value(value: [bool; 2]) -> [bool; 2];
+
+    fn echo_mixed_array_with_value(value: (u8, bool)) -> (u8, bool);
+
+    fn echo_mixed_struct_with_value(value: MyStruct) -> MyStruct;
+
+    fn echo_mixed_enum_with_value(value: MyEnum) -> MyEnum;
+
+    fn echo_u8_vector() -> Vec<u8>;
+
+    fn echo_u8_vector_with_value(value: Vec<u8>) -> Vec<u8>;
 
     fn echo_str_8(value: str[8]) -> str[8];
 
@@ -22,6 +48,43 @@ impl EchoValues for Contract {
 
     fn echo_u8_array() -> [u8; 2] {
         [48, 63]
+    }
+
+    fn echo_u8_array_with_value(value: [u8; 2]) -> [u8; 2] {
+        value
+    }
+
+    fn echo_boolean_array() -> [bool; 2] {
+        [true, false]
+    }
+
+    fn echo_boolean_array_with_value(value: [bool; 2]) -> [bool; 2] {
+        value
+    }
+
+    fn echo_mixed_array_with_value(value: (u8, bool)) -> (u8, bool) {
+        value
+    }
+
+    fn echo_mixed_struct_with_value(value: MyStruct) -> MyStruct {
+        value
+    }
+
+    fn echo_mixed_enum_with_value(value: MyEnum) -> MyEnum {
+        value
+    }
+
+    fn echo_u8_vector() -> Vec<u8> {
+        let mut myVec: Vec<u8> = Vec::new();
+        myVec.push(23);
+        myVec.push(32);
+        myVec.push(78);
+
+        myVec
+    }
+
+    fn echo_u8_vector_with_value(value: Vec<u8>) -> Vec<u8> {
+        value
     }
 
     fn echo_str_8(value: str[8]) -> str[8] {
--- apps/docs-snippets/src/guide/contracts/index.test.ts
+++ apps/docs-snippets/src/guide/contracts/index.test.ts
@@ -19,4 +19,75 @@ describe(__filename, () => {
     expect(res1.value).toMatchObject(expected);
     // #endregion echo-values
   });
+
+  it('should successfully echo u8 array', async () => {
+    // #region echo-values
+    const expected = [48, 63];
+
+    const res1 = await contract.functions.echo_u8_array_with_value(expected).simulate();
+
+    expect(res1.value).toMatchObject(expected);
+    // #endregion echo-values
+  });
+
+  it('should echo boolean array', async () => {
+    const expected = [true, false];
+
+    const res1 = await contract.functions.echo_boolean_array(expected).simulate();
+
+    expect(res1.value).toMatchObject(expected);
+  });
+
+  it('should echo boolean array with value', async () => {
+    const expected = [true, false];
+
+    const res1 = await contract.functions.echo_boolean_array_with_value(expected).simulate();
+
+    expect(res1.value).toMatchObject(expected);
+  });
+
+  it('echos a mixed array with value', async () => {
+    const expected = [73, true];
+
+    const res1 = await contract.functions.echo_mixed_array_with_value(expected).simulate();
+
+    expect(res1.value).toMatchObject(expected);
+  });
+
+  it('echos a mixed struct with value', async () => {
+    const expected = { a: 73, b: true };
+
+    const res1 = await contract.functions.echo_mixed_struct_with_value(expected).simulate();
+
+    expect(res1.value).toMatchObject(expected);
+  });
+
+  it('echos a mixed struct with value', async () => {
+    const expectedA = { a: 73 };
+    const expectedB = { b: true };
+
+    const res1 = await contract.functions.echo_mixed_enum_with_value(expectedA).simulate();
+
+    expect(res1.value).toMatchObject(expectedA);
+
+    const res2 = await contract.functions.echo_mixed_enum_with_value(expectedB).simulate();
+
+    expect(res2.value).toMatchObject(expectedB);
+  });
+
+  it('echos a u8 vector with value', async () => {
+    const expected = [73, 23];
+
+    const res1 = await contract.functions.echo_u8_vector_with_value(expected).simulate();
+
+    expect(res1.value).toMatchObject(expected);
+  });
+
+  it('echos a u8 vector', async () => {
+    const expected = [23, 32, 78];
+
+    const res1 = await contract.functions.echo_u8_vector().simulate();
+
+    expect(res1.value).toMatchObject(expected); // [ 23 ]
+  });
 });
