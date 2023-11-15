83883de7224c574fdd1b1cb8babc041de2607d04
Daniel Bate <djbate23@gmail.com>
2023-11-15 13:57:17 +0000

    feat: rollback boolean changes

--- packages/abi-coder/src/coders/boolean.ts
+++ packages/abi-coder/src/coders/boolean.ts
@@ -5,7 +5,7 @@ import { Coder } from './abstract-coder';
 
 export class BooleanCoder extends Coder<boolean, boolean> {
   constructor() {
-    super('boolean', 'boolean', 1);
+    super('boolean', 'boolean', 8);
   }
 
   encode(value: boolean): Uint8Array {
@@ -14,17 +14,17 @@ export class BooleanCoder extends Coder<boolean, boolean> {
       this.throwError(ErrorCode.ENCODE_ERROR, `Invalid boolean value.`);
     }
 
-    return toBytes(value ? 1 : 0, 1);
+    return toBytes(value ? 8 : 0, 8);
   }
 
   decode(data: Uint8Array, offset: number): [boolean, number] {
-    const bytes = bn(data.slice(offset, offset + 1));
+    const bytes = bn(data.slice(offset, offset + 8));
     if (bytes.isZero()) {
-      return [false, offset + 1];
+      return [false, offset + 8];
     }
     if (!bytes.eq(bn(1))) {
       this.throwError(ErrorCode.DECODE_ERROR, `Invalid boolean value.`);
     }
-    return [true, offset + 1];
+    return [true, offset + 8];
   }
 }
