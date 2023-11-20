2da7d18d947536d2286c8c057a8e624d9c22dca9
Daniel Bate <djbate23@gmail.com>
2023-11-20 16:04:53 +0000

    feat: add missing word padded test coverage for struct coder

--- packages/abi-coder/src/coders/struct.test.ts
+++ packages/abi-coder/src/coders/struct.test.ts
@@ -4,6 +4,7 @@ import { bn } from '@fuel-ts/math';
 import { U32_MAX } from '../../test/utils/constants';
 
 import { BooleanCoder } from './boolean';
+import { NumberCoder } from './number';
 import { StructCoder } from './struct';
 import { U64Coder } from './u64';
 
@@ -32,6 +33,33 @@ describe('StructCoder', () => {
 
   it('should not throw given correctly typed inputs', () => {
     expect(() => coder.encode({ a: true, b: bn(1234) })).not.toThrow();
+  });
+
+  it('pads to word size for encoded data with small bytes', () => {
+    const options = { isSmallBytes: true };
+    const unpaddedCoder = new StructCoder(STRUCT_NAME, {
+      a: new BooleanCoder(options),
+      b: new BooleanCoder(options),
+    });
+    const expected = new Uint8Array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    const actual = unpaddedCoder.encode({ a: true, b: false });
+
+    expect(actual).toStrictEqual(expected);
+  });
+
+  it('pads new offset to word size when decoding data with small bytes', () => {
+    const options = { isSmallBytes: true };
+    const unpaddedCoder = new StructCoder(STRUCT_NAME, {
+      a: new BooleanCoder(options),
+      b: new BooleanCoder(options),
+    });
+
+    const expectedValue = { a: true, b: false };
+    const expectedLength = 16;
+    const [actualValue, actualLength] = unpaddedCoder.decode(new Uint8Array([1, 0]), 0);
+
+    expect(JSON.stringify(actualValue)).toStrictEqual(JSON.stringify(expectedValue));
+    expect(actualLength).toBe(expectedLength);
   });
 
   it('should not throw when provided with extra inputs', () => {
