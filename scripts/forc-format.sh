4e2fa2f244dc6061f5d726d2b1ba8db9075d7429
nedsalk <nedim.salkic@fuel.sh>
2023-11-24 18:46:55 +0100

    default bash formatter

--- scripts/forc-format.sh
+++ scripts/forc-format.sh
@@ -4,15 +4,18 @@ forc_projects=$(find . -type f -name "Forc.toml")
 main_dir=$(pwd)
 forc_fmt=$(realpath ./packages/forc/forc-binaries/forc-fmt)
 
-CORRECT_AUTHORS="authors = [\"Fuel Labs <contact@fuel.sh>\"]"
-
 for i in $forc_projects; do
-    cd ${i/Forc.toml/''}
-    eval $forc_fmt
-    authors=$(cat Forc.toml | grep authors)
-    if [ -n "$authors" ] && [ "$authors" != "$CORRECT_AUTHORS" ]; then
-        echo $authors
-        # sed -i "s/$authors/$CORRECT_AUTHORS/g" Forc.toml
+    cd "${i/Forc.toml/''}" || exit
+
+    eval "$forc_fmt"
+
+    authors_line=$(
+        awk '/authors.*/ { print NR }' Forc.toml
+    )
+    if [ -n "$authors_line" ]; then
+        new_content=$(awk -v line="$authors_line" 'NR==2 {$0="authors = [\"Fuel Labs <contact@fuel.sh>\"]"} 1' Forc.toml)
+        echo "$new_content" >Forc.toml
     fi
-    cd $main_dir
+
+    cd "$main_dir" || exit
 done
