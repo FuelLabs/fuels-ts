eac51d1eaa26060dc65e05a047b0a2af779c68fb
nedsalk <nedim.salkic@fuel.sh>
2023-11-24 18:46:46 +0100

    checkpoint

--- scripts/forc-check.sh
+++ scripts/forc-check.sh
@@ -5,11 +5,18 @@ main_dir=$(pwd)
 forc_fmt=$(realpath ./packages/forc/forc-binaries/forc-fmt)
 
 ERRORED=0
-
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
 for i in $forc_projects; do
     cd ${i/Forc.toml/''}
     eval $forc_fmt --check
     if [ $? = "1" ]; then
+        ERRORED=1
+    fi
+    authors=$(cat Forc.toml | grep authors)
+    if [ -n "$authors" ] && [ "$authors" != "authors = [\"Fuel Labs <contact@fuel.sh>\"]" ]; then
+        echo -e authors field should be: $RED"authors = [\"Fuel Labs <contact@fuel.sh>\"]"$NC but is $RED"$authors"$NC
         ERRORED=1
     fi
     cd $main_dir
--- scripts/forc-format.sh
+++ scripts/forc-format.sh
@@ -4,8 +4,15 @@ forc_projects=$(find . -type f -name "Forc.toml")
 main_dir=$(pwd)
 forc_fmt=$(realpath ./packages/forc/forc-binaries/forc-fmt)
 
+CORRECT_AUTHORS="authors = [\"Fuel Labs <contact@fuel.sh>\"]"
+
 for i in $forc_projects; do
     cd ${i/Forc.toml/''}
     eval $forc_fmt
+    authors=$(cat Forc.toml | grep authors)
+    if [ -n "$authors" ] && [ "$authors" != "$CORRECT_AUTHORS" ]; then
+        echo $authors
+        # sed -i "s/$authors/$CORRECT_AUTHORS/g" Forc.toml
+    fi
     cd $main_dir
 done
