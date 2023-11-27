13502325e086251d02f45d9ee7151721e56a942e
Daniel Bate <djbate23@gmail.com>
2023-11-27 11:45:01 +0000

    chore: litning

--- packages/utils/src/utils/arrayify.ts
+++ packages/utils/src/utils/arrayify.ts
@@ -2,7 +2,7 @@ import { FuelError, ErrorCode } from '@fuel-ts/errors';
 import type { BytesLike } from 'ethers';
 
 /**
-* Converts a bytes-like value to a `Uint8Array`.
+ * Converts a bytes-like value to a `Uint8Array`.
  *
  * @param value - the value to convert to a Uint8Array
  * @returns the Uint8Array
