ea944649f7de1c7c915b258478fcd45b53a22110
nedsalk <nedim.salkic@fuel.sh>
2023-11-27 11:14:53 +0100

    refactor: check only projects for authors field

--- scripts/forc-check.sh
+++ scripts/forc-check.sh
@@ -6,20 +6,26 @@ forc_fmt=$(realpath ./packages/forc/forc-binaries/forc-fmt)
 
 ERRORED=0
 RED='\033[0;31m'
-BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 for i in $forc_projects; do
-    cd ${i/Forc.toml/''}
-    eval $forc_fmt --check
+    cd "${i/Forc.toml/''}" || exit
+    eval "$forc_fmt" --check
     if [ $? = "1" ]; then
         ERRORED=1
     fi
+
+    # do authors checks only on projects, not on workspaces
+    if [ "$(head -n 1 Forc.toml)" != '[project]' ]; then
+        cd "$main_dir" || exit
+        continue
+    fi
+
     authors=$(cat Forc.toml | grep authors)
-    if [ -n "$authors" ] && [ "$authors" != "authors = [\"Fuel Labs <contact@fuel.sh>\"]" ]; then
+    if [ "$authors" != "authors = [\"Fuel Labs <contact@fuel.sh>\"]" ]; then
         echo -e authors field should be: $RED"authors = [\"Fuel Labs <contact@fuel.sh>\"]"$NC but is $RED"$authors"$NC
         ERRORED=1
     fi
-    cd $main_dir
+    cd "$main_dir" || exit
 done
 
 exit $ERRORED
