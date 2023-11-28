e350e73f2e0df3aa16e094b256eb2b78ff99dbef
Torres-ssf <sergio.uft@gmail.com>
2023-11-28 09:47:41 -0300

    adding new fixtures

--- packages/providers/test/fixtures/inputs-and-outputs.ts
+++ packages/providers/test/fixtures/inputs-and-outputs.ts
@@ -1,6 +1,7 @@
 import { getRandomB256 } from '@fuel-ts/address';
-import { ZeroBytes32 } from '@fuel-ts/address/configs';
+import { BaseAssetId, ZeroBytes32 } from '@fuel-ts/address/configs';
 import { bn } from '@fuel-ts/math';
+import type { InputCoin, InputMessage } from '@fuel-ts/transactions';
 import { InputType, OutputType } from '@fuel-ts/transactions';
 
 import type {
@@ -14,6 +15,42 @@ import type {
   ContractTransactionRequestOutput,
 } from '../../src/transaction-request/output';
 
+export const MOCK_COIN_INPUT: InputCoin = {
+  type: InputType.Coin,
+  txID: ZeroBytes32,
+  outputIndex: 0,
+  owner: ZeroBytes32,
+  amount: bn(100),
+  assetId: BaseAssetId,
+  txPointer: {
+    blockHeight: 0,
+    txIndex: 0,
+  },
+  witnessIndex: 0,
+  maturity: 0,
+  predicateGasUsed: bn(0),
+  predicateLength: 0,
+  predicateDataLength: 0,
+  predicate: '0x',
+  predicateData: '0x',
+};
+
+export const MOCK_MESSAGE_INPUT: InputMessage = {
+  type: InputType.Message,
+  sender: ZeroBytes32,
+  recipient: ZeroBytes32,
+  nonce: ZeroBytes32,
+  amount: bn(20),
+  witnessIndex: 0,
+  dataLength: ZeroBytes32.length,
+  data: ZeroBytes32,
+  predicateGasUsed: bn(0),
+  predicateLength: 0,
+  predicateDataLength: 0,
+  predicate: '0x',
+  predicateData: '0x',
+};
+
 export const MOCK_REQUEST_COIN_INPUT: CoinTransactionRequestInput = {
   type: InputType.Coin,
   id: ZeroBytes32,
@@ -23,6 +60,19 @@ export const MOCK_REQUEST_COIN_INPUT: CoinTransactionRequestInput = {
   txPointer: '0x00000000000000000000000000000000',
   witnessIndex: 0,
   maturity: 0,
+};
+
+export const MOCK_REQUEST_PREDICATE_INPUT: CoinTransactionRequestInput = {
+  type: InputType.Coin,
+  id: ZeroBytes32,
+  amount: bn(100),
+  assetId: '0x0101010101010101010101010101010101010101010101010101010101010101',
+  owner: getRandomB256(),
+  txPointer: '0x00000000000000000000000000000000',
+  witnessIndex: 0,
+  maturity: 0,
+  predicate: '0x862512a2363db2b3a375c0d4bbbd27172180d89f23f2e259bac850ab02619301',
+  predicateGasUsed: bn(0),
 };
 
 export const MOCK_REQUEST_MESSAGE_INPUT: MessageTransactionRequestInput = {
