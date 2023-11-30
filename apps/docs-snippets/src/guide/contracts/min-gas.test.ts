2300bc30a4cfceeace0d816f7775b27dce537e5b
Daniel Bate <djbate23@gmail.com>
2023-11-30 18:13:08 -0300

    fix: doc block

--- apps/docs-snippets/src/guide/contracts/min-gas.test.ts
+++ apps/docs-snippets/src/guide/contracts/min-gas.test.ts
@@ -48,7 +48,7 @@ describe(__filename, () => {
     request.addResources(resources);
 
     /**
-     * Get the transaction cost to se a strict gasLimit and min gasPrice
+     * Get the transaction cost to set a strict gasLimit and min gasPrice
      */
     const txCost = await provider.getTransactionCost(request);
     request.gasPrice = txCost.gasPrice;
