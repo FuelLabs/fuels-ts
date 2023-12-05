2f92bee799f11971f1312601e6d65aec9d3d0da9
nedsalk <nedim.salkic@fuel.sh>
2023-12-05 12:51:39 +0100

    refactor: into sed

--- scripts/forc-format.sh
+++ scripts/forc-format.sh
@@ -3,6 +3,7 @@
 forc_projects=$(find . -type f -name "Forc.toml")
 main_dir=$(pwd)
 forc_fmt=$(realpath ./packages/forc/forc-binaries/forc-fmt)
+authors="authors = [\"Fuel Labs <contact@fuel.sh>\"]"
 
 for i in $forc_projects; do
     cd "${i/Forc.toml/''}" || exit
@@ -15,18 +16,11 @@ for i in $forc_projects; do
         continue
     fi
 
-    authors_line=$(
-        awk '/authors =.*/ { print NR }' Forc.toml
-    )
-
-    new_content=""
-    if [ "$authors_line" = "" ]; then
-        new_content=$(awk 'NR == 2 {print "authors = [\"Fuel Labs <contact@fuel.sh>\"]"} 1' Forc.toml)
+    if [ "$(grep "authors =" Forc.toml)" = "" ]; then
+        sed -i "1 a $authors" Forc.toml
     else
-        new_content=$(awk -v line="$authors_line" 'NR == line {$0="authors = [\"Fuel Labs <contact@fuel.sh>\"]"} 1' Forc.toml)
+        sed -i -E "s/authors =.*/${authors}/g" Forc.toml
     fi
-
-    echo "$new_content" >Forc.toml
 
     cd "$main_dir" || exit
 done
