a46343d1126398ddc97a5d9c54cc55c0ff540a77
Torres-ssf <sergio.uft@gmail.com>
2023-12-05 13:12:45 -0300

    update test to conform with new fee calculations

--- apps/docs-snippets/src/guide/scripts/script-custom-transaction.test.ts
+++ apps/docs-snippets/src/guide/scripts/script-custom-transaction.test.ts
@@ -1,5 +1,5 @@
 import { BN, ContractFactory, BaseAssetId, ScriptTransactionRequest } from 'fuels';
-import type { CoinQuantityLike, Contract, WalletUnlocked } from 'fuels';
+import type { CoinQuantityLike, Contract, WalletUnlocked, Provider } from 'fuels';
 
 import {
   DocSnippetProjectsEnum,
@@ -9,6 +9,7 @@ import { defaultTxParams, getTestWallet } from '../../utils';
 
 describe(__filename, () => {
   let wallet: WalletUnlocked;
+  let provider: Provider;
   let contract: Contract;
 
   const assetIdB = '0x0101010101010101010101010101010101010101010101010101010101010101';
@@ -30,6 +31,7 @@ describe(__filename, () => {
     ];
 
     wallet = await getTestWallet(seedQuantities);
+    provider = wallet.provider;
     const factory = new ContractFactory(contractBin, contractAbi, wallet);
     const { minGasPrice: gasPrice } = wallet.provider.getGasConfig();
     contract = await factory.deployContract({ gasPrice });
@@ -58,17 +60,24 @@ describe(__filename, () => {
     // 2. Instantiate the script main arguments
     const scriptArguments = [contract.id.toB256(), assetIdA, new BN(1000), assetIdB, new BN(500)];
 
-    // 3. Get the resources for inputs and outpoints
-    const quantities: CoinQuantityLike[] = [[1000, assetIdA], [500, assetIdB], [100_000]];
+    // 3. Populate the script data and add the contract input and output
+    request.setData(abiContents, scriptArguments).addContractInputAndOutput(contract.id);
+
+    // 4. Calculate the transaction fee
+    const { maxFee } = await provider.getTransactionCost(request);
+
+    // 5. Get the transaction resources
+    const quantities: CoinQuantityLike[] = [
+      [1000, assetIdA],
+      [500, assetIdB],
+      [maxFee, BaseAssetId],
+    ];
+
     const resources = await wallet.getResourcesToSpend(quantities);
 
-    // 4. Populate the script data and inputs/outputs
-    request
-      .setData(abiContents, scriptArguments)
-      .addContractInputAndOutput(contract.id)
-      .addResources(resources);
+    request.addResources(resources);
 
-    // 5. Send the transaction
+    // 6. Send the transaction
     const tx = await wallet.sendTransaction(request);
     await tx.waitForResult();
 
