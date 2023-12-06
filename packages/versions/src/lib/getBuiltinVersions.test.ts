f08e6ea51f02cae752a153d6ec33984b95e75e76
Torres-ssf <sergio.uft@gmail.com>
2023-12-06 08:05:17 -0300

    ajusting expected versions

--- packages/versions/src/lib/getBuiltinVersions.test.ts
+++ packages/versions/src/lib/getBuiltinVersions.test.ts
@@ -2,8 +2,7 @@ import { readVersionsFromFiles } from '../../scripts/rewriteVersions';
 
 import { getBuiltinVersions } from './getBuiltinVersions';
 
-// TODO: unskip test after Forc release
-describe.skip('getBuiltinVersions.js', () => {
+describe('getBuiltinVersions.js', () => {
   test('should return received version of default', () => {
     const versions = getBuiltinVersions();
     const versionsFromFiles = readVersionsFromFiles();
--- packages/versions/src/lib/getBuiltinVersions.ts
+++ packages/versions/src/lib/getBuiltinVersions.ts
@@ -1,7 +1,7 @@
 export function getBuiltinVersions() {
   return {
-    FORC: 'feat/transaction-policies',
-    FUEL_CORE: 'git:master',
+    FORC: '0.48.0',
+    FUEL_CORE: '0.21.0',
     FUELS: '0.67.0',
   };
 }
