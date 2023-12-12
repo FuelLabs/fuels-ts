95dda6d6c1ea6df089237a3af147b713db606ee5
torres-ssf <sergio.uft@gmail.com>
2023-12-12 12:51:27 -0300

    improving doc page Asset Transfers

--- apps/docs-snippets/src/guide/cookbook/transferring-assets.test.ts
+++ apps/docs-snippets/src/guide/cookbook/transferring-assets.test.ts
@@ -8,29 +8,29 @@ import {
 import { getTestWallet } from '../../utils';
 
 describe(__filename, () => {
-  let senderWallet: WalletUnlocked;
+  let sender: WalletUnlocked;
   let deployedContract: Contract;
   let provider: Provider;
 
   beforeAll(async () => {
-    senderWallet = await getTestWallet();
+    sender = await getTestWallet();
 
     const { abiContents, binHexlified } = getDocsSnippetsForcProject(
       DocSnippetProjectsEnum.COUNTER
     );
-    provider = senderWallet.provider;
-    const factory = new ContractFactory(binHexlified, abiContents, senderWallet);
-    const { minGasPrice } = senderWallet.provider.getGasConfig();
+    provider = sender.provider;
+    const factory = new ContractFactory(binHexlified, abiContents, sender);
+    const { minGasPrice } = sender.provider.getGasConfig();
     deployedContract = await factory.deployContract({ gasPrice: minGasPrice });
   });
 
-  it('should successfully transfer asset to another wallet', async () => {
+  it('should successfully transfer asset to another account', async () => {
     // #region transferring-assets-1
     // #context import { Wallet, BN, BaseAssetId } from 'fuels';
 
-    // #context const senderWallet = Wallet.fromPrivateKey('...');
+    // #context const sender = Wallet.fromPrivateKey('...');
     const destination = Wallet.generate({
-      provider: senderWallet.provider,
+      provider: sender.provider,
     });
     const amountToTransfer = 500;
     const assetId = BaseAssetId;
@@ -42,7 +42,7 @@ describe(__filename, () => {
       gasLimit: 1_000,
     };
 
-    const response = await senderWallet.transfer(
+    const response = await sender.transfer(
       destination.address,
       amountToTransfer,
       assetId,
@@ -59,9 +59,82 @@ describe(__filename, () => {
     // #endregion transferring-assets-1
   });
 
-  it('should successfully transfer asset to a deployed contract', async () => {
-    const contractId = Address.fromAddressOrString(deployedContract.id);
+  it('should successfully prepare transfer to another account', async () => {
+    const destination = Wallet.generate({
+      provider: sender.provider,
+    });
+
+    const amountToTransfer = 200;
+    const assetId = BaseAssetId;
+
+    const { minGasPrice } = provider.getGasConfig();
+
+    const txParams: TxParams = {
+      gasPrice: minGasPrice,
+      gasLimit: 1_000,
+    };
+
     // #region transferring-assets-2
+    const transactionRequest = await sender.createTransfer(
+      destination.address,
+      amountToTransfer,
+      assetId,
+      txParams
+    );
+
+    const chainId = provider.getChainId();
+
+    const transactionId = transactionRequest.getTransactionId(chainId);
+
+    const response = await sender.sendTransaction(transactionRequest);
+
+    const { id } = await response.wait();
+
+    // The transaction id should is the same as the one returned by the transaction request
+    expect(id).toEqual(transactionId);
+    // #endregion transferring-assets-2
+  });
+
+  it('should validate that modifying the transaction request will result in another TX ID', async () => {
+    const destination = Wallet.generate({
+      provider: sender.provider,
+    });
+
+    const amountToTransfer = 200;
+    const assetId = BaseAssetId;
+
+    const { minGasPrice } = provider.getGasConfig();
+
+    const txParams: TxParams = {
+      gasPrice: minGasPrice,
+      gasLimit: 1_000,
+    };
+
+    // #region transferring-assets-3
+    const transactionRequest = await sender.createTransfer(
+      destination.address,
+      amountToTransfer,
+      assetId,
+      txParams
+    );
+
+    const chainId = provider.getChainId();
+
+    const transactionId = transactionRequest.getTransactionId(chainId);
+
+    transactionRequest.maturity = 1;
+
+    const response = await sender.sendTransaction(transactionRequest);
+
+    const { id } = await response.wait();
+
+    expect(id).not.toEqual(transactionId);
+    // #endregion transferring-assets-3
+  });
+
+  it('should successfully prepare transfer transaction request', async () => {
+    const contractId = Address.fromAddressOrString(deployedContract.id);
+    // #region transferring-assets-4
     // #context import { Wallet, BN, BaseAssetId } from 'fuels';
 
     // #context const senderWallet = Wallet.fromPrivateKey('...');
@@ -79,17 +152,12 @@ describe(__filename, () => {
       gasLimit: 1_000,
     };
 
-    const tx = await senderWallet.transferToContract(
-      contractId,
-      amountToTransfer,
-      assetId,
-      txParams
-    );
+    const tx = await sender.transferToContract(contractId, amountToTransfer, assetId, txParams);
     expect(new BN(contractBalance).toNumber()).toBe(0);
 
     await tx.waitForResult();
 
     expect(new BN(await deployedContract.getBalance(assetId)).toNumber()).toBe(amountToTransfer);
-    // #endregion transferring-assets-2
+    // #endregion transferring-assets-4
   });
 });
