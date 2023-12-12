0a598f3869b38a9d928effff6aff4004cb17a660
torres-ssf <sergio.uft@gmail.com>
2023-12-12 09:06:14 -0300

    add test case for mint tx

--- packages/providers/src/transaction-summary/calculate-transaction-fee.test.ts
+++ packages/providers/src/transaction-summary/calculate-transaction-fee.test.ts
@@ -4,6 +4,7 @@ import { describe } from 'node:test';
 import { MOCK_CHAIN } from '../../test/fixtures/chain';
 import {
   MOCK_TX_CREATE_RAW_PAYLOAD,
+  MOCK_TX_MINT_RAW_PAYLOAD,
   MOCK_TX_SCRIPT_RAW_PAYLOAD,
 } from '../../test/fixtures/transaction-summary';
 
@@ -68,5 +69,30 @@ describe('calculateTransactionFee', () => {
     expect(feeFromGasUsed.toNumber()).toEqual(expectedfeeFromGasUsed.toNumber());
     expect(maxFee.toNumber()).toEqual(expectedmaxFee.toNumber());
     expect(minFee.toNumber()).toEqual(expectedminFee.toNumber());
+  });
+
+  it('should properly calculate the transaction fee (MINT TX)', () => {
+    const transactionRawPayload = MOCK_TX_MINT_RAW_PAYLOAD;
+    const { gasCosts } = MOCK_CHAIN.consensusParameters;
+
+    const gasPriceFactor = 92;
+    const gasPerByte = 4;
+
+    const { fee, feeFromGasUsed, maxFee, minFee } = calculateTransactionFee({
+      consensusParameters: {
+        feeParams: {
+          gasPriceFactor,
+          gasPerByte,
+        },
+        gasCosts,
+      },
+      gasUsed: bn(1),
+      rawPayload: transactionRawPayload,
+    });
+
+    expect(fee.toNumber()).toEqual(0);
+    expect(feeFromGasUsed.toNumber()).toEqual(0);
+    expect(maxFee.toNumber()).toEqual(0);
+    expect(minFee.toNumber()).toEqual(0);
   });
 });
