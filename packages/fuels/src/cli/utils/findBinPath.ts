c32f4c763940ebdecc2f69df2fda54e47f6aceb8
Dhaiwat <dhaiwatpandya@gmail.com>
2023-12-14 12:08:11 +0530

    add some console logs

--- packages/fuels/src/cli/utils/findBinPath.ts
+++ packages/fuels/src/cli/utils/findBinPath.ts
@@ -7,10 +7,15 @@ const npmWhich = require('npm-which')(__dirname);
 export function findBinPath(binCommandName: string) {
   let binPath = npmWhich.sync(binCommandName);
 
+  console.log('binPath', binPath);
+
   if (!existsSync(binPath)) {
+    console.log('binPath does not exist');
     // The user might be using bun, which has a different structure for binaries inside node_modules
     binPath = join('node_modules', '.bin', binCommandName);
   }
+
+  console.log('final binPath', binPath);
 
   return binPath;
 }
