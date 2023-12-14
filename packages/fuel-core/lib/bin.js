1ac2a1581784fdc5db7a0ada148c7673985eab74
Dhaiwat <dhaiwatpandya@gmail.com>
2023-12-14 12:28:41 +0530

    add console llog

--- packages/fuel-core/lib/bin.js
+++ packages/fuel-core/lib/bin.js
@@ -6,4 +6,5 @@ import { spawn } from 'child_process';
 import binPath from './index.js';
 
 const args = process.argv.slice(2);
+console.log('binPath inside fuel-core bin.js', binPath);
 spawn(binPath, args, { stdio: 'inherit' }).on('exit', process.exit);
