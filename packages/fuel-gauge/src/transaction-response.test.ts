3bc43255934d69fed24a3f07e36c4847744af892
nedsalk <nedim.salkic@fuel.sh>
2023-12-17 12:47:01 +0100

    lower --poa to 3s

--- packages/fuel-gauge/src/transaction-response.test.ts
+++ packages/fuel-gauge/src/transaction-response.test.ts
@@ -84,7 +84,7 @@ describe('TransactionSummary', () => {
 
   it('should ensure waitForResult always waits for the transaction to be processed', async () => {
     const { cleanup, ip, port } = await launchNode({
-      args: ['--poa-interval-period', '5s'],
+      args: ['--poa-interval-period', '3s'],
     });
     const nodeProvider = await Provider.create(`http://${ip}:${port}/graphql`);
 
@@ -111,5 +111,5 @@ describe('TransactionSummary', () => {
     expect(response.gqlTransaction?.id).toBe(transactionId);
 
     cleanup();
-  }, 25000);
+  });
 });
