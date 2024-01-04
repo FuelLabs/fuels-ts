73f0eccc428eb4c7470e7c1a12b48d9125e0e04e
Nedim Salkić <nedim.salkic@fuel.sh>
2024-01-04 08:18:37 +0100

    Update packages/providers/src/coin-quantityfy.test.ts
    Co-authored-by: Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>

--- packages/providers/src/coin-quantityfy.test.ts
+++ packages/providers/src/coin-quantityfy.test.ts
@@ -1,6 +1,8 @@
 import { coinQuantityfy } from './coin-quantity';
 
-test('coinQuantityfy returns 1 when input is <1', () => {
-  expect(coinQuantityfy([0]).amount.toNumber()).toEqual(1);
-  expect(coinQuantityfy([0.9]).amount.toNumber()).toEqual(1);
+describe('coinQuantityfy', () => {
+  it('should returns 1 when input is < 1', () => {
+    expect(coinQuantityfy([0]).amount.toNumber()).toEqual(1);
+    expect(coinQuantityfy([0.9]).amount.toNumber()).toEqual(1);
+  });
 });
