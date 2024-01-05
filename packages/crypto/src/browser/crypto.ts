6f6b856a22581e2e3acb6c6a84555fa66c74d6c6
nedsalk <nedim.salkic@fuel.sh>
2024-01-05 08:39:20 +0100

    feat: throw errors when parts of global `crypto` not supported

--- packages/crypto/src/browser/crypto.ts
+++ packages/crypto/src/browser/crypto.ts
@@ -9,6 +9,27 @@ if (!crypto) {
   );
 }
 
+if (!crypto.randomUUID) {
+  throw new FuelError(
+    ErrorCode.ENV_DEPENDENCY_MISSING,
+    `Could not find 'crypto.randomUUID' in current browser environment.`
+  );
+}
+
+if (!crypto.subtle) {
+  throw new FuelError(
+    ErrorCode.ENV_DEPENDENCY_MISSING,
+    `Could not find 'crypto.subtle' in current browser environment.`
+  );
+}
+
+if (!crypto.getRandomValues) {
+  throw new FuelError(
+    ErrorCode.ENV_DEPENDENCY_MISSING,
+    `Could not find 'crypto.getRandomValues' in current browser environment.`
+  );
+}
+
 if (!btoa) {
   throw new FuelError(
     ErrorCode.ENV_DEPENDENCY_MISSING,
