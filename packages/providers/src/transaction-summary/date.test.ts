baee3db643e040fd2a38f44f67c550480f8507a7
Peter Smith <peter@blueoceancomputing.co.uk>
2024-01-10 08:22:34 +0000

    feat: added date wrapper

--- packages/providers/src/transaction-summary/date.test.ts
+++ packages/providers/src/transaction-summary/date.test.ts
@@ -1,8 +1,9 @@
 import * as tai64Mod from 'tai64';
 
 import * as dateMod from './date';
+import type { IFuelDate } from './date';
 
-const { fromTai64ToDate: tai64toDate, fromDateToTai64: dateToTai64 } = dateMod;
+const { fromTai64ToDate: tai64toDate, fromDateToTai64: dateToTai64, FuelDate } = dateMod;
 
 describe('transaction-summary/date', () => {
   afterEach(jest.restoreAllMocks);
@@ -34,5 +35,113 @@ describe('transaction-summary/date', () => {
 
     expect(toStringSpy).toHaveBeenCalledTimes(1);
     expect(toStringSpy).toHaveBeenCalledWith(10);
+  });
+
+  it('should convert unix to Date correctly', () => {
+    const date = new Date();
+    const unixTimestamp = date.valueOf().toString();
+    const dateSpy = jest.spyOn(global, 'Date').mockImplementation(() => date);
+
+    const result = dateMod.fromUnixToDate(unixTimestamp);
+
+    expect(dateSpy).toHaveBeenCalledTimes(1);
+    expect(dateSpy).toHaveBeenCalledWith(parseInt(unixTimestamp, 10));
+    expect(result).toEqual(date);
+  });
+
+  it('should convert Date to unix correctly', () => {
+    const date = new Date();
+    const unixTimestamp = date.valueOf().toString();
+
+    const result = dateMod.toDateFromUnix(date);
+
+    expect(result).toEqual(unixTimestamp);
+  });
+});
+
+describe('FuelDate', () => {
+  /** Should implement {@link Date} */
+  test('should implement the Date interface', () => {
+    const date: IFuelDate = new FuelDate();
+
+    expect(date.toISOString).toBeDefined();
+    expect(date.toDateString).toBeDefined();
+    expect(date.toISOString).toBeDefined();
+    expect(date.toJSON).toBeDefined();
+    expect(date.toTimeString).toBeDefined();
+    expect(date.toUTCString).toBeDefined();
+    expect(date.valueOf).toBeDefined();
+  });
+
+  /** Implement {@link IFuelDate} */
+  it('should implment the IFuelDate interface', () => {
+    const date: IFuelDate = new FuelDate();
+
+    expect(date.toUnix).toBeDefined();
+    expect(date.toTai64).toBeDefined();
+  });
+
+  describe('unix', () => {
+    test('should be able to instantiate from unix', () => {
+      const fromUnixToDateSpy = jest.spyOn(dateMod, 'fromUnixToDate');
+      const unixTimestamp = '1694450695000';
+
+      const result = FuelDate.from.unix(unixTimestamp);
+
+      expect(result).toBeDefined();
+      expect(fromUnixToDateSpy).toHaveBeenCalledTimes(1);
+      expect(fromUnixToDateSpy).toHaveBeenCalledWith(unixTimestamp);
+    });
+
+    test('should be able to output a unix timestamp', () => {
+      const unixTimestamp = '1694450695000';
+      const date = FuelDate.from.unix(unixTimestamp);
+
+      const result = date.toUnix();
+
+      expect(result).toEqual(unixTimestamp);
+    });
+
+    test('should be able to output a tai64 timestamp', () => {
+      const unixTimestamp = '1694450695000';
+      const tai64Timestamp = '4611686020121838636';
+      const date = FuelDate.from.unix(unixTimestamp);
+
+      const result = date.toTai64();
+
+      expect(result).toEqual(tai64Timestamp);
+    });
+  });
+
+  describe('tai64', () => {
+    test('should be able to instantiate from tai64', () => {
+      const tai64toDateSpy = jest.spyOn(dateMod, 'fromTai64ToDate');
+      const tai64Timestamp = '4611686020121838636';
+
+      const result = FuelDate.from.tai64(tai64Timestamp);
+
+      expect(result).toBeDefined();
+      expect(tai64toDateSpy).toHaveBeenCalledTimes(1);
+      expect(tai64toDateSpy).toHaveBeenCalledWith(tai64Timestamp);
+    });
+
+    test('should be able to output a unix timestamp', () => {
+      const tai64Timestamp = '4611686020121838636';
+      const unixTimestamp = '1694450695000';
+      const date = FuelDate.from.tai64(tai64Timestamp);
+
+      const result = date.toUnix();
+
+      expect(result).toEqual(unixTimestamp);
+    });
+
+    test('should be able to output a tai64 timestamp', () => {
+      const tai64Timestamp = '4611686020121838636';
+      const date = FuelDate.from.tai64(tai64Timestamp);
+
+      const result = date.toTai64();
+
+      expect(result).toEqual(tai64Timestamp);
+    });
   });
 });
--- packages/providers/src/transaction-summary/date.ts
+++ packages/providers/src/transaction-summary/date.ts
@@ -7,3 +7,29 @@ export const fromTai64ToDate = (tai64Timestamp: string): Date => {
 
 export const fromDateToTai64 = (date: Date): string =>
   TAI64.fromUnix(Math.floor(date.getTime() / 1000)).toString(10);
+
+export const fromUnixToDate = (unix: string): Date => new Date(parseInt(unix, 10));
+
+export const toDateFromUnix = (date: Date): string => date.valueOf().toString();
+
+export interface IFuelDate extends Date {
+  /**
+   * @returns Unix timestamp in seconds
+   */
+  toUnix: () => string;
+
+  /**
+   * @returns Unix timestamp in seconds
+   */
+  toTai64: () => string;
+}
+
+export class FuelDate extends Date implements IFuelDate {
+  static from = {
+    unix: (unix: string): IFuelDate => new FuelDate(fromUnixToDate(unix)),
+    tai64: (tai64: string): IFuelDate => new FuelDate(fromTai64ToDate(tai64)),
+  };
+
+  toUnix = () => toDateFromUnix(this);
+  toTai64 = () => fromDateToTai64(this);
+}
