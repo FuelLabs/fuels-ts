d7045d66e510ff78aa04f7d4752f37a6353216f1
Peter Smith <peter@blueoceancomputing.co.uk>
2024-01-12 16:34:36 +0000

    chore: consolidated date and time

--- packages/providers/src/utils/index.ts
+++ packages/providers/src/utils/index.ts
@@ -1,6 +1,5 @@
 export * from './receipts';
 export * from './block-explorer';
-export * from './date';
 export * from './gas';
 export * from './json';
 export * from './sleep';
--- packages/providers/src/utils/time.ts
+++ packages/providers/src/utils/time.ts
@@ -1,3 +1,5 @@
+import { TAI64 } from 'tai64';
+
 /**
  * @hidden
  *
@@ -40,3 +42,37 @@ export const fromTai64ToUnix = (tai64Timestamp: string): number =>
  */
 export const fromUnixToTai64 = (unixTimestampSeconds: number): string =>
   (BigInt(unixTimestampSeconds) + BigInt(2 ** 62) + BigInt(10)).toString();
+
+export const fromTai64ToDate = (tai64Timestamp: string): Date => {
+  const timestamp = TAI64.fromString(tai64Timestamp, 10).toUnix();
+  return new Date(timestamp * 1000);
+};
+
+export const fromDateToTai64 = (date: Date): string =>
+  TAI64.fromUnix(Math.floor(date.getTime() / 1000)).toString(10);
+
+export const fromUnixToDate = (unix: string): Date => new Date(parseInt(unix, 10));
+
+export const fromDateToUnix = (date: Date): string => date.valueOf().toString();
+
+export interface IFuelDate extends Date {
+  /**
+   * @returns Unix timestamp in seconds
+   */
+  toUnix: () => string;
+
+  /**
+   * @returns Unix timestamp in seconds
+   */
+  toTai64: () => string;
+}
+
+export class FuelDate extends Date implements IFuelDate {
+  static from = {
+    unix: (unix: string): IFuelDate => new FuelDate(fromUnixToDate(unix)),
+    tai64: (tai64: string): IFuelDate => new FuelDate(fromTai64ToDate(tai64)),
+  };
+
+  toUnix = (): string => fromDateToUnix(this);
+  toTai64 = (): string => fromDateToTai64(this);
+}
