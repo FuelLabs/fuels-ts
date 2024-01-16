ad601a21d6b3180370d9ff126c99cca9d0502dbe
nedsalk <nedim.salkic@fuel.sh>
2024-01-16 11:15:15 +0100

    test: add tests and fix bugs

---
+++ packages/providers/src/fuel-graphql-request.test.ts
@@ -0,0 +1,170 @@
+import { ErrorCode, FuelError } from '@fuel-ts/errors';
+import { expectToThrowFuelError, safeExec } from '@fuel-ts/errors/test-utils';
+
+import { GetVersionDocument, StatusChangeDocument } from './__generated__/operations';
+import { fuelGraphQLRequest } from './fuel-graphql-request';
+
+const SAMPLE_DOCUMENT = GetVersionDocument;
+const SAMPLE_SUBSCRIPTION_DOCUMENT = StatusChangeDocument;
+
+describe('FuelGraphqlRequest', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  test('Returns correct response when query/mutation response has data property', async () => {
+    const fetchSpy = vi.spyOn(global, 'fetch');
+    const responseBody = { a: 1 };
+    fetchSpy.mockImplementationOnce(() =>
+      Promise.resolve(new Response(JSON.stringify({ data: responseBody })))
+    );
+
+    const { result } = await safeExec(() => fuelGraphQLRequest(fetch, 'url', SAMPLE_DOCUMENT, {}));
+
+    expect(result).toEqual(responseBody);
+  });
+
+  test('Throws error when query/mutation response has errors property', async () => {
+    const fetchSpy = vi.spyOn(global, 'fetch');
+    const errors = [{ message: 'error1' }, { message: 'error2' }];
+    fetchSpy.mockImplementationOnce(() =>
+      Promise.resolve(new Response(JSON.stringify({ errors })))
+    );
+
+    await expectToThrowFuelError(
+      () => fuelGraphQLRequest(fetch, 'url', SAMPLE_DOCUMENT, {}),
+      new FuelError(ErrorCode.INVALID_REQUEST, errors.map((err) => err.message).join('\n\n'))
+    );
+  });
+
+  test('Returns correct response when subscription response has data property', async () => {
+    const fetchSpy = vi.spyOn(global, 'fetch');
+
+    const readableStream = new ReadableStream({
+      start(controller) {
+        const encoder = new TextEncoder();
+
+        controller.enqueue(
+          encoder.encode(`data:${JSON.stringify({ data: { submitAndAwait: { a: 0 } } })}`)
+        );
+        controller.enqueue(
+          encoder.encode(`data:${JSON.stringify({ data: { submitAndAwait: { a: 1 } } })}`)
+        );
+        controller.close();
+      },
+    });
+    fetchSpy.mockImplementationOnce(() => Promise.resolve(new Response(readableStream)));
+
+    let iteration = 0;
+    for await (const { submitAndAwait } of fuelGraphQLRequest(
+      fetch,
+      'url',
+      SAMPLE_SUBSCRIPTION_DOCUMENT,
+      {}
+    ) as AsyncIterable<{ submitAndAwait: { a: number } }>) {
+      expect(submitAndAwait.a).toEqual(iteration);
+      iteration++;
+    }
+  });
+
+  test('Throws error when subscription response has errors property', async () => {
+    const fetchSpy = vi.spyOn(global, 'fetch');
+
+    const errors = [{ message: 'error1' }, { message: 'error2' }];
+
+    const readableStream = new ReadableStream({
+      start(controller) {
+        const encoder = new TextEncoder();
+
+        controller.enqueue(encoder.encode(`data:${JSON.stringify({ errors })}`));
+        controller.close();
+      },
+    });
+    fetchSpy.mockImplementationOnce(() => Promise.resolve(new Response(readableStream)));
+
+    await expectToThrowFuelError(
+      async () => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        for await (const sub of fuelGraphQLRequest(
+          fetch,
+          'url',
+          SAMPLE_SUBSCRIPTION_DOCUMENT,
+          {}
+        ) as AsyncIterable<{ submitAndAwait: { a: number } }>) {
+          // should not be reached
+          expect(true).toBeFalsy();
+        }
+      },
+      new FuelError(ErrorCode.INVALID_REQUEST, errors.map((err) => err.message).join('\n\n'))
+    );
+  });
+
+  test('Closes subscription stream when subscription is cancelled', async () => {
+    const fetchSpy = vi.spyOn(global, 'fetch');
+
+    let canceled = false;
+    const readableStream = new ReadableStream({
+      start(controller) {
+        const encoder = new TextEncoder();
+
+        controller.enqueue(
+          encoder.encode(`data:${JSON.stringify({ data: { submitAndAwait: { a: 0 } } })}`)
+        );
+        controller.enqueue(
+          encoder.encode(`data:${JSON.stringify({ data: { submitAndAwait: { a: 1 } } })}`)
+        );
+      },
+      cancel() {
+        canceled = true;
+      },
+    });
+    fetchSpy.mockImplementationOnce(() => Promise.resolve(new Response(readableStream)));
+
+    let loopBodyHit = false;
+
+    // eslint-disable-next-line no-unreachable-loop
+    for await (const { submitAndAwait } of fuelGraphQLRequest(
+      fetch,
+      'url',
+      SAMPLE_SUBSCRIPTION_DOCUMENT,
+      {}
+    ) as AsyncIterable<{ submitAndAwait: { a: number } }>) {
+      loopBodyHit = true;
+      break;
+    }
+
+    expect(loopBodyHit).toEqual(true);
+    expect(canceled).toEqual(true);
+  });
+
+  test('ignores keep-alive-text messages in subscriptions', async () => {
+    const fetchSpy = vi.spyOn(global, 'fetch');
+
+    const readableStream = new ReadableStream({
+      start(controller) {
+        const encoder = new TextEncoder();
+
+        controller.enqueue(
+          encoder.encode(`data:${JSON.stringify({ data: { submitAndAwait: { a: 0 } } })}`)
+        );
+        controller.enqueue(encoder.encode('keep-alive-text'));
+        controller.enqueue(
+          encoder.encode(`data:${JSON.stringify({ data: { submitAndAwait: { a: 1 } } })}`)
+        );
+        controller.close();
+      },
+    });
+    fetchSpy.mockImplementationOnce(() => Promise.resolve(new Response(readableStream)));
+
+    let iteration = 0;
+    for await (const { submitAndAwait } of fuelGraphQLRequest(
+      fetch,
+      'url',
+      SAMPLE_SUBSCRIPTION_DOCUMENT,
+      {}
+    ) as AsyncIterable<{ submitAndAwait: { a: number } }>) {
+      expect(submitAndAwait.a).toEqual(iteration);
+      iteration++;
+    }
+  });
+});
--- packages/providers/src/fuel-graphql-request.ts
+++ packages/providers/src/fuel-graphql-request.ts
@@ -8,10 +8,9 @@
 
 */
 
+import { FuelError } from '@fuel-ts/errors';
 import type { DocumentNode } from 'graphql';
 import { print } from 'graphql';
-
-import { fuelGraphQLSubscriber } from './fuel-graphql-subscriber';
 
 function isSubscription({ definitions: defs }: DocumentNode) {
   const opDef = defs.find((op) => op.kind === 'OperationDefinition');
@@ -23,13 +22,108 @@ function isSubscription({ definitions: defs }: DocumentNode) {
   return false;
 }
 
+export type FuelGraphQLSubscriberOptions = {
+  url: string;
+  request: RequestInit;
+  fetchFn: typeof fetch;
+};
+
+export class FuelGraphqlSubscriber implements AsyncIterator<unknown> {
+  private stream!: ReadableStreamDefaultReader<Uint8Array>;
+  private static textDecoder = new TextDecoder();
+
+  private static parseBytesStream(
+    bytes: Uint8Array | undefined
+  ): Record<string, unknown> | FuelError | undefined {
+    if (bytes === undefined) {
+      return undefined;
+    }
+
+    const text = this.textDecoder.decode(bytes);
+
+    // https://github.com/FuelLabs/fuel-core/blob/e1e631902f762081d2124d9c457ddfe13ac366dc/crates/fuel-core/src/graphql_api/service.rs#L247
+    if (text === 'keep-alive-text') {
+      // the text can sometimes be a keep-alive message
+      return undefined;
+    }
+
+    const { data, errors } = JSON.parse(text.split('data:')[1]);
+
+    if (Array.isArray(errors)) {
+      return new FuelError(
+        FuelError.CODES.INVALID_REQUEST,
+        errors.map((err) => err.message).join('\n\n')
+      );
+    }
+
+    return data as Record<string, unknown>;
+  }
+
+  public constructor(private options: FuelGraphQLSubscriberOptions) {}
+
+  private async setStream() {
+    const { url, request, fetchFn } = this.options;
+    const response = await fetchFn(`${url}-sub`, {
+      ...request,
+      headers: {
+        ...request.headers,
+        Accept: 'text/event-stream',
+      },
+    });
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    this.stream = response.body!.getReader();
+  }
+
+  private async readStream(): Promise<IteratorResult<unknown, unknown>> {
+    const { value, done } = await this.stream.read();
+
+    const parsed = FuelGraphqlSubscriber.parseBytesStream(value);
+
+    if (parsed === undefined && !done) {
+      // we recursively wait for the next message until it's a proper gql response or the stream is done i.e. {value: undefined, done: true}
+      return this.readStream();
+    }
+
+    return { value: parsed, done };
+  }
+
+  async next(): Promise<IteratorResult<unknown, unknown>> {
+    if (!this.stream) {
+      await this.setStream();
+    }
+
+    const { value, done } = await this.readStream();
+    if (value instanceof FuelError) {
+      throw value;
+    }
+
+    return { value, done };
+  }
+
+  /**
+   * Gets called when `break` is called in a `for-await-of` loop.
+   */
+  async return(): Promise<IteratorResult<unknown, undefined>> {
+    await this.stream.cancel();
+    this.stream.releaseLock();
+    return { done: true, value: undefined };
+  }
+
+  [Symbol.asyncIterator](): AsyncIterator<unknown, unknown, undefined> {
+    return this;
+  }
+}
+
 async function handleQueryAndMutation(fetchFn: typeof fetch, url: string, request: RequestInit) {
   const response = await fetchFn(url, request);
 
   const { data, errors } = await response.json();
 
-  if (errors) {
-    throw new Error(JSON.stringify(errors));
+  if (Array.isArray(errors)) {
+    throw new FuelError(
+      FuelError.CODES.INVALID_REQUEST,
+      errors.map((err) => err.message).join('\n\n')
+    );
   }
 
   return data;
@@ -50,7 +144,7 @@ export function fuelGraphQLRequest(
   };
 
   if (isSubscription(operation)) {
-    return fuelGraphQLSubscriber({
+    return new FuelGraphqlSubscriber({
       fetchFn,
       url,
       request,
