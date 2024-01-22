c135567e24b0841e2c55fa239bd8448db99f024e
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-01-22 14:15:46 -0300

    add type

--- apps/docs/.vitepress/theme/index.ts
+++ apps/docs/.vitepress/theme/index.ts
@@ -3,10 +3,11 @@ import { defineComponent } from 'vue';
 // @ts-ignore
 import LogoComponent from './components/LogoComponent.vue';
 import './styles/custom.css';
+import { EnhanceAppContext } from 'vitepress';
 
 export default defineComponent({
   ...DefaultTheme,
-  enhanceApp({ app }: { app: any }) {
+  enhanceApp({ app }: EnhanceAppContext) {
     app.component('LogoComponent', LogoComponent);
   },
 });
