f963777a57f5fbedfa8c9ad4e09c30387aa91cc5
Daniel Bate <djbate23@gmail.com>
2024-01-22 17:34:22 +0000

    chore: add refactor doc

--- packages/abi-coder/src/abi-coder.ts
+++ packages/abi-coder/src/abi-coder.ts
@@ -69,6 +69,9 @@ export abstract class AbiCoder {
     ];
   }
 
+  /**
+   * TODO: Refactor into factory
+   */
   private static getCoderImpl(
     resolvedAbiType: ResolvedAbiType,
     options: EncodingOptions = {
