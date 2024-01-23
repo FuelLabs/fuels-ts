76a30ddf1d2547df015319d6903a77e8ed8b99d2
Daniel Bate <djbate23@gmail.com>
2024-01-23 15:59:05 +0000

    feat: remove vec encode support

--- packages/abi-coder/src/coders/v1/vec.ts
+++ packages/abi-coder/src/coders/v1/vec.ts
@@ -1,9 +1,8 @@
-import { ErrorCode } from '@fuel-ts/errors';
+import { ErrorCode, FuelError } from '@fuel-ts/errors';
 import { bn } from '@fuel-ts/math';
 
 import { WORD_SIZE } from '../../constants';
-import type { Uint8ArrayWithDynamicData } from '../../utilities';
-import { concatWithDynamicData, BASE_VECTOR_OFFSET, chunkByLength } from '../../utilities';
+import { chunkByLength } from '../../utilities';
 import type { TypesOfCoder } from '../abstract-coder';
 import { Coder } from '../abstract-coder';
 import { U64Coder } from '../v0/u64';
@@ -18,33 +17,13 @@ export class VecCoder<TCoder extends Coder> extends Coder<
   coder: TCoder;
 
   constructor(coder: TCoder) {
-    super('struct', `struct Vec`, coder.encodedLength + BASE_VECTOR_OFFSET);
+    super('struct', `struct Vec`, coder.encodedLength + WORD_SIZE);
     this.coder = coder;
   }
 
-  encode(value: InputValueOf<TCoder>): Uint8Array {
-    if (!Array.isArray(value)) {
-      this.throwError(ErrorCode.ENCODE_ERROR, `Expected array value.`);
-    }
-
-    const parts: Uint8Array[] = [];
-
-    // pointer (ptr)
-    const pointer: Uint8ArrayWithDynamicData = new U64Coder().encode(BASE_VECTOR_OFFSET);
-    // pointer dynamicData, encode the vector now and attach to its pointer
-    pointer.dynamicData = {
-      0: concatWithDynamicData(Array.from(value).map((v) => this.coder.encode(v))),
-    };
-
-    parts.push(pointer);
-
-    // capacity (cap)
-    parts.push(new U64Coder().encode(value.length));
-
-    // length (len)
-    parts.push(new U64Coder().encode(value.length));
-
-    return concatWithDynamicData(parts);
+  encode(_value: InputValueOf<TCoder>): Uint8Array {
+    throw new FuelError(ErrorCode.ENCODE_ERROR, `Vec encode unsupported in v1`);
+    return new Uint8Array();
   }
 
   decode(data: Uint8Array, offset: number): [DecodedValueOf<TCoder>, number] {
