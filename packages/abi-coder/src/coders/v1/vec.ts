8999b83451b671c43f51045a01b784a34bc4b330
Daniel Bate <djbate23@gmail.com>
2024-01-23 14:58:54 +0000

    feat: reintroduce standard vec encoding

--- packages/abi-coder/src/coders/v1/vec.ts
+++ packages/abi-coder/src/coders/v1/vec.ts
@@ -22,9 +22,29 @@ export class VecCoder<TCoder extends Coder> extends Coder<
     this.coder = coder;
   }
 
-  encode(_value: InputValueOf<TCoder>): Uint8Array {
-    console.log('HIT NEW VECTOR ENCODE');
-    return new Uint8Array(0);
+  encode(value: InputValueOf<TCoder>): Uint8Array {
+    if (!Array.isArray(value)) {
+      this.throwError(ErrorCode.ENCODE_ERROR, `Expected array value.`);
+    }
+
+    const parts: Uint8Array[] = [];
+
+    // pointer (ptr)
+    const pointer: Uint8ArrayWithDynamicData = new U64Coder().encode(BASE_VECTOR_OFFSET);
+    // pointer dynamicData, encode the vector now and attach to its pointer
+    pointer.dynamicData = {
+      0: concatWithDynamicData(Array.from(value).map((v) => this.coder.encode(v))),
+    };
+
+    parts.push(pointer);
+
+    // capacity (cap)
+    parts.push(new U64Coder().encode(value.length));
+
+    // length (len)
+    parts.push(new U64Coder().encode(value.length));
+
+    return concatWithDynamicData(parts);
   }
 
   decode(data: Uint8Array, offset: number): [DecodedValueOf<TCoder>, number] {
