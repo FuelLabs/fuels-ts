0d162cb5986109d05c793a376c88cb1d89c73c75
Daniel Bate <djbate23@gmail.com>
2024-01-30 13:11:25 +0000

    feat: treat raw slice as u8 in tests

--- packages/fuel-gauge/src/raw-slice.test.ts
+++ packages/fuel-gauge/src/raw-slice.test.ts
@@ -43,7 +43,7 @@ describe('Raw Slice Tests', () => {
     const { value } = await contractInstance.functions
       .return_raw_slice(INPUT)
       .txParams({ gasPrice, gasLimit: 10_000 })
-      .call<BN[]>();
+      .call();
 
     expect(value).toStrictEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
   });
