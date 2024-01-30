5385759f0eadd90d37edff4315f46cb01574663d
Daniel Bate <djbate23@gmail.com>
2024-01-30 17:57:45 +0000

    feat: add TOB validation to boolean coder

--- packages/abi-coder/src/coders/v1/boolean.ts
+++ packages/abi-coder/src/coders/v1/boolean.ts
@@ -4,10 +4,8 @@ import { bn, toBytes } from '@fuel-ts/math';
 import { Coder } from '../abstract-coder';
 
 export class BooleanCoder extends Coder<boolean, boolean> {
-  paddingLength: number;
   constructor() {
     super('boolean', 'boolean', 1);
-    this.paddingLength = 1;
   }
 
   encode(value: boolean): Uint8Array {
@@ -17,20 +15,24 @@ export class BooleanCoder extends Coder<boolean, boolean> {
       throw new FuelError(ErrorCode.ENCODE_ERROR, `Invalid boolean value.`);
     }
 
-    return toBytes(value ? 1 : 0, this.paddingLength);
+    return toBytes(value ? 1 : 0, this.encodedLength);
   }
 
   decode(data: Uint8Array, offset: number): [boolean, number] {
-    const bytes = bn(data.slice(offset, offset + this.paddingLength));
+    if (data.length < this.encodedLength) {
+      throw new FuelError(ErrorCode.DECODE_ERROR, `Invalid boolean data size.`);
+    }
+
+    const bytes = bn(data.slice(offset, offset + this.encodedLength));
 
     if (bytes.isZero()) {
-      return [false, offset + this.paddingLength];
+      return [false, offset + this.encodedLength];
     }
 
     if (!bytes.eq(bn(1))) {
       throw new FuelError(ErrorCode.DECODE_ERROR, `Invalid boolean value.`);
     }
 
-    return [true, offset + this.paddingLength];
+    return [true, offset + this.encodedLength];
   }
 }
