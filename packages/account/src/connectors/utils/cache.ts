d3fb5f4a9e38abca352bf14b975d5a8753425339
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-01-31 22:02:54 -0300

    add connectors helpers

---
+++ packages/account/src/connectors/utils/cache.ts
@@ -0,0 +1,37 @@
+export type CacheFor = {
+  [key: string]: {
+    timeout: number;
+    value: unknown;
+  } | null;
+};
+
+type CacheForOptions = {
+  key: string;
+  cache: CacheFor;
+  cacheTime: number;
+};
+
+export function cacheFor<F extends (...args: unknown[]) => Promise<unknown>>(
+  fn: F,
+  { cache, cacheTime, key }: CacheForOptions
+): F {
+  return (async (...args: unknown[]) => {
+    if (cache[key] && cache[key]?.value) {
+      return cache[key]!.value as ReturnType<F>;
+    }
+    clearTimeout(cache[key]?.timeout);
+    const result = await fn(...args);
+
+    // Create cache auto clean
+    cache[key] = {
+      timeout: Number(
+        setTimeout(() => {
+          cache[key] = null;
+        }, cacheTime)
+      ),
+      value: result,
+    };
+
+    return result;
+  }) as F;
+}
---
+++ packages/account/src/connectors/utils/index.ts
@@ -0,0 +1,4 @@
+export * from './cache';
+export * from './dispatch-fuel-connector-event';
+export * from './get-asset-by-chain';
+export * from './promises';
