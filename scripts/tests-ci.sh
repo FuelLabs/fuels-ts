96293370847df2d9824cca9fbc553eab358e53f5
Anderson Arboleya <anderson@arboleya.me>
2024-02-02 14:14:04 -0300

    Using `forc` from `xunilrj/v0.49.2` branch

--- scripts/tests-ci.sh
+++ scripts/tests-ci.sh
@@ -9,13 +9,13 @@ pnpm node:run > /dev/null 2>&1 &
 echo "Started Fuel-Core node in background."
 
 if [[ "$*" == *"--browser"* ]]; then
-    pnpm test:browser
+    FORC_IMPLICIT_STD_GIT_BRANCH=xunilrj/v0.49.2 pnpm test:browser
     TEST_RESULT=$?
 elif [[ "$*" == *"--node"* ]]; then
-    pnpm test
+    FORC_IMPLICIT_STD_GIT_BRANCH=xunilrj/v0.49.2 pnpm test
     TEST_RESULT=$?
 else
-    pnpm test
+    FORC_IMPLICIT_STD_GIT_BRANCH=xunilrj/v0.49.2 pnpm test
     TEST_RESULT=$?
 fi
 
