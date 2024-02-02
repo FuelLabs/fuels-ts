6eba4d5c3a97defcc2bb64abbc7e0f9111d480af
nedsalk <nedim.salkic@fuel.sh>
2024-02-02 14:45:47 +0100

    test: better test case for less than 1

--- packages/providers/src/coin-quantityfy.test.ts
+++ packages/providers/src/coin-quantityfy.test.ts
@@ -9,7 +9,7 @@ describe('coinQuantityfy', () => {
   it('amount that is less than 1 is rounded up to 1', () => {
     expect(coinQuantityfy([Number.MIN_SAFE_INTEGER]).amount.toNumber()).toEqual(1);
     expect(coinQuantityfy([0]).amount.toNumber()).toEqual(1);
-    expect(coinQuantityfy([0.9]).amount.toNumber()).toEqual(1);
+    expect(coinQuantityfy([1 - Number.EPSILON]).amount.toNumber()).toEqual(1);
   });
   test('amount of return is set properly', () => {
     expect(coinQuantityfy([2]).amount.toNumber()).toEqual(2);
