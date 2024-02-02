a865f05bcbe039aa7b94bd778ce9f3b892561843
nedsalk <nedim.salkic@fuel.sh>
2024-02-02 14:39:03 +0100

    test: max integer + 1

--- packages/providers/src/coin-quantityfy.test.ts
+++ packages/providers/src/coin-quantityfy.test.ts
@@ -6,10 +6,11 @@ import { coinQuantityfy } from './coin-quantity';
  * @group node
  */
 describe('coinQuantityfy', () => {
-  it('should returns 1 when input is < 1', () => {
+  it('should return expected values', () => {
     expect(coinQuantityfy([0]).amount.toNumber()).toEqual(1);
     expect(coinQuantityfy([0.9]).amount.toNumber()).toEqual(1);
     expect(coinQuantityfy([2]).amount.toNumber()).toEqual(2);
-    expect(coinQuantityfy([new BN(Number.MAX_SAFE_INTEGER + 1)]).amount.toNumber()).toEqual(2);
+    const maxPlusOne = new BN(Number.MAX_SAFE_INTEGER).add(new BN(1));
+    expect(coinQuantityfy([maxPlusOne]).amount.toString()).toEqual(maxPlusOne.toString());
   });
 });
