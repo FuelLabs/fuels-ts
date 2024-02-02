c624fd2726d6a4abdb4628ff0dcba0a6a571c03d
nedsalk <nedim.salkic@fuel.sh>
2024-02-02 14:46:34 +0100

    test: better min_value test

--- packages/providers/src/coin-quantityfy.test.ts
+++ packages/providers/src/coin-quantityfy.test.ts
@@ -7,7 +7,7 @@ import { coinQuantityfy } from './coin-quantity';
  */
 describe('coinQuantityfy', () => {
   it('amount that is less than 1 is rounded up to 1', () => {
-    expect(coinQuantityfy([Number.MIN_SAFE_INTEGER]).amount.toNumber()).toEqual(1);
+    expect(coinQuantityfy([Number.MIN_VALUE]).amount.toNumber()).toEqual(1);
     expect(coinQuantityfy([0]).amount.toNumber()).toEqual(1);
     expect(coinQuantityfy([1 - Number.EPSILON]).amount.toNumber()).toEqual(1);
   });
