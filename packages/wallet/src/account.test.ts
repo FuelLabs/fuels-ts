cff6af409de7fcfe9a723024c2d03365d4d6daba
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-02-08 20:31:35 -0300

    removing test cases

--- packages/wallet/src/account.test.ts
+++ packages/wallet/src/account.test.ts
@@ -1,18 +1,9 @@
-import { Address } from '@fuel-ts/address';
 import { BaseAssetId } from '@fuel-ts/address/configs';
 import { bn } from '@fuel-ts/math';
-import type {
-  CallResult,
-  Coin,
-  CoinQuantity,
-  Message,
-  Resource,
-  TransactionRequestLike,
-} from '@fuel-ts/providers';
-import { TransactionResponse, ScriptTransactionRequest, Provider } from '@fuel-ts/providers';
+import type { Coin, CoinQuantity, Message, Resource } from '@fuel-ts/providers';
+import { ScriptTransactionRequest, Provider } from '@fuel-ts/providers';
 import * as providersMod from '@fuel-ts/providers';
 
-import type { TxParamsType } from './account';
 import { Account } from './account';
 import { FUEL_NETWORK_URL } from './configs';
 
@@ -294,149 +285,13 @@ describe('Account', () => {
     expect(addResourcesSpy).toHaveBeenCalledWith(resourcesToSpend);
   });
 
-  it('should execute transfer just as fine', async () => {
-    const amount = bn(1);
-    const assetId = '0x0101010101010101010101010101010101010101010101010101010101010101';
-    const destination = Address.fromAddressOrString(
-      '0x0202020202020202020202020202020202020202020202020202020202020202'
-    );
-    const txParam: TxParamsType = {
-      gasLimit: bn(1),
-      gasPrice: bn(1),
-      maturity: 1,
-    };
-
-    const transactionCost: providersMod.TransactionCost = {
-      gasUsed: bn(234),
-      gasPrice: bn(1),
-      minGasPrice: bn(1),
-      maxFee: bn(2),
-      minFee: bn(1),
-      receipts: [],
-      requiredQuantities: [],
-      maxGas: bn(1),
-      minGas: bn(1),
-      usedFee: bn(1),
-    };
-
-    const request = new ScriptTransactionRequest();
-    vi.spyOn(providersMod, 'ScriptTransactionRequest').mockImplementation(() => request);
-
-    const transactionResponse = new TransactionResponse('transactionId', provider);
-
-    const addCoinOutputSpy = vi.spyOn(request, 'addCoinOutput');
-
-    const fundSpy = vi.spyOn(Account.prototype, 'fund').mockImplementation(() => Promise.resolve());
-
-    const sendTransactionSpy = vi
-      .spyOn(Account.prototype, 'sendTransaction')
-      .mockImplementation(() => Promise.resolve(transactionResponse));
-
-    const getTransactionCost = vi
-      .spyOn(Provider.prototype, 'getTransactionCost')
-      .mockImplementation(() => Promise.resolve(transactionCost));
-
-    const account = new Account(
-      '0x09c0b2d1a486c439a87bcba6b46a7a1a23f3897cc83a94521a96da5c23bc58db',
-      provider
-    );
-
-    await account.transfer(destination, amount, assetId, txParam);
-
-    expect(addCoinOutputSpy).toHaveBeenCalledTimes(1);
-    expect(addCoinOutputSpy).toHaveBeenCalledWith(destination, amount, assetId);
-
-    expect(getTransactionCost).toHaveBeenCalledTimes(1);
-
-    expect(fundSpy).toHaveBeenCalledTimes(1);
-    expect(fundSpy).toHaveBeenCalledWith(
-      request,
-      transactionCost.requiredQuantities,
-      transactionCost.maxFee
-    );
-
-    expect(sendTransactionSpy).toHaveBeenCalledTimes(1);
-    expect(sendTransactionSpy).toHaveBeenCalledWith(request);
-  });
-
-  it('should execute withdrawToBaseLayer just fine', async () => {
-    const recipient = Address.fromRandom();
-    const txParams: TxParamsType = {};
-    const amount = bn(1);
-
-    const assetId = '0x0101010101010101010101010101010101010101010101010101010101010101';
-
-    const request = new ScriptTransactionRequest();
-
-    const quantities: CoinQuantity[] = [
-      {
-        amount: bn(1),
-        assetId,
-      },
-    ];
-    const cost: providersMod.TransactionCost = {
-      gasPrice: bn(1),
-      gasUsed: bn(1),
-      maxFee: bn(1),
-      maxGas: bn(1),
-      minFee: bn(1),
-      minGas: bn(1),
-      minGasPrice: bn(1),
-      receipts: [],
-      requiredQuantities: quantities,
-      usedFee: bn(1),
-    };
-
-    const transactionResponse = {} as unknown as TransactionResponse;
-
-    const scriptTransactionRequest = vi
-      .spyOn(providersMod, 'ScriptTransactionRequest')
-      .mockImplementation(() => request);
-
-    const getTransactionCost = vi
-      .spyOn(providersMod.Provider.prototype, 'getTransactionCost')
-      .mockImplementation(() => Promise.resolve(cost));
-
-    const fund = vi.spyOn(Account.prototype, 'fund').mockImplementation(() => Promise.resolve());
-
-    const sendTransaction = vi
-      .spyOn(Account.prototype, 'sendTransaction')
-      .mockImplementation(() => Promise.resolve(transactionResponse));
-
-    const account = new Account(
-      '0x09c0b2d1a486c439a87bcba6b46a7a1a23f3897cc83a94521a96da5c23bc58db',
-      provider
-    );
-
-    let result = await account.withdrawToBaseLayer(recipient, amount, txParams);
-
-    expect(result).toEqual(transactionResponse);
-
-    expect(scriptTransactionRequest).toHaveBeenCalledTimes(1);
-
-    expect(sendTransaction).toHaveBeenCalledTimes(1);
-    expect(sendTransaction).toHaveBeenCalledWith(request);
-
-    expect(getTransactionCost).toHaveBeenCalledTimes(1);
-    expect(fund).toHaveBeenCalledTimes(1);
-
-    // without txParams
-    result = await account.withdrawToBaseLayer(recipient, amount);
-
-    expect(result).toEqual(transactionResponse);
-
-    expect(scriptTransactionRequest).toHaveBeenCalledTimes(2);
-
-    expect(sendTransaction).toHaveBeenCalledTimes(2);
-    expect(sendTransaction).toHaveBeenCalledWith(request);
-  });
-
   it('should execute sendTransaction just fine', async () => {
-    const transactionRequestLike: TransactionRequestLike = {
+    const transactionRequestLike: providersMod.TransactionRequestLike = {
       type: providersMod.TransactionType.Script,
     };
     const transactionRequest = new ScriptTransactionRequest();
-    const transactionResponse = 'transactionResponse' as unknown as TransactionResponse;
+    const transactionResponse =
+      'transactionResponse' as unknown as providersMod.TransactionResponse;
 
     const transactionRequestify = vi.spyOn(providersMod, 'transactionRequestify');
 
@@ -468,11 +323,11 @@ describe('Account', () => {
   });
 
   it('should execute simulateTransaction just fine', async () => {
-    const transactionRequestLike: TransactionRequestLike = {
+    const transactionRequestLike: providersMod.TransactionRequestLike = {
       type: providersMod.TransactionType.Script,
     };
     const transactionRequest = new ScriptTransactionRequest();
-    const callResult = 'callResult' as unknown as CallResult;
+    const callResult = 'callResult' as unknown as providersMod.CallResult;
 
     const transactionRequestify = vi
       .spyOn(providersMod, 'transactionRequestify')
