b6dcc7898f8dbe9d9fd29df7686a8261f0a7b004
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-02-08 12:09:13 -0300

    moving test-utils exports to test-utils dir

--- packages/account/tsup.config.ts
+++ packages/account/tsup.config.ts
@@ -6,7 +6,7 @@ const configs: Options = {
   entry: {
     index: 'src/index.ts',
     configs: 'src/configs.ts',
-    'test-utils': 'src/test-utils.ts',
+    'test-utils': 'src/test-utils/index.ts',
   },
 };
 
