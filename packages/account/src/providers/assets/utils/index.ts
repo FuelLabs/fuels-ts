911584f7bc7d3e787a861bf8fc423664f27c5fa5
Dhaiwat <dhaiwatpandya@gmail.com>
2024-02-13 20:45:16 -0600

    feat: migrate over @fuels/assets package into the TS SDK

---
+++ packages/account/src/providers/assets/utils/index.ts
@@ -0,0 +1,3 @@
+export * from './network';
+export * from './resolveIconPath';
+export * from './url';
--- packages/account/tsup.config.ts
+++ packages/account/tsup.config.ts
@@ -1,5 +1,10 @@
 import { tsupDefaults } from '@internal/tsup';
+import { mkdirSync, writeFileSync } from 'fs';
+import { join } from 'path';
 import type { Options } from 'tsup';
+
+import { assets } from './src/providers/assets';
+import { resolveIconPath } from './src/providers/assets/utils';
 
 const configs: Options = {
   ...tsupDefaults,
@@ -7,6 +12,18 @@ const configs: Options = {
     index: 'src/index.ts',
     configs: 'src/configs.ts',
     'test-utils': 'src/test-utils/index.ts',
+  },
+  env: {
+    BASE_URL: process.env.ASSETS_BASE_URL || '',
+  },
+  // eslint-disable-next-line @typescript-eslint/require-await
+  onSuccess: async () => {
+    const outputDir = join(__dirname, 'src', 'providers', 'assets', 'images');
+    mkdirSync(outputDir, {
+      recursive: true,
+    });
+    const assetsData = resolveIconPath(process.env.ASSETS_BASE_URL as string, assets);
+    writeFileSync(join(outputDir, './assets.json'), JSON.stringify(assetsData));
   },
 };
 
