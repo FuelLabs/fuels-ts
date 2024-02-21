c2624e51e44b2aa983877ede2815861312c84c5b
Anderson Arboleya <anderson@arboleya.me>
2024-02-21 08:56:36 -0300

    Making param optional and removing directory conditionally

--- packages/fuels/test/utils/resetDiskAndMocks.ts
+++ packages/fuels/test/utils/resetDiskAndMocks.ts
@@ -4,8 +4,8 @@ import { existsSync, rmSync } from 'fs';
  * Cleanup routine
  */
 
-export function resetDiskAndMocks(dirPath: string) {
-  if (existsSync(dirPath)) {
+export function resetDiskAndMocks(dirPath?: string) {
+  if (dirPath && existsSync(dirPath)) {
     rmSync(dirPath, { recursive: true });
   }
   vi.restoreAllMocks();
