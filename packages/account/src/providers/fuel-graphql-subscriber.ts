1d8e7da09ce6611145a9f531b6d7275ca70c3bb6
nedsalk <nedim.salkic@fuel.sh>
2024-02-23 10:27:10 +0100

    refactor: remove unused method

--- packages/account/src/providers/fuel-graphql-subscriber.ts
+++ packages/account/src/providers/fuel-graphql-subscriber.ts
@@ -13,33 +13,6 @@ export class FuelGraphqlSubscriber implements AsyncIterator<unknown> {
   private stream!: ReadableStreamDefaultReader<Uint8Array>;
   private static textDecoder = new TextDecoder();
 
-  private static parseBytesStream(
-    bytes: Uint8Array | undefined
-  ): Record<string, unknown> | FuelError | undefined {
-    if (bytes === undefined) {
-      return undefined;
-    }
-
-    const text = this.textDecoder.decode(bytes);
-
-    // https://github.com/FuelLabs/fuel-core/blob/e1e631902f762081d2124d9c457ddfe13ac366dc/crates/fuel-core/src/graphql_api/service.rs#L247
-    // this is the real keep-alive message sent by the server
-    if (text === ':keep-alive-text\n\n') {
-      return undefined;
-    }
-
-    const { data, errors } = JSON.parse(text.split('data:')[1]);
-
-    if (Array.isArray(errors)) {
-      return new FuelError(
-        FuelError.CODES.INVALID_REQUEST,
-        errors.map((err) => err.message).join('\n\n')
-      );
-    }
-
-    return data as Record<string, unknown>;
-  }
-
   public constructor(private options: FuelGraphQLSubscriberOptions) {}
 
   private async setStream() {
