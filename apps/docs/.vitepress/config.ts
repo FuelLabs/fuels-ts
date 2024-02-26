5046ea330a3435c6a54f3b2bff980302358a6bbc
Daniel Bate <djbate23@gmail.com>
2024-02-26 16:44:12 +0000

    docs: document creating tx requests from invocation scopes and delete redundant doc

--- apps/docs/.vitepress/config.ts
+++ apps/docs/.vitepress/config.ts
@@ -346,12 +346,12 @@ export default defineConfig({
                 link: '/guide/cookbook/custom-transactions',
               },
               {
-                text: 'Deposit And Withdraw',
-                link: '/guide/cookbook/deposit-and-withdraw',
+                text: 'Custom Transactions from Contract Calls',
+                link: '/guide/cookbook/custom-transactions-from-contract-calls',
               },
               {
-                text: 'Transfer All Assets',
-                link: '/guide/cookbook/transfer-all-assets',
+                text: 'Deposit And Withdraw',
+                link: '/guide/cookbook/deposit-and-withdraw',
               },
             ],
           },
