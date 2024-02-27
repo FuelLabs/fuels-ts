4ae4a9542bc78403ba17d309cb5c82b73bbd88bf
Peter Smith <peter@blueoceancomputing.co.uk>
2024-02-27 18:59:45 +0000

    chore: added FuelError for expectToBeInRange method

--- packages/utils/src/test-utils/expectToBeInRange.ts
+++ packages/utils/src/test-utils/expectToBeInRange.ts
@@ -1,8 +1,17 @@
+import { ErrorCode, FuelError } from "@fuel-ts/errors";
+
+/**
+ * Expect that the value is within the range of min and max
+ * 
+ * @throws {FuelError} {@link ErrorCode.INVALID_DATA}
+ * When the value is more than the max value
+ * When the value is less than the min value
+ */
 export const expectToBeInRange = (params: { value: number; min: number; max: number }) => {
   const { value, min, max } = params;
   if (value >= min && value <= max) {
     return true;
   }
 
-  throw new Error(`Expected value: '${value}' to be within range: '${min}-${max}'`);
+  throw new FuelError(ErrorCode.INVALID_DATA, `Expected value: '${value}' to be within range: '${min}-${max}'`);
 };
