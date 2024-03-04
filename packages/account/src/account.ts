6d14e95a2f4a193c9bc6c3078349a7f16e369e7d
Daniel Bate <djbate23@gmail.com>
2024-03-04 09:41:06 +0000

    feat: change tx signing error message

--- packages/account/src/account.ts
+++ packages/account/src/account.ts
@@ -503,7 +503,7 @@ export class Account extends AbstractAccount {
    */
   async signTransaction(hashedTransaction: string): Promise<string> {
     if (!this._connector) {
-      throw new FuelError(ErrorCode.MISSING_CONNECTOR, 'A connector is required to sign messages.');
+      throw new FuelError(ErrorCode.MISSING_CONNECTOR, 'A connector is required to sign transactions.');
     }
     return this._connector.signTransaction(this.address.toString(), hashedTransaction);
   }
