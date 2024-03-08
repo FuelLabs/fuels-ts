8153b74d46ba4a67f686e05c70e69da3d5cd52e4
nedsalk <nedim.salkic@fuel.sh>
2024-03-08 18:08:45 +0100

    docs: explain to always run the command

--- scripts/changeset-version-with-docs.ts
+++ scripts/changeset-version-with-docs.ts
@@ -15,6 +15,11 @@ import { error } from 'console';
       execSync(`git commit -m"ci(scripts): update versions"`);
     }
 
+    /**
+     * This is the base command that has to run always.
+     * Release CIs were failing when there were only empty changesets because we weren't running this command.
+     * See more here: https://github.com/FuelLabs/fuels-ts/pull/1847
+     */
     execSync(`changeset version`);
   } catch (err) {
     error(err.toString());
