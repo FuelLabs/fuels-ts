5842beabde1a69d8965b01855fdb09efaf22912e
Nedim Salkić <nedim.salkic@fuel.sh>
2024-03-11 13:08:08 +0100

    Update packages/versions/src/lib/checkFuelCoreVersionCompatibility.ts
    Co-authored-by: Anderson Arboleya <anderson@arboleya.me>

--- packages/versions/src/lib/checkFuelCoreVersionCompatibility.ts
+++ packages/versions/src/lib/checkFuelCoreVersionCompatibility.ts
@@ -4,7 +4,7 @@ import { majorEq, minorEq, patchEq } from './semver';
 export function checkFuelCoreVersionCompatibility(networkVersion: string) {
   const { FUEL_CORE: supportedVersion } = getBuiltinVersions();
 
-  if (networkVersion.match(/^[0-9]+\.[0-9]+\.[0-9]+$/) === null) {
+  if (/^[0-9]+\.[0-9]+\.[0-9]+$/m.test(networkVersion)) {
     console.warn(
       `You're running against an unreleased fuel-core version: ${networkVersion}. Things may work as expected, but it's not guaranteed. Please use a released version.`
     );
