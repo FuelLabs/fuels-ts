a69c0fefaa26b38e08e13ae9365c325e7123d407
nedsalk <nedim.salkic@fuel.sh>
2024-03-11 13:09:34 +0100

    fix: import `warn` to silence linter

--- packages/versions/src/lib/checkFuelCoreVersionCompatibility.ts
+++ packages/versions/src/lib/checkFuelCoreVersionCompatibility.ts
@@ -1,3 +1,5 @@
+import { warn } from 'console';
+
 import { getBuiltinVersions } from './getBuiltinVersions';
 import { majorEq, minorEq, patchEq } from './semver';
 
@@ -5,7 +7,7 @@ export function checkFuelCoreVersionCompatibility(networkVersion: string) {
   const { FUEL_CORE: supportedVersion } = getBuiltinVersions();
 
   if (/^[0-9]+\.[0-9]+\.[0-9]+$/m.test(networkVersion)) {
-    console.warn(
+    warn(
       `You're running against an unreleased fuel-core version: ${networkVersion}. Things may work as expected, but it's not guaranteed. Please use a released version.`
     );
   }
