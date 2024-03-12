7ab39e1eb40fb3ae7502a565abeb3e1250258c7d
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-03-12 11:10:16 -0300

    creating more code snippets for doc page

--- apps/docs-snippets/src/guide/contracts/read-only-calls.test.ts
+++ apps/docs-snippets/src/guide/contracts/read-only-calls.test.ts
@@ -45,4 +45,41 @@ describe(__filename, () => {
     // #endregion read-only-calls-3
     expect(value).toEqual(15);
   });
+
+  it('should successfully execute a contract call without a wallet', async () => {
+    const counterContract = await createAndDeployContractFromProject(
+      DocSnippetProjectsEnum.COUNTER
+    );
+
+    // #region read-only-calls-6
+    const { value: valueUpdateOnlyOnDryRun } = await counterContract.functions
+      .increment_count(1)
+      .get();
+
+    // The dry-run returns the incremented value, but the actual value remains the same
+    // #context console.log(valueUpdateOnlyOnDryRun.toNumber());
+    // #context > 1
+    // #endregion read-only-calls-6
+    expect(valueUpdateOnlyOnDryRun.toNumber()).toEqual(1);
+
+    // #region read-only-calls-7
+    const { value: realValue } = await counterContract.functions.get_count().get();
+
+    // #context console.log(realValue.toNumber());
+    // #context > 0
+    // #endregion read-only-calls-7
+
+    expect(realValue.toNumber()).toEqual(0);
+
+    // #region read-only-calls-8
+    await counterContract.functions.increment_count(1).call();
+
+    const { value } = await counterContract.functions.get_count().get();
+
+    // #context console.log(value.toNumber());
+    // #context > 1
+    // #endregion read-only-calls-8
+
+    expect(value.toNumber()).toEqual(1);
+  });
 });
