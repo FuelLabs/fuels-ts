7a2162ee3788440febed06ce9a0c9280f845092a
Dhaiwat <dhaiwatpandya@gmail.com>
2024-03-13 17:49:11 +0530

    prettify comment
    Co-authored-by: Anderson Arboleya <anderson@arboleya.me>

--- templates/nextjs/fuels.config.ts
+++ templates/nextjs/fuels.config.ts
@@ -3,5 +3,9 @@ import { createConfig } from 'fuels';
 export default createConfig({
   workspace: './sway-programs',
   output: './src/sway-api',
-  fuelCorePort: 4000, // This is the same value as the port specified in the .env.local file. Please keep these values in sync.
+  /*
+    This is the same value as the port specified in the `.env.local` file.
+    Please keep these values in sync.
+  */
+  fuelCorePort: 4000,
 });
