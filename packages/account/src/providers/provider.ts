21cf82564c4384110f9bc7290ca962d797bfba5a
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-03-14 14:32:08 -0300

    making change on code

--- packages/account/src/providers/provider.ts
+++ packages/account/src/providers/provider.ts
@@ -659,6 +659,7 @@ export default class Provider {
       utxoValidation: utxoValidation || false,
     });
     const receipts = gqlReceipts.map(processGqlReceipt);
+    console.log(receipts);
     return {
       receipts,
     };
