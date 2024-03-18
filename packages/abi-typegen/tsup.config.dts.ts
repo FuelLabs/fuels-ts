75b0a18fcf2b3ea083b131c164054e50325d3393
Peter Smith <peter@blueoceancomputing.co.uk>
2024-03-18 17:42:55 +0000

    chore: linting

--- packages/abi-typegen/tsup.config.dts.ts
+++ packages/abi-typegen/tsup.config.dts.ts
@@ -5,9 +5,9 @@ import tsupDefaults from './tsup.config';
 const configs: Options = {
   ...tsupDefaults,
   tsconfig: 'tsconfig.dts.json',
-  'sourcemap': true,
-  'dts': true,
-  clean: false
+  sourcemap: true,
+  dts: true,
+  clean: false,
 };
 
 export default configs;
