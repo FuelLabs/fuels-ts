c88685986b5ccc60b39abb9c2c8944668f0c8cf2
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-03-19 19:29:23 -0300

    wip: implemented extractTxError

--- packages/program/src/errors.ts
+++ packages/program/src/errors.ts
@@ -1,6 +1,18 @@
-import type { TransactionResult } from '@fuel-ts/account';
+import type { TransactionResultReceipt, TransactionResult, FailureStatus } from '@fuel-ts/account';
+import type { GqlTransactionStatusFragmentFragment } from '@fuel-ts/account/dist/providers/__generated__/operations';
+import { ErrorCode, FuelError } from '@fuel-ts/errors';
+import { bn } from '@fuel-ts/math';
+import type { ReceiptRevert } from '@fuel-ts/transactions';
 import { ReceiptType } from '@fuel-ts/transactions';
+import {
+  FAILED_REQUIRE_SIGNAL,
+  FAILED_ASSERT_EQ_SIGNAL,
+  FAILED_ASSERT_SIGNAL,
+  FAILED_TRANSFER_TO_ADDRESS_SIGNAL,
+  FAILED_ASSERT_NE_SIGNAL,
+} from '@fuel-ts/transactions/configs';
 
+import { PANIC_DOC_URL, PANIC_REASONS } from './configs';
 import { RevertErrorCodes } from './revert/revert-error-codes';
 import { getDocs } from './utils';
 
@@ -34,3 +46,83 @@ export class ScriptResultDecoderError extends Error {
     new RevertErrorCodes(result.receipts).assert(this);
   }
 }
+
+export const assemblePanicError = (status: FailureStatus) => {
+  let errorMessage = `The transaction failed with reason: "${status.reason}".`;
+  if (PANIC_REASONS.includes(status.reason)) {
+    errorMessage = `${errorMessage}\n\n`.concat(
+      `You can read more about this error at:\n\n`.concat(
+        `${PANIC_DOC_URL}#variant.${status.reason}\n\n`
+      )
+    );
+  }
+
+  return errorMessage;
+};
+
+export const assembleRevertError = (
+  receipts: Array<TransactionResultReceipt>,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  logs: Array<any>
+) => {
+  let errorMessage = '';
+
+  const revertReceipt = receipts.find(({ type }) => type === ReceiptType.Revert) as ReceiptRevert;
+
+  if (revertReceipt) {
+    const reasonHex = bn(revertReceipt.val).toHex();
+
+    switch (reasonHex) {
+      case FAILED_REQUIRE_SIGNAL:
+        errorMessage = `The transaction reverted because of a "require" statement has thrown "${logs[0]}".`;
+        break;
+
+      case FAILED_ASSERT_EQ_SIGNAL:
+        errorMessage = `The transaction reverted because of an "assert_eq" statement comparing "${logs[1]}" and "${logs[0]}".`;
+        break;
+
+      case FAILED_ASSERT_NE_SIGNAL:
+        errorMessage = `The transaction reverted because of an "assert_ne" statement comparing "${logs[1]}" and "${logs[0]}."`;
+        break;
+
+      case FAILED_ASSERT_SIGNAL:
+        errorMessage = `The transaction reverted because of an "assert" statement failed to evaluate to true.`;
+        break;
+
+      case FAILED_TRANSFER_TO_ADDRESS_SIGNAL:
+        errorMessage = `The transaction reverted because missing "OutputChange"(s).`;
+        break;
+
+      default:
+        errorMessage = `The transaction reverted because of an unknown reason.`;
+    }
+  }
+
+  return errorMessage;
+};
+
+interface IExtractTxError {
+  receipts: Array<TransactionResultReceipt>;
+  status?: GqlTransactionStatusFragmentFragment | null;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  logs: Array<any>;
+}
+
+export const extractTxError = (params: IExtractTxError) => {
+  const { receipts, status, logs } = params;
+
+  const wasPanic = receipts.some(({ type }) => type === ReceiptType.Panic);
+
+  let err: string;
+
+  if (status?.type === 'FailureStatus' && wasPanic) {
+    err = assemblePanicError(status);
+  } else {
+    err = assembleRevertError(receipts, logs);
+  }
+
+  err = err.concat(`\n\nlogs: ${JSON.stringify(logs, null, 2)}`);
+  err = err.concat(`\n\nreceipts: ${JSON.stringify(receipts, null, 2)}`);
+
+  return new FuelError(ErrorCode.ABI_MAIN_METHOD_MISSING, err);
+};
