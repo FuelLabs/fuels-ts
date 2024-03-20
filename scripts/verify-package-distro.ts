938a3150d356eecce5b385a66886710d62b336e9
Peter Smith <peter@blueoceancomputing.co.uk>
2024-03-20 11:47:44 +0000

    feat: added verify-package-distro command

---
+++ scripts/verify-package-distro.ts
@@ -0,0 +1,50 @@
+import { execSync } from 'child_process';
+import { log, error } from 'console';
+import { readFileSync } from 'fs';
+import { globSync } from 'glob';
+import { dirname, join } from 'path';
+
+/**
+ * Requires `@arethetypeswrong/cli` installed globally
+ * 
+ * ```console
+ * npm i -g @arethetypeswrong/cli
+ * ```
+ */
+interface PackageJson {
+  name: string;
+  private: boolean;
+  exports: Record<string, {
+    require: string;
+    import: string;
+  }>
+}
+
+/**
+ * Get only the package's with exports
+ */
+const packagesWithExports = globSync(`{packages,apps}/*/package.json`)
+  .map((path) => {
+    const pkgJson = JSON.parse(readFileSync(path, 'utf-8')) as PackageJson;
+    return { ...pkgJson, path: dirname(path) };
+  })
+  .filter((json) => !json.private && json.exports)
+
+let errorCode = 0;
+
+/**
+ * 1). Verify that each package can be packed and types are correct
+ */
+packagesWithExports.forEach((pkg) => {
+  try {
+    const result = execSync(`attw ${pkg.path} --pack`);
+    log(result.toString());
+  } catch (e) {
+    error(`Error | Package export types`);
+    error(pkg.path)
+    error(e);
+    errorCode = 1;
+  }
+})
+
+process.exit(errorCode);
