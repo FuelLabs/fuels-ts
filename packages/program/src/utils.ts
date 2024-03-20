1f735a50824bc81b8da388e5c1807214dd85ee08
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-03-20 19:23:38 -0300

    remove code related to ScriptResultDecoderError

--- packages/program/src/utils.ts
+++ packages/program/src/utils.ts
@@ -1,34 +1,4 @@
-import type { TransactionResult } from '@fuel-ts/account';
 import { ErrorCode, FuelError } from '@fuel-ts/errors';
-
-import { PANIC_REASONS, PANIC_DOC_URL } from './configs';
-
-/**
- * @hidden
- */
-const getFailureReason = (reason: string): string => {
-  if (PANIC_REASONS.includes(reason)) {
-    return reason;
-  }
-
-  return reason === 'Revert(123)' ? 'MismatchedSelector' : 'unknown';
-};
-
-/**
- * @hidden
- */
-export const getDocs = (
-  status: TransactionResult['gqlTransaction']['status']
-): { doc: string; reason: string } => {
-  if (status?.type === 'FailureStatus') {
-    const reason = getFailureReason(status.reason);
-    return {
-      doc: reason !== 'unknown' ? `${PANIC_DOC_URL}#variant.${reason}` : PANIC_DOC_URL,
-      reason,
-    };
-  }
-  return { doc: PANIC_DOC_URL, reason: 'unknown' };
-};
 
 /**
  * @hidden
