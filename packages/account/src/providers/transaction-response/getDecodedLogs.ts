0a95041ceaca68f3a995d714ae8577cb2c75d587
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-03-25 13:19:55 -0300

    add comment to helper function

--- packages/account/src/providers/transaction-response/getDecodedLogs.ts
+++ packages/account/src/providers/transaction-response/getDecodedLogs.ts
@@ -10,6 +10,22 @@ export function getDecodedLogs<T = unknown>(
   mainAbi: JsonAbi,
   externalAbis: Record<string, JsonAbi> = {}
 ): T[] {
+  /**
+   * This helper decodes logs from transaction receipts.
+   * It loops through all receipts and decodes two types of logs:
+   * - ReceiptType.LogData
+   * - ReceiptType.Log
+   *
+   * The "mainAbi" parameter represents the ABI of the main contract used to create the transaction
+   * or the ABI from a script used within a "BaseInvocationScope" context.
+   * The "externalAbis" parameter is a record of contract ABIs that are also part of the transaction.
+   * These ABIs were added using `contract.addContracts` or through a multicall with `contract.multiCall`.
+   *
+   * @param receipts - The array of transaction result receipts.
+   * @param mainAbi - The ABI of the main contract.
+   * @param externalAbis - The record of external contract ABIs.
+   * @returns An array of decoded logs from Sway projects.
+   */
   return receipts.reduce((logs: T[], receipt) => {
     if (receipt.type === ReceiptType.LogData || receipt.type === ReceiptType.Log) {
       const interfaceToUse = externalAbis[receipt.id]
