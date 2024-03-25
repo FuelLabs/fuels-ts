c253ba05ef828ae3f6e29c9c0218f3118ae49658
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-03-25 13:49:04 -0300

    implement helper getAbisFromAllCalls on Program package

--- packages/program/src/utils.ts
+++ packages/program/src/utils.ts
@@ -1,7 +1,9 @@
-import type { TransactionResult } from '@fuel-ts/account';
+import type { JsonAbisFromAllCalls, TransactionResult } from '@fuel-ts/account';
 import { ErrorCode, FuelError } from '@fuel-ts/errors';
+import type { AbstractContract } from '@fuel-ts/interfaces';
 
 import { PANIC_REASONS, PANIC_DOC_URL } from './configs';
+import type { InvocationScopeLike } from './types';
 
 /**
  * @hidden
@@ -39,4 +41,26 @@ export function assert(condition: unknown, message: string): asserts condition {
   if (!condition) {
     throw new FuelError(ErrorCode.TRANSACTION_ERROR, message);
   }
+}
+
+/**
+ * @hidden
+ *
+ * Gets the ABI from an array of InvocationScopeLike.
+ */
+export function getAbisFromAllCalls(functionScopes: Array<InvocationScopeLike>) {
+  return functionScopes.reduce((acc, funcScope, i) => {
+    const { program, externalAbis } = funcScope.getCallConfig();
+
+    if (i === 0) {
+      acc.main = program.interface.jsonAbi;
+      acc.otherContractsAbis = {};
+    } else {
+      acc.otherContractsAbis[(<AbstractContract>program).id.toB256()] = program.interface.jsonAbi;
+    }
+
+    acc.otherContractsAbis = { ...acc.otherContractsAbis, ...externalAbis };
+
+    return acc;
+  }, {} as JsonAbisFromAllCalls);
 }
