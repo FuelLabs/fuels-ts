20f657bce91502ded13b3bad29fa346580a3cda8
nedsalk <nedim.salkic@fuel.sh>
2024-03-25 11:47:20 +0100

    just to unpublish old releases

--- scripts/changeset/update-baseBranch.ts
+++ scripts/changeset/update-baseBranch.ts
@@ -5,14 +5,20 @@ import path from 'path';
 const changesetPath = path.join(process.cwd(), '.changeset', 'config.json');
 const changesetConfig = JSON.parse(readFileSync(changesetPath, 'utf-8')) as { baseBranch: string };
 
+const currentBranchName = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+
 /**
  * Update the base branch in the changeset config file to ensure that the changeset is published based on the correct branch.
  * This is necessary because we publish both from the master and release/* branches.
  * https://github.com/changesets/changesets/blob/main/docs/config-file-options.md#basebranch-git-branch-name
  */
-const currentBranchName = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
-changesetConfig.baseBranch = currentBranchName;
+if (changesetConfig.baseBranch !== currentBranchName) {
+  execSync(
+    `echo "updating baseBranch of .changeset/config.json from ${changesetConfig.baseBranch} to ${currentBranchName}"`
+  );
+  changesetConfig.baseBranch = currentBranchName;
 
-writeFileSync(changesetPath, JSON.stringify(changesetConfig, null, 2), 'utf-8');
-
-execSync('changeset publish --no-git-tag', { stdio: 'inherit' });
+  writeFileSync(changesetPath, JSON.stringify(changesetConfig, null, 2), 'utf-8');
+  execSync(`git add .changeset/config.json`);
+  execSync(`git commit -m"ci(scripts): update baseBranch of .changeset/config.json"`);
+}
