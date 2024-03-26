4d6e27e3789b30fd31e6c4b45fa1d88eacb1a39e
Peter Smith <peter@blueoceancomputing.co.uk>
2024-03-26 08:56:49 +0000

    chore: added types for network urls

--- packages/account/src/configs.ts
+++ packages/account/src/configs.ts
@@ -1,6 +1,6 @@
-export const FUEL_NETWORK_URL =
+export const FUEL_NETWORK_URL: string =
   typeof process !== 'undefined'
     ? process?.env?.FUEL_NETWORK_URL || 'http://127.0.0.1:4000/graphql'
     : 'http://127.0.0.1:4000/graphql';
 
-export const FUEL_LATEST_TESTNET_URL = 'https://beta-5.fuel.network/graphql';
+export const FUEL_LATEST_TESTNET_URL: string = 'https://beta-5.fuel.network/graphql';
