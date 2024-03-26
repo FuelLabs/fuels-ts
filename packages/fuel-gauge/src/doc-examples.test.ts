be5468d2ab33beaab84f6fe4cdcc3ca76c4d04e5
Peter Smith <peter@blueoceancomputing.co.uk>
2024-03-26 08:55:50 +0000

    chore: clean up previous doc-example

--- packages/fuel-gauge/src/doc-examples.test.ts
+++ packages/fuel-gauge/src/doc-examples.test.ts
@@ -266,24 +266,9 @@ describe('Doc Examples', () => {
   });
 
   it('can connect to testnet', async () => {
-    // #region provider-testnet
-    // #import { Provider, Wallet, FUEL_LATEST_TESTNET_URL };
-
-    // Setup a provider, with the Testnet URL.
-    // > FUEL_LATEST_TESTNET_URL=https://beta-5.fuel.network/graphql
     const provider = await Provider.create(FUEL_LATEST_TESTNET_URL);
-    
-    // Create our Testnet wallet (with a private key).
     const PRIVATE_KEY = 'a1447cd75accc6b71a976fd3401a1f6ce318d27ba660b0315ee6ac347bf39568';
     const wallet = Wallet.fromPrivateKey(PRIVATE_KEY, provider);
-
-    // Perform a balance check.
-    const balances = await wallet.getBalances()
-    // [{ assetId: '0x', amount: 42 }, ...]
-    // #endregion provider-testnet
-
-    expect(balances).toBeTruthy();
-    expect(balances).toBeInstanceOf(Array);
 
     // #region signer-address
     const signer = new Signer(PRIVATE_KEY);
