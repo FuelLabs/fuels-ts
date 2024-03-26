ebf7c538ee0f8972ab8854136cad7300532ffe83
Peter Smith <peter@blueoceancomputing.co.uk>
2024-03-26 08:08:10 +0000

    chore: added FUEL_LATEST_TESTNET_URL constant

--- packages/account/src/configs.ts
+++ packages/account/src/configs.ts
@@ -2,3 +2,5 @@ export const FUEL_NETWORK_URL =
   typeof process !== 'undefined'
     ? process?.env?.FUEL_NETWORK_URL || 'http://127.0.0.1:4000/graphql'
     : 'http://127.0.0.1:4000/graphql';
+
+export const FUEL_LATEST_TESTNET_URL = 'https://beta-5.fuel.network/graphql';
