99b163e5d1758c0269edcfddb54e8cfcdcca0d25
nedsalk <nedim.salkic@fuel.sh>
2024-03-27 08:42:45 +0100

    test release of old patch

--- scripts/changeset/update-baseBranch.ts
+++ scripts/changeset/update-baseBranch.ts
@@ -14,15 +14,20 @@ const currentBranchName = execSync('git rev-parse --abbrev-ref HEAD').toString()
  */
 if (changesetConfig.baseBranch !== currentBranchName) {
   execSync(
-    `echo "updating baseBranch of .changeset/config.json from ${changesetConfig.baseBranch} to ${currentBranchName}"`
+    `echo "updating baseBranch of .changeset/config.json from ${changesetConfig.baseBranch} to ${currentBranchName}"`,
+    { stdio: 'inherit' }
   );
   changesetConfig.baseBranch = currentBranchName;
 
   writeFileSync(changesetPath, JSON.stringify(changesetConfig, null, 2), 'utf-8');
 
-  execSync('git config user.name "github-actions[bot]"');
-  execSync('git config user.email "github-actions[bot]@users.noreply.github.com"');
+  execSync('git config user.name "github-actions[bot]"', { stdio: 'inherit' });
+  execSync('git config user.email "github-actions[bot]@users.noreply.github.com"', {
+    stdio: 'inherit',
+  });
 
-  execSync(`git add .changeset/config.json`);
-  execSync(`git commit -m"ci(scripts): update baseBranch of .changeset/config.json"`);
+  execSync(`git add .changeset/config.json`, { stdio: 'inherit' });
+  execSync(`git commit -m"ci(scripts): update baseBranch of .changeset/config.json"`, {
+    stdio: 'inherit',
+  });
 }
