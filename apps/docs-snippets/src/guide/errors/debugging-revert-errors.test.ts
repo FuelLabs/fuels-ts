e8971ccb55a020911a63d81ba965ef7ae9affdde
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-03-27 13:11:31 -0300

    refact test suite for doc snippets

--- apps/docs-snippets/src/guide/errors/debugging-revert-errors.test.ts
+++ apps/docs-snippets/src/guide/errors/debugging-revert-errors.test.ts
@@ -10,47 +10,49 @@ import { createAndDeployContractFromProject } from '../../utils';
 /**
  * @group node
  */
-test('logs out custom require messages for error enums when tx reverts', async () => {
-  const contract = await createAndDeployContractFromProject(DocSnippetProjectsEnum.REVERT_ERRORS);
+describe(__filename, () => {
+  it('logs out custom require messages for error enums when tx reverts', async () => {
+    const contract = await createAndDeployContractFromProject(DocSnippetProjectsEnum.REVERT_ERRORS);
 
-  // #region revert-errors-4
-  expect(() => contract.functions.test_function_with_custom_error().call()).rejects.toThrow(
-    'The transaction reverted because of a "require" statement has thrown "InvalidInput".'
-  );
-  // #endregion revert-errors-4
-});
+    // #region revert-errors-4
+    expect(() => contract.functions.test_function_with_custom_error().call()).rejects.toThrow(
+      'The transaction reverted because of a "require" statement has thrown "InvalidInput".'
+    );
+    // #endregion revert-errors-4
+  });
 
-test('logs out custom require messages for require statements using str array when tx reverts', async () => {
-  const contract = await createAndDeployContractFromProject(DocSnippetProjectsEnum.REVERT_ERRORS);
+  it('logs out custom require messages for require statements using str array when tx reverts', async () => {
+    const contract = await createAndDeployContractFromProject(DocSnippetProjectsEnum.REVERT_ERRORS);
 
-  // #region revert-errors-7
-  expect(() => contract.functions.test_function_with_str_array_message().call()).rejects.toThrow(
-    'The transaction reverted because of a "require" statement has thrown "This is also a revert error".'
-  );
-  // #endregion revert-errors-7
-});
+    // #region revert-errors-7
+    expect(() => contract.functions.test_function_with_str_array_message().call()).rejects.toThrow(
+      'The transaction reverted because of a "require" statement has thrown "This is also a revert error".'
+    );
+    // #endregion revert-errors-7
+  });
 
-test('logs out a generic error message for require statements with a simple string message', async () => {
-  const contract = await createAndDeployContractFromProject(DocSnippetProjectsEnum.REVERT_ERRORS);
+  it('logs out a generic error message for require statements with a simple string message', async () => {
+    const contract = await createAndDeployContractFromProject(DocSnippetProjectsEnum.REVERT_ERRORS);
 
-  // #region revert-errors-5
-  expect(() => contract.functions.test_function().call()).rejects.toThrow(
-    'String slices can not be decoded from logs. Convert the slice to `str[N]` with `__to_str_array`'
-  );
-  // #endregion revert-errors-5
-});
+    // #region revert-errors-5
+    expect(() => contract.functions.test_function().call()).rejects.toThrow(
+      'String slices can not be decoded from logs. Convert the slice to `str[N]` with `__to_str_array`'
+    );
+    // #endregion revert-errors-5
+  });
 
-test('logs out custom require messages for script calls', async () => {
-  const { binHexlified, abiContents } = getDocsSnippetsForcProject(
-    DocSnippetProjectsEnum.REVERT_ERRORS_SCRIPT
-  );
+  it('logs out custom require messages for script calls', async () => {
+    const { binHexlified, abiContents } = getDocsSnippetsForcProject(
+      DocSnippetProjectsEnum.REVERT_ERRORS_SCRIPT
+    );
 
-  const provider = await Provider.create(FUEL_NETWORK_URL);
-  const wallet = await generateTestWallet(provider, [[1_000_000, BaseAssetId]]);
+    const provider = await Provider.create(FUEL_NETWORK_URL);
+    const wallet = await generateTestWallet(provider, [[1_000_000, BaseAssetId]]);
 
-  const script = new Script(binHexlified, abiContents, wallet);
+    const script = new Script(binHexlified, abiContents, wallet);
 
-  expect(() => script.functions.main().call()).rejects.toThrow(
-    'The transaction reverted because of a "require" statement has thrown "This is a revert error".'
-  );
+    expect(() => script.functions.main().call()).rejects.toThrow(
+      'The transaction reverted because of a "require" statement has thrown "This is a revert error".'
+    );
+  });
 });
