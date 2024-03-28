6d7c20c9842dedbd3124859f8a10dd7c9973de14
Daniel Bate <djbate23@gmail.com>
2024-03-28 15:09:04 +0000

    feat: upgrade to forc 0.52.0

--- packages/fuel-gauge/src/experimental-predicate.test.ts
+++ packages/fuel-gauge/src/experimental-predicate.test.ts
@@ -1,5 +1,5 @@
 import { readFileSync } from 'fs';
-import type { WalletLocked, WalletUnlocked } from 'fuels';
+import type { JsonAbi, Provider, WalletLocked, WalletUnlocked } from 'fuels';
 import { BaseAssetId, Predicate, bn } from 'fuels';
 import { join } from 'path';
 
@@ -7,7 +7,10 @@ import { fundPredicate, setupWallets } from './predicate/utils/predicate';
 
 let wallet: WalletUnlocked;
 let receiver: WalletLocked;
-let predicate: Predicate<[number]>;
+let bytecode: Buffer;
+let abi: JsonAbi;
+let provider: Provider;
+
 const U8_MAX = 255;
 const U16_MAX = 65535;
 const U32_MAX = 4294967295;
@@ -16,24 +19,22 @@ const U256_MAX = bn(2).pow(256).sub(1);
 const B512 =
   '0x8e9dda6f7793745ac5aacf9e907cae30b2a01fdf0d23b7750a85c6a44fca0c29f0906f9d1f1e92e6a1fb3c3dcef3cc3b3cdbaae27e47b9d9a4c6a4fce4cf16b2';
 
-beforeAll(async () => {
-  [wallet, receiver] = await setupWallets();
-  const name = 'predicate-echo';
-  const path = join(
-    __dirname,
-    `../test/fixtures/forc-projects-experimental/${name}/out/release/${name}`
-  );
-  const bytes = readFileSync(`${path}.bin`);
-  const abi = JSON.parse(readFileSync(`${path}-abi.json`, 'utf8'));
-
-  predicate = new Predicate(bytes, wallet.provider, abi);
-  await fundPredicate(wallet, predicate, 100_000);
-});
-
 /**
  * @group node
  */
 describe('Experimental Predicate', () => {
+  beforeAll(async () => {
+    [wallet, receiver] = await setupWallets();
+    const name = 'predicate-echo';
+    const path = join(
+      __dirname,
+      `../test/fixtures/forc-projects-experimental/${name}/out/release/${name}`
+    );
+    bytecode = readFileSync(`${path}.bin`);
+    abi = JSON.parse(readFileSync(`${path}-abi.json`, 'utf8'));
+    provider = wallet.provider;
+  });
+
   it('echos struct', async () => {
     const struct = {
       a: U8_MAX,
@@ -74,7 +75,14 @@ describe('Experimental Predicate', () => {
       },
     };
 
-    predicate.setData(struct);
+    const predicate = new Predicate({
+      bytecode,
+      provider,
+      abi,
+      inputData: struct,
+    });
+    await fundPredicate(wallet, predicate, 100_000);
+
     const tx = await predicate.transfer(receiver.address, 100, BaseAssetId);
     await tx.waitForResult();
   });
