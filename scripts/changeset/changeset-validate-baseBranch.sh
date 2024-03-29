941825906da72ef8f84ae7b98e93efa7a790f9ac
nedsalk <nedim.salkic@fuel.sh>
2024-03-29 07:10:23 +0100

    update message

--- scripts/changeset/changeset-validate-baseBranch.sh
+++ scripts/changeset/changeset-validate-baseBranch.sh
@@ -3,6 +3,6 @@
 BASE_BRANCH=$(jq ".baseBranch" <.changeset/config.json | sed "s/\"//g")
 
 if [ "$BASE_BRANCH" != "$GITHUB_BASE_REF" ]; then
-    echo "Changeset config's baseBranch property does not match PR base branch. Please update the changeset's config to match the PR base branch."
+    echo "Changeset config.json's baseBranch does not match PR base branch. Please update the changeset's config from $BASE_BRANCH to $GITHUB_BASE_REF."
     exit 1
 fi
