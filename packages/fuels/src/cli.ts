7445778d0f003cad8a9b95bc0a27161011ccbe8c
Nedim Salkić <nedim.salkic@fuel.sh>
2024-03-29 06:41:41 +0100

    chore: test patching old releases (#1974)

--- packages/fuels/src/cli.ts
+++ packages/fuels/src/cli.ts
@@ -22,6 +22,8 @@ export const onPreAction = (command: Command) => {
 };
 
 export const configureCli = () => {
+  // eslint-disable-next-line no-console
+  console.log('test release');
   const program = new Command();
   program.name('fuels');
 
