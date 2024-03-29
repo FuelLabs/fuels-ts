9763b19bba2bb29403a4a753ebea5cd44654b4c1
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-03-29 08:37:54 -0300

    make test case use proper tx assemble flow

--- apps/docs-snippets/src/guide/cookbook/custom-transactions-contract-calls.test.ts
+++ apps/docs-snippets/src/guide/cookbook/custom-transactions-contract-calls.test.ts
@@ -45,13 +45,13 @@ describe('Custom Transactions from Contract Calls', () => {
     // Connect to the contract
     const contractInstance = new Contract(contract.id, abi, senderWallet);
     // Create an invocation scope for the contract function you'd like to call in the transaction
-    const scope = contractInstance.functions.increment_count(amountToRecipient);
+    const scope = contractInstance.functions
+      .increment_count(amountToRecipient)
+      .addTransfer(receiverWallet.address, amountToRecipient, BaseAssetId);
     // Fund the transaction
     await scope.fundWithRequiredCoins();
     // Build a transaction request from the invocation scope
     const transactionRequest = await scope.getTransactionRequest();
-    // Add coin output for the recipient
-    transactionRequest.addCoinOutput(receiverWallet.address, amountToRecipient, BaseAssetId);
     // Submit the transaction
     const response = await senderWallet.sendTransaction(transactionRequest);
     await response.waitForResult();
