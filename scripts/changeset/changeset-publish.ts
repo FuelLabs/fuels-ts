0af1affa967bc757f745ae7908abd2b0e681fc43
nedsalk <nedim.salkic@fuel.sh>
2024-04-01 09:03:24 +0200

    fix: tag

--- scripts/changeset/changeset-publish.ts
+++ scripts/changeset/changeset-publish.ts
@@ -1,5 +1,5 @@
 import { execSync } from 'child_process';
 
 const TAG_NAME = process.argv[2];
-execSync(`pnpm changeset publish --no-git-tag --tag latest-release`);
+execSync(`pnpm changeset publish --no-git-tag --tag testing-release`);
 execSync(`git tag -d ${TAG_NAME}`);
