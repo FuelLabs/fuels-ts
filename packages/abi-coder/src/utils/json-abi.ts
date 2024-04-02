2ca112fa981dab66986e796693a1c4c39e16a696
Peter Smith <peter@blueoceancomputing.co.uk>
2024-04-02 15:54:52 +0100

    chore: fixing error message

--- packages/abi-coder/src/utils/json-abi.ts
+++ packages/abi-coder/src/utils/json-abi.ts
@@ -5,7 +5,7 @@ import type { JsonAbi, JsonAbiArgument, JsonAbiType } from '../types/JsonAbi';
 export const findFunctionByName = (abi: JsonAbi, name: string) => {
   const fn = abi.functions.find((f) => f.name === name);
   if (!fn) {
-    throw new FuelError(ErrorCode.FUNCTION_NOT_FOUND, `Function "${name}" not found in the ABI`);
+    throw new FuelError(ErrorCode.FUNCTION_NOT_FOUND, `Function with name '${name}' doesn't exist in the ABI`);
   }
   return fn;
 };
@@ -13,7 +13,7 @@ export const findFunctionByName = (abi: JsonAbi, name: string) => {
 export const findTypeById = (abi: JsonAbi, typeId: number): JsonAbiType => {
   const type = abi.types.find((t) => t.typeId === typeId);
   if (!type) {
-    throw new FuelError(ErrorCode.TYPE_NOT_FOUND, `Unable to find type with ID ${typeId}`);
+    throw new FuelError(ErrorCode.TYPE_NOT_FOUND, `Type with typeId '${typeId}' doesn't exist in the ABI.`);
   }
   return type;
 };
