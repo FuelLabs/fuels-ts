83c26e1d26f841b477f8e9bb99418a60a96ff4c2
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-04-06 18:02:52 -0300

    setting up chain configs for launch node

--- packages/utils/src/utils/defaultChainConfig.ts
+++ packages/utils/src/utils/defaultChainConfig.ts
@@ -1,6 +1,12 @@
-import chainConfigJson from './chainConfig.json';
+import chainConfigJson from './chainConfigs/chainConfig.json';
+import metadataJson from './chainConfigs/metadata.json';
+import stateConfigJson from './chainConfigs/stateConfig.json';
 
-export const defaultChainConfig = chainConfigJson;
+export const defaultChainConfigs = {
+  chainConfigJson,
+  metadataJson,
+  stateConfigJson,
+};
 
 export const defaultConsensusKey =
   '0xa449b1ffee0e2205fa924c6740cc48b3b473aa28587df6dab12abc245d1f5298';
