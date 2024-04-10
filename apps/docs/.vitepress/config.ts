05e7fd49874d50d9935c444f3a72f12242201168
Dhaiwat <dhaiwatpandya@gmail.com>
2024-04-10 18:38:39 +0530

    restructure docs folders

--- apps/docs/.vitepress/config.ts
+++ apps/docs/.vitepress/config.ts
@@ -384,7 +384,7 @@ export default defineConfig({
               {
                 text: 'Unit conversion',
                 link: '/guide/utilities/unit-conversion',
-              }
+              },
             ],
           },
           {
@@ -451,11 +451,11 @@ export default defineConfig({
             items: [
               {
                 text: 'npm create fuels',
-                link: '/guide/cli/npm-create-fuels/',
+                link: '/guide/npm-create-fuels/',
                 items: [
                   {
                     text: 'Options',
-                    link: '/guide/cli/npm-create-fuels/options',
+                    link: '/guide/npm-create-fuels/options',
                   },
                 ],
               },
