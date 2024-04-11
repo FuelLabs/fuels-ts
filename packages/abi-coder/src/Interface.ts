d123917773c1df58eb6684de12076232c3185657
Daniel Bate <djbate23@gmail.com>
2024-04-11 10:51:56 +0100

    feat: deprecate v1 encoding support for configurables

--- packages/abi-coder/src/Interface.ts
+++ packages/abi-coder/src/Interface.ts
@@ -99,7 +99,6 @@ export class Interface<TAbi extends JsonAbi = JsonAbi> {
 
     return AbiCoder.encode(this.jsonAbi, configurable.configurableType, value, {
       isRightPadded: true,
-      encoding: this.jsonAbi.encoding,
     });
   }
 
