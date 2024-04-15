526ce39b8cdaec740df81e874ef79eeacb3ce671
Peter Smith <peter@blueoceancomputing.co.uk>
2024-04-15 11:58:19 +0100

    chore: use latest fuel frontend packages

--- templates/nextjs/src/lib.ts
+++ templates/nextjs/src/lib.ts
@@ -1,11 +1,17 @@
-import { BN, WalletLocked, WalletUnlocked } from 'fuels';
+import { Account, BN, WalletLocked, WalletUnlocked } from 'fuels';
 
 export const NODE_URL = `http://127.0.0.1:${
   process.env.NEXT_PUBLIC_FUEL_NODE_PORT || 4000
 }/graphql`;
 
+/**
+ * Enable the Fuel dev connector.
+ * @see {@link https://docs.fuel.network/docs/wallet/dev/getting-started/#using-default-connectors}
+ */
+export const ENABLE_FUEL_DEV_CONNECTOR = process.env.NEXT_PUBLIC_ENABLE_FUEL_DEV_CONNECTOR === 'true';
+
 export interface AppWallet {
-  wallet?: WalletLocked | WalletUnlocked;
+  wallet?: Account | null | undefined;
   walletBalance?: BN;
   refreshWalletBalance?: () => Promise<void>;
 }
--- templates/nextjs/src/pages/_app.tsx
+++ templates/nextjs/src/pages/_app.tsx
@@ -1,14 +1,30 @@
 import { Layout } from "@/components/Layout";
 import "@/styles/globals.css";
-import { FuelProvider } from "@fuel-wallet/react";
+import { FuelProvider } from "@fuels/react";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import type { FuelConfig } from "fuels";
+import { defaultConnectors } from "@fuels/connectors";
+import { ENABLE_FUEL_DEV_CONNECTOR } from "@/lib";
+import React from "react";
+
+export const queryClient: QueryClient = new QueryClient()
+export const fuelConfig: FuelConfig = {
+  connectors: defaultConnectors({
+    devMode: ENABLE_FUEL_DEV_CONNECTOR,
+  })
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <FuelProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </FuelProvider>
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <FuelProvider fuelConfig={fuelConfig}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </FuelProvider>
+      </QueryClientProvider>
+    </React.StrictMode>
   );
 }
