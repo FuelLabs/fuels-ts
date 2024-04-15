f28899dadeeff62a0fd912c594c11057c671a09a
Peter Smith <peter@blueoceancomputing.co.uk>
2024-04-15 12:18:02 +0100

    chore: updated decimal constants

--- packages/math/src/configs.ts
+++ packages/math/src/configs.ts
@@ -6,10 +6,10 @@ export const DEFAULT_DECIMAL_UNITS = 9;
 export const DECIMAL_FUEL = 9;
 
 // Decimal units of Ether
-export const DECIMAL_WEI = 0;
-export const DECIMAL_KWEI = 3;
-export const DECIMAL_MWEI = 6;
+export const DECIMAL_WEI = 18;
+export const DECIMAL_KWEI = 15;
+export const DECIMAL_MWEI = 12;
 export const DECIMAL_GWEI = 9;
-export const DECIMAL_SZADO = 12;
-export const DECIMAL_FINNEY = 15;
-export const DECIMAL_ETHER = 18;
+export const DECIMAL_SZADO = 6;
+export const DECIMAL_FINNEY = 3;
+export const DECIMAL_ETHER = 0;
