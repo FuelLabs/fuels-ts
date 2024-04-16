5c530cd5463f938c2933a8071a85f85978c0b43a
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-04-16 15:13:23 -0300

    add transaction request test utilities

---
+++ packages/account/src/test-utils/transactionRequest.ts
@@ -0,0 +1,46 @@
+import { UTXO_ID_LEN } from '@fuel-ts/abi-coder';
+import { getRandomB256 } from '@fuel-ts/address';
+import { ZeroBytes32 } from '@fuel-ts/address/configs';
+import { bn } from '@fuel-ts/math';
+import { InputType } from '@fuel-ts/transactions';
+import { hexlify, randomBytes } from 'ethers';
+
+import type {
+  CoinTransactionRequestInput,
+  ContractTransactionRequestInput,
+  MessageTransactionRequestInput,
+} from '../providers';
+
+export const generateFakeRequestInputCoin = (
+  partial: Partial<CoinTransactionRequestInput> = {}
+): CoinTransactionRequestInput => ({
+  id: hexlify(randomBytes(UTXO_ID_LEN)),
+  type: InputType.Coin,
+  owner: getRandomB256(),
+  amount: bn(100),
+  assetId: ZeroBytes32,
+  txPointer: '0x00000000000000000000000000000000',
+  witnessIndex: 0,
+  ...partial,
+});
+
+export const generateFakeRequestInputMessage = (
+  partial: Partial<MessageTransactionRequestInput> = {}
+): MessageTransactionRequestInput => ({
+  nonce: getRandomB256(),
+  type: InputType.Message,
+  sender: getRandomB256(),
+  recipient: getRandomB256(),
+  amount: bn(100),
+  witnessIndex: 0,
+  ...partial,
+});
+
+export const generateFakeRequestInputContract = (
+  partial: Partial<ContractTransactionRequestInput> = {}
+): ContractTransactionRequestInput => ({
+  contractId: getRandomB256(),
+  type: InputType.Contract,
+  txPointer: '0x00000000000000000000000000000000',
+  ...partial,
+});
