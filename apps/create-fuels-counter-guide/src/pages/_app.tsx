74f7236cbb1dc99f8a5c27f7503dd5031d793e51
Peter Smith <peter@blueoceancomputing.co.uk>
2024-04-16 19:12:13 +0100

    chore: set fuelConfig on page load

--- apps/create-fuels-counter-guide/src/pages/_app.tsx
+++ apps/create-fuels-counter-guide/src/pages/_app.tsx
@@ -6,16 +6,21 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { FuelConfig } from "fuels";
 import { defaultConnectors } from "@fuels/connectors";
 import { ENABLE_FUEL_DEV_CONNECTOR } from "@/lib";
-import React from "react";
+import React, { useEffect } from "react";
 
-export const queryClient: QueryClient = new QueryClient()
-export const fuelConfig: FuelConfig = {
-  connectors: defaultConnectors({
-    devMode: ENABLE_FUEL_DEV_CONNECTOR,
-  })
-}
+const queryClient: QueryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
+  let fuelConfig: FuelConfig = {};
+
+  useEffect(() => {
+    fuelConfig = {
+      connectors: defaultConnectors({
+        devMode: ENABLE_FUEL_DEV_CONNECTOR
+      })
+    }
+  }, [])
+
   return (
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
