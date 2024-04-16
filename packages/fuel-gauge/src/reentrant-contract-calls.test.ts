e26fc412693581ffb07f25529f58787a3f113635
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-04-16 09:41:13 -0300

    add another test suite

--- packages/fuel-gauge/src/reentrant-contract-calls.test.ts
+++ packages/fuel-gauge/src/reentrant-contract-calls.test.ts
@@ -1,26 +1,33 @@
 import { generateTestWallet } from '@fuel-ts/account/test-utils';
-import { BaseAssetId, ContractFactory, FUEL_NETWORK_URL, Provider, ReceiptType } from 'fuels';
+import type { Contract, WalletUnlocked } from 'fuels';
+import { BaseAssetId, ContractFactory, FUEL_NETWORK_URL, Provider, ReceiptType, bn } from 'fuels';
 
-import type { FuelGaugeProjectsEnum } from '../test/fixtures';
-import { getFuelGaugeForcProject } from '../test/fixtures';
+import { FuelGaugeProjectsEnum, getFuelGaugeForcProject } from '../test/fixtures';
 
-const bar = getFuelGaugeForcProject('bar' as FuelGaugeProjectsEnum);
-const foo = getFuelGaugeForcProject('foo' as FuelGaugeProjectsEnum);
+const bar = getFuelGaugeForcProject(FuelGaugeProjectsEnum.REENTRANT_BAR);
+const foo = getFuelGaugeForcProject(FuelGaugeProjectsEnum.REENTRANT_FOO);
+const storageTest = getFuelGaugeForcProject(FuelGaugeProjectsEnum.STORAGE_TEST_CONTRACT);
 
 /**
  * @group node
  */
 describe('Reentrant Contract Calls', () => {
-  it('should ensure the SDK returns the proper value for a reentrant call', async () => {
+  let barContract: Contract;
+  let fooContract: Contract;
+  let wallet: WalletUnlocked;
+
+  beforeAll(async () => {
     const provider = await Provider.create(FUEL_NETWORK_URL);
-    const wallet = await generateTestWallet(provider, [[10_000, BaseAssetId]]);
+    wallet = await generateTestWallet(provider, [[10_000, BaseAssetId]]);
 
     const factoryBar = new ContractFactory(bar.binHexlified, bar.abiContents, wallet);
-    const barContract = await factoryBar.deployContract();
+    barContract = await factoryBar.deployContract();
 
     const factoryFoo = new ContractFactory(foo.binHexlified, foo.abiContents, wallet);
-    const fooContract = await factoryFoo.deployContract();
+    fooContract = await factoryFoo.deployContract();
+  });
 
+  it('should ensure the SDK returns the proper value for a reentrant call', async () => {
     const {
       value,
       transactionResult: { receipts },
@@ -29,20 +36,54 @@ describe('Reentrant Contract Calls', () => {
       .addContracts([barContract])
       .call();
 
+    /**
+     * First, the test will call:
+     *   Foo.foo()
+     *
+     * Then `Foo` will call `Bar`:
+     *   Foo.foo() --\> Bar.bar()
+     *
+     * And `Bar` will call `Foo` again:
+     *   Bar.bar() --\> Foo.baz()
+     *
+     * Because two functions from `Foo` contract were called, and both
+     * return values, two `Return` receipts are generated with the same
+     * contract ID.
+     *
+     * The SDK should be able to distinguish them and correctly return
+     * number 42 (from `Foo.foo`) instead of 1337 (from `Foo.baz`).
+     */
     const returnReceipts = receipts.filter(
       (r) => r.type === ReceiptType.Return && r.id === fooContract.id.toB256()
     );
 
-    /**
-     * Foo contract will call its foo function which will call the bar function from the
-     * Bar contract, then the Bar contract within bar function will call the Foo contract again
-     * but this time the baz function wich will return 1337. At this point the foo function (original
-     * called function) will continue its execution and return 42. Because 2 functions from Foo
-     * contract were called and both of them return values, 2 Return receipts are generated with
-     * the same contract ID. The SDK should return the value from the last one, which is the
-     * returned value from the foo function.
-     */
     expect(value.toNumber()).toBe(42);
     expect(returnReceipts.length).toBe(2);
+  });
+
+  it('should ensure the SDK returns the proper value for a reentrant call on multi-call', async () => {
+    const storageContract = await new ContractFactory(
+      storageTest.binHexlified,
+      storageTest.abiContents,
+      wallet
+    ).deployContract({ storageSlots: storageTest.storageSlots });
+
+    const reentrantCall = fooContract.functions.foo(
+      { value: fooContract.id.toB256() },
+      { value: barContract.id.toB256() }
+    );
+
+    const result = await fooContract
+      .multiCall([reentrantCall, storageContract.functions.return_var3(), reentrantCall])
+      .call();
+
+    const expectedReentrantValue = 42;
+
+    // return value from first call to reentrant contract call
+    expect(bn(result.value[0]).toNumber()).toBe(expectedReentrantValue);
+    // return value from call to a different contract
+    expect(bn(result.value[1]).toNumber()).not.toBe(expectedReentrantValue);
+    // return value from second call to reentrant contract call
+    expect(bn(result.value[2]).toNumber()).toBe(expectedReentrantValue);
   });
 });
