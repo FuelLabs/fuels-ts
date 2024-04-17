71de1a7b0c24de92de6244b4877bbb94a6ecc6ea
chad <chad.nehemiah94@gmail.com>
2024-04-17 16:13:10 -0500

    fix: update browser hmac

--- packages/crypto/src/browser/hmac.ts
+++ packages/crypto/src/browser/hmac.ts
@@ -1,4 +1,4 @@
-import { toHex } from '@fuel-ts/math';
+import { hexlify } from '@fuel-ts/utils';
 import { hmac } from '@noble/hashes/hmac';
 import { sha256 } from '@noble/hashes/sha256';
 import { sha512 } from '@noble/hashes/sha512';
@@ -11,6 +11,6 @@ export const computeHmac: CryptoApi['computeHmac'] = (
   data: Uint8Array
 ): string => {
   const hash = algorithm === 'sha256' ? sha256 : sha512;
-  const signature = hmac(hash, key, data);
-  return toHex(signature);
+  const signature = hmac.create(hash, key).update(data).digest();
+  return hexlify(signature);
 };
