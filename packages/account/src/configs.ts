f965a6595849dd297d250614fe784cfaeff5b99d
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-04-18 11:42:31 -0300

    add constant for test net

--- packages/account/src/configs.ts
+++ packages/account/src/configs.ts
@@ -4,3 +4,4 @@ export const FUEL_NETWORK_URL: string =
     : 'http://127.0.0.1:4000/v1/graphql';
 
 export const FUEL_BETA_5_NETWORK_URL: string = 'https://beta-5.fuel.network/graphql';
+export const FUEL_TEST_NET_NETWORK_URL: string = 'https://testnet.swayswap.io/v1/graphql';
