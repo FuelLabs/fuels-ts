0ff874317c239d8e4055a5aec711e4992c9f3d06
Peter Smith <peter@blueoceancomputing.co.uk>
2024-04-18 15:34:21 +0100

    chore: added fuelsVersion to create-fuels

--- packages/create-fuels/scripts/rewriteTemplatePackageJson.ts
+++ packages/create-fuels/scripts/rewriteTemplatePackageJson.ts
@@ -2,12 +2,13 @@ import { versions } from '@fuel-ts/versions';
 import { readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
 
+const fuelsVersion = versions.FUELS
 const filepath = join(__dirname, '../templates/nextjs/package.json');
 
 let contents = readFileSync(filepath, 'utf-8');
 
 contents = contents.replace(/xprebuild/g, 'prebuild');
-contents = contents.replace(/"fuels": "workspace:\*"/, `"fuels": "${versions.FUELS}"`);
+contents = contents.replace(/"fuels": "workspace:\*"/, `"fuels": "${fuelsVersion}"`);
 contents = contents.replace(/"build": "pnpm run prebuild && next build"/, `"build": "next build"`);
 
 writeFileSync(filepath, contents);
