1a875afaf79341d007abf21371fcaa07e7b106f1
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-04-18 10:39:34 -0300

    ajusting test funding

--- packages/fuel-gauge/src/reentrant-contract-calls.test.ts
+++ packages/fuel-gauge/src/reentrant-contract-calls.test.ts
@@ -18,7 +18,7 @@ describe('Reentrant Contract Calls', () => {
 
   beforeAll(async () => {
     const provider = await Provider.create(FUEL_NETWORK_URL);
-    wallet = await generateTestWallet(provider, [[10_000, BaseAssetId]]);
+    wallet = await generateTestWallet(provider, [[20_000, BaseAssetId]]);
 
     const factoryBar = new ContractFactory(bar.binHexlified, bar.abiContents, wallet);
     barContract = await factoryBar.deployContract();
