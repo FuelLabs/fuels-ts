54ceda4d5e2249871128538f7d5bc03a6043ec0f
chad <chad.nehemiah94@gmail.com>
2024-04-18 21:02:10 -0500

    test: update hmac test

--- packages/crypto/test/hmac.test.ts
+++ packages/crypto/test/hmac.test.ts
@@ -10,9 +10,12 @@ describe('computeHmac node & browser', () => {
   it('should compute HMAC correctly', () => {
     const key = '0x0102030405060708090a0b0c0d0e0f10';
     const data = '0x11121314151617181920212223242526';
+    const sha256Length = 64;
+    const sha512Length = 128;
+    const prefix = '0x';
 
-    expect(computeHmac('sha256', key, data).length).toBe(64);
-    expect(computeHmac('sha512', key, data).length).toBe(128);
+    expect(computeHmac('sha256', key, data).length).toBe(sha256Length + prefix.length);
+    expect(computeHmac('sha512', key, data).length).toBe(sha512Length + prefix.length);
   });
 });
 
