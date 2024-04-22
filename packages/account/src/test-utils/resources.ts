3258a46e658efa6f97e2ebed526dc2fb961236ba
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-04-22 07:42:23 -0300

    add test utilities

---
+++ packages/account/src/test-utils/resources.ts
@@ -0,0 +1,29 @@
+import { BYTES_32, UTXO_ID_LEN } from '@fuel-ts/abi-coder';
+import { Address } from '@fuel-ts/address';
+import { ZeroBytes32 } from '@fuel-ts/address/configs';
+import { randomBytes } from '@fuel-ts/crypto';
+import { bn } from '@fuel-ts/math';
+import { hexlify } from '@fuel-ts/utils';
+
+import type { Coin, MessageCoin } from '../providers';
+
+export const generateFakeCoin = (partial: Partial<Coin> = {}): Coin => ({
+  id: hexlify(randomBytes(UTXO_ID_LEN)),
+  amount: bn(100),
+  assetId: ZeroBytes32,
+  owner: Address.fromRandom(),
+  maturity: 0,
+  blockCreated: bn(0),
+  txCreatedIdx: bn(0),
+  ...partial,
+});
+
+export const generateFakeMessageCoin = (partial: Partial<MessageCoin> = {}): MessageCoin => ({
+  sender: Address.fromRandom(),
+  recipient: Address.fromRandom(),
+  assetId: ZeroBytes32,
+  amount: bn(100),
+  nonce: hexlify(randomBytes(BYTES_32)),
+  daHeight: bn(0),
+  ...partial,
+});
