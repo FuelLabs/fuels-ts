80eeb7416410fc05eb3d373c6366ae8115b87a3f
Peter Smith <peter@blueoceancomputing.co.uk>
2024-04-23 17:37:07 +0100

    Reverted changes to fuels template version

--- packages/create-fuels/scripts/rewriteTemplatePackageJson.ts
+++ packages/create-fuels/scripts/rewriteTemplatePackageJson.ts
@@ -1,12 +1,12 @@
+import { versions } from '@fuel-ts/versions';
 import { readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
 
-const fuelsVersion = '0.81.0';
 const filepath = join(__dirname, '../templates/nextjs/package.json');
 
 let contents = readFileSync(filepath, 'utf-8');
 
 contents = contents.replace(/xprebuild/g, 'prebuild');
-contents = contents.replace(/"fuels": "workspace:\*"/, `"fuels": "${fuelsVersion}"`);
+contents = contents.replace(/"fuels": "workspace:\*"/, `"fuels": "${versions.FUELS}"`);
 
 writeFileSync(filepath, contents);
