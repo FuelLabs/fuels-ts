bdbd7e60d37518e81b720688670c3334b3357ab8
Daniel Bate <djbate23@gmail.com>
2024-04-23 16:26:52 +0100

    chore: use experimental encoding for abi coder

--- packages/abi-coder/test/utils/constants.ts
+++ packages/abi-coder/test/utils/constants.ts
@@ -1,17 +1,25 @@
 import { bn } from '@fuel-ts/math';
 
 export const U8_MAX = 2 ** 8 - 1;
-export const U8_MAX_ENCODED = new Uint8Array([255, 0, 0, 0, 0, 0, 0, 0]);
+export const U8_MAX_ENCODED = new Uint8Array([255]);
+export const U8_MIN_ENCODED = new Uint8Array([0]);
 export const U16_MAX = 2 ** 16 - 1;
-export const U16_MAX_ENCODED = new Uint8Array([0, 0, 0, 0, 0, 0, 255, 255]);
+export const U16_MAX_ENCODED = new Uint8Array([255, 255]);
+export const U16_MIN_ENCODED = new Uint8Array([0, 0]);
 export const U32_MAX = 2 ** 32 - 1;
-export const U32_MAX_ENCODED = new Uint8Array([0, 0, 0, 0, 255, 255, 255, 255]);
+export const U32_MAX_ENCODED = new Uint8Array([255, 255, 255, 255]);
+export const U32_MIN_ENCODED = new Uint8Array([0, 0, 0, 0]);
 export const U64_MAX = bn(2).pow(64).sub(1);
 export const U64_MAX_ENCODED = new Uint8Array([255, 255, 255, 255, 255, 255, 255, 255]);
+export const U64_MIN_ENCODED = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0]);
 export const U256_MAX = bn(2).pow(256).sub(1);
 export const U256_MAX_ENCODED = new Uint8Array(32).fill(255);
+export const U256_MIN_ENCODED = new Uint8Array(32).fill(0);
 
 export const EMPTY_8_BYTE_ARRAY = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0]);
+export const ENUM_FIRST_INDEX = EMPTY_8_BYTE_ARRAY;
+export const ENUM_SECOND_INDEX = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 1]);
+export const ENUM_THIRD_INDEX = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 2]);
 
 export const STRING_MIN_DECODED = '';
 export const STRING_MIN_ENCODED = new Uint8Array();
@@ -34,7 +42,8 @@ export const BYTE_MIN_ENCODED = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0]);
 export const BYTE_MAX_DECODED = U8_MAX;
 export const BYTE_MAX_ENCODED = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 255]);
 
-export const BOOL_TRUE_ENCODED = new Uint8Array([1, 0, 0, 0, 0, 0, 0, 0]);
+export const BOOL_TRUE_ENCODED = new Uint8Array([1]);
+export const BOOL_FALSE_ENCODED = new Uint8Array([0]);
 
 export const B512_DECODED =
   '0x8e9dda6f7793745ac5aacf9e907cae30b2a01fdf0d23b7750a85c6a44fca0c29f0906f9d1f1e92e6a1fb3c3dcef3cc3b3cdbaae27e47b9d9a4c6a4fce4cf16b2';
