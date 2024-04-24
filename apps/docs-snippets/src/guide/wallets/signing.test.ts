567c34a325f108b3bc617e16f3e489b22c48588b
Daniel Bate <djbate23@gmail.com>
2024-04-24 21:43:40 +0100

    chore: fix changes in merged tests

--- apps/docs-snippets/src/guide/wallets/signing.test.ts
+++ apps/docs-snippets/src/guide/wallets/signing.test.ts
@@ -1,7 +1,6 @@
 import { seedTestWallet } from '@fuel-ts/account/test-utils';
 import {
   Address,
-  BaseAssetId,
   FUEL_NETWORK_URL,
   Provider,
   ScriptTransactionRequest,
@@ -9,15 +8,18 @@ import {
   WalletUnlocked,
   hashMessage,
 } from 'fuels';
+import { b } from 'vitest/dist/suite-ynYMzeLu';
 
 /**
  * @group node
  */
 describe(__filename, () => {
   let provider: Provider;
+  let baseAssetId: string;
 
   beforeAll(async () => {
     provider = await Provider.create(FUEL_NETWORK_URL);
+    baseAssetId = provider.getBaseAssetId();
   });
 
   it('should sign a message using wallet instance', async () => {
@@ -44,7 +46,7 @@ describe(__filename, () => {
 
   it('should sign a transaction using wallet instance [DETAILED]', async () => {
     const wallet = WalletUnlocked.generate({ provider });
-    await seedTestWallet(wallet, [[10_000, BaseAssetId]]);
+    await seedTestWallet(wallet, [[10_000, baseAssetId]]);
 
     // #region signing-2
     const request = new ScriptTransactionRequest({
@@ -52,7 +54,7 @@ describe(__filename, () => {
       gasPrice: 1,
     });
 
-    request.addCoinOutput(Address.fromRandom(), 1000, BaseAssetId);
+    request.addCoinOutput(Address.fromRandom(), 1000, baseAssetId);
 
     const { maxFee, gasUsed, requiredQuantities } = await provider.getTransactionCost(request);
 
@@ -77,14 +79,14 @@ describe(__filename, () => {
 
   it('should sign a transaction using wallet instance [SIMPLIFIED]', async () => {
     const wallet = WalletUnlocked.generate({ provider });
-    await seedTestWallet(wallet, [[10_000, BaseAssetId]]);
+    await seedTestWallet(wallet, [[10_000, baseAssetId]]);
 
     const request = new ScriptTransactionRequest({
       gasLimit: 10000,
       gasPrice: 1,
     });
 
-    request.addCoinOutput(Address.fromRandom(), 1000, BaseAssetId);
+    request.addCoinOutput(Address.fromRandom(), 1000, baseAssetId);
 
     const { maxFee, gasUsed, requiredQuantities } = await provider.getTransactionCost(request);
 
