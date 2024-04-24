94cc76c29f7d971a370155a1b85ae5db6abe1867
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-04-24 12:12:26 -0300

    implement isDefined helper

---
+++ packages/utils/src/utils/isDefined.test.ts
@@ -0,0 +1,16 @@
+import { isDefined } from './isDefined';
+
+describe('isDefined', () => {
+  it('should return true for non-undefined values', () => {
+    expect(isDefined(0)).toBe(true);
+    expect(isDefined('')).toBe(true);
+    expect(isDefined(false)).toBe(true);
+    expect(isDefined(null)).toBe(true);
+    expect(isDefined({})).toBe(true);
+    expect(isDefined([])).toBe(true);
+  });
+
+  it('should return false for undefined values', () => {
+    expect(isDefined(undefined)).toBe(false);
+  });
+});
