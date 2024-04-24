8d4a00954fdcb4735c98cb11c1fa5b57d2f02451
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-04-24 13:01:01 -0300

    using fuel core 0.24.3

--- packages/account/src/providers/provider.test.ts
+++ packages/account/src/providers/provider.test.ts
@@ -62,7 +62,7 @@ describe('Provider', () => {
 
     const version = await provider.getVersion();
 
-    expect(version).toEqual('0.24.2');
+    expect(version).toEqual('0.24.3');
   });
 
   it('can call()', async () => {
--- packages/versions/src/lib/getBuiltinVersions.ts
+++ packages/versions/src/lib/getBuiltinVersions.ts
@@ -1,7 +1,7 @@
 export function getBuiltinVersions() {
   return {
     FORC: '0.51.1',
-    FUEL_CORE: '0.24.2',
+    FUEL_CORE: '0.24.3',
     FUELS: '0.81.0',
   };
 }
