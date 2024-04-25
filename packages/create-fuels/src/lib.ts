d89e4bb6921b3801fd93da5fc1d0ceb40218286f
Dhaiwat <dhaiwatpandya@gmail.com>
2024-04-25 14:45:44 +0530

    add error msg if installation fails

--- packages/create-fuels/src/lib.ts
+++ packages/create-fuels/src/lib.ts
@@ -1,4 +1,6 @@
+import chalk from 'chalk';
 import { execSync } from 'child_process';
+import { log } from 'console';
 
 export const checkIfFuelUpInstalled = () => {
   try {
@@ -9,6 +11,17 @@ export const checkIfFuelUpInstalled = () => {
   }
 };
 
-export const installFuelUp = () => {
-  execSync(`curl https://install.fuel.network | sh`, { stdio: 'inherit' });
+export const installFuelUp = (isVerbose: boolean = false) => {
+  try {
+    execSync(`curl https://install.fuel.network | sh`, { stdio: 'inherit' });
+  } catch (error) {
+    if (isVerbose) {
+      log(error);
+    }
+    log(
+      chalk.red(
+        'An error occurred while installing `fuelup`. Please try again, or try installing it manually. See https://docs.fuel.network/guides/installation/#running-fuelup-init for more information.'
+      )
+    );
+  }
 };
