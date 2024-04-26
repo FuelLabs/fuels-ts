33538abd5279ad83ba62ee2c066b5cbf8fe4c309
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-04-26 13:12:21 -0300

    ajusting helper function signature

--- packages/fuel-gauge/src/predicate/utils/predicate/assertBalances.ts
+++ packages/fuel-gauge/src/predicate/utils/predicate/assertBalances.ts
@@ -1,13 +1,11 @@
 import { bn, toHex } from 'fuels';
-import type { InputValue, BN, BigNumberish, WalletLocked } from 'fuels';
+import type { BN, BigNumberish, WalletLocked } from 'fuels';
 
-export const assertBalances = async <T extends InputValue[]>(
+export const assertBalances = async (
   receiver: WalletLocked,
   initialReceiverBalance: BN,
   amountToReceiver: BigNumberish
 ): Promise<void> => {
-  // Check there are UTXO locked with the predicate hash
-  // !isSkippingInitialReceiverBalance && expect(initialReceiverBalance.toHex()).toEqual(toHex(0));
   expect(initialReceiverBalance.toHex()).toEqual(toHex(0));
 
   // Check the balance of the receiver
