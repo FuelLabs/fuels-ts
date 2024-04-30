337bc5ab6222434013a864e2d1e782338df71270
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-04-30 17:53:56 -0300

    add tests for TransactionUploadCoder

--- packages/transactions/src/coders/transaction.test.ts
+++ packages/transactions/src/coders/transaction.test.ts
@@ -399,4 +399,136 @@ describe('TransactionCoder', () => {
       JSON.parse(JSON.stringify(transaction))
     );
   });
+
+  it('Can encode/decode TransactionUpload without inputs, outputs and witnesses', () => {
+    const transaction: Transaction<TransactionType.Upload> = {
+      type: TransactionType.Upload,
+      root: B256,
+      witnessIndex: U16,
+      subsectionIndex: U16,
+      subsectionsNumber: U16,
+      proofSetCount: 3,
+      policyTypes: 7,
+      inputsCount: 0,
+      outputsCount: 0,
+      witnessesCount: 0,
+      proofSet: [B256, B256, B256],
+      policies: [
+        { type: PolicyType.Tip, data: bn(U32) },
+        { type: PolicyType.WitnessLimit, data: bn(U32) },
+        { type: PolicyType.Maturity, data: U32 },
+      ],
+      inputs: [],
+      outputs: [],
+      witnesses: [],
+    };
+
+    const encoded = hexlify(new TransactionCoder().encode(transaction));
+
+    expect(encoded).toEqual(
+      '0x0000000000000004d5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930b00000000000003840000000000000384000000000000038400000000000000030000000000000007000000000000000000000000000000000000000000000000d5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930bd5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930bd5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930b00000000000003e800000000000003e800000000000003e8'
+    );
+
+    const [decoded, offset] = new TransactionCoder().decode(arrayify(encoded), 0);
+
+    expect(offset).toEqual((encoded.length - 2) / 2);
+    expect(JSON.parse(JSON.stringify(decoded))).toMatchObject(
+      JSON.parse(JSON.stringify(transaction))
+    );
+  });
+
+  it('Can encode/decode TransactionUpload with inputs, outputs and witnesses', () => {
+    const transaction: Transaction<TransactionType.Upload> = {
+      type: TransactionType.Upload,
+      root: B256,
+      witnessIndex: U16,
+      subsectionIndex: U16,
+      subsectionsNumber: U16,
+      proofSetCount: 5,
+      policyTypes: 15,
+      inputsCount: 2,
+      outputsCount: 2,
+      witnessesCount: 3,
+      proofSet: [B256, B256, B256, B256, B256],
+      policies: [
+        { type: PolicyType.Tip, data: bn(U32) },
+        { type: PolicyType.WitnessLimit, data: bn(U32) },
+        { type: PolicyType.Maturity, data: U32 },
+        { type: PolicyType.MaxFee, data: U64 },
+      ],
+      inputs: [
+        {
+          type: InputType.Coin,
+          txID: B256,
+          outputIndex: 0,
+          owner: B256,
+          amount: bn(0),
+          assetId: B256,
+          txPointer: {
+            blockHeight: 0,
+            txIndex: 0,
+          },
+          witnessIndex: 0,
+          predicateGasUsed: bn(0),
+          predicateLength: bn(0),
+          predicateDataLength: bn(0),
+          predicate: '0x',
+          predicateData: '0x',
+        },
+        {
+          type: InputType.Contract,
+          txID: B256,
+          outputIndex: 0,
+          balanceRoot: B256,
+          stateRoot: B256,
+          contractID: B256,
+          txPointer: {
+            blockHeight: 0,
+            txIndex: 0,
+          },
+        },
+      ],
+      outputs: [
+        {
+          type: OutputType.Coin,
+          to: B256,
+          amount: bn(1),
+          assetId: B256,
+        },
+        {
+          type: OutputType.Change,
+          to: B256,
+          amount: bn(1),
+          assetId: B256,
+        },
+      ],
+      witnesses: [
+        {
+          dataLength: 1,
+          data: '0x01',
+        },
+        {
+          dataLength: 2,
+          data: '0x0101',
+        },
+        {
+          dataLength: 3,
+          data: '0x010101',
+        },
+      ],
+    };
+
+    const encoded = hexlify(new TransactionCoder().encode(transaction));
+
+    expect(encoded).toEqual(
+      '0x0000000000000004d5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930b0000000000000384000000000000038400000000000003840000000000000005000000000000000f000000000000000200000000000000020000000000000003d5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930bd5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930bd5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930bd5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930bd5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930b00000000000003e800000000000003e800000000000003e800000000000000200000000000000000d5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930b0000000000000000d5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930b0000000000000000d5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930b0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001d5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930b0000000000000000d5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930bd5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930b00000000000000000000000000000000d5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930b0000000000000000d5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930b0000000000000001d5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930b0000000000000002d5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930b0000000000000001d5579c46dfcc7f18207013e65b44e4cb4e2c2298f4ac457ba8f82743f31e930b000000000000000101000000000000000000000000000002010100000000000000000000000000030101010000000000'
+    );
+
+    const [decoded, offset] = new TransactionCoder().decode(arrayify(encoded), 0);
+
+    expect(offset).toEqual((encoded.length - 2) / 2);
+    expect(JSON.parse(JSON.stringify(decoded))).toMatchObject(
+      JSON.parse(JSON.stringify(transaction))
+    );
+  });
 });
