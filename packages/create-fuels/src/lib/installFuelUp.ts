6983086f797c9cb2d3f872f9e303c13a38cae730
Peter Smith <peter@blueoceancomputing.co.uk>
2024-05-02 16:50:32 +0100

    Added installFuelUp tests

---
+++ packages/create-fuels/src/lib/installFuelUp.ts
@@ -0,0 +1,23 @@
+import { execSync } from 'child_process';
+import { log } from 'console';
+import ora from 'ora';
+
+export const installFuelUp = (isVerbose: boolean = false) => {
+  const installFuelUpSpinner = ora({
+    text: 'Installing fuelup..',
+    color: 'green',
+  });
+  try {
+    execSync(`curl https://install.fuel.network | sh`, { stdio: 'inherit' });
+    installFuelUpSpinner.succeed('Successfully installed fuelup!');
+  } catch (error) {
+    if (isVerbose) {
+      log(error);
+    }
+    log(
+      installFuelUpSpinner.fail(
+        `An error occurred while installing 'fuelup'. Please try again, or try installing it manually. See https://docs.fuel.network/guides/installation/#running-fuelup-init for more information.`
+      )
+    );
+  }
+};
