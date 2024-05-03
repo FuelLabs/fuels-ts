5e5a146d89642518af111b3bade979a8b63b87fd
Peter Smith <peter@blueoceancomputing.co.uk>
2024-05-03 12:59:00 +0100

    Added tests for promptForPackageManager

--- packages/create-fuels/src/cli.ts
+++ packages/create-fuels/src/cli.ts
@@ -11,7 +11,7 @@ import prompts from 'prompts';
 import packageJson from '../package.json';
 
 import { checkIfFuelUpInstalled, installFuelUp } from './lib';
-import { promptFuelUpInstall } from './prompts';
+import { promptForPackageManager, promptFuelUpInstall } from './prompts';
 
 const log = (...data: unknown[]) => {
   process.stdout.write(`${data.join(' ')}\n`);
@@ -47,23 +47,6 @@ async function promptForProjectPath() {
   );
 
   return res.projectName as string;
-}
-
-async function promptForPackageManager() {
-  const packageManagerInput = await prompts(
-    {
-      type: 'select',
-      name: 'packageManager',
-      message: 'Select a package manager',
-      choices: [
-        { title: 'pnpm', value: 'pnpm' },
-        { title: 'npm', value: 'npm' },
-      ],
-      initial: 0,
-    },
-    { onCancel: () => process.exit(0) }
-  );
-  return packageManagerInput.packageManager as string;
 }
 
 async function promptForProgramsToInclude({
--- packages/create-fuels/src/prompts/index.ts
+++ packages/create-fuels/src/prompts/index.ts
@@ -1,1 +1,2 @@
 export * from './promptFuelUpInstall';
+export * from './promptForPackageManager';
