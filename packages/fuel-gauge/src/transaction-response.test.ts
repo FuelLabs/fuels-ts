84b92a9fe69f6e885898ff045571caa6fd405b28
nedsalk <nedim.salkic@fuel.sh>
2024-05-13 13:46:03 +0200

    increase ttl

--- packages/fuel-gauge/src/transaction-response.test.ts
+++ packages/fuel-gauge/src/transaction-response.test.ts
@@ -251,7 +251,7 @@ describe('TransactionResponse', () => {
     'should throw error for a SqueezedOut status update [submitAndAwait]',
     async () => {
       const { cleanup, ip, port } = await launchNode({
-        args: ['--poa-instant', 'false', '--poa-interval-period', '1s', '--tx-pool-ttl', '200ms'],
+        args: ['--poa-instant', 'false', '--poa-interval-period', '2s', '--tx-pool-ttl', '500ms'],
         loggingEnabled: true,
         debugEnabled: true,
       });
