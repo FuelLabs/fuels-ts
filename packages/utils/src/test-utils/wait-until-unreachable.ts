1139701af5c8f102b910bac7a17e49e86b75cfb7
nedsalk <nedim.salkic@fuel.sh>
2024-05-15 10:16:40 +0200

    add description to `waitUntilUnreachable`

--- packages/utils/src/test-utils/wait-until-unreachable.ts
+++ packages/utils/src/test-utils/wait-until-unreachable.ts
@@ -2,6 +2,11 @@ import { sleep } from '../utils/sleep';
 
 import { urlIsLive } from './url-is-live';
 
+/**
+ * @hidden
+ *
+ * Mainly used to verify that a fuel-core node has been killed after cleanup is called.
+ */
 export async function waitUntilUnreachable(url: string) {
   const isLive = await urlIsLive(url);
   if (!isLive) {
