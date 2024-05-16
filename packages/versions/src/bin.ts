3d4e4785165c725706917aba4760b9b54ba3d026
Daniel Bate <djbate23@gmail.com>
2024-05-16 18:09:41 +0100

    feat: make versions bin async

--- packages/versions/src/bin.ts
+++ packages/versions/src/bin.ts
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
+import { error } from 'console';
+
 import { runVersions } from './cli';
 
-runVersions();
+(async () => {
+  await runVersions();
+})().catch((e) => {
+  error(e);
+  process.exit(1);
+});
