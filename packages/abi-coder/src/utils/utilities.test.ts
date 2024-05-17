27f22fe9f27f4d06ae2e185037e5a45ad7a86c5c
Daniel Bate <djbate23@gmail.com>
2024-05-17 11:39:39 +0100

    test: add false test case

--- packages/abi-coder/src/utils/utilities.test.ts
+++ packages/abi-coder/src/utils/utilities.test.ts
@@ -5,7 +5,7 @@ import { hasNestedOption, type TCoders } from './utilities';
  * @group browser
  */
 describe('findNestedOption', () => {
-  it('finds a deeply nested option', () => {
+  it('finds a deeply nested option - true', () => {
     const coders: TCoders = {
       DeepEnum: {
         name: 'DeepEnum',
@@ -53,5 +53,55 @@ describe('findNestedOption', () => {
     };
 
     expect(hasNestedOption(coders)).toBe(true);
+  });
+
+  it('finds a deeply nested option - false', () => {
+    const coders: TCoders = {
+      DeepEnum: {
+        name: 'DeepEnum',
+        type: 'enum DeepEnum',
+        encodedLength: 36,
+        // @ts-expect-error enum coder contains coders
+        coders: {
+          a: {
+            name: 'tuple',
+            type: '(boolean, [enum Option; 3])',
+            encodedLength: 28,
+            coders: [
+              {
+                name: 'boolean',
+                type: 'boolean',
+                encodedLength: 1,
+                options: { padToWordSize: false },
+              },
+              {
+                name: 'array',
+                type: '[enum Option; 3]',
+                encodedLength: 27,
+                // Deeply nested option
+                coder: {
+                  name: 'AnotherEnum',
+                  type: 'enum NotAnOption',
+                  encodedLength: 9,
+                  coders: {
+                    None: { name: 'tuple', type: '()', encodedLength: 0, coders: [] },
+                    Some: {
+                      name: 'number',
+                      type: 'u8',
+                      encodedLength: 1,
+                      baseType: 'u8',
+                      options: { padToWordSize: false },
+                    },
+                  },
+                },
+                length: 3,
+              },
+            ],
+          },
+        },
+      },
+    };
+
+    expect(hasNestedOption(coders)).toBe(false);
   });
 });
