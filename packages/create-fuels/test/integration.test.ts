2bcb8b1abcebb0c323ff85ccc352bee575dd18eb
Peter Smith <peter@blueoceancomputing.co.uk>
2024-05-21 18:34:19 +0100

    feat: added integration test

--- packages/create-fuels/test/integration.test.ts
+++ packages/create-fuels/test/integration.test.ts
@@ -1,17 +1,53 @@
+import { safeExec } from "@fuel-ts/errors/test-utils";
 import { execSync } from "child_process";
 
+import type { ProjectPaths} from "./utils/bootstrapProject";
+import { bootstrapProject, copyTemplate, resetFilesystem } from "./utils/bootstrapProject";
+import { generateArgs } from "./utils/generateArgs";
+import { filterOriginalTemplateFiles, getAllFiles } from "./utils/templateFiles";
+
 const fuelsVersion = process.env.PUBLISHED_NPM_VERSION;
-const projectName = 'test-project';
+const programsToInclude = { contract: true, predicate: true, script: true };
 
 /**
  * @group integration
  */
-describe('CLI', () => {
+describe('CLI - Integration', () => {
+  const paths: ProjectPaths = bootstrapProject(__filename);;
+  const args = generateArgs(programsToInclude, paths.root).join(' ');
+
   beforeAll(() => {
-    execSync(`pnpm create fuels@${fuelsVersion} ${projectName} --pnpm --contract --predicate --script`, { stdio: 'inherit' })
+    copyTemplate(paths.sourceTemplate, paths.template);
   })
 
-  it('should have all the files', () => {
-    console.log(process.env.PUBLISHED_NPM_VERSION)
+  afterAll(() => {
+    resetFilesystem(paths.root);
+    vi.resetAllMocks();
   });
+
+  it('should perform `pnpm create fuels`', async () => {
+    let expectedTemplateFiles = await getAllFiles(paths.template);
+    expectedTemplateFiles = filterOriginalTemplateFiles(expectedTemplateFiles, programsToInclude);
+
+    // Perform create fuels
+    const { error: createFuelsError } = await safeExec(
+      () => execSync(`pnpm create fuels@${fuelsVersion} ${args}`, { stdio: 'inherit' })
+    );
+    expect(createFuelsError).toBeUndefined();
+
+    // Perform install
+    // const { error: installError } = await safeExec(
+    //   () => execSync(`pnpm install --ignore-workspace`, { cwd: paths.root, stdio: 'inherit' })
+    // )
+    // expect(installError).toBeUndefined();
+
+    // Perform `fuels build`
+    // const { error: buildError } = await safeExec(
+    //   () => execSync(`pnpm fuels build`, { cwd: paths.root, stdio: 'inherit' })
+    // )
+    // expect(buildError).toBeUndefined();
+
+    const actualTemplateFiles = await getAllFiles(paths.root);
+    expect(actualTemplateFiles.sort()).toEqual(expectedTemplateFiles.sort());
+  }, { timeout: 60000 });
 })
