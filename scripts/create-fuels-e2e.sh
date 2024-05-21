7b555d17c9f6ba9abbf972a37bf3a28a5553bd76
Dhaiwat <dhaiwatpandya@gmail.com>
2024-05-21 09:18:09 -0700

    refactor into shell script & rename worflow file

---
+++ scripts/create-fuels-e2e.sh
@@ -0,0 +1,5 @@
+(cd templates/nextjs && pnpm run fuels:dev) &
+cp templates/nextjs/.env.example templates/nextjs/.env.local
+(cd templates/nextjs && pnpm run dev) &
+pnpm exec playwright install --with-deps
+pnpm exec playwright test
