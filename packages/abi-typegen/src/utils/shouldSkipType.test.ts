3c2a0e51b9b6462db4ce2d47fd3effd3b3fbc9e2
nedsalk <nedim.salkic@fuel.sh>
2024-05-22 15:53:09 +0200

    skip RawBytes

--- packages/abi-typegen/src/utils/shouldSkipType.test.ts
+++ packages/abi-typegen/src/utils/shouldSkipType.test.ts
@@ -7,6 +7,9 @@ import { supportedTypes } from './supportedTypes';
 describe('types.ts', () => {
   test('should always skip these types', () => {
     expect(shouldSkipAbiType({ type: 'struct RawVec' })).toEqual(true);
+    expect(shouldSkipAbiType({ type: 'struct std::vec::RawVec' })).toEqual(true);
+    expect(shouldSkipAbiType({ type: 'struct RawBytes' })).toEqual(true);
+    expect(shouldSkipAbiType({ type: 'struct std::bytes::RawBytes' })).toEqual(true);
   });
 
   test('should never skip known types', () => {
