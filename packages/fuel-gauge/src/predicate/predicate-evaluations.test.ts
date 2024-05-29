fbcb069683a0cd4bae0044bbe14e96aa73d511a9
nedsalk <nedim.salkic@fuel.sh>
2024-05-29 13:39:37 +0200

    use factory for predicate

--- packages/fuel-gauge/src/predicate/predicate-evaluations.test.ts
+++ packages/fuel-gauge/src/predicate/predicate-evaluations.test.ts
@@ -1,23 +1,17 @@
-import type { InputValue, Provider, WalletLocked, WalletUnlocked } from 'fuels';
-import { Address, Predicate, Wallet } from 'fuels';
+import { Address, Wallet } from 'fuels';
 import { launchTestNode } from 'fuels/test-utils';
 
-import { FuelGaugeProjectsEnum, getFuelGaugeForcProject } from '../../test/fixtures';
+import {
+  PredicateTrueAbi__factory,
+  PredicateFalseAbi__factory,
+} from '../../test/typegen/predicates';
 
-import { setupWallets, assertBalances, fundPredicate } from './utils/predicate';
+import { assertBalances, fundPredicate } from './utils/predicate';
 
 /**
  * @group node
  */
 describe('Predicate', () => {
-  const { binHexlified: predicateBytesTrue } = getFuelGaugeForcProject(
-    FuelGaugeProjectsEnum.PREDICATE_TRUE
-  );
-
-  const { binHexlified: predicateBytesFalse } = getFuelGaugeForcProject(
-    FuelGaugeProjectsEnum.PREDICATE_FALSE
-  );
-
   describe('Evaluations', () => {
     it('calls a no argument predicate and returns true', async () => {
       using launched = await launchTestNode();
@@ -30,10 +24,7 @@ describe('Predicate', () => {
       const receiver = Wallet.fromAddress(Address.fromRandom(), provider);
       const initialReceiverBalance = await receiver.getBalance();
 
-      const predicate = new Predicate({
-        bytecode: predicateBytesTrue,
-        provider,
-      });
+      const predicate = PredicateTrueAbi__factory.createInstance(provider);
 
       await fundPredicate(wallet, predicate, 200_000);
 
@@ -64,10 +55,7 @@ describe('Predicate', () => {
 
       const receiver = Wallet.fromAddress(Address.fromRandom(), provider);
 
-      const predicate = new Predicate({
-        bytecode: predicateBytesTrue,
-        provider,
-      });
+      const predicate = PredicateFalseAbi__factory.createInstance(provider);
 
       await fundPredicate(wallet, predicate, 200_000);
 
