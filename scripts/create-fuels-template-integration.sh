4205e99140e693ec1cf0e4a8661cf85ca7025772
Dhaiwat <dhaiwatpandya@gmail.com>
2024-05-30 01:06:12 -0500

    chore: merge the create fuels integration tests into one step

--- scripts/create-fuels-template-integration.sh
+++ scripts/create-fuels-template-integration.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-(cd templates/nextjs && pnpm run fuels:dev) &
-cp templates/nextjs/.env.example templates/nextjs/.env.local
-(cd templates/nextjs && pnpm run dev) &
+npx create-fuels@next create-fuels-integration-test-project -c -p -s --pnpm
+(cd test-project && pnpm run fuels:dev) &
+cp test-project/.env.example test-project/.env.local
+(cd test-project && pnpm run dev) &
 pnpm exec playwright install --with-deps
 pnpm exec playwright test
