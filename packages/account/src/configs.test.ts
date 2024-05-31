9a07505d38c66be430825562d9d3da5433250111
Peter Smith <peter@blueoceancomputing.co.uk>
2024-05-31 16:49:56 +0100

    chore: update testnet URL

--- packages/account/src/configs.test.ts
+++ packages/account/src/configs.test.ts
@@ -9,7 +9,7 @@ describe('Configs', () => {
 
   it('exports FUEL_TESTNET_NETWORK_URL', async () => {
     const configs = await import('./configs');
-    expect(configs.FUEL_TESTNET_NETWORK_URL).toBe('https://devnet.fuel.network/v1/graphql');
+    expect(configs.FUEL_TESTNET_NETWORK_URL).toBe('https://testnet.fuel.network/v1/graphql');
   });
 });
 
