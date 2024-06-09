afc7691126b4867187408fa2616a6f2bc56b1fb1
Daniel Bate <djbate23@gmail.com>
2024-06-09 13:37:33 +0100

    chore: nits and make encode test more descriptive

--- apps/docs-snippets/src/guide/encoding/encode-and-decode.test.ts
+++ apps/docs-snippets/src/guide/encoding/encode-and-decode.test.ts
@@ -78,41 +78,51 @@ describe('encode and decode', () => {
   it('encodes and decodes', async () => {
     // #region encode-and-decode-3
     // #import { JsonAbi, JsonAbiArgument, Script, AbiCoder, FunctionInvocationResult, ReceiptType, TransactionResultReturnDataReceipt, arrayify};
+    // import { factory } from './sway-programs-api';
 
+    // Retrieve the script ABI and bytecode that we use to inform the encoding
     const abi: JsonAbi = factory.abi;
-    const bin: string = factory.bin;
-    const argument: JsonAbiArgument = abi.functions
-      .find((f) => f.name === 'main')
-      ?.inputs.find((i) => i.name === 'inputted_amount') as JsonAbiArgument;
+    const bytecode: string = factory.bin;
 
-    // Create the script invocation scope
-    const script = new Script(bin, abi, wallet);
+    // Create the invocation scope for the script call, passing the initial
+    // value for the configurable constant
+    const script = new Script(bytecode, abi, wallet);
     const initialValue = 10;
     script.setConfigurableConstants({ AMOUNT: initialValue });
     const invocationScope = script.functions.main(0);
 
-    // Create the transaction request
+    // Create the transaction request, this can be picked off the invocation
+    // scope so the script bytecode is preset on the transaction
     const request = await invocationScope.getTransactionRequest();
 
-    // Encode the argument we want to pass to the function
+    // Encode the argument we want to pass to the function. The argument is required
+    // as a function parameter for all `AbiCoder` functions and we can pick it off
+    // of the ABI itself
+    const argument: JsonAbiArgument = abi.functions
+      .find((f) => f.name === 'main')
+      ?.inputs.find((i) => i.name === 'inputted_amount') as JsonAbiArgument;
     const argumentToAdd = 10;
+    // Using the `AbiCoder`, we can now use the argument and the ABI to form it of the
+    // encoding that is required for value that we are passing to it. Here we are passing
+    // a u32 which takes 4 bytes up of property space
     const encodedArguments = AbiCoder.encode(abi, argument, [argumentToAdd]);
-    // encodedArguments = new Uint8Array([0, 0, 0, 10]
+    // Therefore the value of 10 will be encoded to:
+    // Uint8Array([0, 0, 0, 10]
 
-    // Set the encoded arguments on the request
+    // The encoded value can now be set on the transaction via the script data property
     request.scriptData = encodedArguments;
 
-    // Estimate and fund the transaction
+    // Now we can build out the rest of the transaction, including funding it
     const txCost = await wallet.provider.getTransactionCost(request);
     request.maxFee = txCost.maxFee;
     request.gasLimit = txCost.gasUsed;
     await wallet.fund(request, txCost);
 
-    // Submit the transaction
+    // Submit the built transaction
     const response = await wallet.sendTransaction(request);
     await response.waitForResult();
 
-    // Get result of contract call
+    // Get result of the transaction, including the contract call result
     const invocationResult = await FunctionInvocationResult.build(
       [invocationScope],
       response,
@@ -120,20 +130,28 @@ describe('encode and decode', () => {
       script
     );
 
-    // Get the return data receipt that contains the returned bytes
+    // We can pick off the decoded value like so:
+    const { value } = invocationResult;
+
+    // Or we can decode the returned bytes ourselves, by retrieving the return data
+    // receipt that contains the returned bytes. We can get this by filtering on
+    // the returned receipt types
     const returnDataReceipt = invocationResult.transactionResult.receipts.find(
       (r) => r.type === ReceiptType.ReturnData
     ) as TransactionResultReturnDataReceipt;
+    // The data is in hex format so it makes sense to use arrayify so that the data
+    // is more human readable
     const returnData = arrayify(returnDataReceipt.data);
     // returnData = new Uint8Array([0, 0, 0, 20]
 
-    // Decode the return data
+    // And now we can decode the returned bytes in a similar fashion to how they were
+    // encoded
     const [decodedReturnData] = AbiCoder.decode(abi, argument, returnData, 0);
     // 20
     // #endregion encode-and-decode-3
 
     const totalValue = argumentToAdd + initialValue;
-    expect(invocationResult.value).toBe(totalValue);
+    expect(value).toBe(totalValue);
     expect(decodedReturnData).toBe(totalValue);
     expect(encodedArguments).toStrictEqual(new Uint8Array([0, 0, 0, 10]));
     expect(returnData).toStrictEqual(new Uint8Array([0, 0, 0, 20]));
--- apps/docs/src/guide/encoding/encode-and-decode.md
+++ apps/docs/src/guide/encoding/encode-and-decode.md
@@ -1,21 +1,37 @@
 # Encode and Decode
 
-Encoding is the process of transforming raw data into bytecode as per a set of rules. Decoding is the reverse process, transforming bytecode back to raw data. The SDK provides the `AbiCoder` class to encode and decode data.
+In order to interact with the FuelVM, types must be encoded and decoded as per the [argument encoding specification](https://docs.fuel.network/docs/specs/abi/argument-encoding/). The SDK provides the `AbiCoder` class to encode and decode data.
 
-It has 3 static methods: `encode`, `decode`, and `getCoder`, `encode` and `decode` describe the aforementioned processes, while `getCoder` returns an instance of the internal coder required to serialize the passed type, and is used internally by the `encode` and `decode` methods. All methods expect you to pass the [ABI](https://docs.fuel.network/docs/specs/abi/json-abi-format/) and ABI Argument as function parameters, in order to deduce the specific type coders that will be required to parse the data.
+It has three static methods:
+
+- `encode`
+- `decode`
+- `getCoder`
+
+The methods `encode` and `decode` describe the aforementioned process, while `getCoder` returns an instance of the internal coder required to serialize the passed type. This coder is then used internally by the `encode` and `decode` methods.
+
+All methods expect you to pass the [ABI](https://docs.fuel.network/docs/specs/abi/json-abi-format/) and ABI Argument as function parameters to deduce the specific type coders that will be required to parse the data.
 
 Imagine we are working with the following script that returns the sum of two `u32` integers:
 
 <<< @/../../docs-snippets/test/fixtures/forc-projects/sum-script/src/main.sw#encode-and-decode-1{rust:line-numbers}
 
+When you build this script, using:
+
+```sh
+forc build
+```
+
 It will produce the following ABI:
 
 <<< @/../../docs-snippets/src/guide/encoding/encode-and-decode.test.ts#encode-and-decode-2{ts:line-numbers}
 
-Now, let's prepare some data to pass to the `main` function to retrieve the combined integer. The function is expecting a `u32` integer and will return one. So here we are going to encode the `u32` to pass it to the function, and receive the same same `u32` as bytes back, that we will need to decode.
+Now, let's prepare some data to pass to the `main` function to retrieve the combined integer. The function expects and returns a `u32` integer. So here, we will encode the `u32` to pass it to the function and receive the same `u32` back, as bytes, that we'll use for decoding.
 
 We can do both of these with the `AbiCoder`:
 
 <<< @/../../docs-snippets/src/guide/encoding/encode-and-decode.test.ts#encode-and-decode-3{ts:line-numbers}
 
-A similar approach can be taken with [Predicates](../predicates/index.md) however you must set the encoded values to the `predicateData` property. [Contracts](../contracts/index.md) require more care and although you can utilise the `scriptData` property, the arguments must be encoded as part of the [contract call script](https://docs.fuel.network/docs/sway/sway-program-types/smart_contracts/#calling-a-smart-contract-from-a-script), therefore it is recommended to use the helper classes provided by the SDK.
+A similar approach can be taken with [Predicates](../predicates/index.md); however, you must set the encoded values to the `predicateData` property.
+
+[Contracts](../contracts/index.md) require more care. Although you can utilize the `scriptData` property, the arguments must be encoded as part of the [contract call script](https://docs.fuel.network/docs/sway/sway-program-types/smart_contracts/#calling-a-smart-contract-from-a-script). Therefore, it is recommended to use a `FunctionInvocationScope` when working with contracts which will be instantiated for you when [submitting a contract function](../contracts/methods.md), and therefore handles all the encoding.
