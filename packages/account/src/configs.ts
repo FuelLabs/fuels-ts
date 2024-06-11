2a77256d2563f1b30b669668c4aee81540bda81c
Peter Smith <peter@blueoceancomputing.co.uk>
2024-06-11 07:41:38 +0100

    chore: removed type from network urls

--- packages/account/src/configs.ts
+++ packages/account/src/configs.ts
@@ -3,5 +3,5 @@ export const FUEL_NETWORK_URL: string =
     ? process?.env?.FUEL_NETWORK_URL || 'http://127.0.0.1:4000/v1/graphql'
     : 'http://127.0.0.1:4000/v1/graphql';
 
-export const DEVNET_NETWORK_URL: string = 'https://devnet.fuel.network/v1/graphql';
-export const TESTNET_NETWORK_URL: string = 'https://testnet.fuel.network/v1/graphql';
+export const DEVNET_NETWORK_URL = 'https://devnet.fuel.network/v1/graphql';
+export const TESTNET_NETWORK_URL = 'https://testnet.fuel.network/v1/graphql';
