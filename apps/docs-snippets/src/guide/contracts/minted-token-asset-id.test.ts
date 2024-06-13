bc87c70dcc439cc24bd296a304d1ea21b317bd2c
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-06-13 09:25:26 -0300

    docs: add reference for `getMintedAssetId` (#2507)

---
+++ apps/docs-snippets/src/guide/contracts/minted-token-asset-id.test.ts
@@ -0,0 +1,33 @@
+import type { Contract } from 'fuels';
+import { bn, getMintedAssetId } from 'fuels';
+
+import { DocSnippetProjectsEnum } from '../../../test/fixtures/forc-projects';
+import { createAndDeployContractFromProject } from '../../utils';
+
+/**
+ * @group node
+ */
+describe(__filename, () => {
+  let contract: Contract;
+
+  beforeAll(async () => {
+    contract = await createAndDeployContractFromProject(DocSnippetProjectsEnum.TOKEN);
+  });
+
+  it('should successfully execute contract call with forwarded amount', async () => {
+    // #region minted-token-asset-id-2
+    // #import { bn, getMintedAssetId };
+
+    // Any valid bits256 string can be used as a sub ID
+    const subID = '0xc7fd1d987ada439fc085cfa3c49416cf2b504ac50151e3c2335d60595cb90745';
+    const mintAmount = bn(1000);
+
+    const txResult = await contract.functions.mint_coins(subID, mintAmount).call();
+
+    const mintedAssetId = getMintedAssetId(subID, contract.id.toB256());
+    // #endregion minted-token-asset-id-2
+
+    expect(mintedAssetId).toBeDefined();
+    expect(txResult.transactionResult.isStatusSuccess).toBeTruthy();
+  });
+});
