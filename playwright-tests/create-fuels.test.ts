82edcd88b52c054614abd28f2f62a959fc1c2789
Peter Smith <peter@blueoceancomputing.co.uk>
2024-06-14 11:50:02 +0100

    chore: added top-up balance check

--- playwright-tests/create-fuels.test.ts
+++ playwright-tests/create-fuels.test.ts
@@ -32,6 +32,24 @@ test('counter contract - increment function call works properly', async ({ page 
   await expect(counter).toBeVisible();
 });
 
+test('top-up wallet button', async ({ page }) => {
+  await page.goto(WEB_SERVER_URL, { waitUntil: 'networkidle' });
+  await page.waitForTimeout(2000);
+
+  // Check empty balance
+  const walletBalance = page.getByTestId('wallet-balance');
+  await expect(walletBalance).toContainText('Balance: 0.000 ETH');
+
+  // Perform top-up
+  const topUpWalletButton = page.getByText('Top-up Wallet');
+  await topUpWalletButton.click();
+
+  await page.waitForTimeout(1000);
+
+  // Expect the balance to be updated
+  await expect(walletBalance).not.toContainText('Balance: 0.000 ETH');
+});
+
 test('faucet page', async ({ page }) => {
   await page.goto(FAUCET_URL, { waitUntil: 'networkidle' });
 
