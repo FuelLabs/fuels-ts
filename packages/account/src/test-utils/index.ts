e495a4dea0f8b54e3e5bf4b0d01c84ba2e1f3c40
Peter Smith <peter@blueoceancomputing.co.uk>
2024-06-18 17:07:40 +0100

    fix: added `test-utils` to `@fuel-ts/account` entrypoint

--- packages/account/src/test-utils/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './generateTestWallet';
-export * from './seedTestWallet';
-export * from './launchNode';
-export * from './setup-test-provider-and-wallets';
-export * from './wallet-config';
-export * from './test-message';
-export * from './asset-id';
