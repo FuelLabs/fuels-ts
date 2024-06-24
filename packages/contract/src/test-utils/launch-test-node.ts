c23e76e2377b5630d39cc385e4e514947f342ade
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-06-24 16:11:00 -0300

    adjust launch test node types

--- packages/contract/src/test-utils/launch-test-node.ts
+++ packages/contract/src/test-utils/launch-test-node.ts
@@ -6,20 +6,19 @@ import type {
 } from '@fuel-ts/account/test-utils';
 import { FuelError } from '@fuel-ts/errors';
 import type { BytesLike } from '@fuel-ts/interfaces';
-import type { Contract } from '@fuel-ts/program';
 import type { SnapshotConfigs } from '@fuel-ts/utils';
 import { readFileSync } from 'fs';
 import * as path from 'path';
 import { mergeDeepRight } from 'ramda';
 
-import type { DeployContractOptions } from '../contract-factory';
+import type { DeployContractOptions, DeployContractResult } from '../contract-factory';
 
 interface ContractDeployer {
   deployContract(
     bytecode: BytesLike,
     wallet: Account,
     options?: DeployContractOptions
-  ): Promise<Contract>;
+  ): Promise<DeployContractResult>;
 }
 
 interface DeployContractConfig {
@@ -49,7 +48,7 @@ interface LaunchTestNodeOptions<TContractConfigs extends DeployContractConfig[]>
   contractsConfigs: TContractConfigs;
 }
 type TContracts<T extends DeployContractConfig[]> = {
-  [K in keyof T]: Awaited<ReturnType<T[K]['deployer']['deployContract']>>;
+  [K in keyof T]: Awaited<ReturnType<T[K]['deployer']['deployContract']>>['contract'];
 };
 interface LaunchTestNodeReturn<TFactories extends DeployContractConfig[]>
   extends SetupTestProviderAndWalletsReturn {
@@ -152,13 +151,16 @@ export async function launchTestNode<TFactories extends DeployContractConfig[]>(
   try {
     for (let i = 0; i < configs.length; i++) {
       const config = configs[i];
-      contracts.push(
-        await config.deployer.deployContract(
-          config.bytecode,
-          getWalletForDeployment(config, wallets),
-          config.options ?? {}
-        )
+      const options: DeployContractOptions = {
+        awaitExecution: true,
+        ...config.options,
+      };
+      const { contract } = await config.deployer.deployContract(
+        config.bytecode,
+        getWalletForDeployment(config, wallets),
+        options
       );
+      contracts.push(contract);
     }
   } catch (err) {
     cleanup();
