4606e3e7fc68f3787e71879e567dce23fb9422d0
Peter Smith <peter@blueoceancomputing.co.uk>
2024-06-26 15:23:22 +0100

    chore: updated test to use checksum

--- packages/account/test/fuel-core-schema.test.ts
+++ packages/account/test/fuel-core-schema.test.ts
@@ -1,21 +1,33 @@
 import { safeExec } from '@fuel-ts/errors/test-utils';
 import { execSync } from 'child_process';
+import type { BinaryToTextEncoding } from 'crypto';
+import { createHash } from 'crypto';
 import { readFile } from 'fs/promises';
 
 const FUEL_CORE_SCHEMA_FILE_PATH = 'packages/account/src/providers/fuel-core-schema.graphql';
 const FUEL_CORE_SCHEMA_SYNC_COMMAND = 'pnpm --filter @fuel-ts/account build:schema';
 
-/**
- * @group node
- */
+function generateChecksum(
+  utf8: Buffer,
+  algorithm: string = 'md5',
+  encoding: BinaryToTextEncoding = 'hex'
+) {
+  return createHash(algorithm || 'md5')
+    .update(utf8.toString(), 'utf8')
+    .digest(encoding || 'hex');
+}
+
 describe('fuel-core-schema.graphql', () => {
   it('should not change on schema build', async () => {
-    const preSyncSchema = await readFile(FUEL_CORE_SCHEMA_FILE_PATH);
+    const preSyncChecksum = await readFile(FUEL_CORE_SCHEMA_FILE_PATH).then(generateChecksum);
 
     await safeExec(() => execSync(FUEL_CORE_SCHEMA_SYNC_COMMAND));
 
-    const postSyncSchema = await readFile(FUEL_CORE_SCHEMA_FILE_PATH);
+    const postSyncChecksum = await readFile(FUEL_CORE_SCHEMA_FILE_PATH).then(generateChecksum);
 
-    expect(preSyncSchema.toString()).toEqual(postSyncSchema.toString());
+    expect(
+      preSyncChecksum,
+      `\nThe schema at ${FUEL_CORE_SCHEMA_FILE_PATH} is not in sync with fuel-core.\n\nRun '${FUEL_CORE_SCHEMA_SYNC_COMMAND}' to update it.\n\n`
+    ).toEqual(postSyncChecksum);
   });
 });
