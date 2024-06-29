2839890ac381b71eca7cc86f57974862e5607e60
Anderson Arboleya <anderson@arboleya.me>
2024-06-29 15:48:44 -0300

    Disambiguating variable name

--- packages/fuels/src/api/snippets/allinone.ts
+++ packages/fuels/src/api/snippets/allinone.ts
@@ -7,13 +7,13 @@ import { CounterContract, DispatcherScript, VaultPredicate } from './typegend';
 export async function main() {
   const { wallet, contract, script, predicate } = await fuels(NETWORK_URL);
 
-  const vault = wallet('0x..');
+  const account = wallet('0x..');
 
-  const walletBalances = await vault.getBalances();
+  const walletBalances = await account.getBalances();
   const predicateBalances = await predicate(VaultPredicate).getBalances();
 
   const { value: contractValue } = await contract(CounterContract).functions.getCount().get();
-  const { value: scriptValue } = await script(DispatcherScript, vault).functions.main().get();
+  const { value: scriptValue } = await script(DispatcherScript, account).functions.main().get();
 
   console.log({ walletBalances, predicateBalances, contractValue, scriptValue });
 }
