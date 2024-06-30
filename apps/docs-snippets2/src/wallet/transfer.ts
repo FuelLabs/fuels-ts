ac70dd69a090bfdcefb1af2011361f6b4eea698f
Anderson Arboleya <anderson@arboleya.me>
2024-06-30 12:48:25 -0300

    Removing snippet

--- apps/docs-snippets2/src/wallet/transfer.ts
+++
@@ -1,9 +0,0 @@
-import { fuels, NETWORK_URL } from 'fuels/api';
-
-const { wallet, provider } = await fuels(NETWORK_URL);
-
-const { waitForResult } = await wallet('0x..').transfer('0x..', 10, provider.getBaseAssetId());
-
-const { gasUsed } = await waitForResult();
-
-console.log({ gasUsed });
