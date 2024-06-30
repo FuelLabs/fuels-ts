44efa68a5d87c2981c9fbd11d771c29f9d289fd6
Anderson Arboleya <anderson@arboleya.me>
2024-06-30 12:58:16 -0300

    Using real data

--- apps/docs-snippets2/src/wallet/getBalance.ts
+++ apps/docs-snippets2/src/wallet/getBalance.ts
@@ -1,7 +1,9 @@
-import { fuels, NETWORK_URL } from 'fuels/api';
+import { fuels, TESTNET_NETWORK_URL } from 'fuels/api';
 
-const { wallet } = await fuels(NETWORK_URL);
+const MY_WALLET_ADDRESS = '0x767caf5b08eba21c561078a4d5be09bbd7f16b9eca22699a61f1edd9e456126f';
 
-const balances = await wallet('0x..').getBalances();
+const { wallet } = await fuels(TESTNET_NETWORK_URL);
+
+const balances = await wallet(MY_WALLET_ADDRESS).getBalances();
 
 console.log({ balances });
