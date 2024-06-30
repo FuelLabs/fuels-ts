9df2471949cfb917d3991e46a3c0d3a1ac6d583b
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-06-30 15:11:24 -0300

    purge InvocationResult class

--- packages/program/src/index.ts
+++ packages/program/src/index.ts
@@ -2,7 +2,6 @@ export * from './types';
 export * from './utils';
 export { FunctionInvocationScope } from './functions/invocation-scope';
 export { MultiCallInvocationScope } from './functions/multicall-scope';
-export { InvocationResult } from './functions/invocation-results';
 export { default as Contract } from './contract';
 export { ScriptRequest } from './script-request';
 export { InstructionSet } from './instruction-set';
