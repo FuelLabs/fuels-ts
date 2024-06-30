e78dc6de0270bf7d0ba72c9f3fd0edcb291e97d8
Anderson Arboleya <anderson@arboleya.me>
2024-06-30 13:12:53 -0300

    Fixing test

--- apps/docs-snippets2/src/wallet/getBalance.test.ts
+++ apps/docs-snippets2/src/wallet/getBalance.test.ts
@@ -1,6 +1,6 @@
 import { main } from './getBalance.wrapped';
 
 test('Has balance', async () => {
-  const [{ balance }] = await main();
-  expect(balance).toBeTruthy();
+  const { balances } = await main();
+  expect(balances).toBeTruthy();
 });
