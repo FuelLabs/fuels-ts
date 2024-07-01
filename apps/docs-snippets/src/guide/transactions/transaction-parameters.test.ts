7c378b4fb95724addc01b1d50ff6034d323c3cea
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-07-01 09:00:38 -0300

    add new code snippets for transaction parameters page

--- apps/docs-snippets/src/guide/transactions/transaction-parameters.test.ts
+++ apps/docs-snippets/src/guide/transactions/transaction-parameters.test.ts
@@ -1,4 +1,4 @@
-import type { Contract, TxParams } from 'fuels';
+import type { BN, Contract, TxParams } from 'fuels';
 import { ScriptTransactionRequest, bn } from 'fuels';
 import { expectTypeOf } from 'vitest';
 
@@ -22,24 +22,61 @@ describe(__filename, () => {
     contract = await createAndDeployContractFromProject(DocSnippetProjectsEnum.COUNTER);
   });
 
-  it('matches tx param types', () => {
+  it('validates all parameters types', () => {
     // #region transaction-parameters-1
+    // #import { BN, bn };
+
+    const gasLimit: BN = bn(1000);
+    // #endregion transaction-parameters-1
+
+    // #region transaction-parameters-2
+    // #import { BN, bn };
+
+    const maxFee: BN = bn(10_000);
+    // #endregion transaction-parameters-2
+
+    // #region transaction-parameters-3
+    // #import { BN, bn };
+
+    const tip: BN = bn(100);
+    // #endregion transaction-parameters-3
+
+    // #region transaction-parameters-4
+    const maturity = 10;
+    // #endregion transaction-parameters-4
+
+    // #region transaction-parameters-5
+    // #import { BN, bn };
+
+    const witnessLimit: BN = bn(5000);
+    // #endregion transaction-parameters-5
+
+    expectTypeOf(gasLimit).toMatchTypeOf<BN>();
+    expectTypeOf(maxFee).toMatchTypeOf<BN>();
+    expectTypeOf(tip).toMatchTypeOf<BN>();
+    expectTypeOf(maturity).toMatchTypeOf<number>();
+    expectTypeOf(witnessLimit).toMatchTypeOf<BN>();
+  });
+
+  it('matches tx param types', () => {
+    // #region transaction-parameters-6
     // #import { TxParams, bn };
 
     const txParams: TxParams = {
       gasLimit: bn(1), // BigNumberish or undefined
-      maturity: 1, // number or undefined
       maxFee: bn(1), // BigNumberish or undefined
+      maturity: 1, // number or undefined
+      tip: bn(1), // BigNumberish or undefined
       witnessLimit: bn(1), // BigNumberish or undefined
       variableOutputs: 1, // number or undefined
     };
-    // #endregion transaction-parameters-1
+    // #endregion transaction-parameters-6
 
     expectTypeOf(txParams).toMatchTypeOf<TxParams>();
   });
 
   it('executes a script transaction request with txParams', () => {
-    // #region transaction-parameters-2
+    // #region transaction-parameters-7
     // #import { ScriptTransactionRequest };
 
     // Instantiate the transaction request using a ScriptTransactionRequest
@@ -48,20 +85,20 @@ describe(__filename, () => {
       script: scriptBytecode,
       gasLimit: 100,
     });
-    // #endregion transaction-parameters-2
+    // #endregion transaction-parameters-7
 
     expect(transactionRequest.gasLimit.toNumber()).toBe(100);
   });
 
   it('executes contract call with txParams', async () => {
-    // #region transaction-parameters-3
+    // #region transaction-parameters-8
     const { transactionResult } = await contract.functions
       .increment_count(15)
       .txParams({
         variableOutputs: 1,
       })
       .call();
-    // #endregion transaction-parameters-3
+    // #endregion transaction-parameters-8
 
     expect(transactionResult.isStatusSuccess).toBeTruthy();
   });
