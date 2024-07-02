004f3f2379500858ef02d242b5027494b7a65b7d
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-07-02 14:29:06 -0300

    move code to helper

--- packages/program/src/response.ts
+++ packages/program/src/response.ts
@@ -4,14 +4,14 @@ import type {
   TransactionResult,
   CallResult,
 } from '@fuel-ts/account';
-import { getDecodedLogs, getGasUsedFromReceipts } from '@fuel-ts/account';
+import { getGasUsedFromReceipts } from '@fuel-ts/account';
 import type { AbstractContract, AbstractProgram } from '@fuel-ts/interfaces';
 import type { TransactionType } from '@fuel-ts/transactions';
 
 import { decodeContractCallScriptResult } from './contract-call-script';
 import { callResultToInvocationResult } from './script-request';
 import type { InvocationScopeLike, FunctionResult, DryRunResult } from './types';
-import { getAbisFromAllCalls } from './utils';
+import { getResultLogs } from './utils';
 
 /** @hidden */
 export const extractInvocationResult = <T>(
@@ -52,14 +52,15 @@ export const buildFunctionResult = async <T>(
   params: BuiltFunctionResultParams
 ): Promise<FunctionResult<T>> => {
   const { funcScope, isMultiCall, program, transactionResponse } = params;
+
   const txResult = await transactionResponse.waitForResult();
+
   const { receipts } = txResult;
 
   const functionScopes = Array.isArray(funcScope) ? funcScope : [funcScope];
   const mainCallConfig = functionScopes[0]?.getCallConfig();
 
-  const { main, otherContractsAbis } = getAbisFromAllCalls(functionScopes);
-  const logs = mainCallConfig ? getDecodedLogs(receipts, main, otherContractsAbis) : [];
+  const logs = getResultLogs(receipts, mainCallConfig, functionScopes);
   const value = extractInvocationResult<T>(functionScopes, receipts, isMultiCall, logs);
   const gasUsed = getGasUsedFromReceipts(receipts);
 
@@ -87,11 +88,11 @@ type BuiltDryRunResultParams = {
 /** @hidden * */
 export const buildDryRunResult = <T>(params: BuiltDryRunResultParams): DryRunResult<T> => {
   const { funcScopes, callResult, isMultiCall } = params;
-  const functionScopes = Array.isArray(funcScopes) ? funcScopes : [funcScopes];
   const { receipts } = callResult;
+  const functionScopes = Array.isArray(funcScopes) ? funcScopes : [funcScopes];
   const mainCallConfig = functionScopes[0]?.getCallConfig();
-  const { main, otherContractsAbis } = getAbisFromAllCalls(functionScopes);
-  const logs = mainCallConfig ? getDecodedLogs(receipts, main, otherContractsAbis) : [];
+
+  const logs = getResultLogs(receipts, mainCallConfig, functionScopes);
   const value = extractInvocationResult<T>(functionScopes, receipts, isMultiCall, logs);
   const gasUsed = getGasUsedFromReceipts(receipts);
 
