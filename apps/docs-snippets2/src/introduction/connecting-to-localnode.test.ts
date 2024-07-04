939f9d6dab284974c02f655044f11a526d0e2103
Anderson Arboleya <anderson@arboleya.me>
2024-07-04 17:45:17 -0300

    Validating if `nertworkUrl` was properly mocked

--- apps/docs-snippets2/src/introduction/connecting-to-localnode.test.ts
+++ apps/docs-snippets2/src/introduction/connecting-to-localnode.test.ts
@@ -1,12 +1,14 @@
-import { mockLocalNetwork } from '../../test/importMocked';
+import { mockLocalNetworkWithTestNode } from '../../test/importMocked';
 
 /**
  * @group node
  */
 test('it works', async () => {
-  await mockLocalNetwork();
+  const { provider } = await mockLocalNetworkWithTestNode();
   const { main } = await import('./connecting-to-localnode.wrapped');
-  const { balances } = await main();
+  const { providerUrl, balances } = await main();
+
+  expect(providerUrl).toEqual(provider.url);
   expect(balances).toBeTruthy();
   expect(balances).toBeInstanceOf(Array);
 });
--- apps/docs-snippets2/test/importMocked.ts
+++ apps/docs-snippets2/test/importMocked.ts
@@ -2,7 +2,7 @@ import { launchTestNode } from 'fuels/test-utils';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { vi } from 'vitest';
 
-export const mockLocalNetwork = async () => {
+export const mockLocalNetworkWithTestNode = async () => {
   const { provider } = await launchTestNode();
   vi.doMock('fuels', async () => {
     const mod = await vi.importActual('fuels');
