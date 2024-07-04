3dd1e3fce7c9457cef60e7040d44b4f200f8a2aa
Anderson Arboleya <anderson@arboleya.me>
2024-07-04 17:40:20 -0300

    Adding mock utility

---
+++ apps/docs-snippets2/test/importMocked.ts
@@ -0,0 +1,16 @@
+import { launchTestNode } from 'fuels/test-utils';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { vi } from 'vitest';
+
+export const mockLocalNetwork = async () => {
+  const { provider } = await launchTestNode();
+  vi.doMock('fuels', async () => {
+    const mod = await vi.importActual('fuels');
+    return {
+      __esModule: true,
+      ...mod,
+      LOCAL_NETWORK_URL: provider.url,
+    };
+  });
+  return { provider };
+};
