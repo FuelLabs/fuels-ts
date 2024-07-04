5732d8c8f1f4115e06c73cc1a0f3e1f153fef516
Anderson Arboleya <anderson@arboleya.me>
2024-07-04 20:34:17 -0300

    Mocking only for localhost

--- apps/docs-snippets2/test/importMocked.ts
+++ apps/docs-snippets2/test/importMocked.ts
@@ -1,3 +1,5 @@
+import type { ProviderOptions } from 'fuels';
+import { Provider } from 'fuels';
 import { launchTestNode } from 'fuels/test-utils';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { vi } from 'vitest';
@@ -5,12 +7,18 @@ import { vi } from 'vitest';
 export const mockLocalNetworkWithTestNode = async () => {
   const { provider } = await launchTestNode();
   vi.doMock('fuels', async () => {
-    const mod = await vi.importActual('fuels');
+    const module = await vi.importActual('fuels');
     return {
+      ...module,
       __esModule: true,
-      ...mod,
       Provider: {
-        create: () => provider,
+        create: (url: string, options: ProviderOptions): Promise<Provider> => {
+          const isLocalReg = /127\.0\.0\.1|localhost/;
+          if (isLocalReg.test(url)) {
+            return Promise.resolve(provider);
+          }
+          return Provider.create(url, options);
+        },
       },
     };
   });
