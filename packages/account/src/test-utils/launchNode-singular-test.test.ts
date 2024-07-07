3db78c06ab094be543b6815ce8e45d3e699a7b0a
nedsalk <nedim.salkic@fuel.sh>
2024-07-07 15:43:01 +0200

    add comment

--- packages/account/src/test-utils/launchNode-singular-test.test.ts
+++ packages/account/src/test-utils/launchNode-singular-test.test.ts
@@ -1,6 +1,10 @@
 import { launchNode } from './launchNode';
 
-describe('only one test in this file to verify correct behavior', () => {
+/**
+ * The test runner creates an test environment per file,
+ * which we can use to isolate the faulty behavior.
+ */
+describe('launchNode-singular-test', () => {
   let killedNodeUrl = '';
   afterAll(async () => {
     await expect(fetch(killedNodeUrl)).rejects.toThrow('fetch failed');
