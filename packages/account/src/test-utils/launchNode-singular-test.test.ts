aeb7ff8bc9d6fc04cbadd063cdb7ab84b8682900
nedsalk <nedim.salkic@fuel.sh>
2024-07-07 16:45:37 +0200

    add `.only` to be explicit about it

--- packages/account/src/test-utils/launchNode-singular-test.test.ts
+++ packages/account/src/test-utils/launchNode-singular-test.test.ts
@@ -12,7 +12,7 @@ describe('launchNode-singular-test', () => {
   afterAll(async () => {
     await expect(fetch(killedNodeUrl)).rejects.toThrow('fetch failed');
   });
-  test('synchronous cleanup kills node before test runner exits', async () => {
+  test.only('synchronous cleanup kills node before test runner exits', async () => {
     const { cleanup, url } = await launchNode();
     killedNodeUrl = url;
     cleanup();
