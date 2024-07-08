23ffcaa4ec6eae47166a98a12e3d92ca4b4f7387
Dhaiwat <dhaiwatpandya@gmail.com>
2024-07-08 02:28:42 -0400

    initialise connectors properly

--- templates/nextjs/src/app/layout.tsx
+++ templates/nextjs/src/app/layout.tsx
@@ -3,11 +3,18 @@
 import { Layout } from "@/components/Layout";
 import "@/styles/globals.css";
 import { FuelProvider } from "@fuels/react";
-import React, { ReactNode, useEffect, useState } from "react";
+import React, { ReactNode, useEffect, useMemo, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { FuelConfig } from "fuels";
-import { defaultConnectors } from "@fuels/connectors";
-import { ENABLE_FUEL_DEV_CONNECTOR } from "@/lib";
+import { Provider } from "fuels";
+import {
+  BakoSafeConnector,
+  BurnerWalletConnector,
+  FuelWalletConnector,
+  FuelWalletDevelopmentConnector,
+  FueletWalletConnector,
+  WalletConnectConnector,
+} from "@fuels/connectors";
+import { NODE_URL } from "@/lib";
 import { ActiveWalletProvider } from "@/hooks/useActiveWallet";
 
 const queryClient = new QueryClient();
@@ -17,22 +24,36 @@ interface RootLayoutProps {
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  const [fuelConfig, setFuelConfig] = useState<FuelConfig>({});
+  const [isMounted, setIsMounted] = useState(false);
+  const providerToUse = useMemo(() => Provider.create(NODE_URL), [NODE_URL]);
 
   useEffect(() => {
-    setFuelConfig({
-      connectors: defaultConnectors({
-        devMode: ENABLE_FUEL_DEV_CONNECTOR,
-      }),
-    });
+    setIsMounted(true);
   }, []);
+
+  if (!isMounted) return null;
 
   return (
     <html lang="en" className="bg-black text-white">
       <body>
         <React.StrictMode>
           <QueryClientProvider client={queryClient}>
-            <FuelProvider fuelConfig={fuelConfig}>
+            <FuelProvider
+              fuelConfig={{
+                connectors: [
+                  new FuelWalletConnector(),
+                  new BurnerWalletConnector({
+                    fuelProvider: providerToUse,
+                  }),
+                  new WalletConnectConnector({
+                    fuelProvider: providerToUse,
+                  }),
+                  new BakoSafeConnector(),
+                  new FueletWalletConnector(),
+                  new FuelWalletDevelopmentConnector(),
+                ],
+              }}
+            >
               <ActiveWalletProvider>
                 <Layout>{children}</Layout>
               </ActiveWalletProvider>
