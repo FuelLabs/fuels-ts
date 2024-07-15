fd9a886f12f5e1e66c4a941f9e165d50b50e821f
Anderson Arboleya <anderson@arboleya.me>
2024-07-15 18:44:04 -0300

    Trim snippets

--- apps/docs-snippets2/src/introduction/connecting-to-localnode.ts
+++ apps/docs-snippets2/src/introduction/connecting-to-localnode.ts
@@ -3,12 +3,10 @@ import { Provider, Wallet, LOCAL_NETWORK_URL } from 'fuels';
 
 const ADDRESS = '0x767caf5b08eba21c561078a4d5be09bbd7f16b9eca22699a61f1edd9e456126f';
 
-// Create a provider using the `LOCAL_NETWORK_URL` constant
 const provider = await Provider.create(LOCAL_NETWORK_URL);
-
-// Then use it when creating a wallet
 const wallet = Wallet.fromAddress(ADDRESS, provider);
-const balances = await wallet.getBalances();
 
-console.log({ balances, providerUrl: provider.url });
+const { balances } = await wallet.getBalances();
+
+console.log(balances);
 // #endregion main
