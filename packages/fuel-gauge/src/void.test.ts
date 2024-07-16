fd41c5f00a5c9d34ba870b3c98c7694681905bde
Peter Smith <peter@blueoceancomputing.co.uk>
2024-07-16 17:44:45 +0100

    feat: converted `void` tests to echo based, added NativeEnums

--- packages/fuel-gauge/src/void.test.ts
+++ packages/fuel-gauge/src/void.test.ts
@@ -1,29 +1,49 @@
+import type { DeployContractConfig } from 'fuels/test-utils';
 import { launchTestNode } from 'fuels/test-utils';
 
 import { VoidAbi__factory } from '../test/typegen';
+import type { NativeEnumInput } from '../test/typegen/contracts/VoidAbi';
 import VoidAbiHex from '../test/typegen/contracts/VoidAbi.hex';
+import type { Option } from '../test/typegen/contracts/common';
 
 /**
  * @group node
  */
-describe('Vector Tests', () => {
-  it('should return a void', async () => {
-    using launched = await launchTestNode({
-      contractsConfigs: [
-        {
-          deployer: VoidAbi__factory,
-          bytecode: VoidAbiHex,
-        },
-      ],
-    });
+describe('Void Tests', () => {
+  const contractsConfigs: DeployContractConfig[] = [
+    {
+      deployer: VoidAbi__factory,
+      bytecode: VoidAbiHex,
+    },
+  ];
+
+  it('should handle Option::None', async () => {
+    using launched = await launchTestNode({ contractsConfigs });
 
     const {
       contracts: [voidContract],
     } = launched;
 
-    const { waitForResult } = await voidContract.functions.return_void().call();
+    const optionNone: Option<number> = undefined;
+
+    const { waitForResult } = await voidContract.functions.echo_void(optionNone).call();
     const { value } = await waitForResult();
 
-    expect(value).toEqual(undefined);
+    expect(value).toEqual(optionNone);
+  });
+
+  it('should handle NativeEnum', async () => {
+    using launched = await launchTestNode({ contractsConfigs });
+
+    const {
+      contracts: [voidContract],
+    } = launched;
+
+    const enumValue: NativeEnumInput = 'C' as NativeEnumInput;
+
+    const { waitForResult } = await voidContract.functions.echo_native_enum(enumValue).call();
+    const { value } = await waitForResult();
+
+    expect(value).toEqual(enumValue);
   });
 });
--- packages/fuel-gauge/test/fixtures/forc-projects/void/src/main.sw
+++ packages/fuel-gauge/test/fixtures/forc-projects/void/src/main.sw
@@ -1,11 +1,30 @@
 contract;
 
+enum NativeEnum {
+    A: (),
+    B: (),
+    C: (),
+}
+
 abi VoidContract {
-    fn return_void() -> Option<u8>;
+    fn echo_void(value: Option<u8>) -> Option<u8>;
+    fn echo_native_enum(value: NativeEnum) -> NativeEnum;
 }
 
 impl VoidContract for Contract {
-    fn return_void() -> Option<u8> {
-        Option::None
+    fn echo_void(value: Option<u8>) -> Option<u8> {
+        assert_eq(value, Option::None);
+
+        value
+    }
+
+    fn echo_native_enum(value: NativeEnum) -> NativeEnum {
+        match value {
+            NativeEnum::A => assert(false),
+            NativeEnum::B => assert(false),
+            NativeEnum::C => assert(true),
+        };
+
+        value
     }
 }
