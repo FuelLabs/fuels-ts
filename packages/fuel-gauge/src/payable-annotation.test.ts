acc25b6b4a05c588bdacb7a1d4538b0f676b0065
Nedim Salkić <nedim.salkic@fuel.sh>
2024-07-18 16:29:20 +0200

    Update packages/fuel-gauge/src/payable-annotation.test.ts
    Co-authored-by: Chad Nehemiah <chad.nehemiah94@gmail.com>

--- packages/fuel-gauge/src/payable-annotation.test.ts
+++ packages/fuel-gauge/src/payable-annotation.test.ts
@@ -39,7 +39,21 @@ test("don't allow sending coins to non-payable functions", async () => {
   const baseAssetId = contract.provider.getBaseAssetId();
 
   // This should fail because the function is not payable
-  await expect(() =>
+expect(async () => {
+    const tx = await contract.functions
+      .non_payable()
+      .callParams({
+        forward: {
+          amount: bn(100),
+          assetId: baseAssetId,
+        },
+      })
+      .call();
+
+    await tx.waitForResult();
+  }).rejects.toThrowError(
+    `The target function non_payable cannot accept forwarded funds as it's not marked as 'payable'.`
+  );
     contract.functions
       .non_payable()
       .callParams({
