f3e89d22200cf484e4d316cf347954bc6db9f203
Dhaiwat <dhaiwatpandya@gmail.com>
2024-07-19 14:50:39 +0530

    docs: add more comments around the `create-fuels` template app (#2772)
    * docs: add more comments around the `create-fuels` template app
    
    * replicate changes to the counter example
    
    * add changeset

--- apps/create-fuels-counter-guide/src/lib.ts
+++ apps/create-fuels-counter-guide/src/lib.ts
@@ -1,16 +1,23 @@
 import { Account, BN, TESTNET_NETWORK_URL } from 'fuels';
 
 // #region deploying-dapp-to-testnet-lib-current-environment
+// The two environments for the dapp are local and testnet.
 export const Environments = {
   LOCAL: 'local',
   TESTNET: 'testnet',
 } as const;
 type Environment = (typeof Environments)[keyof typeof Environments];
 
+/**
+ * The current environment is determined by the
+ * `NEXT_PUBLIC_DAPP_ENVIRONMENT` environment variable.
+ * If it's not set, the default is `local`.
+ */
 export const CURRENT_ENVIRONMENT: Environment =
   (process.env.NEXT_PUBLIC_DAPP_ENVIRONMENT as Environment) || Environments.LOCAL;
 // #endregion deploying-dapp-to-testnet-lib-current-environment
 
+// The node URL is determined by the current environment too.
 export const NODE_URL =
   CURRENT_ENVIRONMENT === Environments.LOCAL
     ? `http://127.0.0.1:${process.env.NEXT_PUBLIC_FUEL_NODE_PORT || 4000}/v1/graphql`
