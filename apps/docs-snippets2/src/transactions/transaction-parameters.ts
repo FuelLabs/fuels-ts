16abcb4f029b6d5fdc909389ab4551196ae4a41a
Anderson Arboleya <anderson@arboleya.me>
2024-07-20 09:39:39 -0300

    Cleanup

--- apps/docs-snippets2/src/transactions/transaction-parameters.ts
+++ apps/docs-snippets2/src/transactions/transaction-parameters.ts
@@ -1,12 +1,6 @@
 // #region full
 import type { TxParams } from 'fuels';
-import {
-  bn,
-  LOCAL_NETWORK_URL,
-  Provider,
-  ScriptTransactionRequest,
-  Wallet,
-} from 'fuels';
+import { bn, LOCAL_NETWORK_URL, Provider, ScriptTransactionRequest, Wallet } from 'fuels';
 
 import { WALLET_PVT_KEY } from '../env';
 import { CounterAbi__factory } from '../typegend';
@@ -18,35 +12,31 @@ const { storageSlots } = CounterAbi__factory;
 const provider = await Provider.create(LOCAL_NETWORK_URL);
 const wallet = Wallet.fromPrivateKey(WALLET_PVT_KEY, provider);
 
-const deploy = await CounterAbi__factory.deployContract(
-  counterBytecode,
-  wallet,
-  {
-    storageSlots,
-  }
-);
+const deploy = await CounterAbi__factory.deployContract(counterBytecode, wallet, {
+  storageSlots,
+});
+
+const { contract } = await deploy.waitForResult();
 
 // #region transaction-parameters-6
 const txParams: TxParams = {
   // #region transaction-parameters-1
-  gasLimit: bn(69242), // BigNumberish or undefined
+  gasLimit: bn(69242),
   // #endregion transaction-parameters-1
   // #region transaction-parameters-2
-  maxFee: bn(69242), // BigNumberish or undefined
+  maxFee: bn(69242),
   // #endregion transaction-parameters-2
   // #region transaction-parameters-3
-  tip: bn(100), // BigNumberish or undefined
+  tip: bn(100),
   // #endregion transaction-parameters-3
   // #region transaction-parameters-4
-  maturity: 1, // BigNumberish or undefined
+  maturity: 1,
   // #endregion transaction-parameters-4
   // #region transaction-parameters-5
-  witnessLimit: bn(5000), // BigNumberish or undefined
+  witnessLimit: bn(5000),
   // #endregion transaction-parameters-5
 };
 // #endregion transaction-parameters-6
-
-const { contract } = await deploy.waitForResult();
 
 // #region transaction-parameters-7
 const transactionRequest = new ScriptTransactionRequest({
@@ -57,7 +47,7 @@ const transactionRequest = new ScriptTransactionRequest({
 
 // #region transaction-parameters-8
 const { waitForResult } = await contract.functions
-  .increment_count(15) // contract method
+  .increment_count(15) // contract fn
   .txParams(txParams)
   .call();
 
