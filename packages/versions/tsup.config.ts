85c4877b0157ef7d4a54c08e821f63e53f1b0afc
Anderson Arboleya <anderson@arboleya.me>
2024-07-20 16:13:35 -0300

    Fixing build configs

--- packages/versions/tsup.config.ts
+++ packages/versions/tsup.config.ts
@@ -1,6 +1,7 @@
 import { indexBinAndCliConfig } from '@internal/tsup';
 
 export default {
+  ...indexBinAndCliConfig,
   entry: {
     ...indexBinAndCliConfig.entry,
     'cli-utils': 'src/cli-utils.ts',
