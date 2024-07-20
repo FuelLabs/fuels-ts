3f756e969d58cd0f9a559c9be5869efac92f6dcb
Anderson Arboleya <anderson@arboleya.me>
2024-07-20 09:39:31 -0300

    Trim env consts

--- apps/docs-snippets2/scripts/launcher.ts
+++ apps/docs-snippets2/scripts/launcher.ts
@@ -4,7 +4,7 @@ using node = await launchTestNode({ walletsConfig: { count: 5 } });
 
 export const LOCAL_NETWORK_URL = node.provider.url;
 
-const [w0, w2, w3, w4, w5] = node.wallets;
+const [w0, w2, w3] = node.wallets;
 
 export const WALLET_ADDRESS = w0.address;
 export const WALLET_PVT_KEY = w0.privateKey;
@@ -14,9 +14,3 @@ export const WALLET_PVT_KEY_2 = w2.privateKey;
 
 export const WALLET_ADDRESS_3 = w3.address;
 export const WALLET_PVT_KEY_3 = w3.privateKey;
-
-export const WALLET_ADDRESS_4 = w4.address;
-export const WALLET_PVT_KEY_4 = w4.privateKey;
-
-export const WALLET_ADDRESS_5 = w5.address;
-export const WALLET_PVT_KEY_5 = w5.privateKey;
--- apps/docs-snippets2/src/env.ts
+++ apps/docs-snippets2/src/env.ts
@@ -6,9 +6,3 @@ export const WALLET_PVT_KEY_2 = '<overriden-by-wrapper>';
 
 export const WALLET_ADDRESS_3 = '<overriden-by-wrapper>';
 export const WALLET_PVT_KEY_3 = '<overriden-by-wrapper>';
-
-export const WALLET_ADDRESS_4 = '<overriden-by-wrapper>';
-export const WALLET_PVT_KEY_4 = '<overriden-by-wrapper>';
-
-export const WALLET_ADDRESS_5 = '<overriden-by-wrapper>';
-export const WALLET_PVT_KEY_5 = '<overriden-by-wrapper>';
