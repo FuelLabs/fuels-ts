c68ea148b7dd403ac7100a3e0ad3060f25c3543b
Anderson Arboleya <anderson@arboleya.me>
2024-07-19 21:20:30 -0300

    Validate env constants existence

--- apps/docs-snippets2/scripts/wrap-snippets.ts
+++ apps/docs-snippets2/scripts/wrap-snippets.ts
@@ -14,14 +14,46 @@ export const wrapSnippet = (filepath: string) => {
   let imports = raw.match(importsReg)?.toString() ?? '';
   const snippet = imports.length ? raw.split(imports)[1] : raw;
 
-  // creates node launcher injector and replaces `LOCAL_NETWORK_URL`
+  // 1—— creates node launcher injector
+  const keys = ['LOCAL_NETWORK_URL', 'WALLET_ADDRESS', 'WALLET_PVT_KEY'];
+
+  const envImportReg = /import.+\{.+([\s\S]+).+\}.+from.+'\.\.\/env';/gm;
+  if (envImportReg.test(imports)) {
+    // match all import groups up to the `env.ts` import
+    const allImports = imports.match(envImportReg)?.[0];
+    const envImport = `import ${allImports?.split('import ').pop()}`;
+
+    // isolater the `env.ts` import line and extract its members
+    const envMembersReg = /import.+\{(.+)\}.+from.+'\.\.\/env'/m;
+    const envConstants = envImport.match(envMembersReg)?.[1].trim().split(/,\s?/);
+
+    // check and warns against unsupported env members
+    envConstants?.forEach((envConst) => {
+      const found = keys.find((k) => k === envConst);
+      if (!found) {
+        throw new Error(`Mocked env consty '${envConst}' not supported in code snippets.`);
+      }
+    });
+
+    // Remove the env import line, favoring the
+    // inline declared variables.
+    imports = imports.replace(envImport, '');
+  }
+
   let nodeLauncher = '';
-  if (/LOCAL_NETWORK_URL/.test(imports)) {
-    imports = imports.replace(/LOCAL_NETWORK_URL/, 'TESTNET_NETWORK_URL');
+
+  const localNetworkReg = /LOCAL_NETWORK_URL/;
+  if (localNetworkReg.test(imports)) {
+    // 2—— replaces `LOCAL_NETWORK_URL`
+    imports = imports.replace(localNetworkReg, 'TESTNET_NETWORK_URL');
     imports += `\nimport { launchTestNode } from 'fuels/test-utils'`;
+
+    // 3—— injects launched node and env constants
     nodeLauncher = [
       'using node = await launchTestNode();',
       'const LOCAL_NETWORK_URL = node.provider.url;',
+      'const WALLET_ADDRESS = node.wallets[0].address;',
+      'const WALLET_PVT_KEY = node.wallets[0].privateKey;',
     ].join('\n  ');
   }
 
