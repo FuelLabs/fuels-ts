28cce95d371d700322c139f822850092d8e4f010
Anderson Arboleya <anderson@arboleya.me>
2024-07-20 00:45:37 -0300

    Syncing env and launcher template

--- apps/docs-snippets2/src/env.ts
+++ apps/docs-snippets2/src/env.ts
@@ -1,2 +1,14 @@
-export const WALLET_ADDRESS: string = '<overriden-by-wrapper>';
-export const WALLET_PVT_KEY: string = '<overriden-by-wrapper>';
+export const WALLET_ADDRESS = '<overriden-by-wrapper>';
+export const WALLET_PVT_KEY = '<overriden-by-wrapper>';
+
+export const WALLET_ADDRESS_2 = '<overriden-by-wrapper>';
+export const WALLET_PVT_KEY_2 = '<overriden-by-wrapper>';
+
+export const WALLET_ADDRESS_3 = '<overriden-by-wrapper>';
+export const WALLET_PVT_KEY_3 = '<overriden-by-wrapper>';
+
+export const WALLET_ADDRESS_4 = '<overriden-by-wrapper>';
+export const WALLET_PVT_KEY_4 = '<overriden-by-wrapper>';
+
+export const WALLET_ADDRESS_5 = '<overriden-by-wrapper>';
+export const WALLET_PVT_KEY_5 = '<overriden-by-wrapper>';
