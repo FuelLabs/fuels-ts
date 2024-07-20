501d913308743621892b7b9c804d8e83f289774b
Anderson Arboleya <anderson@arboleya.me>
2024-07-20 09:52:35 -0300

    Ignoring experimental file

--- apps/docs-snippets2/scripts/wrap-snippets.ts
+++ apps/docs-snippets2/scripts/wrap-snippets.ts
@@ -73,6 +73,12 @@ export const wrapSnippet = (filepath: string) => {
 */
 const dir = 'src/**';
 const src = `${dir}/*.ts`;
-const ignore = [`src/typegend/**`, `src/env.ts`, `${dir}/*.test.ts`, `${dir}/*.wrapped.ts`];
+const ignore = [
+  `src/typegend/**`,
+  `src/env.ts`,
+  `src/transactions/new-api.ts`,
+  `${dir}/*.test.ts`,
+  `${dir}/*.wrapped.ts`,
+];
 
 globSync(src, { ignore }).forEach(wrapSnippet);
