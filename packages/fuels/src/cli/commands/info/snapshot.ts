ba9b5050d54dc96011ecb58ae4dd96e68db8eba5
Anderson Arboleya <anderson@arboleya.me>
2024-07-20 21:49:21 -0300

    Commenting out fuelup stuff

--- packages/fuels/src/cli/commands/info/snapshot.ts
+++ packages/fuels/src/cli/commands/info/snapshot.ts
@@ -1,14 +1,13 @@
 import { versions } from '@fuel-ts/versions';
 import { getSystemVersions } from '@fuel-ts/versions/cli-utils';
-import { execSync } from 'node:child_process';
 import os from 'node:os';
 
 export const snapshot = {
   sdkVersions: versions,
   toolchainVersions: getSystemVersions(),
-  fuelupShow: execSync('fuelup show')?.toString('utf8'),
+  // fuelupShow: execSync('fuelup show')?.toString('utf8'),
   nodeVersion: process.version,
-  // browserVersion: detectBrowser(), ?
+  // browserVersion: detectBrowser() ?
   osType: os.type(),
   osArch: os.arch(),
 };
