818d95b7cc67c23c8ac36577ba51f57b17fe48f7
Anderson Arboleya <anderson@arboleya.me>
2024-07-21 13:52:28 -0300

    Refactoring `factory` contract template

--- packages/abi-typegen/src/templates/contract/factory.ts
+++ packages/abi-typegen/src/templates/contract/factory.ts
@@ -4,13 +4,13 @@ import { renderHbsTemplate } from '../renderHbsTemplate';
 import factoryTemplate from './factory.hbs';
 
 export function renderFactoryTemplate(params: { abi: Abi }) {
-  const { name: capitalizedName, rawContents, storageSlotsContents } = params.abi;
+  const { camelizedName, capitalizedName, rawContents, storageSlotsContents } = params.abi;
   const abiJsonString = JSON.stringify(rawContents, null, 2);
   const storageSlotsJsonString = storageSlotsContents ?? '[]';
 
   const text = renderHbsTemplate({
     template: factoryTemplate,
-    data: { capitalizedName, abiJsonString, storageSlotsJsonString },
+    data: { camelizedName, capitalizedName, abiJsonString, storageSlotsJsonString },
   });
 
   return text;
