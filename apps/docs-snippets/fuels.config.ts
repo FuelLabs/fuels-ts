1ac5d408aba63d37dd2b0b7e0f224337eb4b4ee9
Peter Smith <peter@blueoceancomputing.co.uk>
2024-07-24 09:15:36 +0100

    chore: enable forc debug

--- apps/docs-snippets/fuels.config.ts
+++ apps/docs-snippets/fuels.config.ts
@@ -3,7 +3,7 @@ import { createConfig } from 'fuels';
 export default createConfig({
   workspace: './test/fixtures/forc-projects',
   output: './test/typegen',
-  forcBuildFlags: ['--debug'],
+  forcBuildFlags: [],
   forcPath: 'fuels-forc',
   fuelCorePath: 'fuels-core',
 });
