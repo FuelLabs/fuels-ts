09fa2eba19fbcf3844342ee621ca1b1a55d3b475
Peter Smith <peter@blueoceancomputing.co.uk>
2024-07-29 11:49:53 +0100

    chore: added script options test

--- packages/fuel-gauge/src/script-with-options.test.ts
+++ packages/fuel-gauge/src/script-with-options.test.ts
@@ -1,8 +1,9 @@
+import type { BigNumberish } from 'fuels';
 import { bn } from 'fuels';
 import { launchTestNode } from 'fuels/test-utils';
 
 import { ScriptWithOptionsAbi__factory } from '../test/typegen';
-import type { ScriptWithOptionsAbiConfigurables } from '../test/typegen/scripts/factories/ScriptWithOptionsAbi__factory';
+import type { Option } from '../test/typegen/contracts/common';
 
 /**
  * @group node
@@ -18,25 +19,31 @@ describe('Script With Options', () => {
 
     const scriptWithOptions = ScriptWithOptionsAbi__factory.createInstance(wallet);
     const expectedValue = true;
-    const configurables: ScriptWithOptionsAbiConfigurables = { X: bn(1), Y: bn(2), Z: bn(3) };
+    const OPTION_SOME: Option<BigNumberish> = bn(1);
+    const OPTION_NONE: Option<BigNumberish> = undefined;
 
     // Script with no inputs
-    scriptWithOptions.setConfigurableConstants({ X: undefined, Y: undefined, Z: undefined });
     const { waitForResult: call1 } = await scriptWithOptions.functions.main().call();
     const { value: value1 } = await call1();
     expect(value1).toBe(expectedValue);
 
     // Script with single input
-    scriptWithOptions.setConfigurableConstants({ X: bn(1), Y: undefined, Z: undefined });
-    const { waitForResult: call2 } = await scriptWithOptions.functions.main(bn(1)).call();
+    const { waitForResult: call2 } = await scriptWithOptions.functions.main(OPTION_SOME).call();
     const { value: value2 } = await call2();
     expect(value2).toBe(expectedValue);
 
-    // Script with two inputs
+    // Script with three input
     const { waitForResult: call3 } = await scriptWithOptions.functions
-      .main(undefined, undefined)
+      .main(OPTION_SOME, OPTION_SOME, OPTION_SOME)
       .call();
     const { value: value3 } = await call3();
     expect(value3).toBe(expectedValue);
+
+    // Script with mix of optional input
+    const { waitForResult: call4 } = await scriptWithOptions.functions
+      .main(OPTION_SOME, OPTION_NONE, OPTION_NONE)
+      .call();
+    const { value: value4 } = await call4();
+    expect(value4).toBe(expectedValue);
   });
 });
--- packages/fuel-gauge/test/fixtures/forc-projects/script-with-options/src/main.sw
+++ packages/fuel-gauge/test/fixtures/forc-projects/script-with-options/src/main.sw
@@ -1,14 +1,5 @@
 script;
 
-configurable {
-    X: Option<u8> = Option::None,
-    Y: Option<u8> = Option::None,
-    Z: Option<u8> = Option::None,
-}
-
 fn main(x: Option<u8>, y: Option<u8>, z: Option<u8>) -> bool {
-    assert_eq(x, X);
-    assert_eq(y, Y);
-    assert_eq(z, Z);
     true
 }
