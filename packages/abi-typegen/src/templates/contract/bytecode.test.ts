04e07908d2937cc0d43416722974e9c59fccbab4
Dhaiwat <dhaiwatpandya@gmail.com>
2024-07-30 16:22:21 +0530

    fix test

--- packages/abi-typegen/src/templates/contract/bytecode.test.ts
+++ packages/abi-typegen/src/templates/contract/bytecode.test.ts
@@ -8,6 +8,22 @@ import { mockVersions } from '../../../test/utils/mockVersions';
 import { renderBytecodeTemplate } from './bytecode';
 
 /**
+ * We are removing the bytecode from the file contents
+ * because the compressed bytecode is not deterministic.
+ * It changes every time the file is compiled.
+ * This is why we are removing this line from the file contents,
+ * and asserting that the rest of the file contents are the same.
+ */
+const removeBytecodeFromFileContents = (fileContents: string) => {
+  const bytecodeRegex = /const compresssedBytecode = '(.*)';/gm;
+  const match = bytecodeRegex.exec(fileContents);
+  if (!match) {
+    throw new Error('Could not find bytecode in file contents.');
+  }
+  return fileContents.replace(bytecodeRegex, '');
+};
+
+/**
  * @group node
  */
 describe('templates/contract/bytecode', () => {
@@ -25,6 +41,8 @@ describe('templates/contract/bytecode', () => {
     // validating
     restore();
 
-    expect(rendered.trim()).toEqual(bytecodeTemplate.trim());
+    expect(removeBytecodeFromFileContents(rendered.trim())).toEqual(
+      removeBytecodeFromFileContents(bytecodeTemplate.trim())
+    );
   });
 });
