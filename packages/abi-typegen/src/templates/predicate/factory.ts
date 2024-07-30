c1494c390b693f9cd957a81e99336b85cfe661fb
nedsalk <nedim.salkic@fuel.sh>
2024-07-30 15:12:38 +0200

    added `as JsonAbi` type assertions to fix type errors

--- packages/abi-typegen/src/templates/predicate/factory.ts
+++ packages/abi-typegen/src/templates/predicate/factory.ts
@@ -33,7 +33,7 @@ export function renderFactoryTemplate(params: { abi: Abi }) {
   const { structs } = formatStructs({ types });
   const { imports } = formatImports({
     types,
-    baseMembers: ['Predicate', 'Provider', 'InputValue'],
+    baseMembers: ['type JsonAbi', 'Predicate', 'Provider', 'InputValue'],
   });
   const { formattedConfigurables } = formatConfigurables({ configurables });
 
