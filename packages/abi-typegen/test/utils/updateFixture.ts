2210e68cabbb3024690cecf2f5e2e8b723b2d163
Anderson Arboleya <anderson@arboleya.me>
2024-08-01 11:26:42 -0300

    Nits

--- packages/abi-typegen/test/utils/updateFixture.ts
+++ packages/abi-typegen/test/utils/updateFixture.ts
@@ -1,10 +1,12 @@
 import { writeFileSync } from 'fs';
 
 export function autoUpdateFixture(path: string, contents: string) {
-  if (process.env.UPDATE_FIXTURES) {
+  if (process.env.UPDATE_FIXTURES === 'yes') {
     if (!/fixtures/.test(path)) {
       throw new Error(`This path may no be a fixture: ${path}`);
     }
+    const { log } = console;
+    log('Updated fixture', path);
     writeFileSync(path, contents);
   }
   return contents;
