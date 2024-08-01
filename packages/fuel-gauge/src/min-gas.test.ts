b5ac6d0d1a4cd732079349eedfefb6583b313aac
Peter Smith <peter@blueoceancomputing.co.uk>
2024-08-01 17:18:27 +0100

    chore: fix min-gas test

--- packages/fuel-gauge/src/min-gas.test.ts
+++ packages/fuel-gauge/src/min-gas.test.ts
@@ -117,7 +117,9 @@ describe('Minimum gas tests', () => {
     /**
      * Setup predicate
      */
-    const predicate = new ComplexPredicate(provider, [bn(1000)]);
+    const predicate = new ComplexPredicate(provider, {
+      data: [bn(1000)],
+    });
 
     /**
      * Fund the predicate
@@ -165,7 +167,9 @@ describe('Minimum gas tests', () => {
     /**
      * Setup predicate
      */
-    const predicate = new ComplexPredicate(provider, [bn(1000)]);
+    const predicate = new ComplexPredicate(provider, {
+      data: [bn(1000)],
+    });
 
     /**
      * Fund the predicate
