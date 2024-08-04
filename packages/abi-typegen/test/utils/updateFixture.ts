3a23bbdb861a02432b11409babeffcc02c6948f3
Anderson Arboleya <anderson@arboleya.me>
2024-08-04 11:02:26 -0300

    Going boolean

--- packages/abi-typegen/test/utils/updateFixture.ts
+++ packages/abi-typegen/test/utils/updateFixture.ts
@@ -1,7 +1,7 @@
 import { writeFileSync } from 'fs';
 
 export function autoUpdateFixture(path: string, contents: string) {
-  if (process.env.UPDATE_FIXTURES === 'yes') {
+  if (process.env.UPDATE_FIXTURES === 'true') {
     if (!/fixtures/.test(path)) {
       throw new Error(`This path may no be a fixture: ${path}`);
     }
