4896a62dfacb9b86904f1ca7d8402b6dc8d6649b
Anderson Arboleya <anderson@arboleya.me>
2024-08-06 08:51:32 -0300

    Unfixing what wasn't broken

--- apps/docs-snippets/src/guide/predicates/predicate-custom-transactions.test.ts
+++ apps/docs-snippets/src/guide/predicates/predicate-custom-transactions.test.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Provider, ScriptTransactionRequest, Wallet, TESTNET_NETWORK_URL as _ } from 'fuels';
+import { Provider, ScriptTransactionRequest, Wallet, TESTNET_NETWORK_URL } from 'fuels';
 import { launchTestNode } from 'fuels/test-utils';
 
 import { ConfigurablePin as PredicateFactory } from '../../../test/typegen';
@@ -19,6 +19,7 @@ describe('Predicate Custom Transactions', () => {
 
     const SENDER_PVT_KEY = testSender.privateKey;
     const RECEIVER_ADDRESS = testReceiver.address;
+    // eslint-disable-next-line @typescript-eslint/no-shadow
     const TESTNET_NETWORK_URL = testProvider.url;
 
     const initialRecieverBalance = await testReceiver.getBalance(testProvider.getBaseAssetId());
