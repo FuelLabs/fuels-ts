0641c7d79dee3dc538a905d5465c92bba88868c9
Daniel Bate <djbate23@gmail.com>
2024-08-08 11:32:28 +0100

    test: add missing properties to test chain config

--- packages/account/test/fixtures/chain.ts
+++ packages/account/test/fixtures/chain.ts
@@ -144,6 +144,16 @@ export const MOCK_CHAIN: GqlChainInfoFragment = {
         base: '2',
         gasPerUnit: '0',
       },
+      bldd: {
+        type: 'LightOperation',
+        base: '1',
+        unitsPerGas: '5000',
+      },
+      bsiz: {
+        type: 'LightOperation',
+        base: '1',
+      unitsPerGas: '5000',
+      },
       cfe: {
         type: 'HeavyOperation',
         base: '2',
