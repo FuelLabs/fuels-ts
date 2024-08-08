ac7c062738bab97b692f76ca6ef27f050d1bb6af
Peter Smith <peter@blueoceancomputing.co.uk>
2024-08-08 17:00:08 +0100

    chore: using fuel-core branch

--- packages/versions/src/lib/getBuiltinVersions.ts
+++ packages/versions/src/lib/getBuiltinVersions.ts
@@ -1,7 +1,7 @@
 export function getBuiltinVersions() {
   return {
     FORC: '0.62.0',
-    FUEL_CORE: '0.31.0',
+    FUEL_CORE: 'git:feature/transaction-from-status',
     FUELS: '0.93.0',
   };
 }
