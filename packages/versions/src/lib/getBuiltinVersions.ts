29d6ba3c49377c79e24c8529ac7c42e62a2ac231
Anderson Arboleya <anderson@arboleya.me>
2024-08-16 09:49:08 -0300

    The branch is gone - temporarily switching to `master`

--- packages/versions/src/lib/getBuiltinVersions.ts
+++ packages/versions/src/lib/getBuiltinVersions.ts
@@ -2,7 +2,7 @@ import type { Versions } from './types';
 
 export function getBuiltinVersions(): Versions {
   return {
-    FORC: 'git:esdrubal/abi_changes',
+    FORC: 'git:master',
     FUEL_CORE: '0.32.1',
     FUELS: '0.93.0',
   };
