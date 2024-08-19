bd174ca4a5e7a3083317b67ae71c064d34697d48
Anderson Arboleya <anderson@arboleya.me>
2024-08-19 11:55:26 -0300

    Replacing sway `branch` by `0.63.0` version

--- packages/versions/src/lib/getBuiltinVersions.ts
+++ packages/versions/src/lib/getBuiltinVersions.ts
@@ -2,7 +2,7 @@ import type { Versions } from './types';
 
 export function getBuiltinVersions(): Versions {
   return {
-    FORC: 'git:master',
+    FORC: '0.63.0',
     FUEL_CORE: '0.33.0',
     FUELS: '0.93.0',
   };
