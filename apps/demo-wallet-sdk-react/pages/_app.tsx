503d6cd7c3dcab7c1d672f05ec1b17ceadc9073e
Dhaiwat <dhaiwatpandya@gmail.com>
2024-08-28 20:28:37 +0530

    sync deps

--- apps/demo-wallet-sdk-react/pages/_app.tsx
+++ apps/demo-wallet-sdk-react/pages/_app.tsx
@@ -22,7 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
           fuelConfig={{
             connectors: [
               new FuelWalletConnector(),
-              new WalletConnectConnector(),
+              new WalletConnectConnector({}),
               new BakoSafeConnector(),
               new FueletWalletConnector(),
               new FuelWalletDevelopmentConnector(),
