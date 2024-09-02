1797323e0a8952d37407c16b5f08d754341ad861
nedsalk <nedim.salkic@fuel.sh>
2024-09-02 14:57:39 +0200

    bump version

--- packages/account/src/providers/provider.test.ts
+++ packages/account/src/providers/provider.test.ts
@@ -143,7 +143,7 @@ describe('Provider', () => {
 
     const version = await provider.getVersion();
 
-    expect(version).toEqual('0.34.0');
+    expect(version).toEqual('0.35.0');
   });
 
   it('can call()', async () => {
