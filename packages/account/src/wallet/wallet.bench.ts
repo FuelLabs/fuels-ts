b23cd12e2c59877616c9eeda3077a2836bb82cc9
chad <chad.nehemiah94@gmail.com>
2024-09-02 16:20:08 -0500

    build: resolve build issues

--- packages/account/src/wallet/wallet.bench.ts
+++ packages/account/src/wallet/wallet.bench.ts
@@ -1,11 +1,7 @@
-import { bn } from '@fuel-ts/math';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { bench } from 'vitest';
 
-import { FUEL_NETWORK_URL } from '../configs';
-import type { TransactionRequestLike, TransactionResponse } from '../providers';
-import { Provider, transactionRequestify } from '../providers';
-import { generateTestWallet } from '../test-utils';
+import { setupTestProviderAndWallets } from '../test-utils';
 
 import { Wallet } from './wallet';
 import { WalletLocked, WalletUnlocked } from './wallets';
@@ -16,87 +12,34 @@ const expectedPublicKey =
 const expectedAddress = 'fuel1785jcs4epy625cmjuv9u269rymmwv6s6q2y9jhnw877nj2j08ehqce3rxf';
 const expectedLockedAddress = 'fuel1tac0aml37g57f227zptw3dxcp59jfdt9vayxpnpp80dswynuuxcs8eme0m';
 
-let provider: Provider;
-let baseAssetId: string;
-
-beforeAll(async () => {
-  provider = await Provider.create(FUEL_NETWORK_URL);
-  baseAssetId = provider.getBaseAssetId();
-});
-
 /**
  * @group node
  */
 describe('Wallet Benchmarks', () => {
-  bench('Instantiate a new Unlocked wallet', () => {
+  bench('Instantiate a new Unlocked wallet', async () => {
+    using launched = await setupTestProviderAndWallets();
+    const { provider } = launched;
+
     const unlockedWallet = new WalletUnlocked(expectedPrivateKey, provider);
 
     expect(unlockedWallet.publicKey).toEqual(expectedPublicKey);
     expect(unlockedWallet.address.toAddress()).toEqual(expectedAddress);
   });
 
-  bench('Instantiate from a constructor', () => {
+  bench('Instantiate from a constructor', async () => {
+    using launched = await setupTestProviderAndWallets();
+    const { provider } = launched;
     const lockedWallet = new WalletLocked(expectedPrivateKey, provider);
 
     expect(lockedWallet.address.toAddress()).toEqual(expectedLockedAddress);
   });
 
-  bench('Instantiate from an address', () => {
+  bench('Instantiate from an address', async () => {
+    using launched = await setupTestProviderAndWallets();
+    const { provider } = launched;
     const lockedWallet = Wallet.fromAddress(expectedAddress, provider);
 
     expect(lockedWallet.address.toAddress()).toEqual(expectedAddress);
     expect(lockedWallet).toBeInstanceOf(WalletLocked);
   });
-
-  bench(
-    'Sending a transaction via a custom provider on a public wallet to the contract instance',
-    async () => {
-      const externalWallet = await generateTestWallet(provider, [
-        {
-          amount: bn(1_000_000_000),
-          assetId: baseAssetId,
-        },
-      ]);
-      const externalWalletReceiver = await generateTestWallet(provider);
-
-      // Create a custom provider to emulate a external signer
-      // like Wallet Extension or a Hardware wallet
-
-      // Set custom provider to contract instance
-      class ProviderCustom extends Provider {
-        // eslint-disable-next-line @typescript-eslint/require-await
-        static async connect(url: string) {
-          const newProvider = new ProviderCustom(url, {});
-          return newProvider;
-        }
-
-        async sendTransaction(
-          transactionRequestLike: TransactionRequestLike
-        ): Promise<TransactionResponse> {
-          const transactionRequest = transactionRequestify(transactionRequestLike);
-          // Simulate a external request of signature
-          const signedTransaction = await externalWallet.signTransaction(transactionRequest);
-          transactionRequest.updateWitnessByOwner(
-            externalWallet.address.toB256(),
-            signedTransaction
-          );
-          return super.sendTransaction(transactionRequestLike);
-        }
-      }
-
-      const customProvider = await ProviderCustom.connect(FUEL_NETWORK_URL);
-      const lockedWallet = Wallet.fromAddress(externalWallet.address, customProvider);
-
-      const response = await lockedWallet.transfer(
-        externalWalletReceiver.address,
-        bn(1_000_000),
-        baseAssetId,
-        { gasLimit: 10_000 }
-      );
-      await response.wait();
-
-      const balance = await externalWalletReceiver.getBalance(baseAssetId);
-      expect(balance.eq(1_000_000)).toBeTruthy();
-    }
-  );
 });
