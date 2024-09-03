f05196e766813efb015e7fc34d4261c7d185df13
Daniel Bate <djbate23@gmail.com>
2024-09-03 13:43:55 +0100

    chore: remove redundant lgos

--- scripts/tests-ui.sh
+++ scripts/tests-ui.sh
@@ -4,13 +4,11 @@ ROOT_DIR=$(pwd)
 PLAYWRIGHT_DIR="$ROOT_DIR"
 PROJECT_DIR="${PROJECT_DIR-"$ROOT_DIR/templates/vite"}"
 
-cd $PROJECT_DIR && pnpm run fuels:dev 2>&1 &
+cd $PROJECT_DIR && pnpm run fuels:dev > /dev/null 2>&1 &
 sleep 5
 
 cd $PROJECT_DIR && pnpm run dev > /dev/null 2>&1 &
 sleep 5
-
-cat $PROJECT_DIR/src/sway-api/contract-ids.json
 
 cd $PLAYWRIGHT_DIR
 pnpm exec playwright install --with-deps  > /dev/null 2>&1
