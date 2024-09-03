346fa17569584fad9a1b8060893534874ea169cb
Daniel Bate <djbate23@gmail.com>
2024-09-03 12:42:11 +0100

    chore: increase integration test dev server timeout and expose logs

--- scripts/tests-ui.sh
+++ scripts/tests-ui.sh
@@ -4,8 +4,8 @@ ROOT_DIR=$(pwd)
 PLAYWRIGHT_DIR="$ROOT_DIR"
 PROJECT_DIR="${PROJECT_DIR-"$ROOT_DIR/templates/vite"}"
 
-cd $PROJECT_DIR && pnpm run fuels:dev > /dev/null 2>&1 &
-sleep 5
+cd $PROJECT_DIR && pnpm run fuels:dev &
+sleep 15
 
 cd $PROJECT_DIR && pnpm run dev > /dev/null 2>&1 &
 sleep 5
