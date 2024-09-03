24ffb06aa5b505a8b76a7a85576714bb7cf6ad63
Peter Smith <peter@blueoceancomputing.co.uk>
2024-09-03 13:43:15 +0100

    chore: added basic test infrastructure

---
+++ packages/fuel-gauge/src/abi/AbiGen.test.ts
@@ -0,0 +1,20 @@
+import { log } from 'console';
+
+import { AbiProjectsEnum, getAbiForcProject } from '../../test/fixtures/forc-projects';
+
+describe('AbiGen', () => {
+  test('contract', () => {
+    const { abiContents } = getAbiForcProject(AbiProjectsEnum.ABI_CONTRACT);
+    log(abiContents);
+  });
+
+  test('script', () => {
+    const { abiContents } = getAbiForcProject(AbiProjectsEnum.ABI_SCRIPT);
+    log(abiContents);
+  });
+
+  test('predicate', () => {
+    const { abiContents } = getAbiForcProject(AbiProjectsEnum.ABI_PREDICATE);
+    log(abiContents);
+  });
+});
