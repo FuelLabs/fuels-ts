60c6f06486a615d353933026ee60c5489c807d35
Dhaiwat <dhaiwatpandya@gmail.com>
2024-09-04 23:45:40 +0530

    add to nextjs template too

---
+++ templates/nextjs/test/ui/ui.test.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+
+const WEB_SERVER_URL = 'http://localhost:3000';
+
+test.extend({
+  page: async ({ page }, use) => {
+    await page.evaluate(() => window.localStorage.clear());
+    await use(page);
+  },
+});
+
+test('counter contract - increment function call works properly', async ({ page }) => {
+  await page.goto(WEB_SERVER_URL, { waitUntil: 'networkidle' });
+
+  await page.waitForTimeout(2000);
+
+  const topUpWalletButton = page.getByText('Top-up Wallet');
+  await topUpWalletButton.click();
+
+  await page.waitForTimeout(2000);
+
+  const initialCounterValue = page.getByTestId('counter');
+  await expect(initialCounterValue).toHaveText('0');
+
+  const incrementButton = page.getByText('Increment Counter');
+  await incrementButton.click();
+
+  await page.waitForTimeout(2000);
+
+  const counterValueAfterIncrement = page.getByTestId('counter');
+  await expect(counterValueAfterIncrement).toHaveText('1');
+});
