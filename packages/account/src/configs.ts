141fa0076b004614ec384f71b2563a81d70eb615
Dhaiwat <dhaiwatpandya@gmail.com>
2024-09-05 11:10:06 +0530

    export `MAINNET_NETWORK_URL`

--- packages/account/src/configs.ts
+++ packages/account/src/configs.ts
@@ -1,4 +1,4 @@
 export const DEVNET_NETWORK_URL = 'https://devnet.fuel.network/v1/graphql';
 export const TESTNET_NETWORK_URL = 'https://testnet.fuel.network/v1/graphql';
-// TODO: replace placeholder with mainnet network url
-// export const NETWORK_URL = '';
+
+export const MAINNET_NETWORK_URL = 'https://mainnet.fuel.network/v1/graphql';
