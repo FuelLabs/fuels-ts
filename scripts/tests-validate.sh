f557f972170779db88cd7a73fb3c932407527287
Dhaiwat <dhaiwatpandya@gmail.com>
2024-09-05 09:59:57 +0530

    Update tests-validate.sh

--- scripts/tests-validate.sh
+++ scripts/tests-validate.sh
@@ -3,6 +3,10 @@
 ROOT=$(cd "$(dirname "$0")/.."; pwd)
 
 FILES=$(find $ROOT/{apps,packages,internal} -name '*.test.ts')
+
+# ignore files in apps/create-fuels-counter-guide/test/ui
+FILES=$(echo $FILES | grep -v "apps/create-fuels-counter-guide/test/ui")
+
 INVALID_FILES=$(grep -LE "@group\s+(node|browser|e2e|integration)" $FILES)
 
 if [ ! -z "$INVALID_FILES" ]; then
