0400ffea2a857cb4809165200ea683687ffe4fb6
Dhaiwat <dhaiwatpandya@gmail.com>
2024-09-06 20:48:06 +0530

    add comment

--- templates/vite/test/ui/test-ui.sh
+++ templates/vite/test/ui/test-ui.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script installs the necessary dependencies for the UI tests and runs them.
+
 pnpm exec playwright install --with-deps  > /dev/null 2>&1
 pnpm exec playwright test
 TEST_RESULT=$?
