493d7ad45f10ee6d6ee0b52165c64cf7d66114fd
chad <chad.nehemiah94@gmail.com>
2024-09-07 14:49:27 -0500

    restore relevant abi-coder files

--- packages/abi-coder/tsup.config.ts
+++ packages/abi-coder/tsup.config.ts
@@ -1,12 +1,3 @@
-import { tsupDefaults } from '@internal/tsup';
-import type { Options } from 'tsup';
+import { index } from '@internal/tsup';
 
-const configs: Options = {
-  ...tsupDefaults,
-  entry: {
-    index: 'src/index.ts',
-    profile: 'test/perf/profile.ts',
-  },
-};
-
-export default configs;
+export default index;
