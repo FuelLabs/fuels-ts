42f33b29ed26f3ddf805ac5b01799ea721ce2092
Dhaiwat <dhaiwatpandya@gmail.com>
2024-09-09 10:40:45 +0530

    fix test

--- packages/account/src/configs.test.ts
+++ packages/account/src/configs.test.ts
@@ -1,4 +1,4 @@
-import { DEVNET_NETWORK_URL } from '@internal/utils';
+import { DEVNET_NETWORK_URL, TESTNET_NETWORK_URL } from '@internal/utils';
 
 /**
  * @group node
@@ -12,6 +12,6 @@ describe('Configs', () => {
 
   it('exports TESTNET_NETWORK_URL', async () => {
     const configs = await import('./configs');
-    expect(configs.TESTNET_NETWORK_URL).toBe(DEVNET_NETWORK_URL);
+    expect(configs.TESTNET_NETWORK_URL).toBe(TESTNET_NETWORK_URL);
   });
 });
