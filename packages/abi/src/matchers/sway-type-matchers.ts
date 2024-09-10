8e59b8355b3f8ff24c2949eb013a2ac1fccf90b5
nedsalk <nedim.salkic@fuel.sh>
2024-09-10 10:33:26 +0200

    update generics

--- packages/abi/src/matchers/sway-type-matchers.ts
+++ packages/abi/src/matchers/sway-type-matchers.ts
@@ -35,20 +35,20 @@ const u64: Matcher = (type) => type === 'u64';
 const u256: Matcher = (type) => type === 'u256';
 const b256: Matcher = (type) => type === 'b256';
 
-export const genericRegEx = /^generic ([^\s]+)$/m;
+export const genericRegEx = /^generic.+$/;
 const generic: Matcher = (type) => genericRegEx.test(type);
 
-export const stringRegEx = /^str\[(.+)\]$/m;
+export const stringRegEx = /str\[(?<length>[0-9]+)\]/;
 const string: Matcher = (type) => stringRegEx.test(type);
 
-export const tupleRegEx = /^\([_,\s]+\)$/m;
+export const tupleRegEx = /^\((?<items>.*)\)$/m;
 const tuple: Matcher = (type) => tupleRegEx.test(type);
 
-export const arrayRegEx = /^\[_; ([0-9]+)\]$/m;
+export const arrayRegEx = /\[(?<item>[\w\s\\[\]]+);\s*(?<length>[0-9]+)\]/;
 const array: Matcher = (type) => arrayRegEx.test(type);
 
 const struct: Matcher = (type) =>
-  /^struct (.+::)?(.+)$/m.test(type) &&
+  /^struct .+$/m.test(type) &&
   !/^struct (std::.*)?(AssetId|B512|Vec|RawVec|EvmAddress|Bytes|String|RawBytes)$/m.test(type);
 const assetId: Matcher = (type) => type === 'struct std::asset_id::AssetId';
 const b512: Matcher = (type) => type === 'struct std::b512::B512';
@@ -59,8 +59,7 @@ const vector: Matcher = (type) => type === 'struct std::vec::Vec';
 
 const option: Matcher = (type) => type === 'enum std::option::Option';
 const result: Matcher = (type) => type === 'enum std::result::Result';
-const enumMatcher: Matcher = (type) =>
-  !option(type) && !result(type) && /^enum (.+::)?(.+)$/m.test(type);
+const enumMatcher: Matcher = (type) => !option(type) && !result(type) && /^enum .*$/m.test(type);
 
 const rawUntypedPtr: Matcher = (type) => type === 'raw untyped ptr';
 const rawUntypedSlice: Matcher = (type) => type === 'raw untyped slice';
