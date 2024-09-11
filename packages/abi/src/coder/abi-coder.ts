43f35b32f00725ac5fb5431c957d1e92922c7450
nedsalk <nedim.salkic@fuel.sh>
2024-09-11 09:39:54 +0200

    remove irrelevant changes

--- packages/abi/src/coder/abi-coder.ts
+++ packages/abi/src/coder/abi-coder.ts
@@ -1,11 +1,2 @@
-import { getCoder } from './encoding/v1/coder-matcher';
-import type { Coder } from './encoding/v1/coders/types';
-
 // Placeholder
-export class AbiCoder {
-  public encode(type: string, value: unknown) {
-    const coder = getCoder(type) as Coder & { encode: (value: unknown) => unknown };
-
-    return coder.encode(value);
-  }
-}
+export class AbiCoder {}
