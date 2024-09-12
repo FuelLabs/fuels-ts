deb38f2c4c4f7e1fec4aec9dbc2887969fc17301
Dhaiwat <dhaiwatpandya@gmail.com>
2024-09-12 11:13:28 +0530

    Update tests-template.sh

--- scripts/tests-template.sh
+++ scripts/tests-template.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd templates/vite/sway-programs && pnpm forc test
+cd templates/vite/sway-programs && pnpm fuels-forc test
 cd templates/nextjs/sway-programs && pnpm forc test
 cd apps/create-fuels-counter-guide/sway-programs && pnpm forc test
