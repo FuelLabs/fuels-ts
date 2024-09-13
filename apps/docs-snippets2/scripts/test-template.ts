00234d610826a94b8413df2f3c5f06a60b4c1ad1
Anderson Arboleya <anderson@arboleya.me>
2024-09-13 16:26:29 -0300

    Forcing assertion failures to throw

--- apps/docs-snippets2/scripts/test-template.ts
+++ apps/docs-snippets2/scripts/test-template.ts
@@ -1,7 +1,25 @@
+/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable no-global-assign */
+const consoleBkp = { ...console };
+
+afterAll(() => {
+  console = { ...consoleBkp };
+});
+
 test('%NAME%', async () => {
   // TEST NODE LAUNCHER ———>>>
   // %NODE_LAUNCHER%
   // <<<——— TEST NODE LAUNCHER
+
+  console = {
+    ...console,
+    assert(isTruthy, errorMsg) {
+      if (!isTruthy) {
+        throw new Error(errorMsg);
+      }
+    },
+  };
+
   // SNIPPET ———>>>
   // %SNIPPET%
   // <<<——— SNIPPET
