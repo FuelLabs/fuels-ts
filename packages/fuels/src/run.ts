431990d7fd4965c8823d1bfb57f44cf784565545
Dhaiwat <dhaiwatpandya@gmail.com>
2024-09-16 18:08:37 +0530

    chore: inform users if their `fuels` version is outdated (#3108)
    * test `reportUserVersion`
    
    * move util to `versions` package
    
    * export via cli and not index
    
    * log fuels version
    
    * fetch latest version
    
    * log out comparison
    
    * add changeset
    
    * fix tests
    
    * disable pr release
    
    * add tests
    
    * re-use `getBuiltinVersions`
    
    * move to `fuels` CLI from `versions`
    
    * revert test changes
    
    * revert tests
    
    * add tests
    
    * add testing group
    
    * add changeset
    
    * add a test case
    
    * print a msg if fetch fails
    
    * add error msg in one more place
    
    * fix test
    
    ---------
    
    Co-authored-by: Chad Nehemiah <chad.nehemiah94@gmail.com>

--- packages/fuels/src/run.ts
+++ packages/fuels/src/run.ts
@@ -1,6 +1,8 @@
 import { configureCli } from './cli';
+import { checkForAndDisplayUpdates } from './cli/utils/checkForAndDisplayUpdates';
+import { error } from './cli/utils/logger';
 
 export const run = async (argv: string[]) => {
   const program = configureCli();
-  return program.parseAsync(argv);
+  return Promise.all([await checkForAndDisplayUpdates().catch(error), program.parseAsync(argv)]);
 };
