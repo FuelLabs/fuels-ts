40a5a1aa10df4c04abbef8beb2a84eda01b54da0
Peter Smith <peter@blueoceancomputing.co.uk>
2024-09-16 21:10:22 +0100

    chore: consistent matcher type

--- packages/abi/src/matchers/sway-type-matchers.ts
+++ packages/abi/src/matchers/sway-type-matchers.ts
@@ -24,46 +24,49 @@ export type SwayType =
   | 'rawUntypedPtr' // might not need it
   | 'rawUntypedSlice'; // might not need it
 
-type Matcher = (type: string) => boolean;
+export type Matcher<T = boolean> = (opts: { swayType: string }) => T;
 
-const voidMatcher: Matcher = (type) => type === '()';
-const bool: Matcher = (type) => type === 'bool';
-const u8: Matcher = (type) => type === 'u8';
-const u16: Matcher = (type) => type === 'u16';
-const u32: Matcher = (type) => type === 'u32';
-const u64: Matcher = (type) => type === 'u64';
-const u256: Matcher = (type) => type === 'u256';
-const b256: Matcher = (type) => type === 'b256';
+const voidMatcher: Matcher = ({ swayType }) => swayType === '()';
+const bool: Matcher = ({ swayType }) => swayType === 'bool';
+const u8: Matcher = ({ swayType }) => swayType === 'u8';
+const u16: Matcher = ({ swayType }) => swayType === 'u16';
+const u32: Matcher = ({ swayType }) => swayType === 'u32';
+const u64: Matcher = ({ swayType }) => swayType === 'u64';
+const u256: Matcher = ({ swayType }) => swayType === 'u256';
+const b256: Matcher = ({ swayType }) => swayType === 'b256';
 
 export const GENERIC_REGEX = /^generic ([^\s]+)$/m;
-const generic: Matcher = (type) => GENERIC_REGEX.test(type);
+const generic: Matcher = ({ swayType }) => GENERIC_REGEX.test(swayType);
 
 export const STRING_REGEX = /^str\[(?<length>[0-9]+)\]/;
-const string: Matcher = (type) => STRING_REGEX.test(type);
+const string: Matcher = ({ swayType }) => STRING_REGEX.test(swayType);
 
 export const TUPLE_REGEX = /^\((?<items>.+)\)$/m;
-const tuple: Matcher = (type) => TUPLE_REGEX.test(type);
+const tuple: Matcher = ({ swayType }) => TUPLE_REGEX.test(swayType);
 
 export const ARRAY_REGEX = /^\[(?<item>[\w\s\\[\]]+);\s*(?<length>[0-9]+)\]/;
-const array: Matcher = (type) => ARRAY_REGEX.test(type);
+const array: Matcher = ({ swayType }) => ARRAY_REGEX.test(swayType);
 
 const STRUCT_REGEX = /^struct .+$/m;
 const STRUCT_STD_REGEX =
   /^struct std::.*(AssetId|B512|Vec|RawVec|EvmAddress|Bytes|String|RawBytes)$/m;
-const struct: Matcher = (type) => STRUCT_REGEX.test(type) && !STRUCT_STD_REGEX.test(type);
-const assetId: Matcher = (type) => type === 'struct std::asset_id::AssetId';
-const b512: Matcher = (type) => type === 'struct std::b512::B512';
-const bytes: Matcher = (type) => type === 'struct std::bytes::Bytes';
-const evmAddress: Matcher = (type) => type === 'struct std::vm::evm::evm_address::EvmAddress';
-const stdString: Matcher = (type) => type === 'struct std::string::String';
-const vector: Matcher = (type) => type === 'struct std::vec::Vec';
+const struct: Matcher = ({ swayType }) =>
+  STRUCT_REGEX.test(swayType) && !STRUCT_STD_REGEX.test(swayType);
+const assetId: Matcher = ({ swayType }) => swayType === 'struct std::asset_id::AssetId';
+const b512: Matcher = ({ swayType }) => swayType === 'struct std::b512::B512';
+const bytes: Matcher = ({ swayType }) => swayType === 'struct std::bytes::Bytes';
+const evmAddress: Matcher = ({ swayType }) =>
+  swayType === 'struct std::vm::evm::evm_address::EvmAddress';
+const stdString: Matcher = ({ swayType }) => swayType === 'struct std::string::String';
+const vector: Matcher = ({ swayType }) => swayType === 'struct std::vec::Vec';
 
-const option: Matcher = (type) => type === 'enum std::option::Option';
-const result: Matcher = (type) => type === 'enum std::result::Result';
-const enumMatcher: Matcher = (type) => !option(type) && !result(type) && /^enum .*$/m.test(type);
+const option: Matcher = ({ swayType }) => swayType === 'enum std::option::Option';
+const result: Matcher = ({ swayType }) => swayType === 'enum std::result::Result';
+const enumMatcher: Matcher = ({ swayType }) =>
+  !option({ swayType }) && !result({ swayType }) && /^enum .*$/m.test(swayType);
 
-const rawUntypedPtr: Matcher = (type) => type === 'raw untyped ptr';
-const rawUntypedSlice: Matcher = (type) => type === 'raw untyped slice';
+const rawUntypedPtr: Matcher = ({ swayType }) => swayType === 'raw untyped ptr';
+const rawUntypedSlice: Matcher = ({ swayType }) => swayType === 'raw untyped slice';
 
 export const swayTypeMatchers: Record<SwayType, Matcher> = {
   void: voidMatcher,
@@ -98,12 +101,12 @@ export const swayTypeMatchers: Record<SwayType, Matcher> = {
 
 const swayTypeMatcherEntries = Object.entries(swayTypeMatchers);
 
-export function createMatcher<T>(mappings: Record<SwayType, T>) {
+export function createMatcher<T>(mappings: Record<SwayType, T>): Matcher<T> {
   return (opts: { swayType: string }): T => {
     const { swayType } = opts;
 
     for (const [key, matcher] of swayTypeMatcherEntries) {
-      if (matcher(swayType)) {
+      if (matcher({ swayType })) {
         if (key in mappings) {
           return mappings[key as SwayType];
         }
