bf080cdeeb21b101a06199bb4bd4bd366ddaff32
Daniel Bate <djbate23@gmail.com>
2024-09-16 09:52:51 +0100

    chore: fix hook

--- templates/vite/fuels.config.ts
+++ templates/vite/fuels.config.ts
@@ -1,14 +1,11 @@
 import { createConfig } from 'fuels';
 import dotenv from 'dotenv';
 
-import { useEnvironment } from './src/hooks/useEnvironment';
+import { providerUrl } from './src/lib';
 
 dotenv.config({
   path: ['.env.local', '.env'],
 });
-
-// eslint-disable-next-line react-hooks/rules-of-hooks
-const { providerUrl } = useEnvironment();
 
 // If your node is running on a port other than 4000, you can set it here
 const fuelCorePort = +(process.env.VITE_FUEL_NODE_PORT as string) || 4000;
