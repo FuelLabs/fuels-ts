2861ef9743e3b0b64da5072a85638ed343f65c7c
Peter Smith <peter@blueoceancomputing.co.uk>
2024-09-17 06:42:41 +0100

    feat: added b256 and b512 tests

---
+++ packages/fuel-gauge/src/abi/encoding/b256.test.ts
@@ -0,0 +1,90 @@
+import type { AbiSpecificationV1 } from '@fuel-ts/abi';
+import { AbiCoder } from '@fuel-ts/abi';
+import { FuelError, toBytes } from 'fuels';
+import { expectToThrowFuelError } from 'fuels/test-utils';
+
+import { B256_DECODED, B256_ENCODED } from '../constants';
+import { AbiProjectsEnum, getAbiForcProject } from '../utils';
+
+const { abiContents: contractAbi } = getAbiForcProject(AbiProjectsEnum.ABI_CONTRACT);
+const contract = AbiCoder.fromAbi(contractAbi as AbiSpecificationV1);
+
+describe('types_b256', () => {
+  const fn = contract.functions.types_b256;
+
+  describe('encode', () => {
+    it('should encode value [valid b256 hex]', () => {
+      const value = B256_DECODED;
+      const expected = B256_ENCODED;
+
+      const encoded = fn.output.encode(value);
+
+      expect(encoded).toStrictEqual(expected);
+    });
+
+    it.todo('should fail to encode value [too short]', async () => {
+      const value = B256_DECODED.slice(-1);
+
+      await expectToThrowFuelError(
+        () => fn.output.encode(value),
+        new FuelError(FuelError.CODES.ENCODE_ERROR, 'Invalid b256 value.')
+      );
+    });
+
+    it.todo('should fail to encode value [too long]', async () => {
+      const value = `${B256_DECODED}PLUS`;
+
+      await expectToThrowFuelError(
+        () => fn.output.encode(value),
+        new FuelError(FuelError.CODES.ENCODE_ERROR, 'Invalid b256 value.')
+      );
+    });
+
+    it.todo('should fail to encode value [not a hex]', async () => {
+      const value = `not a hex value`;
+
+      await expectToThrowFuelError(
+        () => fn.output.encode(value),
+        new FuelError(FuelError.CODES.ENCODE_ERROR, 'Invalid b256 value.')
+      );
+    });
+  });
+
+  describe('decode', () => {
+    it('should decode value [valid b256 hex]', () => {
+      const value = B256_ENCODED;
+      const expected = B256_DECODED;
+
+      const encoded = fn.output.decode(value);
+
+      expect(encoded).toStrictEqual(expected);
+    });
+
+    it.todo('should fail to decode value [too short]', async () => {
+      const value = B256_ENCODED.slice(-1);
+
+      await expectToThrowFuelError(
+        () => fn.output.decode(value),
+        new FuelError(FuelError.CODES.DECODE_ERROR, 'Invalid b256 value.')
+      );
+    });
+
+    it.todo('should fail to decode value [too long]', async () => {
+      const value = new Uint8Array([...B256_ENCODED, 0]);
+
+      await expectToThrowFuelError(
+        () => fn.output.decode(value),
+        new FuelError(FuelError.CODES.DECODE_ERROR, 'Invalid b256 value.')
+      );
+    });
+
+    it.todo('should fail to decode value [not hex]', async () => {
+      const value = toBytes('not a hex value');
+
+      await expectToThrowFuelError(
+        () => fn.output.decode(value),
+        new FuelError(FuelError.CODES.DECODE_ERROR, 'Invalid b256 value.')
+      );
+    });
+  });
+});
---
+++ packages/fuel-gauge/src/abi/encoding/b512.test.ts
@@ -0,0 +1,90 @@
+import type { AbiSpecificationV1 } from '@fuel-ts/abi';
+import { AbiCoder } from '@fuel-ts/abi';
+import { FuelError, toBytes } from 'fuels';
+import { expectToThrowFuelError } from 'fuels/test-utils';
+
+import { B512_DECODED, B512_ENCODED } from '../constants';
+import { AbiProjectsEnum, getAbiForcProject } from '../utils';
+
+const { abiContents: contractAbi } = getAbiForcProject(AbiProjectsEnum.ABI_CONTRACT);
+const contract = AbiCoder.fromAbi(contractAbi as AbiSpecificationV1);
+
+describe('types_b512', () => {
+  const fn = contract.functions.types_b512;
+
+  describe('encode', () => {
+    it('should encode value [valid b512 hex]', () => {
+      const value = B512_DECODED;
+      const expected = B512_ENCODED;
+
+      const encoded = fn.output.encode(value);
+
+      expect(encoded).toStrictEqual(expected);
+    });
+
+    it.todo('should fail to encode value [too short]', async () => {
+      const value = B512_DECODED.slice(-1);
+
+      await expectToThrowFuelError(
+        () => fn.output.encode(value),
+        new FuelError(FuelError.CODES.ENCODE_ERROR, 'Invalid b512 value.')
+      );
+    });
+
+    it.todo('should fail to encode value [too long]', async () => {
+      const value = `${B512_DECODED}PLUS`;
+
+      await expectToThrowFuelError(
+        () => fn.output.encode(value),
+        new FuelError(FuelError.CODES.ENCODE_ERROR, 'Invalid b512 value.')
+      );
+    });
+
+    it.todo('should fail to encode value [not a hex]', async () => {
+      const value = `not a hex value`;
+
+      await expectToThrowFuelError(
+        () => fn.output.encode(value),
+        new FuelError(FuelError.CODES.ENCODE_ERROR, 'Invalid b512 value.')
+      );
+    });
+  });
+
+  describe('decode', () => {
+    it('should decode value [valid b512 hex]', () => {
+      const value = B512_ENCODED;
+      const expected = B512_DECODED;
+
+      const encoded = fn.output.decode(value);
+
+      expect(encoded).toStrictEqual(expected);
+    });
+
+    it.todo('should fail to decode value [too short]', async () => {
+      const value = B512_ENCODED.slice(-1);
+
+      await expectToThrowFuelError(
+        () => fn.output.decode(value),
+        new FuelError(FuelError.CODES.DECODE_ERROR, 'Invalid b512 value.')
+      );
+    });
+
+    it.todo('should fail to decode value [too long]', async () => {
+      const value = new Uint8Array([...B512_ENCODED, 0]);
+
+      await expectToThrowFuelError(
+        () => fn.output.decode(value),
+        new FuelError(FuelError.CODES.DECODE_ERROR, 'Invalid b512 value.')
+      );
+    });
+
+    it.todo('should fail to decode value [not hex]', async () => {
+      const value = toBytes('not a hex value');
+
+      await expectToThrowFuelError(
+        () => fn.output.decode(value),
+        new FuelError(FuelError.CODES.DECODE_ERROR, 'Invalid b512 value.')
+      );
+    });
+  });
+});
