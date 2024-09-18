240f40c9a4667e72c6d212715bfdf6f91bac6108
Dhaiwat <dhaiwatpandya@gmail.com>
2024-09-18 15:36:04 +0530

    debug

--- packages/create-fuels/src/cli.ts
+++ packages/create-fuels/src/cli.ts
@@ -139,17 +139,18 @@ export const runScaffoldCli = async ({
 
   fileCopySpinner.succeed('Copied template files!');
 
-  // if (opts.install) {
-  //   const installDepsSpinner = ora({
-  //     text: 'Installing dependencies..',
-  //     color: 'green',
-  //   }).start();
+  if (opts.install) {
+    const installDepsSpinner = ora({
+      text: 'Installing dependencies..',
+      color: 'green',
+    }).start();
 
-  //   process.chdir(projectPath);
-  //   execSync(packageManager.install, { stdio: verboseEnabled ? 'inherit' : 'pipe' });
+    process.chdir(projectPath);
+    // execSync(packageManager.install, { stdio: verboseEnabled ? 'inherit' : 'pipe' });
+    execSync('pnpm install');
 
-  //   installDepsSpinner.succeed('Installed dependencies!');
-  // }
+    installDepsSpinner.succeed('Installed dependencies!');
+  }
 
   // Remove typegen files from gitignore
   const gitignorePath = join(projectPath, '.gitignore');
