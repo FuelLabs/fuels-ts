5adcfa2748ce63cfc0a4b76ddbb6a84d8206f3e7
Anderson Arboleya <anderson@arboleya.me>
2024-09-20 18:06:46 -0300

    DRY

--- packages/fuels/scripts/build-proxy-contract.tsx
+++ packages/fuels/scripts/build-proxy-contract.tsx
@@ -18,11 +18,14 @@ const FACTORY_PATH = `src/cli/commands/deploy/proxy/types/Src14OwnedProxyFactory
 const proxyContents = readFileSync(PROXY_PATH, "utf-8");
 const contentsContents = readFileSync(FACTORY_PATH, "utf-8");
 
+const relativeIndexPath = `"../../../../..";`;
+
 writeFileSync(
   PROXY_PATH,
-  proxyContents.replace(/("|')fuels("|');/g, `"../../../../..";`),
+  proxyContents.replace(/("|')fuels("|');/g, relativeIndexPath),
 );
+
 writeFileSync(
   FACTORY_PATH,
-  contentsContents.replace(/"fuels";/g, `"../../../../..";`),
+  contentsContents.replace(/"fuels";/g, relativeIndexPath),
 );
