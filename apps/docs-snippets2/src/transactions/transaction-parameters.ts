c35939a860cb2564d95dc2ea546ceddb55c3f443
Anderson Arboleya <anderson@arboleya.me>
2024-09-25 07:45:02 -0300

    Nit II

--- apps/docs-snippets2/src/transactions/transaction-parameters.ts
+++ apps/docs-snippets2/src/transactions/transaction-parameters.ts
@@ -51,9 +51,9 @@ const {
   transactionResult: { isStatusSuccess },
 } = await waitForResult();
 
+console.log('Transaction request', transactionRequest);
 console.log('Transaction status', isStatusSuccess);
 console.log('Transaction value', value);
-console.log('Transaction request', transactionRequest);
 
 // #endregion transaction-parameters-8
 // #endregion full
