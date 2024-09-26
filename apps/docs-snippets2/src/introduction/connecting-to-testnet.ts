fff7047f07660f1eb9b506e6c9ab3eae5d74c06a
Anderson Arboleya <anderson@arboleya.me>
2024-09-26 07:45:56 -0300

    feat: new infra for code snippets (#2651)
    * Starting new code-snippets app
    
    * Porting getting-started snippets
    
    * Ignoring new app in changesets
    
    * Adding changeset
    
    * Ignoring wrapped snippets files (generated)
    
    * Ignore test files trying to import files that haven’t been built yet
    
    * Using real code/file rather than code-string
    
    * Adding example using `launchTestNode` and `LOCAL_NETWORK_URL` overrides
    
    * Skipping until forc workspace has at least one member
    
    * Renaming variable
    
    * Fixing regex
    
    * Ignoring lint issues individually
    
    * Removing from browser group
    
    * Lintfix
    
    * Restoring console after snippet
    
    * Keeping wrapper free from weird hacks
    
    * Adding mock utility
    
    * Using mocking utility
    
    * Validating if `nertworkUrl` was properly mocked
    
    * Mock provider instead?
    
    * Mocking only for localhost
    
    * Adjusting snippets
    
    * Wrapping snippet in a try/catch
    
    * Adding blank spaces
    
    * Removing mocking utility
    
    * Adding pass-through options for `console.log` calls
    
    * Adding new slot for injecting test node launcher into wrapped snippet
    
    * Injecting node launcher for snippets using `LOCAL_NETWORK_URL`
    
    * Trim snippets
    
    * Cleanup snippets tests
    
    * Stop ignoring test for `docs-snippets2`
    
    * Re-adding `docs-snippets2` tests to knip ignore list
    
    * Using internal builds
    
    * Fixing ignore rule
    
    * Adding basic counter Sway project
    
    * Improving scripts
    
    * Adding sample env file
    
    * Standardizing snippets and tests
    
    * Validate env constants existence
    
    * Drafting new snippet
    
    * Adjusting script command
    
    * Removing unnecessary wallet
    
    * Extacting launcher into another file
    
    * New API draft
    
    * Syncing env and launcher template
    
    * Better docs
    
    * Adding new sway script
    
    * Removing API draft
    
    * Fixing file location
    
    * Add script to workspace
    
    * Improve snippet
    
    * Using new snippets
    
    * Add new API draft
    
    * Matching extra blankspace
    
    * Ignoring env file
    
    * Adding region marks
    
    * Avoid repetition
    
    * Trim env consts
    
    * Cleanup
    
    * Playing around new API
    
    * Tidying up
    
    * Ignoring experimental file
    
    * Ignoring generated files in eslint
    
    * Adjusting wallets count
    
    * Moving chunk around
    
    * Adjusting ignore rule
    
    * Improving imports manipulation
    
    * Adding comments
    
    * Adjusting template markers
    
    * Tyop
    
    * Update draft
    
    * Update title
    
    * Adjust deployment usage
    
    * Updating example
    
    * Removing temp draft
    
    * Removing temp draft section
    
    * Updating broken test
    
    * Using `NETWORK_URL` across the board
    
    * Reword comment
    
    * Adding scripts dir to tsconfig
    
    * Starting with more wallets
    
    * Automatically generating tests for code snippets
    
    * Forcing assertion failures to throw
    
    * WIP
    
    * Changing back to `console.log`
    
    * Adding testing groups to template
    
    * Renaming variable
    
    * Removing redundancy in template handling
    
    * Upgrading dependency
    
    * Updating lock file
    
    * Lintfix - keeping eslint disable at the top
    
    * Normalizing language across all snippets
    
    * Nit
    
    * Nit II
    
    * Fixing package names
    
    * Adjusting ignored package names
    
    * Deleting obsolete files
    
    ---------
    
    Co-authored-by: Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>

---
+++ apps/docs-snippets2/src/introduction/connecting-to-testnet.ts
@@ -0,0 +1,12 @@
+// #region main
+import { Provider, Wallet } from 'fuels';
+
+const ADDRESS = '0x767caf5b08eba21c561078a4d5be09bbd7f16b9eca22699a61f1edd9e456126f';
+
+const provider = await Provider.create('https://testnet.fuel.network/v1/graphql');
+const wallet = Wallet.fromAddress(ADDRESS, provider);
+
+const { balances } = await wallet.getBalances();
+
+console.log('Balances', balances);
+// #endregion main
