f7bacd106b0f32d42e9583a14a24acf483f7db08
Daniel Bate <djbate23@gmail.com>
2024-09-27 14:51:45 +0100

    feat: `create fuels` template revamp (#3125)
    Co-authored-by: Daniel Bate <djbate23@gmail.com>
    Co-authored-by: Dhaiwat <dhaiwatpandya@gmail.com>

--- templates/vite/eslint.config.js
+++ templates/vite/eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config({
   extends: [js.configs.recommended, ...tseslint.configs.recommended],
@@ -18,9 +18,6 @@ export default tseslint.config({
   },
   rules: {
     ...reactHooks.configs.recommended.rules,
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
-})
+});
