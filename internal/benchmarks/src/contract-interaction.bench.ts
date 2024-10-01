301555508f1b4701f48c19747b86c5dfa6a2e8d4
Chad Nehemiah <chad.nehemiah94@gmail.com>
2024-10-01 13:36:51 -0500

    chore: run benchmarking utility in devnet environment (#3166)

--- internal/benchmarks/src/contract-interaction.bench.ts
+++ internal/benchmarks/src/contract-interaction.bench.ts
@@ -1,12 +1,19 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
-import type { WalletUnlocked } from 'fuels';
-import { bn } from 'fuels';
+import { DEVNET_NETWORK_URL } from '@internal/utils';
+import { WalletUnlocked, bn, Provider } from 'fuels';
 import { launchTestNode, TestAssetId } from 'fuels/test-utils';
 import { bench } from 'vitest';
 
 import type { CounterContract, CallTestContract } from '../test/typegen/contracts';
-import { CounterContractFactory, CallTestContractFactory } from '../test/typegen/contracts';
+import {
+  CounterContractFactory,
+  CallTestContractFactory,
+  LargeContractFactory,
+} from '../test/typegen/contracts';
+
+import { isDevnet, runBenchmark } from './config';
+
 /**
  * @group node
  * @group browser
@@ -16,55 +23,82 @@ describe('Contract Interaction Benchmarks', () => {
   let callTestContract: CallTestContract;
   let wallet: WalletUnlocked;
   let cleanup: () => void;
-  beforeEach(async () => {
-    const launched = await launchTestNode({
-      contractsConfigs: [{ factory: CounterContractFactory }, { factory: CallTestContractFactory }],
-    });
 
-    cleanup = launched.cleanup;
-    contract = launched.contracts[0];
-    callTestContract = launched.contracts[1];
-    wallet = launched.wallets[0];
+  const setupTestEnvironment = async () => {
+    if (isDevnet) {
+      const provider = await Provider.create(DEVNET_NETWORK_URL);
+      wallet = new WalletUnlocked(process.env.DEVNET_WALLET_PVT_KEY as string, provider);
+
+      const { waitForResult } = await CounterContractFactory.deploy(wallet);
+      contract = (await waitForResult()).contract;
+
+      const { waitForResult: waitForResultCallTestContract } =
+        await CallTestContractFactory.deploy(wallet);
+      callTestContract = (await waitForResultCallTestContract()).contract;
+    } else {
+      const launched = await launchTestNode({
+        contractsConfigs: [
+          { factory: CounterContractFactory },
+          { factory: CallTestContractFactory },
+        ],
+      });
+
+      cleanup = launched.cleanup;
+      contract = launched.contracts[0];
+      callTestContract = launched.contracts[1];
+      wallet = launched.wallets[0];
+    }
+  };
+
+  beforeAll(setupTestEnvironment);
+
+  afterAll(() => {
+    if (!isDevnet && cleanup) {
+      cleanup();
+    }
   });
 
-  afterEach(() => {
-    cleanup();
-  });
-
-  bench('should successfully execute a contract read function', async () => {
+  runBenchmark('should successfully execute a contract read function', async () => {
     const tx = await contract.functions.get_count().call();
-
     const { value } = await tx.waitForResult();
-
-    expect(JSON.stringify(value)).toEqual(JSON.stringify(bn(0)));
+    expect(value).toBeDefined();
   });
 
-  bench('should successfully execute a contract multi call', async () => {
+  runBenchmark('should successfully execute a contract multi call', async () => {
+    const initialValue = 100;
     const tx = await contract
-      .multiCall([contract.functions.increment_counter(100), contract.functions.get_count()])
+      .multiCall([
+        contract.functions.increment_counter(initialValue),
+        contract.functions.get_count(),
+      ])
       .call();
-
     const { value } = await tx.waitForResult();
-
-    expect(JSON.stringify(value)).toEqual(JSON.stringify([bn(100), bn(100)]));
+    expect(value).toBeDefined();
   });
 
-  bench('should successfully write to a contract', async () => {
+  runBenchmark('should successfully write to a contract', async () => {
     const tx = await contract.functions.increment_counter(100).call();
     await tx.waitForResult();
   });
 
-  bench('should successfully execute a contract mint', async () => {
+  runBenchmark('should successfully execute a contract mint', async () => {
     const tx = await callTestContract.functions.mint_coins(TestAssetId.A.value, bn(100)).call();
-
     await tx.waitForResult();
   });
 
-  bench('should successfully execute a contract deploy', async () => {
+  runBenchmark('should successfully execute a contract deploy', async () => {
     const factory = new CounterContractFactory(wallet);
     const { waitForResult } = await factory.deploy();
     const { contract: deployedContract } = await waitForResult();
+    expect(deployedContract).toBeDefined();
+  });
 
+  bench('should successfully execute a contract deploy as blobs', async () => {
+    const factory = new LargeContractFactory(wallet);
+    const { waitForResult } = await factory.deployAsBlobTx({
+      chunkSizeMultiplier: 0.9,
+    });
+    const { contract: deployedContract } = await waitForResult();
     expect(deployedContract).toBeDefined();
   });
 });
