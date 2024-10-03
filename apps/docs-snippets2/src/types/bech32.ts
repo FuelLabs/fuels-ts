c73f80926b8cb0ccc1769571b37b9b67cc8e11bf
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-10-03 15:24:45 -0300

    improve bech32 doc snippets

--- apps/docs-snippets2/src/types/bech32.ts
+++ apps/docs-snippets2/src/types/bech32.ts
@@ -1,15 +1,16 @@
+// #region bech32-2
 import { Address } from 'fuels';
 
-// #region bech32-2
 const address = Address.fromRandom();
 
-console.log('Bech32 address', address.bech32Address.toString());
+console.log('Bech32', address.bech32Address.toString());
 
 // fuel1d5cfwekq78r0zq73g7eg0747etkaxxltrqx5tncm7lvg89awe3hswhqjhs
 // #endregion bech32-2
 
 // #region addresses-1
-const bech32 = 'fuel1d5cfwekq78r0zq73g7eg0747etkaxxltrqx5tncm7lvg89awe3hswhqjhs';
+const bech32 =
+  'fuel1d5cfwekq78r0zq73g7eg0747etkaxxltrqx5tncm7lvg89awe3hswhqjhs';
 // #endregion addresses-1
 
-expect(bech32).toBe('fuel1d5cfwekq78r0zq73g7eg0747etkaxxltrqx5tncm7lvg89awe3hswhqjhs');
+console.log('bech32', bech32);
