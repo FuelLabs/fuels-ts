33e1ea171c49fdebba49ad1334ea747bed6b1e53
nedsalk <nedim.salkic@fuel.sh>
2024-10-04 15:30:50 +0200

    refactor

--- packages/abi/src/gen/abi-gen.ts
+++ packages/abi/src/gen/abi-gen.ts
@@ -1,6 +1,3 @@
-import { writeFile } from 'fs';
-import { join } from 'path';
-
 import { getRenderer } from './renderers/getRenderer';
 import type { ProgramDetails } from './utils/get-program-details';
 
@@ -15,25 +12,8 @@ export interface AbiGenResult {
 }
 
 export class AbiGen {
-  public results: AbiGenResult[];
-  constructor({ programDetails, mode }: AbiGenInput) {
+  public static generate({ programDetails, mode }: AbiGenInput) {
     const render = getRenderer(mode);
-    this.results = render(programDetails);
-  }
-
-  /**
-   * Writes the `AbiGen` results onto the file system.
-   *
-   * @param outputDir directory to output `AbiGen` results in.
-   */
-  public async writeResults(outputDir: string) {
-    const writePromises = this.results.map(({ filename, content }) => {
-      const filePath = join(outputDir, filename);
-      return writeFile(filePath, content, (err) => {
-        throw Error();
-      });
-    });
-
-    await Promise.all(writePromises);
+    return render(programDetails);
   }
 }
