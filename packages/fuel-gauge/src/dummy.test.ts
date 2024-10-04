623614a6c83cab774b46fd5b3b00ad4d40dce940
chad <chad.nehemiah94@gmail.com>
2024-10-04 18:37:09 -0500

    chore: add test for configurable scripts Co-authored-by: Sergio Torres <30977845+Torres-ssf@users.noreply.github.com>

--- packages/fuel-gauge/src/dummy.test.ts
+++ packages/fuel-gauge/src/dummy.test.ts
@@ -1,5 +1,5 @@
 import type { TransactionScript } from 'fuels';
-import { ContractFactory, hexlify } from 'fuels';
+import { ContractFactory, hexlify, Script } from 'fuels';
 import { launchTestNode } from 'fuels/test-utils';
 
 import { ScriptDummy } from '../test/typegen';
@@ -55,5 +55,37 @@ describe('first try', () => {
     console.log('transaciton result receipts for set config: ', transactionResult.receipts);
 
     expect(scriptBytes).not.equal(actualBytes);
+  });
+
+  it('Should call another script after deploying script with configurable using script program', async () => {
+    using launch = await launchTestNode();
+
+    const {
+      wallets: [wallet],
+    } = launch;
+
+    const factory = new ContractFactory(ScriptDummy.bytecode, ScriptDummy.abi, wallet);
+    const configurable = {
+      PIN: 1000,
+    };
+    const { waitForResult } = await factory.deployAsBlobTxForScript({
+      configurableConstants: configurable,
+    });
+
+    const { transactionResult } = await waitForResult();
+
+    const script = new Script(ScriptDummy.bytecode, ScriptDummy.abi, wallet);
+
+    const otherConfigurable = {
+      PIN: 4592,
+    };
+
+    script.setConfigurableConstants(otherConfigurable);
+
+    const { waitForResult: waitForResult2 } = await script.functions.main().call();
+
+    const { transactionResult: transactionResult2 } = await waitForResult2();
+
+    console.log('transaciton result receipts for third test config: ', transactionResult2.logs);
   });
 });
