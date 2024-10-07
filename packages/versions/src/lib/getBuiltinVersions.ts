9cb84fb9a289dfb412a98d8c53f06fd877e4adaf
nedsalk <nedim.salkic@fuel.sh>
2024-10-07 09:30:38 +0200

    fix builtin versions

--- packages/versions/src/lib/getBuiltinVersions.ts
+++ packages/versions/src/lib/getBuiltinVersions.ts
@@ -2,7 +2,7 @@ import type { Versions } from './types';
 
 export function getBuiltinVersions(): Versions {
   return {
-    FORC: '0.64.0',
+    FORC: '0.65.1',
     FUEL_CORE: '0.37.1',
     FUELS: '0.94.9',
   };
