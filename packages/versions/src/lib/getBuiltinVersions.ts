d4e839f238ba3625689c80ec6401816e946ae04c
Daniel Bate <djbate23@gmail.com>
2024-10-07 15:37:11 +0700

    chore: `fuel-core@0.37.1`, `forc@0.65.1`, and `create-fuels` toolchains (#3265)
    * chore: upgrade fuel-core
    
    * chore: changeset
    
    * chore: upgrade `forc` and all toolchain versions to `testnet` (#3267)
    
    update all toolchain versions
    
    * chore: changeset update
    
    * Update afraid-pets-drive.md
    
    * fix builtin versions
    
    * fix test
    
    * ignore all forc builds, loaders included
    
    * chore: update changeset
    
    Co-authored-by: Peter Smith <peter@blueoceancomputing.co.uk>
    
    ---------
    
    Co-authored-by: Nedim Salkić <nedim.salkic@fuel.sh>
    Co-authored-by: Peter Smith <peter@blueoceancomputing.co.uk>

--- packages/versions/src/lib/getBuiltinVersions.ts
+++ packages/versions/src/lib/getBuiltinVersions.ts
@@ -2,8 +2,8 @@ import type { Versions } from './types';
 
 export function getBuiltinVersions(): Versions {
   return {
-    FORC: '0.64.0',
-    FUEL_CORE: '0.37.0',
+    FORC: '0.65.1',
+    FUEL_CORE: '0.37.1',
     FUELS: '0.94.9',
   };
 }
