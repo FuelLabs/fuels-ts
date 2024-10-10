cb8fb8c1894146d575215d4188110240333ac251
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-10-10 12:46:34 -0300

    rollback change proxy contract

--- packages/fuels/src/cli/commands/deploy/proxy/types/Src14OwnedProxyFactory.ts
+++ packages/fuels/src/cli/commands/deploy/proxy/types/Src14OwnedProxyFactory.ts
@@ -23,7 +23,7 @@ export class Src14OwnedProxyFactory extends ContractFactory {
     super(bytecode, Src14OwnedProxy.abi, accountOrProvider);
   }
 
-  override deploy<TContract extends Contract = Contract>(
+  deploy<TContract extends Contract = Contract>(
     deployOptions?: DeployContractOptions
   ): Promise<DeployContractResult<TContract>> {
     return super.deploy({
