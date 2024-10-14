47044a7f4fcc0ac93a61466aa9c6790ccc5d7ccb
Anderson Arboleya <anderson@arboleya.me>
2024-10-14 20:56:21 -0300

    Upgrading `fuel-core` to `0.40.0`

--- packages/versions/src/lib/getBuiltinVersions.ts
+++ packages/versions/src/lib/getBuiltinVersions.ts
@@ -3,7 +3,7 @@ import type { Versions } from './types';
 export function getBuiltinVersions(): Versions {
   return {
     FORC: '0.65.2',
-    FUEL_CORE: '0.39.0',
+    FUEL_CORE: '0.40.0',
     FUELS: '0.96.1',
   };
 }
