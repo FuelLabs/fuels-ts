3fcf960f9b2e2fc14bd12ebd5204fb7b8545376a
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-10-16 14:59:10 -0300

    remove unused snippet files

--- apps/docs-snippets2/src/types/evm-address.ts
+++
@@ -1,9 +0,0 @@
-// #region addresses-3
-import type { EvmAddress } from 'fuels';
-
-const evmAddress: EvmAddress = {
-  bits: '0x000000000000000000000000210cf886ce41952316441ae4cac35f00f0e882a6',
-};
-// #endregion addresses-3
-
-console.log('evmAddress', evmAddress);
