36a22e5ed69dc87f2c0b8f3c31461e484b4334ab
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-10-16 11:17:33 -0300

    rename addresses snippets files

--- apps/docs-snippets2/src/types/address/creating-an-address.ts
+++ apps/docs-snippets2/src/types/address/creating-an-address.ts
@@ -1,9 +1,6 @@
 // #region full
 import { Address } from 'fuels';
 
-const ADDRESS_CHECKSUM =
-  '0xcfe7b143Aca69a0984b481ab950716047586772586475AEE82fb10B719d52A76';
-
 const ADDRESS_BECH32 =
   'fuel1elnmzsav56dqnp95sx4e2pckq36cvae9ser44m5zlvgtwxw49fmqd7e42e';
 
