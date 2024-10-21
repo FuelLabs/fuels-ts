a098d1d4980ab267d2a559a1d107b30c1e389eba
Sérgio Torres <30977845+Torres-ssf@users.noreply.github.com>
2024-10-21 08:41:55 -0300

    Update apps/docs-snippets2/src/types/address/utilities-function-2.ts
    Co-authored-by: Peter Smith <peter@blueoceancomputing.co.uk>

--- apps/docs-snippets2/src/types/address/utilities-function-2.ts
+++ apps/docs-snippets2/src/types/address/utilities-function-2.ts
@@ -4,7 +4,7 @@ import { Address } from 'fuels';
 const dataFromInput: string =
   '0xf1e92c42b90934aa6372e30bc568a326f6e66a1a0288595e6e3fbd392a4f3e6e';
 
-// if the input string can't be resolved this will throw an error
+// If the input string can't be resolved this will throw an error
 const address = Address.fromDynamicInput(dataFromInput);
 // #endregion full
 console.log('address', address);
