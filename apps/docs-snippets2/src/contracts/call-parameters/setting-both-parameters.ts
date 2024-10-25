cf6fbcb2208446090cc74f0490f99ef819eba8e2
nedsalk <nedim.salkic@fuel.sh>
2024-10-25 11:50:49 +0200

    moved call-parameters

---
+++ apps/docs-snippets2/src/contracts/call-parameters/setting-both-parameters.ts
@@ -0,0 +1,31 @@
+import { Provider, Wallet } from 'fuels';
+
+import { LOCAL_NETWORK_URL, WALLET_PVT_KEY } from '../../env';
+import { ReturnContextFactory } from '../../typegend';
+
+const provider = await Provider.create(LOCAL_NETWORK_URL);
+const wallet = Wallet.fromPrivateKey(WALLET_PVT_KEY, provider);
+
+const deploy = await ReturnContextFactory.deploy(wallet);
+const { contract } = await deploy.waitForResult();
+
+// #region setting-both-parameters
+const contractCallGasLimit = 4_000;
+const transactionGasLimit = 100_000;
+
+const { waitForResult } = await contract.functions
+  .return_context_amount()
+  .callParams({
+    forward: [10, provider.getBaseAssetId()],
+    gasLimit: contractCallGasLimit,
+  })
+  .txParams({
+    gasLimit: transactionGasLimit,
+  })
+  .call();
+
+// #endregion setting-both-parameters
+const {
+  transactionResult: { isStatusSuccess },
+} = await waitForResult();
+console.log('tx successful', isStatusSuccess);
