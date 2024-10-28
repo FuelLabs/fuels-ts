9dc373a47dab88eb0d215af5d0850906e7bc34ca
chad <chad.nehemiah94@gmail.com>
2024-10-28 16:08:26 +0400

    ci: update pretest script for temp builds

--- apps/docs-snippets2/scripts/pretest.sh
+++ apps/docs-snippets2/scripts/pretest.sh
@@ -1,4 +1,5 @@
 # Check if node is already running at port 4000, if not start it
+# TODO: This is a temporary solution to avoid conflicts with the test node in docs-snippets
 if ! lsof -t -i:4000 > /dev/null; then
   pnpm fuels node > /dev/null 2>&1 &
 fi
@@ -8,9 +9,6 @@ pnpm fuels build
 
 # Deploys projects (needed for loader bytecode)
 pnpm fuels deploy
-
-# Kills the node
-lsof -t -i:4000 | xargs -r kill
 
 # Checks for type errors
 pnpm tsc --noEmit
