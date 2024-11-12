aceecb48cbc799b1729c8a7438635478f5be39bb
YaTut1901 <ovanya1901@gmail.com>
2024-11-12 13:55:10 +0200

    chore(packages): added noImplicitOverride option to config

---
+++ .changeset/strange-starfishes-raise.md
@@ -0,0 +1,9 @@
+---
+"@fuel-ts/account": patch
+"@fuel-ts/address": patch
+"@fuel-ts/program": patch
+"@fuel-ts/script": patch
+"fuels": patch
+---
+
+added noImplicitOverride option to config
--- packages/account/src/predicate/predicate.ts
+++ packages/account/src/predicate/predicate.ts
@@ -132,7 +132,9 @@ export class Predicate<
    * @param transactionRequestLike - The transaction request-like object.
    * @returns A promise that resolves to the transaction response.
    */
-  sendTransaction(transactionRequestLike: TransactionRequestLike): Promise<TransactionResponse> {
+  override sendTransaction(
+    transactionRequestLike: TransactionRequestLike
+  ): Promise<TransactionResponse> {
     const transactionRequest = transactionRequestify(transactionRequestLike);
 
     return super.sendTransaction(transactionRequest, { estimateTxDependencies: false });
@@ -144,7 +146,9 @@ export class Predicate<
    * @param transactionRequestLike - The transaction request-like object.
    * @returns A promise that resolves to the call result.
    */
-  simulateTransaction(transactionRequestLike: TransactionRequestLike): Promise<CallResult> {
+  override simulateTransaction(
+    transactionRequestLike: TransactionRequestLike
+  ): Promise<CallResult> {
     const transactionRequest = transactionRequestify(transactionRequestLike);
     return super.simulateTransaction(transactionRequest, { estimateTxDependencies: false });
   }
@@ -205,7 +209,7 @@ export class Predicate<
    * @param excludedIds - IDs of resources to be excluded from the query.
    * @returns A promise that resolves to an array of Resources.
    */
-  async getResourcesToSpend(
+  override async getResourcesToSpend(
     quantities: CoinQuantityLike[] /** IDs of coins to exclude */,
     excludedIds?: ExcludeResourcesOption
   ): Promise<Resource[]> {
@@ -227,7 +231,7 @@ export class Predicate<
    * @param coins - An array of `FakeResources` objects representing the coins.
    * @returns An array of `Resource` objects with generated properties.
    */
-  generateFakeResources(coins: FakeResources[]): Array<Resource> {
+  override generateFakeResources(coins: FakeResources[]): Array<Resource> {
     return super.generateFakeResources(coins).map((coin) => ({
       ...coin,
       predicate: hexlify(this.bytes),
