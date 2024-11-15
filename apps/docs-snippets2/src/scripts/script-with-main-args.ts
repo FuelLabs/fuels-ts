64818afb096bb26b2befbad63dc219fd6864138d
Peter Smith <peter@blueoceancomputing.co.uk>
2024-11-15 09:57:23 +0700

    chore: use typegen'd script

--- apps/docs-snippets2/src/scripts/script-with-main-args.ts
+++ apps/docs-snippets2/src/scripts/script-with-main-args.ts
@@ -1,20 +1,15 @@
-/* eslint-disable import/first */
-/* eslint-disable import/order */
-import { ScriptMainArgs } from '../typegend';
-
-const bytecode = ScriptMainArgs.bytecode;
-const abi = ScriptMainArgs.abi;
-
 // #region full
+import { bn, Provider, Wallet } from 'fuels';
+
 import { LOCAL_NETWORK_URL, WALLET_PVT_KEY } from '../env';
-import { bn, Provider, Script, Wallet } from 'fuels';
+import { ScriptMainArgs } from '../typegend';
 
 const provider = await Provider.create(LOCAL_NETWORK_URL);
 const wallet = Wallet.fromPrivateKey(WALLET_PVT_KEY, provider);
 
 const foo = 3;
 
-const scriptInstance = new Script(bytecode, abi, wallet);
+const scriptInstance = new ScriptMainArgs(wallet);
 
 const { waitForResult } = await scriptInstance.functions.main(foo).call();
 
