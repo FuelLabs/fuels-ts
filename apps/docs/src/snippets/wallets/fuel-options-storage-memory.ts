b8411ceb41da9a39a43716240059c9719603453a
Peter Smith <peter@blueoceancomputing.co.uk>
2024-11-15 12:42:41 +0700

    chore: testing deleting the snippets folder

--- apps/docs/src/snippets/wallets/fuel-options-storage-memory.ts
+++
@@ -1,8 +0,0 @@
-// #region fuel-options-storage-memory
-import { Fuel, MemoryStorage } from 'fuels';
-
-const sdkWithMemoryStorage = await new Fuel({
-  storage: new MemoryStorage(),
-}).init();
-// #endregion fuel-options-storage-memory
-console.log('sdkWithMemoryStorage', sdkWithMemoryStorage);
--- apps/docs/src/snippets/wallets/instantiating/generate.ts
+++
@@ -1,8 +0,0 @@
-// #region instantiating-wallets-1
-import { Wallet } from 'fuels';
-import type { WalletUnlocked } from 'fuels';
-
-const wallet: WalletUnlocked = Wallet.generate();
-// #endregion instantiating-wallets-1
-
-console.log('Wallet should be defined', wallet);
