dc338ab6e4d02aef3085f1ba1246359eb35b46d0
Torres-ssf <sergio.uft@gmail.com>
2024-11-28 15:19:38 -0300

    cleaning

--- scripts/lint-md-links.ts
+++ scripts/lint-md-links.ts
@@ -5,36 +5,23 @@ import { globSync } from 'glob';
 const { log } = console;
 
 (() => {
-  const includePattern = '**/*.md';
-  const ignorePatterns = [
-    '**/node_modules/**',
-    'apps/demo/**',
-    'apps/demo-react-cra/**',
-    'apps/demo-nextjs/**',
-    '.changeset/**',
-    '**/CHANGELOG.md',
-  ];
-
-  const allFiles = globSync(includePattern);
-
-  const mdFiles = globSync(includePattern, { ignore: ignorePatterns });
-  const skippedFiles = allFiles.filter((file) => !mdFiles.includes(file));
-
-  log('##################### Markdown files to be checked #####################');
-  log(mdFiles.join('\n'));
-
-  log('##################### Markdown files being skipped #####################');
-  log(skippedFiles.join('\n'));
+  const mdFiles = globSync('**/*.md', {
+    ignore: [
+      '**/node_modules/**',
+      'apps/demo/**',
+      'apps/demo-react-cra/**',
+      'apps/demo-nextjs/**',
+      '.changeset/**',
+      '**/CHANGELOG.md',
+    ],
+  });
 
   try {
     execSync(`pnpm markdown-link-check -q -c ./link-check.config.json ${mdFiles.join(' ')}`, {
       stdio: 'inherit',
     });
-    log('##################### All markdown files passed link checks #####################');
   } catch {
-    log(
-      '##################### Some files have broken links. Please fix them #####################'
-    );
+    log('Some files have broken links. Please fix them.');
     process.exit(1);
   }
 })();
