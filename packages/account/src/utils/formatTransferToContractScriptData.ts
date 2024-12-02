7814cef4ef6ca69fdcbffd93eb363ab6f1f811d3
Peter Smith <peter@blueoceancomputing.co.uk>
2024-12-02 08:51:29 +0000

    chore: proposed suppression of asm error

--- packages/account/src/utils/formatTransferToContractScriptData.ts
+++ packages/account/src/utils/formatTransferToContractScriptData.ts
@@ -11,6 +11,16 @@ interface AssembleTransferToContractParams {
   amount: BN;
 }
 
+// console.warn = (message) => {
+//   if (
+//     message ===
+//     'using deprecated parameters for the initialization function; pass a single object instead'
+//   ) {
+//     return;
+//   }
+//   console.warn(message);
+// };
+
 export const formatTransferToContractScriptData = (
   transferParams: Array<AssembleTransferToContractParams>
 ) => {
