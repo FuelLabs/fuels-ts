9eeaa8589f70b3047001450eff055f3c681f3aeb
Peter Smith <peter@blueoceancomputing.co.uk>
2024-12-06 12:26:55 +0000

    chore: types fixes

--- vitest.d.ts
+++ vitest.d.ts
@@ -1,11 +1,12 @@
 import 'vitest';
 
 interface CustomMatchers<R = unknown> {
-  toEqualBn: () => R;
+  toEqualBn: (bn: string | number) => R;
 }
 
 declare module 'vitest' {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   interface Assertion<T = any> extends CustomMatchers<T> {}
+  interface ExpectStatic extends CustomMatchers {}
   interface AsymmetricMatchersContaining extends CustomMatchers {}
 }
