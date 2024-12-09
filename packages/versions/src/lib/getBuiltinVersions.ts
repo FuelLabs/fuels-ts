194b806391c56cd2a1e25270d0e6d9fa5f8d4ef4
Torres-ssf <sergio.uft@gmail.com>
2024-12-09 14:31:31 -0300

    using fuel-core version from specific branch

--- packages/versions/src/lib/getBuiltinVersions.ts
+++ packages/versions/src/lib/getBuiltinVersions.ts
@@ -3,7 +3,7 @@ import type { Versions } from './types';
 export function getBuiltinVersions(): Versions {
   return {
     FORC: '0.66.4',
-    FUEL_CORE: '0.40.1',
+    FUEL_CORE: 'git:1897-rpc-consistency-proposal',
     FUELS: '0.97.0',
   };
 }
