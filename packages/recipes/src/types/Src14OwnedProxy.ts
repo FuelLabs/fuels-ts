18e659f885299ed9cad4cea9b9d79f8eb4e4d49b
nedsalk <nedim.salkic@fuel.sh>
2024-12-12 18:24:34 +0100

    revert

--- packages/recipes/src/types/Src14OwnedProxy.ts
+++ packages/recipes/src/types/Src14OwnedProxy.ts
@@ -5,7 +5,7 @@
 /* eslint-disable @typescript-eslint/consistent-type-imports */
 
 /*
-  Fuels version: 0.97.1
+  Fuels version: 0.97.0
 */
 
 import { Contract, type InvokeFunction } from '@fuel-ts/program';
--- packages/recipes/src/types/Src14OwnedProxyFactory.ts
+++ packages/recipes/src/types/Src14OwnedProxyFactory.ts
@@ -5,7 +5,7 @@
 /* eslint-disable @typescript-eslint/consistent-type-imports */
 
 /*
-  Fuels version: 0.97.1
+  Fuels version: 0.97.0
 */
 
 import { ContractFactory, type DeployContractOptions } from '@fuel-ts/contract';
