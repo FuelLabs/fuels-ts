cacbacf5d00ad7208ec16b38b7da508bf9e549c0
nedsalk <nedim.salkic@fuel.sh>
2024-12-12 11:53:25 +0100

    revert changes

--- packages/abi/src/coder/abi-coder.ts
+++ packages/abi/src/coder/abi-coder.ts
@@ -1,10 +1,2 @@
-import type { Abi } from '../parser';
-import type { AbiSpecification } from '../parser/abi-parser';
-import { AbiParser } from '../parser/abi-parser';
-
-export class AbiCoder {
-  private abi: Abi;
-  constructor(parsableAbi: AbiSpecification) {
-    this.abi = AbiParser.parse(parsableAbi);
-  }
-}
+// Placeholder
+export class AbiCoder {}
--- packages/abi/src/gen/abi-gen.ts
+++ packages/abi/src/gen/abi-gen.ts
@@ -1,10 +1,1 @@
-import type { Abi } from '../parser';
-import type { AbiSpecification } from '../parser/abi-parser';
-import { AbiParser } from '../parser/abi-parser';
-
-export class AbiGen {
-  private abi: Abi;
-  constructor(parsableAbi: AbiSpecification) {
-    this.abi = AbiParser.parse(parsableAbi);
-  }
-}
+export class AbiGen {}
