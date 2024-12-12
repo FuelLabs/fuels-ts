f6a3fdfba641413b6c5e3fc965c30da15f0b015b
Peter Smith <peter@blueoceancomputing.co.uk>
2024-12-12 07:50:33 +0000

    chore: bumped forc

--- packages/versions/src/lib/getBuiltinVersions.ts
+++ packages/versions/src/lib/getBuiltinVersions.ts
@@ -2,7 +2,7 @@ import type { Versions } from './types';
 
 export function getBuiltinVersions(): Versions {
   return {
-    FORC: '0.66.4',
+    FORC: '0.66.5',
     FUEL_CORE: '0.40.0',
     FUELS: '0.97.0',
   };
