09a9e974f053f8fa357f84eb98ab2d4725ec4e0e
nedsalk <nedim.salkic@fuel.sh>
2024-12-13 16:45:54 +0100

    add doc blocks for `AbiGen` and related types

--- packages/abi/src/gen/abi-gen-types.ts
+++ packages/abi/src/gen/abi-gen-types.ts
@@ -3,20 +3,53 @@ import type { BinaryVersions } from '@fuel-ts/versions';
 import type { Abi } from '../parser';
 
 export interface AbiGenInput {
+  /**
+   * The details of the program to generate the files for.
+   */
   programDetails: ProgramDetails[];
+  /**
+   * The versions of the binaries used to generate the files.
+   */
   versions: BinaryVersions;
+  /**
+   * The mode to generate the files in.
+   * Defaults to 'ts' which generates typescript files.
+   */
   mode?: 'ts';
 }
 
 export interface AbiGenResult {
+  /**
+   * The filename of the generated file.
+   */
   filename: string;
+  /**
+   * The content of the generated file.
+   */
   content: string;
 }
 
 export interface ProgramDetails {
+  /**
+   * The name of the program to generate files for.
+   * This will be used to name the generated files,
+   * as well as throughout the generated code.
+   */
   name: string;
+  /**
+   * The compressed bytecode of the program in base64 format.
+   */
   binCompressed: string;
+  /**
+   * The abi of the program in the format returned via `AbiParser`.
+   */
   abi: Abi;
+  /**
+   * The original abi contents in string format.
+   */
   abiContents: string;
+  /**
+   * The storage slots, if working with a contract.
+   */
   storageSlots?: string;
 }
--- packages/abi/src/gen/abi-gen.ts
+++ packages/abi/src/gen/abi-gen.ts
@@ -1,7 +1,17 @@
 import type { AbiGenInput, AbiGenResult } from './abi-gen-types';
 import { getRenderer } from './renderers/getRenderer';
 
+/**
+ * The main class to generate files for given sway programs.
+ * These contents of these generated files make it easier to interact
+ * with the sway programs, because type definitions are added,
+ * as well as some automatic loading is done for the user.
+ */
 export class AbiGen {
+  /**
+   * @returns an array of generated files for the given program details.
+   * They can be saved to disk as-is or further processed.
+   */
   public static generate({ programDetails, mode, versions }: AbiGenInput): AbiGenResult[] {
     const render = getRenderer(mode);
     return render(programDetails, versions);
