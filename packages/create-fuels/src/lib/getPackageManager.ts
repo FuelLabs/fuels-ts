15b9eba61a93bb030ecb50bb7180994e818f4da2
Dhai <dhaiwatpandya@gmail.com>
2024-12-27 20:58:13 +0530

    use constants for package manager names

--- packages/create-fuels/src/lib/getPackageManager.ts
+++ packages/create-fuels/src/lib/getPackageManager.ts
@@ -6,33 +6,43 @@ const runnableApplicator =
   (command: string = '') =>
     `${commandPrefix} ${command}`;
 
-export const packageMangers = {
+export const packageMangers: Record<
+  PackageManager,
+  {
+    install: string;
+    run: (command: string) => string;
+    name: PackageManager;
+  }
+> = {
   pnpm: {
     install: 'pnpm install',
     run: runnableApplicator('pnpm'),
+    name: 'pnpm',
   },
   npm: {
     install: 'npm install',
     run: runnableApplicator('npm run'),
+    name: 'npm',
   },
   bun: {
     install: 'bun install',
     run: runnableApplicator('bun run'),
+    name: 'bun',
   },
 } as const;
 
 export function getUserPkgManager(): PackageManager {
   const userAgent = process.env.npm_config_user_agent || '';
 
-  if (userAgent.startsWith('pnpm')) {
-    return 'pnpm';
+  if (userAgent.startsWith(packageMangers.pnpm.name)) {
+    return packageMangers.pnpm.name;
   }
 
-  if (userAgent.startsWith('bun')) {
-    return 'bun';
+  if (userAgent.startsWith(packageMangers.bun.name)) {
+    return packageMangers.bun.name;
   }
 
-  return 'npm';
+  return packageMangers.npm.name;
 }
 
 export const getPackageManager = () => {
