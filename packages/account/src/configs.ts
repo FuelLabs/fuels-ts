895dec07e093fe7f9e62ff437d60bb8036a46f7d
Peter Smith <peter@blueoceancomputing.co.uk>
2025-01-03 09:39:59 +0000

    chore: removed network urls

--- packages/account/src/configs.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @deprecated This export is deprecated and will be removed in the future.
- */
-export const DEVNET_NETWORK_URL = 'https://devnet.fuel.network/v1/graphql';
-
-/**
- * @deprecated This export is deprecated and will be removed in the future.
- */
-export const TESTNET_NETWORK_URL = 'https://testnet.fuel.network/v1/graphql';
--- packages/account/tsup.config.ts
+++ packages/account/tsup.config.ts
@@ -9,7 +9,6 @@ const configs: Options = {
   ...tsupDefaults,
   entry: {
     index: 'src/index.ts',
-    configs: 'src/configs.ts',
     'test-utils': 'src/test-utils.ts',
   },
   // eslint-disable-next-line @typescript-eslint/require-await
