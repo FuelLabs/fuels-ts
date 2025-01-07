34998ae58d6017a92259f02f85a182dc664a419a
Daniel Bate <djbate23@gmail.com>
2025-01-07 10:09:58 +0000

    chore: update and fix tests

--- apps/docs/src/guide/transactions/snippets/transaction-subscriptions/transaction-request.ts
+++ apps/docs/src/guide/transactions/snippets/transaction-subscriptions/transaction-request.ts
@@ -2,14 +2,18 @@ import { Provider, ScriptTransactionRequest, Wallet } from 'fuels';
 
 import { LOCAL_NETWORK_URL, WALLET_PVT_KEY } from '../../../../env';
 
-const provider = await Provider.create(LOCAL_NETWORK_URL);
+const provider = new Provider(LOCAL_NETWORK_URL);
 const wallet = Wallet.fromPrivateKey(WALLET_PVT_KEY, provider);
 const recipient = Wallet.generate();
 
 // #region main
 // Create a new transaction request
 const txRequest = new ScriptTransactionRequest();
-txRequest.addCoinOutput(recipient.address, 1_000, provider.getBaseAssetId());
+txRequest.addCoinOutput(
+  recipient.address,
+  1_000,
+  await provider.getBaseAssetId()
+);
 
 // Fund the transaction
 await txRequest.autoCost(wallet);
