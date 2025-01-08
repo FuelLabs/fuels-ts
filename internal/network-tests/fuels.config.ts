a633bb35002ee9f9ad5cbcb13bea6f0a4d92225a
Torres-ssf <sergio.uft@gmail.com>
2025-01-08 10:42:16 -0300

    remove recently added package

--- internal/network-tests/fuels.config.ts
+++
@@ -1,9 +0,0 @@
-import { createConfig } from 'fuels';
-
-export default createConfig({
-  workspace: './src/sway',
-  output: './src/typegen',
-  forcBuildFlags: ['--release'],
-  forcPath: 'fuels-forc',
-  fuelCorePath: 'fuels-core',
-});
--- internal/network-tests/src/sway/script-transfer/src/main.sw
+++
@@ -1,8 +0,0 @@
-script;
-
-use std::asset::transfer;
-
-fn main(address: Identity, asset_id: AssetId, amount: u64) -> bool {
-    transfer(address, asset_id, amount);
-    true
-}
