5ebbed9c11363665253edb7c6c45cd6450e30337
Torres-ssf <sergio.uft@gmail.com>
2025-01-14 10:39:38 -0300

    implement fn to analyze script with predicate tx

---
+++ packages/fuel-gauge/scripts/latency-detection/script-with-predicate-call.ts
@@ -0,0 +1,20 @@
+import { measure } from './helpers';
+import type { BaseParams, PerformanceResult } from './types';
+import { TagEnum } from './types';
+
+export async function scriptWithPredicateCall(params: BaseParams): Promise<PerformanceResult> {
+  const { baseAssetId, contract, callParams, predicate } = params;
+
+  const { duration } = await measure(async () => {
+    contract.account = predicate;
+    const call = await contract.functions
+      .execute_transfer(callParams)
+      .txParams({ variableOutputs: 1 })
+      .callParams({ forward: [100, baseAssetId] })
+      .call();
+
+    return call.waitForResult();
+  });
+
+  return { tag: TagEnum.ScriptWithPredicate, duration };
+}
