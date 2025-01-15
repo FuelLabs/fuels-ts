0e8a7be57607430ae47e77f14aed96487c18e85e
Torres-ssf <sergio.uft@gmail.com>
2025-01-15 13:36:22 -0300

    add maxGas and txPoolStats to nodeInfoFragment

--- packages/account/test/fixtures/nodeInfo.ts
+++ packages/account/test/fixtures/nodeInfo.ts
@@ -4,6 +4,12 @@ export const MOCK_NODE_INFO: GqlNodeInfoFragment = {
   utxoValidation: true,
   vmBacktrace: true,
   maxTx: '4064',
+  maxGas: '1000000000',
+  txPoolStats: {
+    totalGas: '100000000000',
+    totalSize: '10000000000000',
+    txCount: '100',
+  },
   maxDepth: '10',
   nodeVersion: '0.22.0',
 };
