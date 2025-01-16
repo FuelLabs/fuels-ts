99e6a1b02bfa1e7a4ea645d802f8575737831c1c
Torres-ssf <sergio.uft@gmail.com>
2025-01-16 07:57:12 -0300

    unify 2 errors to conform with fuel core changes

--- packages/account/src/providers/utils/handle-gql-error-message.ts
+++ packages/account/src/providers/utils/handle-gql-error-message.ts
@@ -2,23 +2,15 @@ import { ErrorCode, FuelError } from '@fuel-ts/errors';
 import type { GraphQLError } from 'graphql';
 
 export enum GqlErrorMessage {
-  NOT_ENOUGH_COINS = 'not enough coins to fit the target',
-  MAX_COINS_REACHED = 'max number of coins is reached while trying to fit the target',
+  NOT_ENOUGH_COINS_MAX_COINS = 'the target cannot be met due to no coins available or exceeding the 255 coin limit.',
 }
 
 export const handleGqlErrorMessage = (errorMessage: string, rawError: GraphQLError) => {
   switch (errorMessage) {
-    case GqlErrorMessage.NOT_ENOUGH_COINS:
+    case GqlErrorMessage.NOT_ENOUGH_COINS_MAX_COINS:
       throw new FuelError(
         ErrorCode.NOT_ENOUGH_FUNDS,
-        `The account(s) sending the transaction don't have enough funds to cover the transaction.`,
-        {},
-        rawError
-      );
-    case GqlErrorMessage.MAX_COINS_REACHED:
-      throw new FuelError(
-        ErrorCode.MAX_COINS_REACHED,
-        'The account retrieving coins has exceeded the maximum number of coins per asset. Please consider combining your coins into a single UTXO.',
+        `Insufficient funds or too many small value coins. Consider combining UTXOs.`,
         {},
         rawError
       );
