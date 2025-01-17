a30fa14af11a3bf3c6691eb5fede42bf02288a72
nedsalk <nedim.salkic@fuel.sh>
2025-01-17 12:54:37 +0100

    working link checker local

--- scripts/lint-md-links.ts
+++ scripts/lint-md-links.ts
@@ -1,14 +1,31 @@
 #!/usr/bin/env node
-import { execSync } from 'child_process';
+import { exec, execSync, spawn } from 'child_process';
+import { readFileSync, writeFileSync } from 'fs';
 import { globSync } from 'glob';
+import { cwd } from 'process';
 
 const { log } = console;
 
-(() => {
+// eslint-disable-next-line no-void
+void (async () => {
+  const docsApi = spawn(`pnpm vite preview --port 9876 --outDir apps/docs-api/src/api`, {
+    shell: true,
+  });
+
+  await new Promise((resolve) => {
+    docsApi.stdout.on('data', () => {
+      resolve(undefined);
+    });
+  });
+
   const mdFiles = globSync('**/*.md', {
     ignore: ['**/node_modules/**', 'apps/demo-*/**', '.changeset/**', '**/CHANGELOG.md'],
   });
 
+  mdFiles.forEach((filepath) => {
+    const content = readFileSync(filepath).toString();
+    writeFileSync(filepath, content.replace(/DOCS_API_URL/g, 'http://localhost:9876'));
+  });
   // TODO: Stop ignoring doc links in `link-check.config.json`
   // The above requires this to be merged and deployed:
   //  - https://github.com/FuelLabs/fuels-ts/pull/3500
