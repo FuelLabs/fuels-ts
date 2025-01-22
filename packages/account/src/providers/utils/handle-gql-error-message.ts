1321c222bbff4f22a43051e4c8928cc1c8ca4872
Torres-ssf <sergio.uft@gmail.com>
2025-01-22 10:55:24 -0300

    update gql error message to match with any value

--- packages/account/src/providers/utils/handle-gql-error-message.ts
+++ packages/account/src/providers/utils/handle-gql-error-message.ts
@@ -1,8 +1,8 @@
 import { ErrorCode, FuelError } from '@fuel-ts/errors';
 import type { GraphQLError } from 'graphql';
 
-export enum GqlErrorMessage {
-  NOT_ENOUGH_COINS_MAX_COINS = 'the target cannot be met due to no coins available or exceeding the 255 coin limit.',
+enum GqlErrorMessage {
+  NOT_ENOUGH_COINS_MAX_COINS = 'the target cannot be met due to no coins available or exceeding the \\d+ coin limit.',
 }
 
 type GqlError = { message: string } | GraphQLError;
