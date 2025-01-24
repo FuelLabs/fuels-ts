b6218af86e2b02c798bf2b186ef34ee90ffc7bb1
Peter Smith <peter@blueoceancomputing.co.uk>
2025-01-24 10:18:28 +0000

    chore: shuffled assets directory

--- packages/account/src/assets/assets.test.ts
+++ packages/account/src/assets/assets.test.ts
@@ -3,13 +3,13 @@
  * @group browser
  */
 describe('assets', async () => {
-  const { rawAssets, assets } = (await import('./assets'));
+  const { rawAssets, assets } = await import('./assets');
 
   it.each(rawAssets)('$symbol should have icon not resolved to URL', ({ icon }) => {
     expect(icon).not.toContain('/');
   });
 
-  it.each(assets)('$symbol should have icon resolved to URL', async ({ icon }) => {
+  it.each(assets)('$symbol should have icon resolved to URL', ({ icon }) => {
     expect(icon).toContain('https://cdn.fuel.network/assets');
   });
 });
