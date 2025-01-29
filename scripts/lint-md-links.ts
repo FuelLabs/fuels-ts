ff53c1d19b9427b7f394e26cef756337eac5b8a3
nedsalk <nedim.salkic@fuel.sh>
2025-01-29 16:44:59 +0100

    fix linting not exiting

--- scripts/lint-md-links.ts
+++ scripts/lint-md-links.ts
@@ -23,12 +23,12 @@ void (async () => {
   // TODO: Stop ignoring doc links in `link-check.config.json`
   // The above requires this to be merged and deployed:
   //  - https://github.com/FuelLabs/fuels-ts/pull/3500
-  try {
-    execSync(`pnpm markdown-link-check -q -c ./link-check.config.json ${mdFiles.join(' ')}`, {
-      stdio: 'inherit',
-    });
-  } catch {
+  execSync(`pnpm markdown-link-check -q -c ./link-check.config.json ${mdFiles.join(' ')}`, {
+    stdio: 'inherit',
+  });
+})()
+  .catch(() => {
     log('Some files have broken links. Please fix them.');
     process.exit(1);
-  }
-})();
+  })
+  .then(() => process.exit(0));
