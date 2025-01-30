888cfca12182b8c0f78ba49efd3e88e97d1b342c
Peter Smith <peter@blueoceancomputing.co.uk>
2025-01-30 16:13:37 +0000

    chore: finalized API w/ mocks

---
+++ packages/account/src/assets/asset-api.test.ts
@@ -0,0 +1,144 @@
+import { MOCK_ASSET_INFO_BY_OWNER, MOCK_BASE_ASSET, MOCK_FUEL_ASSET, MOCK_NFT_ASSET } from "../../test/fixtures/assets";
+import { getAssetById, TESTNET_ASSET_API_URL, getAssetsByOwner, MAINNET_ASSET_API_URL, AssetInfo } from "./asset-api";
+
+const mockFetch = () => {
+  const jsonResponse = vi.fn();
+  const fetchSpy = vi.spyOn(global, 'fetch').mockResolvedValueOnce({
+    json: jsonResponse
+  } as unknown as Response);
+
+  return {
+    fetch: fetchSpy,
+    json: jsonResponse
+  }
+}
+
+describe('Asset API', () => {
+  describe('getAssetById', () => {
+    it('should get an asset by id [Base Asset - Verified]', async () => {
+      const expected = {
+        assetId: expect.any(String),
+        name: expect.any(String),
+        symbol: expect.any(String),
+        decimals: expect.any(Number),
+        rate: expect.any(Number),
+        icon: expect.any(String),
+        suspicious: expect.any(Boolean),
+        verified: expect.any(Boolean),
+        networks: expect.any(Array),
+      }
+      const { json } = mockFetch();
+      json.mockResolvedValueOnce(MOCK_BASE_ASSET);
+
+      const response = await getAssetById(
+        TESTNET_ASSET_API_URL,
+        MOCK_BASE_ASSET.assetId
+      );
+
+      const assetInfo = response as AssetInfo;
+      expect(assetInfo).toEqual(MOCK_BASE_ASSET)
+      expect(assetInfo).toMatchObject(expected)
+      expect(Object.keys(assetInfo).sort()).toEqual(Object.keys(expected).sort())
+    })
+
+    it('should get an asset by id [Fuel - Verified]', async () => {
+      const expected = {
+        assetId: expect.any(String),
+        contractId: expect.any(String),
+        subId: expect.any(String),
+        icon: expect.any(String),
+        name: expect.any(String),
+        symbol: expect.any(String),
+        decimals: expect.any(Number),
+        rate: expect.any(Number),
+        suspicious: expect.any(Boolean),
+        verified: expect.any(Boolean),
+        networks: expect.any(Array),
+        metadata: expect.any(Object),
+        totalSupply: expect.any(String),
+      }
+      const { json } = mockFetch();
+      json.mockResolvedValueOnce(MOCK_FUEL_ASSET);
+
+      const response = await getAssetById(
+        TESTNET_ASSET_API_URL,
+        MOCK_FUEL_ASSET.assetId
+      );
+
+      const assetInfo = response as AssetInfo;
+      expect(response).toEqual(MOCK_FUEL_ASSET)
+      expect(assetInfo).toMatchObject(expected)
+      expect(Object.keys(assetInfo).sort()).toEqual(Object.keys(expected).sort())
+    })
+
+    it('should get an asset by id [NFT]', async () => {
+      const expected = {
+        assetId: expect.any(String),
+        contractId: expect.any(String),
+        subId: expect.any(String),
+
+        name: expect.any(String),
+        symbol: expect.any(String),
+        decimals: expect.any(Number),
+        totalSupply: expect.any(String),
+
+        suspicious: expect.any(Boolean),
+        verified: expect.any(Boolean),
+        isNFT: expect.any(Boolean),
+
+        metadata: expect.any(Object),
+        collection: expect.any(String),
+
+        owner: expect.any(String),
+        amount: expect.any(String),
+        amountInUsd: expect.nullOrAny(String),
+        uri: expect.any(String),
+      }
+
+      const { json } = mockFetch();
+      json.mockResolvedValueOnce(MOCK_NFT_ASSET);
+
+      const response = await getAssetById(
+        MAINNET_ASSET_API_URL,
+        MOCK_NFT_ASSET.assetId
+      );
+
+      const assetInfo = response as AssetInfo;
+      expect(response).toEqual(MOCK_NFT_ASSET)
+      expect(assetInfo).toMatchObject(expected)
+      expect(Object.keys(assetInfo).sort()).toEqual(Object.keys(expected).sort())
+    });
+
+    it('should return null if asset not found', async () => {
+      const { json } = mockFetch();
+      // The API returns a 200 status code but the response is not valid JSON
+      json.mockRejectedValueOnce(null);
+
+      const response = await getAssetById(
+        TESTNET_ASSET_API_URL,
+        '0x0000000000000000000000000000000000000000000000000000000000000000'
+      );
+
+      expect(response).toBeNull();
+    });
+  });
+
+  describe('getAssetByOwner', () => {
+    it('should get assets by owner', async () => {
+      const { json } = mockFetch();
+      json.mockResolvedValueOnce(MOCK_ASSET_INFO_BY_OWNER);
+
+      const response = await getAssetsByOwner(
+        TESTNET_ASSET_API_URL,
+        MOCK_NFT_ASSET.owner,
+        { last: 10 }
+      );
+
+      expect(response?.data).toEqual([MOCK_NFT_ASSET]);
+      expect(response?.pageInfo).toEqual({
+        count: 1,
+      })
+    });
+  })
+});
+
--- packages/account/src/assets/asset-api.ts
+++ packages/account/src/assets/asset-api.ts
@@ -1,4 +1,5 @@
 import { B256Address } from "@fuel-ts/address";
+import { NetworkEthereum, NetworkFuel } from "./types";
 
 export const MAINNET_ASSET_API_URL = 'https://mainnet-explorer.fuel.network'
 export const TESTNET_ASSET_API_URL = 'https://testnet-explorer.fuel.network'
@@ -35,27 +36,26 @@ const buildQueryString = (parameters: Record<string, string | { toString: () => 
   return query.size > 0 ? `?${query.toString()}` : '';
 };
 
-export interface AssetByIdInfoResponse {
+export interface AssetInfo {
   assetId: string;
   contractId?: string;
-
+  subId?: string;
   name: string;
   symbol: string;
   decimals: number;
-  icon: string;
-
   suspicious: boolean;
   verified: boolean;
-
-  // owner: string;
-  // uri: string;
-
-  /**
-   * For NFT's
-   */
-  subId: string;
+  totalSupply?: string;
+  networks?: (NetworkEthereum | NetworkFuel)[];
+  rate?: number;
+  icon?: string;
+  owner?: string;
+  amount?: string;
+  amountInUsd?: string;
+  uri?: string;
   metadata: Record<string, string>;
-  isNFT: boolean;
+  collection?: string;
+  isNFT?: boolean;
 }
 
 /**
@@ -63,43 +63,16 @@ export interface AssetByIdInfoResponse {
  *
  * @param url {string} - The Base URL of the explorer API
  * @param assetId {string} - The ID of the asset to get information about
- * @returns {Promise<ExplorerAssetResponse>} - The information about the asset
+ * @returns {Promise<AssetInfo>} - The information about the asset
  *
  * @see {@link https://github.com/FuelLabs/fuel-explorer/wiki/Assets-API#instructions-for-consuming-assets-data-from-indexer-api}
  */
-export const getAssetById = (url: string, assetId: B256Address): Promise<AssetByIdInfoResponse | null> => {
-  return request<AssetByIdInfoResponse>(url, `/assets/${assetId}`);
+export const getAssetById = (url: string, assetId: B256Address): Promise<AssetInfo | null> => {
+  return request<AssetInfo>(url, `/assets/${assetId}`);
 };
 
-export interface AssetByOwnerInfo {
-  assetId: string;
-  contractId?: string;
-
-  name: string;
-  symbol: string;
-  decimals: number;
-  icon: string;
-
-  suspicious: boolean;
-  verified: boolean;
-
-  // owner: string;
-  // uri: string;
-
-  /**
-   * For NFT's
-   */
-  subId: string;
-  metadata: Record<string, string>;
-  isNFT: boolean;
-
-  owner: B256Address;
-  amount: string;
-  amountInUsd: string;
-}
-
 export interface AssetsByOwnerResponse {
-  data: AssetByOwnerInfo[];
+  data: AssetInfo[];
   pageInfo: PageInfo;
 }
 
