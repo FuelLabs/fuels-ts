9f083245d0f63ca0bd102d352c006f9127136a0c
nedsalk <nedim.salkic@fuel.sh>
2025-02-07 12:52:00 +0100

    remove todo

--- scripts/lint-md-links.ts
+++ scripts/lint-md-links.ts
@@ -20,9 +20,6 @@ void (async () => {
     ignore: ['**/node_modules/**', 'apps/demo-*/**', '.changeset/**', '**/CHANGELOG.md'],
   });
 
-  // TODO: Stop ignoring doc links in `link-check.config.json`
-  // The above requires this to be merged and deployed:
-  //  - https://github.com/FuelLabs/fuels-ts/pull/3500
   execSync(`pnpm markdown-link-check -q -c ./link-check.config.json ${mdFiles.join(' ')}`, {
     stdio: 'inherit',
   });
