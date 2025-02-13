400717aa33723c9df470e30d7a3b6e9cf082ee3d
Torres-ssf <sergio.uft@gmail.com>
2025-02-13 09:43:49 -0300

    update fuel core to 0.41.7

--- packages/versions/src/lib/getBuiltinVersions.ts
+++ packages/versions/src/lib/getBuiltinVersions.ts
@@ -2,7 +2,7 @@ import type { Versions } from './types';
 
 export function getBuiltinVersions(): Versions {
   return {
-    FUEL_CORE: '0.41.6',
+    FUEL_CORE: '0.41.7',
     FORC: '0.66.7',
     FUELS: '0.99.0',
   };
