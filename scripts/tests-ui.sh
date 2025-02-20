9d6d5918d148fa288b63e2dc4323deb2140df287
Peter Smith <peter@blueoceancomputing.co.uk>
2025-02-20 14:33:53 +0000

    Revert "rollback unneeded changes"
    This reverts commit c5b02d45c44463b3fbf8900dec9b0800bfa0ff0a.

--- scripts/tests-ui.sh
+++ scripts/tests-ui.sh
@@ -9,15 +9,23 @@ cp $PROJECT_DIR/.env.example $PROJECT_DIR/.env.local
 cd $PROJECT_DIR && pnpm run fuels:dev > /dev/null 2>&1 &
 sleep 5
 
-cd $PROJECT_DIR && pnpm run dev > /dev/null 2>&1 &
+echo "Here 1"
+
+cd $PROJECT_DIR && pnpm run dev &
 sleep 5
 
+echo "Here 2"
+
 cd $PLAYWRIGHT_DIR
-pnpm exec playwright install --with-deps  > /dev/null 2>&1
+pnpm exec playwright install --with-deps
 pnpm exec playwright test
 TEST_RESULT=$?
 
+echo "Here 3"
+
 pkill vite
 pkill fuel-core
+
+echo "TEST_RESULT: $TEST_RESULT"
 
 exit $TEST_RESULT
