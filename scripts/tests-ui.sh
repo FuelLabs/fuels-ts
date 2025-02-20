cb1b167796d46fba49adb38a4d26ce951a7c509d
Peter Smith <peter@blueoceancomputing.co.uk>
2025-02-20 16:19:49 +0000

    revert `tests-ui.sh`

--- scripts/tests-ui.sh
+++ scripts/tests-ui.sh
@@ -9,23 +9,15 @@ cp $PROJECT_DIR/.env.example $PROJECT_DIR/.env.local
 cd $PROJECT_DIR && pnpm run fuels:dev > /dev/null 2>&1 &
 sleep 5
 
-echo "Here 1"
-
-cd $PROJECT_DIR && pnpm run dev &
+cd $PROJECT_DIR && pnpm run dev > /dev/null 2>&1 &
 sleep 5
 
-echo "Here 2"
-
 cd $PLAYWRIGHT_DIR
-pnpm exec playwright install --with-deps
+pnpm exec playwright install --with-deps  > /dev/null 2>&1
 pnpm exec playwright test
 TEST_RESULT=$?
 
-echo "Here 3"
-
 pkill vite
 pkill fuel-core
-
-echo "TEST_RESULT: $TEST_RESULT"
 
 exit $TEST_RESULT
