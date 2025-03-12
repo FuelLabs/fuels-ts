c8172325c9f6779b137ca8830352de5f416acb7b
Torres-ssf <sergio.uft@gmail.com>
2025-03-12 20:59:15 -0300

    adjust fuel-core version

--- packages/versions/src/lib/getBuiltinVersions.ts
+++ packages/versions/src/lib/getBuiltinVersions.ts
@@ -2,7 +2,7 @@ import type { Versions } from './types';
 
 export function getBuiltinVersions(): Versions {
   return {
-    FUEL_CORE: 'git:feature/assemble-tx',
+    FUEL_CORE: 'git:master',
     FORC: '0.66.7',
     FUELS: '0.99.0',
   };
