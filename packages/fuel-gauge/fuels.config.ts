88d02ad64fc631d8c73c535baacf61dffa3e069f
Peter Smith <peter@blueoceancomputing.co.uk>
2025-03-13 16:37:47 +0000

    Added experimental flag for `fuel-gauge`

--- packages/fuel-gauge/fuels.config.ts
+++ packages/fuel-gauge/fuels.config.ts
@@ -3,7 +3,7 @@ import { createConfig } from 'fuels';
 export default createConfig({
   workspace: './test/fixtures/forc-projects',
   output: './test/typegen',
-  forcBuildFlags: ['--release'],
+  forcBuildFlags: ['--release', '--experimental', 'storage_domains,partial_eq,try_from_bytes_for_b256'],
   forcPath: 'fuels-forc',
   fuelCorePath: 'fuels-core',
   fuelCorePort: 0,
