c66b53827ab96dcba20e07b58c2fd4ee800ebb9a
Peter Smith <peter@blueoceancomputing.co.uk>
2025-03-14 09:16:58 +0000

    Update packages/fuel-gauge/fuels.config.ts

--- packages/fuel-gauge/fuels.config.ts
+++ packages/fuel-gauge/fuels.config.ts
@@ -3,11 +3,7 @@ import { createConfig } from 'fuels';
 export default createConfig({
   workspace: './test/fixtures/forc-projects',
   output: './test/typegen',
-  forcBuildFlags: [
-    '--release',
-    '--experimental',
-    'storage_domains,partial_eq,try_from_bytes_for_b256',
-  ],
+  forcBuildFlags: ['--release'],
   forcPath: 'fuels-forc',
   fuelCorePath: 'fuels-core',
   fuelCorePort: 0,
