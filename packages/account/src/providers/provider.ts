6202ff6939ea184370629c7aecf5d5562c3b014e
nedsalk <nedim.salkic@fuel.sh>
2025-03-19 14:40:23 +0100

    slim down

--- packages/account/src/providers/provider.ts
+++ packages/account/src/providers/provider.ts
@@ -458,12 +458,7 @@ const BLOCK_HEIGHT_SENSITIVE_OPERATIONS: Array<keyof SdkOperations> = [
   'statusChange',
   'getCoinsToSpend',
   'submitAndAwaitStatus',
-  'getCoins',
-  'getTransaction',
   'getTransactionWithReceipts',
-  'getTransactions',
-  'getTransactionsByOwner',
-  'estimateGasPrice',
 ];
 
 /**
