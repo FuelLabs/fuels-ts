e449f1b0fd5315fbfeafb3e79db6ee1ea49e399f
Torres-ssf <sergio.uft@gmail.com>
2025-03-20 21:03:58 -0300

    fix snippet

--- apps/docs/src/guide/transactions/snippets/assemble-tx/default-behaviors.ts
+++ apps/docs/src/guide/transactions/snippets/assemble-tx/default-behaviors.ts
@@ -1,35 +1,36 @@
 // #region assemble-tx-default-behaviors
 import { Provider, Wallet, type AccountCoinQuantity } from 'fuels';
+import { TestAssetId } from 'fuels/test-utils';
 
 import {
   LOCAL_NETWORK_URL,
   WALLET_PVT_KEY,
   WALLET_PVT_KEY_2,
-  WALLET_ADDRESS_3,
+  WALLET_PVT_KEY_3,
 } from '../../../../env';
 
 // #region transaction-request-3
 const provider = new Provider(LOCAL_NETWORK_URL);
 const accountA = Wallet.fromPrivateKey(WALLET_PVT_KEY, provider);
 const accountB = Wallet.fromPrivateKey(WALLET_PVT_KEY_2, provider);
-const accountC = Wallet.fromPrivateKey(WALLET_ADDRESS_3, provider);
-
+const accountC = Wallet.fromPrivateKey(WALLET_PVT_KEY_3, provider);
+const baseAssetId = await provider.getBaseAssetId();
 const accountCoinQuantities: AccountCoinQuantity[] = [
   {
     amount: 100,
-    assetId: '0x...',
+    assetId: baseAssetId,
     // account defaults to feePayerAccount
     // changeOutputAccount defaults to feePayerAccount
   },
   {
     amount: 200,
-    assetId: '0x...',
+    assetId: TestAssetId.A.value,
     account: accountA,
     // changeOutputAccount defaults to accountA
   },
   {
     amount: 300,
-    assetId: '0x...',
+    assetId: TestAssetId.B.value,
     account: accountB,
     changeOutputAccount: accountC,
     // Both account and changeOutputAccount are explicitly set
