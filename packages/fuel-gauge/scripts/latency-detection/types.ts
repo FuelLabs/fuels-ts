f36b3803e6c2cdf656d9e7fa3945be2147f76f37
Torres-ssf <sergio.uft@gmail.com>
2025-03-25 14:31:35 -0300

    refact types

--- packages/fuel-gauge/scripts/latency-detection/types.ts
+++ packages/fuel-gauge/scripts/latency-detection/types.ts
@@ -1,9 +1,6 @@
 import type { Account, Provider } from 'fuels';
 
 import type { TransferContract } from '../../test/typegen/contracts';
-import type { TransferParamsInputInput } from '../../test/typegen/contracts/TransferContract';
-import type { Vec } from '../../test/typegen/contracts/common';
-import type { PredicateWithConfigurable } from '../../test/typegen/predicates';
 
 export type MeasureResponse<T> = {
   duration: number;
@@ -12,18 +9,19 @@ export type MeasureResponse<T> = {
 
 export enum TagEnum {
   Script = 'script',
+  SimpleTransfer = 'simple-transfer',
   ScriptWithPredicate = 'script-with-predicate',
   MissingOutputVariable = 'missing-output-variable',
   Missing4xOutputVariable = 'missing-4x-output-variable',
 }
+
+export type OperationResult<T = unknown> = T;
 
 export type PerformanceOperationParams = {
   account: Account;
   baseAssetId: string;
   provider: Provider;
   contract: TransferContract;
-  callParams: Vec<TransferParamsInputInput>;
-  predicate: PredicateWithConfigurable;
 };
 
 export type PerformanceResult = {
@@ -31,4 +29,8 @@ export type PerformanceResult = {
   duration: number;
 };
 
-export type Operation = (params: PerformanceOperationParams) => Promise<PerformanceResult>;
+export type Operation = {
+  tag: TagEnum;
+  operation: (params: PerformanceOperationParams) => Promise<OperationResult>;
+  preparatorySteps?: (params: PerformanceOperationParams) => Promise<void>;
+};
