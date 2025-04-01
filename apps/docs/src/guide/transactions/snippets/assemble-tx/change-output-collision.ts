a0d9ebe4a81f6a81b65253478a2e0de6fe3c385f
Torres-ssf <sergio.uft@gmail.com>
2025-04-01 20:21:57 -0300

    update doc snippet

--- apps/docs/src/guide/transactions/snippets/assemble-tx/change-output-collision.ts
+++ apps/docs/src/guide/transactions/snippets/assemble-tx/change-output-collision.ts
@@ -1,5 +1,5 @@
 import type { AccountCoinQuantity } from 'fuels';
-import { Provider, ScriptTransactionRequest, Wallet } from 'fuels';
+import { Provider, Wallet } from 'fuels';
 
 import {
   LOCAL_NETWORK_URL,
@@ -13,19 +13,23 @@ const accountA = Wallet.fromPrivateKey(WALLET_PVT_KEY, provider);
 const accountB = Wallet.fromPrivateKey(WALLET_PVT_KEY_2, provider);
 
 // #region change-output-collision
-const request = new ScriptTransactionRequest();
-
-// Add a change output to the transaction request
-request.addChangeOutput(accountB.address, baseAssetId);
-
 const accountCoinQuantities: AccountCoinQuantity[] = [
   {
     amount: 100,
     assetId: baseAssetId,
     account: accountA,
-    // This will cause a CHANGE_OUTPUT_COLLISION error because the transaction
-    // already has a change output for this assetId to accountB
     changeOutputAccount: accountA,
+  },
+  {
+    amount: 200,
+    assetId: baseAssetId,
+    account: accountB,
+    /**
+     * This will cause a CHANGE_OUTPUT_COLLISION error because the first
+     * accountCoinQuantity already defines a change output for this assetId
+     * to accountA.
+     */
+    changeOutputAccount: accountB,
   },
 ];
 // #endregion change-output-collision
