faa8c4d6fae0a9b511944d123233ad5f3cda9056
Peter Smith <peter@blueoceancomputing.co.uk>
2025-04-08 14:13:18 +0100

    Moved docs

--- packages/account/src/providers/transaction-response/getAllDecodedLogs.ts
+++ packages/account/src/providers/transaction-response/getAllDecodedLogs.ts
@@ -15,6 +15,21 @@ export interface DecodedLogs<T = unknown> {
 
 /**
  * @hidden
+ *
+ * This helper decodes logs from transaction receipts.
+ * It loops through all receipts and decodes two types of logs:
+ * - ReceiptType.LogData
+ * - ReceiptType.Log
+ *
+ * The "mainAbi" parameter represents the ABI of the main contract used to create the transaction
+ * or the ABI from a script used within a "BaseInvocationScope" context.
+ * The "externalAbis" parameter is a record of contract ABIs that are also part of the transaction.
+ * These ABIs were added using `contract.addContracts` or through a multi-call with `contract.multiCall`.
+ *
+ * @param receipts - The array of transaction result receipts.
+ * @param mainAbi - The ABI of the script or main contract.
+ * @param externalAbis - The record of external contract ABIs.
+ * @returns An array of decoded logs from Sway projects.
  */
 export function getAllDecodedLogs<T = unknown>(opts: {
   receipts: Array<TransactionResultReceipt>;
@@ -23,22 +38,6 @@ export function getAllDecodedLogs<T = unknown>(opts: {
 }): DecodedLogs<T> {
   const { receipts, mainAbi, externalAbis = {} } = opts;
 
-  /**
-   * This helper decodes logs from transaction receipts.
-   * It loops through all receipts and decodes two types of logs:
-   * - ReceiptType.LogData
-   * - ReceiptType.Log
-   *
-   * The "mainAbi" parameter represents the ABI of the main contract used to create the transaction
-   * or the ABI from a script used within a "BaseInvocationScope" context.
-   * The "externalAbis" parameter is a record of contract ABIs that are also part of the transaction.
-   * These ABIs were added using `contract.addContracts` or through a multi-call with `contract.multiCall`.
-   *
-   * @param receipts - The array of transaction result receipts.
-   * @param mainAbi - The ABI of the script or main contract.
-   * @param externalAbis - The record of external contract ABIs.
-   * @returns An array of decoded logs from Sway projects.
-   */
   let mainContract = '';
   if (mainAbi.programType === 'contract') {
     const firstCallReceipt = receipts.find(
