7b93a04b0757aa8bd0554cf680105149c1a3b1c0
Torres-ssf <sergio.uft@gmail.com>
2025-05-06 17:02:13 -0300

    add TransactionResponse serialization helpers on a new file

---
+++ packages/account/src/providers/utils/transaction-response-serialization.ts
@@ -0,0 +1,53 @@
+import Provider from '../provider';
+import type { TransactionResponseJson } from '../transaction-response';
+import { TransactionResponse } from '../transaction-response';
+
+import { serializeProviderCache } from './serialization';
+
+/**
+ * NOTE: This is defined within a new file to avoid circular dependencies.
+ */
+
+export const serializeTransactionResponseJson = async (
+  response: TransactionResponse
+): Promise<TransactionResponseJson> => {
+  const { id, status, abis, request, provider, gqlTransaction, preConfirmationStatus } = response;
+  return {
+    id,
+    status,
+    abis,
+    requestJson: JSON.stringify(request),
+    providerUrl: provider.url,
+    providerCache: await serializeProviderCache(provider),
+    gqlTransaction,
+    preConfirmationStatus,
+  };
+};
+
+export const deserializeTransactionResponseJson = (json: TransactionResponseJson) => {
+  const {
+    id,
+    abis,
+    status,
+    providerUrl,
+    requestJson,
+    providerCache,
+    gqlTransaction,
+    preConfirmationStatus,
+  } = json;
+
+  const provider = new Provider(providerUrl, { cache: providerCache });
+  const { chainId } = providerCache.chain.consensusParameters;
+
+  const response = new TransactionResponse(id, provider, Number(chainId), abis);
+
+  if (requestJson) {
+    response.request = JSON.parse(requestJson);
+  }
+
+  response.status = status;
+  response.gqlTransaction = gqlTransaction;
+  response.preConfirmationStatus = preConfirmationStatus;
+
+  return response;
+};
