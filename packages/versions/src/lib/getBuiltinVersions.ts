1da0e8d4afec21150867e3890c7e5b8833731417
Peter Smith <peter@blueoceancomputing.co.uk>
2025-06-02 09:13:37 +0100

    Bumped versions

--- packages/versions/src/lib/getBuiltinVersions.ts
+++ packages/versions/src/lib/getBuiltinVersions.ts
@@ -2,7 +2,7 @@ import type { Versions } from './types';
 
 export function getBuiltinVersions(): Versions {
   return {
-    FUEL_CORE: '0.43.1',
+    FUEL_CORE: '0.44.0',
     FORC: '0.68.1',
     FUELS: '0.101.1',
   };
