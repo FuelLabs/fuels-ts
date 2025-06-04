3318ce3080f9a610cbfa477466a67b680dd1de62
Peter Smith <peter@blueoceancomputing.co.uk>
2025-06-04 10:46:35 +0100

    chore: favour named exports

--- packages/program/src/index.ts
+++ packages/program/src/index.ts
@@ -2,7 +2,7 @@ export * from './types';
 export * from './utils';
 export { FunctionInvocationScope } from './functions/invocation-scope';
 export { MultiCallInvocationScope } from './functions/multicall-scope';
-export { default as Contract } from './contract';
+export { Contract } from './contract';
 export { ScriptRequest } from './script-request';
 export { InstructionSet } from './instruction-set';
 export * from './response';
