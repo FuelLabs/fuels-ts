8b9ad15dd6491120753f15303bead44a15a9a116
Peter Smith <peter@blueoceancomputing.co.uk>
2025-06-06 12:02:24 +0100

    Use correct architecture for Vercel

--- internal/fuel-core/lib/shared.js
+++ internal/fuel-core/lib/shared.js
@@ -30,7 +30,7 @@ const platforms = {
 
 export const getPkgPlatform = () => {
   // Hardcoded for Vercel + GH Actions
-  return 'x86_64-unknown-linux-gnu';
+  return 'aarch64-unknown-linux-gnu';
 
   if (process.platform !== 'darwin' && process.platform !== 'linux') {
     throw new Error(
