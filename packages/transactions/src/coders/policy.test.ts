a0ed250df068c75e1dc742d30b8e1c70f6f61cc5
luizstacio <luizstacio@gmail.com>
2025-10-27 21:01:48 -0300

    feat: add support to fuel-core v0.47.1

--- packages/transactions/src/coders/policy.test.ts
+++ packages/transactions/src/coders/policy.test.ts
@@ -46,6 +46,13 @@ describe('PoliciesCoder', () => {
       expect(encoded).toStrictEqual(Uint8Array.from([0, 0, 0, 0, 0, 0, 46, 224]));
     });
 
+    it('should encode policy correctly (Owner)', () => {
+      const policies: Policy[] = [{ type: PolicyType.Owner, data: bn(12345) }];
+      const encoded = new PoliciesCoder().encode(policies);
+
+      expect(encoded).toStrictEqual(Uint8Array.from([0, 0, 0, 0, 0, 0, 48, 57]));
+    });
+
     it('should encode policy correctly (Tip + MaxFee)', () => {
       const policies: Policy[] = [
         { type: PolicyType.Tip, data: bn(19) },
@@ -132,19 +139,63 @@ describe('PoliciesCoder', () => {
       );
     });
 
-    it('should ensure unsorted policies array will not reflect in error when encoding', () => {
+    it('should encode policy correctly (Tip + Owner)', () => {
       const policies: Policy[] = [
-        { type: PolicyType.MaxFee, data: bn(199) },
+        { type: PolicyType.Tip, data: bn(50) },
+        { type: PolicyType.Owner, data: bn(999) },
+      ];
+      const encoded = new PoliciesCoder().encode(policies);
+
+      expect(encoded).toStrictEqual(
+        Uint8Array.from([0, 0, 0, 0, 0, 0, 0, 50, 0, 0, 0, 0, 0, 0, 3, 231])
+      );
+    });
+
+    it('should encode policy correctly (Owner + MaxFee)', () => {
+      const policies: Policy[] = [
+        { type: PolicyType.Owner, data: bn(777) },
+        { type: PolicyType.MaxFee, data: bn(150) },
+      ];
+      const encoded = new PoliciesCoder().encode(policies);
+
+      expect(encoded).toStrictEqual(
+        Uint8Array.from([0, 0, 0, 0, 0, 0, 0, 150, 0, 0, 0, 0, 0, 0, 3, 9])
+      );
+    });
+
+    it('should encode policy correctly (Tip + WitnessLimit + Maturity + MaxFee + Expiration + Owner)', () => {
+      const policies: Policy[] = [
         { type: PolicyType.Tip, data: bn(28) },
-        { type: PolicyType.Maturity, data: 26 },
         { type: PolicyType.WitnessLimit, data: bn(87) },
+        { type: PolicyType.Maturity, data: 26 },
+        { type: PolicyType.MaxFee, data: bn(199) },
+        { type: PolicyType.Expiration, data: 2000000 },
+        { type: PolicyType.Owner, data: bn(12345) },
       ];
       const encoded = new PoliciesCoder().encode(policies);
 
       expect(encoded).toStrictEqual(
         Uint8Array.from([
           0, 0, 0, 0, 0, 0, 0, 28, 0, 0, 0, 0, 0, 0, 0, 87, 0, 0, 0, 0, 0, 0, 0, 26, 0, 0, 0, 0, 0,
-          0, 0, 199,
+          0, 0, 199, 0, 0, 0, 0, 0, 30, 132, 128, 0, 0, 0, 0, 0, 0, 48, 57,
+        ])
+      );
+    });
+
+    it('should ensure unsorted policies array will not reflect in error when encoding', () => {
+      const policies: Policy[] = [
+        { type: PolicyType.MaxFee, data: bn(199) },
+        { type: PolicyType.Tip, data: bn(28) },
+        { type: PolicyType.Maturity, data: 26 },
+        { type: PolicyType.WitnessLimit, data: bn(87) },
+        { type: PolicyType.Owner, data: bn(555) },
+      ];
+      const encoded = new PoliciesCoder().encode(policies);
+
+      expect(encoded).toStrictEqual(
+        Uint8Array.from([
+          0, 0, 0, 0, 0, 0, 0, 28, 0, 0, 0, 0, 0, 0, 0, 87, 0, 0, 0, 0, 0, 0, 0, 26, 0, 0, 0, 0, 0,
+          0, 0, 199, 0, 0, 0, 0, 0, 0, 2, 43,
         ])
       );
     });
@@ -254,6 +305,23 @@ describe('PoliciesCoder', () => {
       });
     });
 
+    it('should decode owner', () => {
+      // owner is 98765
+      const byteArr = [0, 0, 0, 0, 0, 1, 128, 45];
+      const data = Uint8Array.from(byteArr);
+
+      // bitfield is 32 representing owner
+      const policyTypes = PolicyType.Owner;
+
+      const [policies] = new PoliciesCoder().decode(data, 0, policyTypes);
+
+      expect(policies).toHaveLength(1);
+      expect(policies[0]).toStrictEqual({
+        type: PolicyType.Owner,
+        data: bn(byteArr),
+      });
+    });
+
     it('should decode tip and witnessLimit', () => {
       const tipByteArr = [0, 0, 0, 0, 0, 0, 0, 100];
       const witLimitByteArr = [0, 0, 0, 0, 0, 0, 11, 184];
@@ -351,12 +419,59 @@ describe('PoliciesCoder', () => {
       });
     });
 
+    it('should decode tip and owner', () => {
+      const tipByteArr = [0, 0, 0, 0, 0, 0, 0, 100];
+      const ownerByteArr = [0, 0, 0, 0, 0, 0, 3, 231];
+
+      const data = Uint8Array.from([...tipByteArr, ...ownerByteArr]);
+
+      // bitfield is 33 representing tip + owner
+      const policyTypes = PolicyType.Tip + PolicyType.Owner;
+      expect(policyTypes).toBe(33);
+
+      const [policies] = new PoliciesCoder().decode(data, 0, policyTypes);
+
+      expect(policies).toHaveLength(2);
+      expect(policies[0]).toStrictEqual({
+        type: PolicyType.Tip,
+        data: bn(tipByteArr),
+      });
+      expect(policies[1]).toStrictEqual({
+        type: PolicyType.Owner,
+        data: bn(ownerByteArr),
+      });
+    });
+
+    it('should decode owner and maxFee', () => {
+      const ownerByteArr = [0, 0, 0, 0, 0, 0, 3, 9];
+      const maxFeeByteArr = [0, 0, 0, 0, 0, 0, 1, 244];
+
+      const data = Uint8Array.from([...maxFeeByteArr, ...ownerByteArr]);
+
+      // bitfield is 40 representing maxFee + owner
+      const policyTypes = PolicyType.MaxFee + PolicyType.Owner;
+      expect(policyTypes).toBe(40);
+
+      const [policies] = new PoliciesCoder().decode(data, 0, policyTypes);
+
+      expect(policies).toHaveLength(2);
+      expect(policies[0]).toStrictEqual({
+        type: PolicyType.MaxFee,
+        data: bn(maxFeeByteArr),
+      });
+      expect(policies[1]).toStrictEqual({
+        type: PolicyType.Owner,
+        data: bn(ownerByteArr),
+      });
+    });
+
     it('should decode when all policy types are present', () => {
       const tipByteArr = [0, 0, 0, 0, 0, 0, 0, 100];
       const witLimitByteArr = [0, 0, 0, 0, 0, 0, 11, 184];
       const maturityByteArr = [0, 0, 0, 0, 0, 0, 0, 25];
       const maxFeeByteArr = [0, 0, 0, 0, 0, 0, 1, 244];
       const expirationByteArr = [0, 0, 0, 0, 0, 0, 100, 244];
+      const ownerByteArr = [0, 0, 0, 0, 0, 0, 48, 57];
 
       const data = Uint8Array.from([
         ...tipByteArr,
@@ -364,21 +479,23 @@ describe('PoliciesCoder', () => {
         ...maturityByteArr,
         ...maxFeeByteArr,
         ...expirationByteArr,
+        ...ownerByteArr,
       ]);
 
-      // bitfield is 15 representing witnessLimit + maxFee
+      // bitfield is 63 representing all policy types
       const policyTypes =
         PolicyType.Tip +
         PolicyType.WitnessLimit +
         PolicyType.Maturity +
         PolicyType.MaxFee +
-        PolicyType.Expiration;
+        PolicyType.Expiration +
+        PolicyType.Owner;
 
-      expect(policyTypes).toBe(31);
+      expect(policyTypes).toBe(63);
 
       const [policies] = new PoliciesCoder().decode(data, 0, policyTypes);
 
-      expect(policies).toHaveLength(5);
+      expect(policies).toHaveLength(6);
       expect(policies).toStrictEqual([
         {
           type: PolicyType.Tip,
@@ -400,7 +517,31 @@ describe('PoliciesCoder', () => {
           type: PolicyType.Expiration,
           data: bn(expirationByteArr).toNumber(),
         },
+        {
+          type: PolicyType.Owner,
+          data: bn(ownerByteArr),
+        },
       ]);
+    });
+
+    it('should handle unknown policy type 64 and 128', () => {
+      const tipByteArr = [0, 0, 0, 0, 0, 0, 0, 100];
+      const unknownPolicyByteArr = [0, 0, 0, 0, 0, 0, 0, 200];
+      const unknown2PolicyByteArr = [0, 0, 0, 0, 0, 0, 1, 200];
+      const data = Uint8Array.from([
+        ...tipByteArr,
+        ...unknownPolicyByteArr,
+        ...unknown2PolicyByteArr,
+      ]);
+
+      // bitfield is 64 representing all policy types + unknown policy type 64 + unknown policy type 128
+      const policyTypes = PolicyType.Tip + 64 + 128;
+      expect(policyTypes).toBe(193);
+
+      const [policies, offset] = new PoliciesCoder().decode(data, 0, policyTypes);
+
+      expect(policies).toHaveLength(1);
+      expect(offset).toBe(24);
     });
   });
 });
--- packages/versions/src/lib/getBuiltinVersions.ts
+++ packages/versions/src/lib/getBuiltinVersions.ts
@@ -2,7 +2,7 @@ import type { Versions } from './types';
 
 export function getBuiltinVersions(): Versions {
   return {
-    FUEL_CORE: '0.44.0',
+    FUEL_CORE: '0.47.1',
     FORC: '0.68.7',
     FUELS: '0.101.3',
   };
